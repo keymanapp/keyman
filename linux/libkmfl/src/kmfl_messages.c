c03be4b9639584a918c98300432f3ad303e172e3
danglassey <>
2006-01-26 12:31:55 +0000

    Add some copyright notices

--- linux/libkmfl/src/kmfl_messages.c
+++ linux/libkmfl/src/kmfl_messages.c
@@ -1,3 +1,25 @@
+/* kmfl_messages.c
+ * Copyright (C) 2004 Tavultesoft
+ *
+ * This file is part of KMFL Compiler library.
+ *
+ * KMFL Compiler library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * KMFL Compiler library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with KMFL Compiler library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA
+ *
+ */
+
+
 /*
 
 	Keystroke interpreter for keyboard mapping for Linux project
