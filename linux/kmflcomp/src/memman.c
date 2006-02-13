283250a10258691b97e7b2d12dd9bac5ced0dafa
courtjester <>
2006-02-13 23:22:28 +0000

    Update license information to include Tavultesoft and change to GPL from LGPL

--- linux/kmflcomp/src/memman.c
+++ linux/kmflcomp/src/memman.c
@@ -1,20 +1,20 @@
 /* memman.c
- * Copyright (C) 2005 SIL International
+ * Copyright (C) 2005  SIL International and Tavultesoft Pty Ltd
  *
- * This file is part of KMFL Compiler library.
+ * This file is part of the KMFL compiler.
  *
- * KMFL Compiler library is free software; you can redistribute it and/or
- * modify it under the terms of the GNU Lesser General Public
- * License as published by the Free Software Foundation; either
- * version 2.1 of the License, or (at your option) any later version.
+ * The KMFL compiler is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
  *
- * KMFL Compiler library is distributed in the hope that it will be useful,
+ * The KMFL compiler is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
- * Lesser General Public License for more details.
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
  *
- * You should have received a copy of the GNU Lesser General Public
- * License along with KMFL Compiler library; if not, write to the Free Software
+ * You should have received a copy of the GNU General Public License
+ * along with the KMFL compiler; if not, write to the Free Software
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA
  *
  */
--- linux/libkmfl/src/kmfl_load_keyboard.c
+++ linux/libkmfl/src/kmfl_load_keyboard.c
@@ -1,25 +1,25 @@
 /* kmfl_load_keyboard.c
- * Copyright (C) 2004 Tavultesoft
+ * Copyright (C) 2005 SIL International and Tavultesoft Pty Ltd
  *
- * This file is part of KMFL Compiler library.
+ * This file is part of the KMFL library.
  *
- * KMFL Compiler library is free software; you can redistribute it and/or
- * modify it under the terms of the GNU Lesser General Public
+ * The KMFL library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public
  * License as published by the Free Software Foundation; either
  * version 2.1 of the License, or (at your option) any later version.
  *
- * KMFL Compiler library is distributed in the hope that it will be useful,
+ * The KMFL library is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
- * Lesser General Public License for more details.
+ * General Public License for more details.
  *
- * You should have received a copy of the GNU Lesser General Public
- * License along with KMFL Compiler library; if not, write to the Free Software
+ * You should have received a copy of the GNU General Public
+ * License along with the KMFL library; if not, write to the Free Software
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA
  *
  */
 
- /*
+/*
 
 	Keystroke interpreter for keyboard mapping for Linux project
 
