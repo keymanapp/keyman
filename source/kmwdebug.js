8cfc835e51890e96959d77039ed09cc194eb4669
Marc Durdin <marc@durdin.net>
2014-11-05 11:04:54 +1100

    license in headers

--- source/kmwdebug.js
+++ source/kmwdebug.js
@@ -1,9 +1,20 @@
-/**
- * @preserve KeymanWeb 2.0 Copyright 2010 Tavultesoft Pty Ltd
- * 
- * Debug module - not compiled, added as separate script when needed, to be used with debug version of 
- * compiled keymanweb code.    
- */  
+/***
+   KeymanWeb 2.0
+   Copyright 2014 Tavultesoft Pty Ltd
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+***/
+
 
 /*__STARTDEBUG__*/
 /*----------------------------------------------------------------------------------------------------*/
