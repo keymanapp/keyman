c4ca29606c0c4cc4a5fb7e34875e9b8d5dfa0bb4
Gabriel Wong <me@gabrielwong.net>
2017-08-29 15:51:28 +0700

    Fix Android build script on MacOS

--- android/KMEA/build.sh
+++ android/KMEA/build.sh
@@ -19,26 +19,11 @@ display_usage ( ) {
 echo Build KMEA
 
 # Path definitions
-KMA_ROOT=`readlink -f ../`
-KMW_ROOT=`readlink -f ../../web`
+KM_ROOT="$(cd "$( dirname "${BASH_SOURCE[0]}" )/../.." && pwd )"
+KMA_ROOT="$KM_ROOT/android"
+KMW_ROOT="$KM_ROOT/web"
 KMW_SOURCE="$KMW_ROOT/source"
 KMEA_ASSETS="$KMA_ROOT/KMEA/app/src/main/assets"
-
-# Default KMW build artifacts that get copied for KMEA
-declare -A KMW_ARTIFACTS
-KMW_ARTIFACTS=( \
-    ["KEYMAN_JS"]=\
-"$KMW_ROOT/embedded/keyman.js" \
-    ["AJAX_LOADER"]=\
-"$KMW_ROOT/embedded/resources/osk/ajax-loader.gif" \
-    ["KMWOSK_CSS"]=\
-"$KMW_ROOT/embedded/resources/osk/kmwosk.css" \
-    ["KEYMANWEB_OSK_EOT"]=\
-"$KMW_ROOT/embedded/resources/osk/keymanweb-osk.eot" \
-    ["KEYMANWEB_OSK_TTF"]=\
-"$KMW_ROOT/embedded/resources/osk/keymanweb-osk.ttf" \
-    ["KEYMANWEB_OSK_WOFF"]=\
-"$KMW_ROOT/embedded/resources/osk/keymanweb-osk.woff")
 
 warn ( ) {
     echo "$*"
@@ -109,15 +94,15 @@ if [ "$DO_BUILD" = true ]; then
 fi
 if [ "$DO_COPY" = true ]; then
     echo "Copying KMW artifacts"
-    cp ${KMW_ARTIFACTS[AJAX_LOADER]} $KMEA_ASSETS/ajax-loader.gif
-    cp ${KMW_ARTIFACTS[KEYMAN_JS]} $KMEA_ASSETS/keyman.js
-    cp ${KMW_ARTIFACTS[KMWOSK_CSS]} $KMEA_ASSETS/kmwosk.css
-    cp ${KMW_ARTIFACTS[KEYMANWEB_OSK_EOT]} $KMEA_ASSETS/keymanweb-osk.eot
-    cp ${KMW_ARTIFACTS[KEYMANWEB_OSK_TTF]} $KMEA_ASSETS/keymanweb-osk.ttf
-    cp ${KMW_ARTIFACTS[KEYMANWEB_OSK_WOFF]} $KMEA_ASSETS/keymanweb-osk.woff
+    cp $KMW_ROOT/embedded/resources/osk/ajax-loader.gif $KMEA_ASSETS/ajax-loader.gif
+    cp $KMW_ROOT/embedded/keyman.js $KMEA_ASSETS/keyman.js
+    cp $KMW_ROOT/embedded/resources/osk/kmwosk.css $KMEA_ASSETS/kmwosk.css
+    cp $KMW_ROOT/embedded/resources/osk/keymanweb-osk.eot $KMEA_ASSETS/keymanweb-osk.eot
+    cp $KMW_ROOT/embedded/resources/osk/keymanweb-osk.ttf $KMEA_ASSETS/keymanweb-osk.ttf
+    cp $KMW_ROOT/embedded/resources/osk/keymanweb-osk.woff $KMEA_ASSETS/keymanweb-osk.woff
     if [ $? -ne 0 ]; then
         die "ERROR: copying artifacts failed"
-    fi   
+    fi
 fi
 
 echo "Gradle Build of KMEA"
@@ -129,7 +114,7 @@ if [ $? -ne 0 ]; then
 fi
 
 echo "Copying Keyman Engine for Android to KMAPro and Sample apps"
-mv $KMA_ROOT/KMEA/app/build/outputs/aar/app-release.aar $KMA_ROOT/KMAPro/kMAPro/libs/keyman-engine.aar 
+mv $KMA_ROOT/KMEA/app/build/outputs/aar/app-release.aar $KMA_ROOT/KMAPro/kMAPro/libs/keyman-engine.aar
 cp $KMA_ROOT/KMAPro/kMAPro/libs/keyman-engine.aar $KMA_ROOT/Samples/KMSample1/app/libs/keyman-engine.aar
 cp $KMA_ROOT/KMAPro/kMAPro/libs/keyman-engine.aar $KMA_ROOT/Samples/KMSample2/app/libs/keyman-engine.aar
 
