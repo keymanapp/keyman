4c8271f083b4978028d2fe96d400bef03aa5a38d
Gabriel Wong <me@gabrielwong.net>
2017-10-05 10:06:29 +0700

    Migrate KeyboardViewController to Swift

---
+++ ios/engine/KMEI/SystemKeyboard/KeyboardViewController.swift
@@ -0,0 +1,17 @@
+//
+//  KeyboardViewController.swift
+//  SystemKeyboard
+//
+//  Created by Gabriel Wong on 2017-10-05.
+//  Copyright © 2017 SIL International. All rights reserved.
+//
+
+import KeymanEngine
+
+class KeyboardViewController: KMInputViewController {
+  override func viewDidLoad() {
+    KMManager.setApplicationGroupIdentifier("group.KMEI")
+    KMManager.sharedInstance().debugPrintingOn = true
+    super.viewDidLoad()
+  }
+}
