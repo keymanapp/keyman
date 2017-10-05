aba6dd96f75a4a693aa9c9af300f99465dc41ecf
Gabriel Wong <me@gabrielwong.net>
2017-10-05 16:26:54 +0700

    Migrate engine demo app to Swift

---
+++ ios/engine/KMEI/KeymanEngineDemo/AppDelegate.swift
@@ -0,0 +1,46 @@
+//
+//  AppDelegate.swift
+//  KeymanEngineDemo
+//
+//  Created by Gabriel Wong on 2017-10-05.
+//  Copyright © 2017 SIL International. All rights reserved.
+//
+
+import KeymanEngine
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+  var window: UIWindow?
+
+  func application(_ application: UIApplication,
+                   didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey : Any]? = nil) -> Bool {
+    KMManager.setApplicationGroupIdentifier("group.KMEI")
+    KMManager.sharedInstance().debugPrintingOn = true
+    KMManager.sharedInstance().canRemoveDefaultKeyboard = true
+    window = UIWindow(frame: UIScreen.main.bounds)
+
+    let mainViewController = MainViewController(nibName: nil, bundle: nil)
+    let navCon = UINavigationController(rootViewController: mainViewController)
+    window!.rootViewController = navCon
+
+    window!.makeKeyAndVisible()
+    return true
+  }
+
+  func application(_ application: UIApplication, handleOpen url: URL) -> Bool {
+    return true
+  }
+
+  func application(_ application: UIApplication, open url: URL, sourceApplication: String?, annotation: Any) -> Bool {
+    return true
+  }
+
+  func applicationDidEnterBackground(_ application: UIApplication) {
+    KMManager.sharedInstance().unregisterCustomFonts()
+  }
+
+  func applicationWillEnterForeground(_ application: UIApplication) {
+    KMManager.sharedInstance().registerCustomFonts()
+  }
+}
