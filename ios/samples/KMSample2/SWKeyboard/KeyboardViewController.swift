2d30e70e1c67d0e0540425f7412f5e066b049de3
Gabriel Wong <me@gabrielwong.net>
2017-10-05 10:56:33 +0700

    Fix build of Sample2

---
+++ ios/samples/KMSample2/SWKeyboard/KeyboardViewController.swift
@@ -0,0 +1,29 @@
+//
+//  KeyboardViewController.swift
+//  SWKeyboard
+//
+//  Created by Gabriel Wong on 2017-10-05.
+//  Copyright © 2017 Tavultesoft Pty Ltd. All rights reserved.
+//
+
+import KeymanEngine
+import UIKit
+
+class KeyboardViewController: KeymanInputViewController {
+  override func viewDidLoad() {
+    // Replace with your application group id
+    KMManager.setApplicationGroupIdentifier("group.KMSample")
+
+    // Disable before release
+    KMManager.sharedInstance().debugPrintingOn = true
+
+    super.viewDidLoad()
+
+    topBarImageView?.backgroundColor = UIColor(red: 1.0, green: 96.0 / 255.0, blue: 0.0, alpha: 1.0)
+    let label = UILabel(frame: CGRect.zero)
+    label.text = " \(Bundle.main.object(forInfoDictionaryKey: "CFBundleDisplayName")!)"
+    label.sizeToFit()
+    label.textColor = UIColor.white
+    topBarImageView?.addSubview(label)
+  }
+}
