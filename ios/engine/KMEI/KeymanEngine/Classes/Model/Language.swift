80bf2310acffe86ce9cf79876b90991d576ef757
Gabriel Wong <me@gabrielwong.net>
2017-10-24 18:21:11 +0700

    Create structs for Cloud API types

---
+++ ios/engine/KMEI/KeymanEngine/Classes/Model/Language.swift
@@ -0,0 +1,27 @@
+//
+//  Language.swift
+//  KeymanEngine
+//
+//  Created by Gabriel Wong on 2017-10-24.
+//  Copyright © 2017 SIL International. All rights reserved.
+//
+
+import Foundation
+
+/// Language object for Keyman Cloud API 4.0.
+public struct Language: Codable {
+  /// Name of language.
+  public let name: String
+
+  /// ISO639-3 language code.
+  public let id: String
+
+  /// Corresponding Keyboard objects.
+  public let keyboards: [Keyboard]?
+
+  /// Font for input fields (and OSK if `oskFont` is not present).
+  public let font: Font?
+
+  /// Font for the OSK.
+  public let oskFont: Font?
+}
