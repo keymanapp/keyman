237a2bd31f0fa38517b35eb6d0fa0dfc28ac6110
Joshua Horton <joshua_horton@sil.org>
2017-11-06 10:42:00 +0700

    Tweaks the new return value's behavior.

--- web/source/keymanweb.js
+++ web/source/keymanweb.js
@@ -1880,28 +1880,29 @@ if(!window['tavultesoft']['keymanweb']['initialized']) {
       }
 
       var i,j,ss=keymanweb._KeyboardStubs;
-      var success = false;
+      var success = true, anySuccess = false;
 
       for(i=0; i<arguments.length; i++) {           
         for(j=ss.length-1; j>=0; j--) {
           if('Keyboard_'+arguments[i] == ss[j]['KI'] && ss.length > 1) {                 
             ss.splice(j,1);
-            success = true;
+            anySuccess = true;
             break;
           }
         }
+
+        success = false;
       } 
 
-      if(success) {
+      if(anySuccess) {
         // Always reset to the first remaining keyboard
         keymanweb._SetActiveKeyboard(ss[0]['KI'],ss[0]['KLC'],true);
         
         // then update the UI keyboard menu
         keymanweb.doKeyboardUnregistered();
-        return true;
-      } else {
-        return false;
-      }
+      } 
+        
+      return success;
     }
 
 
