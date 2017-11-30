d95599631e908efb89d86ca6abd102a5f6d5c161
Tom Bogle <tom_bogle@sil.org>
2017-11-29 23:43:44 -0500

    Added files to hold input method event handler class

---
+++ mac/Keyman4MacIM/Keyman4MacIM/KMInputMethodEventHandler.h
@@ -0,0 +1,15 @@
+//
+//  KMInputMethodEventHandler.h
+//  Keyman4MacIM
+//
+//  Created by Tom Bogle on 11/22/17.
+//  Copyright © 2017 SIL International. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+#ifndef KMInputMethodEventHandler_h
+#define KMInputMethodEventHandler_h
+
+
+#endif /* KMInputMethodEventHandler_h */
---
+++ mac/Keyman4MacIM/Keyman4MacIM/KMInputMethodEventHandler.m
@@ -0,0 +1,9 @@
+//
+//  KMInputMethodEventHandler.m
+//  Keyman4MacIM
+//
+//  Created by Tom Bogle on 11/22/17.
+//  Copyright © 2017 SIL International. All rights reserved.
+//
+#import "KMInputController.h"
+#include <Carbon/Carbon.h> /* For kVK_ constants. */
