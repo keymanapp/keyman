eb7cab176fe3eb3135c8248f990d3e80e4ac0f20
Tom Bogle <tom_bogle@sil.org>
2018-01-26 12:31:38 -0500

    Added plumbing for unit testing IM.
    This also slightly simplifies the logic in build.sh and even opens the possiblity of unit tests for the test app if that were ever to be desirable.

---
+++ mac/Keyman4MacIM/KeymanTests/KMInputMethodBrowserClientEventHandlerTests.m
@@ -0,0 +1,48 @@
+//
+//  KeymanTests.m
+//  KeymanTests
+//
+//  Created by tom on 1/25/18.
+//  Copyright © 2018 SIL International. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+#import "KMInputMethodEventHandler.h"
+#import "KMInputMethodEventHandlerProtected.h"
+#import "KMInputMethodBrowserClientEventHandler.h"
+
+@interface KeymanTests : XCTestCase
+
+@end
+
+@implementation KeymanTests
+KMInputMethodBrowserClientEventHandler * _im;
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+    _im = [[KMInputMethodBrowserClientEventHandler alloc] init];
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testcheckContextIn_EmptyContextExpected_NoChangeToclientSelectionCanChangeUnexpectedly {
+    // If the expected context is empty, no attempt should be made to check for matching context from
+    // client, so even though the client would also return an empty string, calling checkContextIn
+    // repeatedly has no effect (i.e., it does not set clientSelectionCanChangeUnexpectedly to false).
+    // Therefore a subsequent key down event *should* reult in a call to the client to inquire about
+    // the current selection.
+    // TODO replace this meaningless client with a mock object so we can set results and query it afterwards.
+    id client;
+    for (int i = 0; i < 8; i++)
+    {
+        [_im checkContextIn:client];
+    }
+    NSEvent *event = [NSEvent keyEventWithType:NSEventTypeKeyDown location:NSZeroPoint modifierFlags:0 timestamp:NSTimeIntervalSince1970 windowNumber:0 context:nil characters:@"a" charactersIgnoringModifiers:@"a" isARepeat:NO keyCode:0];
+    [_im handleEvent:event client:client];
+}
+
+@end
--- mac/build.sh
+++ mac/build.sh
@@ -22,10 +22,7 @@ display_usage() {
     echo "                  the deploy option is used to specify preprelease, in which configuration will be"
     echo "                  Release (i.e., -config option is ignored)."
     echo "  -clean          Removes all previously-existing build products for anything to be built before building."
-    # Note: it's fairly unlikely that we will ever need/want unit tests for the IM or the test app, but
-    # FWIW, the -test option is currently only implemented for KeymanEngine4Mac. Not sure exactly what the
-    # problem is, but I can't get it to work with the other two (two different build errors).
-    echo "  -test           Runs KeymanEngine4Mac unit tests (when we get some)"
+    echo "  -test           Runs unit tests"
     echo "  -no-codesign    Disables code-signing for Keyman4MacIM, allowing it to be performed separately later"
     echo "                  (ignored if a deploy option other than 'none' is specified)."
     echo "  -quiet          Do not display any output except for warnings and errors."
@@ -107,7 +104,7 @@ DO_KEYMANTESTAPP=false
 CODESIGNING_SUPPRESSION=""
 BUILD_OPTIONS=""
 BUILD_ACTIONS="build"
-TEST_ACTIONS=""
+#TEST_ACTIONS=""
 CLEAN=false
 QUIET=false
 SKIP_BUILD=false
@@ -176,7 +173,7 @@ while [[ $# -gt 0 ]] ; do
             BUILD_ACTIONS="clean $BUILD_ACTIONS"
             ;;
         -test)
-            TEST_ACTIONS="test -scheme $ENGINE_NAME"
+            BUILD_ACTIONS="$BUILD_ACTIONS test"
             ;;
         -no-codesign)
             if $LOCALDEPLOY || $PREPRELEASE ; then
@@ -220,7 +217,7 @@ if $SKIP_BUILD ; then
     DO_KEYMANIM=false
     DO_KEYMANTESTAPP=false
     BUILD_ACTIONS=""
-    TEST_ACTIONS=""
+   #  TEST_ACTIONS=""
     BUILD_OPTIONS=""
     CODESIGNING_SUPPRESSION=""
 fi
@@ -238,7 +235,7 @@ displayInfo "" \
     "CODESIGNING_SUPPRESSION: $CODESIGNING_SUPPRESSION" \
     "BUILD_OPTIONS: $BUILD_OPTIONS" \
     "BUILD_ACTIONS: $BUILD_ACTIONS" \
-    "TEST_ACTIONS: $TEST_ACTIONS" \
+   # "TEST_ACTIONS: $TEST_ACTIONS" \
     ""
 
 ### START OF THE BUILD ###
@@ -280,17 +277,17 @@ updatePlist() {
 
 if $DO_KEYMANENGINE ; then
     updatePlist "$KME4M_BASE_PATH" "$ENGINE_NAME"
-    execBuildCommand $ENGINE_NAME "xcodebuild -project \"$KME4M_PROJECT_PATH\" $BUILD_OPTIONS $BUILD_ACTIONS $TEST_ACTIONS"
+    execBuildCommand $ENGINE_NAME "xcodebuild -project \"$KME4M_PROJECT_PATH\" $BUILD_OPTIONS $BUILD_ACTIONS -scheme $ENGINE_NAME"
 fi
 
 if $DO_KEYMANIM ; then
     updatePlist "$KM4MIM_BASE_PATH" "$IM_NAME"
-    execBuildCommand $IM_NAME "xcodebuild -project \"$KMIM_PROJECT_PATH\" $CODESIGNING_SUPPRESSION $BUILD_OPTIONS $BUILD_ACTIONS"
+    execBuildCommand $IM_NAME "xcodebuild -project \"$KMIM_PROJECT_PATH\" $CODESIGNING_SUPPRESSION $BUILD_OPTIONS $BUILD_ACTIONS -scheme Keyman"
 fi
 
 if $DO_KEYMANTESTAPP ; then
     updatePlist "$KMTESTAPP_BASE_PATH" "$TESTAPP_NAME"
-    execBuildCommand $TESTAPP_NAME "xcodebuild -project \"$KMTESTAPP_PROJECT_PATH\" $BUILD_OPTIONS $BUILD_ACTIONS"
+    execBuildCommand $TESTAPP_NAME "xcodebuild -project \"$KMTESTAPP_PROJECT_PATH\" $BUILD_OPTIONS $BUILD_ACTIONS -scheme $TESTAPP_NAME"
 fi
 
 # Deploy as requested
