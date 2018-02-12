a7b2352304d3ad4e73482ec92ecdd6121fbb2791
Marc Durdin <marc@keyman.com>
2018-02-12 19:41:58 +0700

    In progress, changeover to using version and new url in download dialog

--- mac/Keyman4MacIM/Keyman4MacIM/KMDownloadKeyboard/KMDownloadKBWindowController.m
+++ mac/Keyman4MacIM/Keyman4MacIM/KMDownloadKeyboard/KMDownloadKBWindowController.m
@@ -23,7 +23,11 @@
     [super windowDidLoad];
     
     [self.webView setFrameLoadDelegate:(id<WebFrameLoadDelegate>)self];
-    [self.webView.mainFrame loadRequest:[NSURLRequest requestWithURL:[NSURL URLWithString:@"https://secure.tavultesoft.com/prog/macosx/10/downloadkeyboards/"]]];
+    
+    NSString *url;
+    url = [NSString stringWithFormat:@"https://keyman.com/go/macos/10.0//download-keyboards/?version=%@", [info objectForKey:@"HeaderTitle"]];    
+    NSString *version = [[[NSBundle mainBundle] infoDictionary] objectForKey:@"CFBundleShortVersionString"];
+    NSString *build = [[[NSBundle mainBundle] infoDictionary] objectForKey:(NSString *)kCFBundleVersionKey];
 }
 
 @end
