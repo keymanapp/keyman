228dacbd670b8854d872880e40e85e627e6d5cb4
Marc Durdin <marc@keyman.com>
2018-02-13 16:07:32 +0700

    Remove failure case

--- windows/src/unit-tests/keyboard-js-info/Keyman.Test.System.KeyboardJSInfoTest.pas
+++ windows/src/unit-tests/keyboard-js-info/Keyman.Test.System.KeyboardJSInfoTest.pas
@@ -57,7 +57,6 @@ begin
     Assert.AreEqual('1.1', Version);
     Assert.AreEqual(True, RTL, 'RTL');
     Assert.AreEqual(False, Mnemonic, 'Mnemonic');
-    Assert.Fail('Testing failure case now...');
   finally
     Free;
   end;
