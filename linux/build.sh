9626d32bfbf75a08319118946e2a998f9c656d5f
glasseyes <dglassey@gmail.com>
2018-04-23 14:03:43 +0700

    [KMFL] set INSTALLDIR for build.sh
    autoreconf projects for consistency

--- linux/build.sh
+++ linux/build.sh
@@ -1,10 +1,27 @@
 #!/bin/sh
 
+# Build KMFL in the required order: kmflcomp libkmfl ibus-kmfl
+# install each to temp install dir INSTALLDIR which is /tmp/kmfl by default
+#   so that libkmfl and ibus-kmfl can use what they need that have been installed
+
+# It must be run from the keyman/linux directory
+
+
 BASEDIR=`pwd`
 
+INSTALLDIR=${INSTALLDIR:-"/tmp/kmfl"}
+
+# autoreconf the projects
+#for proj in kmflcomp libkmfl ibus-kmfl; do
+#	cd $proj
+#	autoreconf
+#	cd $BASEDIR
+#done
+
 for proj in kmflcomp libkmfl ibus-kmfl; do
+	rm -rf build-$proj
 	mkdir build-$proj
 	cd build-$proj
-	CPPFLAGS="-I/tmp/kmfl" ../$proj/configure --prefix=/tmp/kmfl && make && make install
+	CPPFLAGS="-I${INSTALLDIR}/include" LDFLAGS="-L${INSTALLDIR}/lib" ../$proj/configure --prefix=${INSTALLDIR} && make && make install
 	cd $BASEDIR
 done
