66e35ed875763a99f218025d1343884b47634640
Joshua A. Horton <joshua_horton@sil.org>
2018-04-26 14:18:59 +0700

    Fixes KMW landscape display in Firefox, Chrome on mobile devices.

--- web/source/kmwutils.ts
+++ web/source/kmwutils.ts
@@ -442,13 +442,21 @@ namespace com.keyman {
      * @return      {boolean}               
      */       
     landscapeView(): boolean	{ // new for I3363 (Build 301)
+      var orientation: number;
+
       // Assume portrait mode if orientation undefined
-      if(typeof(window.orientation) == 'undefined') {
-        return false;
+      if(typeof window.orientation != 'undefined') { // Used by iOS Safari
+        // Else landscape for +/-90, portrait for 0, +/-180   
+        orientation = window.orientation as number;
+      } else if(typeof window.screen.orientation != 'undefined') { // Used by Firefox, Chrome
+        orientation = window.screen.orientation.angle;
       }
       
-      // Else landscape for +/-90, portrait for 0, +/-180   
-      return (Math.abs(<number>window.orientation/90) == 1); 
+      if(orientation !== undefined) {
+        return (Math.abs(orientation/90) == 1); 
+      } else {
+        return false;
+      }
     }
     
     /**
