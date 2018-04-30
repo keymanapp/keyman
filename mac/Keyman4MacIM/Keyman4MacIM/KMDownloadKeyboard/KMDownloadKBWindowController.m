dce4f02a928bbf019dbe79171740afbb57e98aa3
Tom Bogle <tom_bogle@sil.org>
2018-04-30 17:03:03 -0400

    Fixed mess caused by previous check-in

--- mac/Keyman4MacIM/Keyman4MacIM/KMDownloadKeyboard/KMDownloadKBWindowController.m
+++ mac/Keyman4MacIM/Keyman4MacIM/KMDownloadKeyboard/KMDownloadKBWindowController.m
@@ -30,10 +30,11 @@
     NSString *url = [NSString stringWithFormat:@"https://keyman.com/go/macos/10.0/download-keyboards/?version=%@", version];
     NSLog(@"KMDownloadKBWindowController opening url = %@, version = '%@'", url, version);
     [self.webView.mainFrame loadRequest:[NSURLRequest requestWithURL:[NSURL URLWithString:url]]];
+}
 
 // REVIEW: Compare this implementation to the one in KMKeyboardHelpWindowController. Should we
 // use a consistent prefix?
-- (void) webView:(WebView*) sender decidePolicyForNavigationAction:(NSDictionary*) actionInformation request:(NSURLRequest*) request frame:(WebFrame*) frame decisionListener:(id <WebPolicyDecisionListener>) listener {
+- (void) decidePolicyForNavigationAction:(NSDictionary*) actionInformation request:(NSURLRequest*) request frame:(WebFrame*) frame decisionListener:(id <WebPolicyDecisionListener>) listener {
     NSString* url = [[request URL] description];
     NSLog(@"decidePolicyForNavigationAction, navigating to %@", url);
     if ([request.URL.absoluteString startsWith:@"keyman:link?url="])
