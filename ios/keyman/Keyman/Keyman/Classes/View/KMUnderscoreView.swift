6ec61635f16c32ba25f30d9a773386e74fe02442
Jacob Bullock <jacob.bullock@gmail.com>
2018-04-29 23:49:21 -1000

    implement new super view, used UIColor constants

--- ios/keyman/Keyman/Keyman/Classes/View/KMUnderscoreView.swift
+++ ios/keyman/Keyman/Keyman/Classes/View/KMUnderscoreView.swift
@@ -9,37 +9,22 @@
 import Foundation
 import  UIKit
 
-class KMUnderscoreView: UIView  {
-    required public init?(coder: NSCoder) {
-        super.init(coder: coder)
-        self.commonInit()
-    }
-
-    public override init(frame: CGRect) {
-        super.init(frame: frame)
-        self.commonInit()
-    }
-
-    func commonInit() {
-    }
-
+class KMUnderscoreView: KMView {
     override func draw(_ rect: CGRect) {
-        let color: UIColor = .yellow
-
-       
+        //percentage based widths for the color bar portion of the logo
         let widths: [CGFloat] = [0.6, 0.25, 0.15]
-        let colors: [UIColor] = [ UIColor(red: 244/255, green: 113/255, blue: 32/255, alpha: 1.0),
-                                  UIColor(red: 186/255, green: 32/255, blue: 52/255, alpha: 1.0),
-                                  UIColor(red: 105/255, green: 183/255, blue: 211/255, alpha: 1.0)]
+        let colors: [UIColor] = [ UIColor.keymanOrange,
+                                  UIColor.keymanRed,
+                                  UIColor.keymanBlue]
 
         var x: CGFloat = 0
         for i in 0..<(widths.count) {
             let w: CGFloat = rect.width * widths[i]
-            let drect = CGRect(x: x, y: 0,  width: w, height: rect.height)
+            let drect = CGRect(x: x, y: 0, width: w, height: rect.height)
             let bpath: UIBezierPath = UIBezierPath(rect: drect)
-            
-            let c = colors[i]
-            c.set()
+
+            let color = colors[i]
+            color.set()
             bpath.fill()
 
             x += w
