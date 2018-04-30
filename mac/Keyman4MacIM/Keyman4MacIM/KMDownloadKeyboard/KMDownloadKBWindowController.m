fe06753df3a92a8e1da4ee68f1d8074c7a5d3765
Tom Bogle <tom_bogle@sil.org>
2018-04-30 15:43:01 -0400

    [Mac] 709:Added code to detect link that should open in browser
    This allows "Go to keyman.com" button to work correctly.

--- mac/Keyman4MacIM/Keyman4MacIM/KMDownloadKeyboard/KMDownloadKBWindowController.m
+++ mac/Keyman4MacIM/Keyman4MacIM/KMDownloadKeyboard/KMDownloadKBWindowController.m
@@ -3,7 +3,7 @@
 //  Keyman4MacIM
 //
 //  Created by Serkan Kurt on 18/05/2015.
-//  Copyright (c) 2017 SIL International. All rights reserved.
+//  Copyright (c) 2018 SIL International. All rights reserved.
 //
 
 #import "KMDownloadKBWindowController.h"
@@ -24,10 +24,28 @@
     
     [self.webView setFrameLoadDelegate:(id<WebFrameLoadDelegate>)self];
     
+    [self.webView setGroupName:@"KMDownloadKB"];
+    [self.webView setPolicyDelegate:(id<WebPolicyDelegate>)self];
     NSString *version = [[[NSBundle mainBundle] infoDictionary] objectForKey:@"CFBundleShortVersionString"];
     NSString *url = [NSString stringWithFormat:@"https://keyman.com/go/macos/10.0/download-keyboards/?version=%@", version];
     NSLog(@"KMDownloadKBWindowController opening url = %@, version = '%@'", url, version);
     [self.webView.mainFrame loadRequest:[NSURLRequest requestWithURL:[NSURL URLWithString:url]]];
+
+// REVIEW: Compare this implementation to the one in KMKeyboardHelpWindowController. Should we
+// use a consistent prefix?
+- (void) webView:(WebView*) sender decidePolicyForNavigationAction:(NSDictionary*) actionInformation request:(NSURLRequest*) request frame:(WebFrame*) frame decisionListener:(id <WebPolicyDecisionListener>) listener {
+    NSString* url = [[request URL] description];
+    NSLog(@"decidePolicyForNavigationAction, navigating to %@", url);
+    if ([request.URL.absoluteString startsWith:@"keyman:link?url="])
+    {
+        [listener ignore];
+        url = [request.URL.absoluteString substringFromIndex:[@"keyman:link?url=" length]];
+        [[NSWorkspace sharedWorkspace] openURL: [[NSURL alloc] initWithString:url]];
+    }
+    else
+    {
+        [listener use];
+    }
 }
 
 @end
