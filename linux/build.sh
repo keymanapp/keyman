f3dd382d4b8c9ddf1f251ba7722f804f49029228
glasseyes <dglassey@gmail.com>
2018-05-03 16:20:20 +0700

    autoreconf on build agent

--- linux/build.sh
+++ linux/build.sh
@@ -19,11 +19,11 @@ SUDOINSTALL=${SUDOINSTALL:-"no"}
 INSTALLDIR=${INSTALLDIR:-"/tmp/kmfl"}
 
 # autoreconf the projects
-#for proj in kmflcomp libkmfl ibus-kmfl; do
-#	cd $proj
-#	autoreconf
-#	cd $BASEDIR
-#done
+for proj in kmflcomp libkmfl ibus-kmfl; do
+	cd $proj
+	autoreconf
+	cd $BASEDIR
+done
 
 if [[ "${SUDOINSTALL}" == "uninstall" ]]
 then
