e13918c4b51ef669a8bdb7c1eeda13cf1537e2f4
Tom Bogle <tom_bogle@sil.org>
2018-05-15 16:59:08 -0400

    Made Crashlytics "Easter egg" only happen when verbose logging is on.

--- mac/Keyman4MacIM/Keyman4MacIM/KMInputMethodEventHandler.m
+++ mac/Keyman4MacIM/Keyman4MacIM/KMInputMethodEventHandler.m
@@ -64,10 +64,13 @@ NSRange _previousSelRange;
     //    }
     
     // In Xcode, if Keyman is the active IM and is in "debugMode" and "English plus Spanish" is the current keyboard and you type "Crashlytics force now", it will force a simulated crash to test reporting to fabric.io.
-    if ([self.AppDelegate debugMode])
+    if ([self.AppDelegate debugMode]) {
         NSLog(@"Crashlytics - Preparing to detect Easter egg.");
-    _easterEggForCrashlytics = ([clientAppId isEqual: @"com.apple.dt.Xcode"]) ?
-        [[NSMutableString alloc] init] : nil;
+        _easterEggForCrashlytics = ([clientAppId isEqual: @"com.apple.dt.Xcode"]) ?
+            [[NSMutableString alloc] init] : nil;
+    }
+    else
+        _easterEggForCrashlytics = nil;
     
     // For the Atom editor, this isn't really true (the context CAN change unexpectedly), but we can't get
     // the context, so we pretend/hope it won't.
