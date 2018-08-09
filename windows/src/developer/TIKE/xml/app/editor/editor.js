86d6ad387125c21093770ed9a4a1bf6d5088b191
Marc Durdin <marc@keyman.com>
2018-08-09 13:51:52 +1000

    [Developer] Improve lifecycle for modified state on editor

--- windows/src/developer/TIKE/xml/app/editor/editor.js
+++ windows/src/developer/TIKE/xml/app/editor/editor.js
@@ -1,4 +1,8 @@
-(function() {
+window.editorGlobalContext = {
+  loading: false
+};
+
+(function(context) {
   var editor = null;
   var filename = decodeURIComponent(location.search.substr(1));
   
@@ -8,19 +12,23 @@
     editor.setTheme("ace/theme/monokai");
     editor.session.setMode("ace/mode/keyman");
     editor.session.on('change', function(delta) {
-      $.post("/app/source/file", {
-        Filename: filename,
-        Data: editor.session.getValue()
-        // delta.start, delta.end, delta.lines, delta.action
-      });
-      location.href = 'keyman:command?modified';
+      if(!context.loading) {
+        $.post("/app/source/file", {
+          Filename: filename,
+          Data: editor.session.getValue()
+          // delta.start, delta.end, delta.lines, delta.action
+        });
+        location.href = 'keyman:command?modified';
+      }
     });
     // Start reading the source file
     $.get("/app/source/file", { 
       Filename: filename
     }, function (response) {
+      context.loading = true;
       editor.session.setValue(response);
+      context.loading = false;
     }, 
     "text"); 
   });
-})();
+})(window.editorGlobalContext);
