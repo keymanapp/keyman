ff4066e37426870eb82c5df7cc94b65c9ae978d6
Darcy Wong <darcy_wong@sil.org>
2018-08-30 14:55:52 +0700

    Add splash screen
    This addresses the Android platform of #1069
    
    * Show product logos on app start
    * Then show text (version and copyright dates)

---
+++ android/KMAPro/kMAPro/src/main/java/com/tavultesoft/kmapro/SplashScreenActivity.java
@@ -0,0 +1,48 @@
+package com.tavultesoft.kmapro;
+
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
+import android.os.Bundle;
+import android.os.Handler;
+import android.app.Activity;
+import android.content.Intent;
+import android.widget.TextView;
+
+import java.util.Calendar;
+
+public class SplashScreenActivity extends Activity {
+
+  private static int SPLASH_TIME_OUT = 1000; // msec
+
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_splash_screen);
+
+    TextView version = (TextView)findViewById(R.id.splash_version);
+    String ver = "";
+    PackageInfo pInfo;
+    try {
+      pInfo = getPackageManager().getPackageInfo(getPackageName(), 0);
+      ver = String.format("%s: %s", version.getText(), pInfo.versionName);
+    } catch (PackageManager.NameNotFoundException e) {
+      // Could not get version number
+    }
+    version.setText(ver);
+
+    TextView copyright = (TextView)findViewById(R.id.splash_copyright);
+    int year = Calendar.getInstance().get(Calendar.YEAR);
+    String date = String.format("%s%s", copyright.getText(), year);
+    copyright.setText(date);
+
+    new Handler().postDelayed(new Runnable() {
+      @Override
+      public void run() {
+        Intent intent = new Intent(getApplicationContext(),
+          MainActivity.class);
+        startActivity(intent);
+        finish();
+      }
+    }, SPLASH_TIME_OUT);
+  }
+}
