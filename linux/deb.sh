4d3a88eb1abff5f7ab4f953fb400a3cc45cdac73
glasseyes <dglassey@gmail.com>
2018-09-03 17:16:41 +0700

    also build keyman-config package

--- linux/deb.sh
+++ linux/deb.sh
@@ -7,15 +7,18 @@
 
 set -e
 
-distributions='bionic xenial'
-projects='kmflcomp libkmfl ibus-kmfl'
+distributions="bionic xenial"
+autotool_projects="kmflcomp libkmfl ibus-kmfl"
+all_projects="${autotool_projects} keyman-config"
 echo "distributions: ${distributions}"
+echo "autotool_projects: ${autotool_projects}"
+echo "all_projects: ${all_projects}"
 
 declare -A proj_versions=()
 BASEDIR=`pwd`
 
 # autoreconf the projects
-for proj in ${projects}; do
+for proj in ${autotool_projects}; do
 	cd $proj
 	echo "Reconfiguring $proj"
 	autoreconf
@@ -26,7 +29,7 @@ mkdir -p builddebs
 mkdir -p builddebs/xenial
 mkdir -p builddebs/bionic
 
-for proj in ${projects}; do
+for proj in ${autotool_projects}; do
 	rm -rf deb-$proj
 	mkdir deb-$proj
 	cd deb-$proj
@@ -40,18 +43,35 @@ for proj in ${projects}; do
 	cd ${BASEDIR}
 done
 
-echo ${proj_versions}
+cd keyman-config
+rm -rf dist make_deb build
+python3 setup.py egg_info -b.`date -u +"%Y%m%d%H%M%S"` sdist
+cd dist
+ls *.tar.gz
+tmp=`basename *.tar.gz .tar.gz`
+origname=${tmp/keyman\_config-/keyman-config\_}
+index=`expr index "${origname}" _`
+proj_versions[keyman-config]=${tmp:$index}
+kcversion=${proj_versions[keyman-config]}
 
+echo "keyman-config version: ${kcversion}"
 
-for proj in ${projects}; do
+tar xf keyman_config-${kcversion}.tar.gz
+mv keyman_config-${kcversion} keyman-config-${kcversion}
+tar cfz keyman-config_${kcversion}.orig.tar.gz keyman-config-${kcversion}
+
+cp keyman-config_${kcversion}.orig.tar.gz ${BASEDIR}/builddebs/
+cd ${BASEDIR}
+
+for proj in ${all_projects}; do
 	cd builddebs
 	echo "$proj version ${proj_versions[$proj]}"
 	rm -rf ${proj}-${proj_versions[$proj]}
 	tar xfz ${proj}_${proj_versions[$proj]}.orig.tar.gz
 	cp -a ../$proj/debian ${proj}-${proj_versions[$proj]}
 	cd ${proj}-${proj_versions[$proj]}
-	dch -v ${proj_versions[$proj]}-1${dist} "local build"
 	for dist in ${distributions}; do
+		dch -v ${proj_versions[$proj]}-1${dist} "local build"
 		echo "$dist"
 		pdebuild --pbuilder cowbuilder --buildresult /var/cache/pbuilder/result/${dist} -- --basepath /var/cache/pbuilder/base-${dist}.cow --distribution ${dist} --override-config --othermirror="deb [trusted=yes] file:/var/cache/pbuilder/result/${dist} ./" --bindmounts /var/cache/pbuilder/result/${dist}  --hookdir /var/cache/pbuilder/hook.d/${dist}
 	done
