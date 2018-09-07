0c7703fb323696410defca5a6a42c2987bd2b0eb
glasseyes <dglassey@gmail.com>
2018-09-07 11:13:35 +0700

    add scripts/jenkins.sh for making source packages on jenkins

---
+++ linux/scripts/jenkins.sh
@@ -0,0 +1,21 @@
+#!/bin/bash -e
+
+. \$HOME/ci-builder-scripts/bash/common.sh
+init "$@"
+
+rm -rf builddebs
+rm -rf $1/${1}_*.{dsc,build,buildinfo,changes,tar.?z,log}
+rm -rf ${1}_*.{dsc,build,buildinfo,changes,tar.?z,log}
+
+log "Make source package for $1"
+./scripts/reconf.sh dev $1
+./scripts/dist.sh origdist $1
+./scripts/deb.sh sourcepackage $1
+
+#sign source package
+for file in `ls builddebs/*.dsc`; do
+	log "Signing source package $file"
+	debsign -k\$2 \$file
+done
+
+mv builddebs/* $1
