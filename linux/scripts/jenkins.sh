4d155f9af021f3f98f0f0913811f7b3eca73c715
glasseyes <dglassey@gmail.com>
2018-09-10 23:12:23 +0700

    [linux] fix call to common.sh init
    don't pass on parameters

--- linux/scripts/jenkins.sh
+++ linux/scripts/jenkins.sh
@@ -1,9 +1,10 @@
 #!/bin/bash -e
 
 . $HOME/ci-builder-scripts/bash/common.sh
-init "$@"
+init
 
 # clean up prev deb builds
+log "cleaning previous builds of $1"
 rm -rf builddebs
 rm -rf $1/${1}_*.{dsc,build,buildinfo,changes,tar.?z,log}
 rm -rf ${1}_*.{dsc,build,buildinfo,changes,tar.?z,log}
@@ -16,7 +17,7 @@ log "Make source package for $1"
 #sign source package
 for file in `ls builddebs/*.dsc`; do
 	log "Signing source package $file"
-	debsign -k\$2 \$file
+	debsign -k$2 $file
 done
 
 mv builddebs/* $1
