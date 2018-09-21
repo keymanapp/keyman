7a63053aaf67eee57080337f8cf9247dda7ae715
glasseyes <dglassey@gmail.com>
2018-09-21 15:02:44 +0700

    [linux] list kmp installed in different areas
    user ~/.local/share/keyman or ~/.kmfl
    local shared /usr/local/share/keyman
    OS /usr/share/keyman

--- linux/keyman-config/keyman_config/list_installed_kmp.py
+++ linux/keyman-config/keyman_config/list_installed_kmp.py
@@ -6,9 +6,70 @@ import os
 import json
 from keyman_config.kmpmetadata import parsemetadata, parseinfdata
 
-def get_installed_kmp():
+def get_installed_kmp_os():
+    """
+    Get list of installed keyboards in /usr/share.
+
+    Returns:
+        list: Installed keyboards
+            dict: Keyboard
+                id (str): Keyboard ID
+                name (str): Keyboard name
+                kmpname (str): Keyboard name in local
+                version (str): Keyboard version
+                kmpversion (str):
+                path (str): base path where keyboard is installed
+                description (str): Keyboard description
+    """
+    check_paths = [ "/usr/share/keyman" ]
+    return get_installed_kmp(check_paths)
+
+
+def get_installed_kmp_shared():
+    """
+    Get list of installed keyboards in /usr/local/share.
+
+    Returns:
+        list: Installed keyboards
+            dict: Keyboard
+                id (str): Keyboard ID
+                name (str): Keyboard name
+                kmpname (str): Keyboard name in local
+                version (str): Keyboard version
+                kmpversion (str):
+                path (str): base path where keyboard is installed
+                description (str): Keyboard description
+    """
+    check_paths = [ "/usr/local/share/keyman" ]
+    return get_installed_kmp(check_paths)
+
+def get_installed_kmp_user():
+    """
+    Get list of installed keyboards in user areas.
+
+    Returns:
+        list: Installed keyboards
+            dict: Keyboard
+                id (str): Keyboard ID
+                name (str): Keyboard name
+                kmpname (str): Keyboard name in local
+                version (str): Keyboard version
+                kmpversion (str):
+                path (str): base path where keyboard is installed
+                description (str): Keyboard description
+    """
+    home = os.path.expanduser("~")
+    datahome = os.environ.get("XDG_DATA_HOME", os.path.join(home, ".local", "share"))
+    check_paths = [ os.path.join(datahome, "keyman"), os.path.join(home, ".kmfl") ]
+    return get_installed_kmp(check_paths)
+
+
+def get_installed_kmp(check_paths):
     """
     Get list of installed keyboards.
+
+	Args:
+		check_paths (list): list of paths to check
 
     Returns:
         list: Installed keyboards
@@ -22,32 +83,32 @@ def get_installed_kmp():
                 description (str): Keyboard description
     """
     installed_keyboards = {}
-    check_paths = [ "/usr/share/keyman", "/usr/local/share/keyman" ]
     for keymanpath in check_paths:
-        for o in os.listdir(keymanpath):
-            if os.path.isdir(os.path.join(keymanpath,o)) and o != "icons":
-                name = md_name = version = md_version = description = kbdata = None
-                metadata = parsemetadata(os.path.join(keymanpath, o, "kmp.json"))
-                if not metadata[0]:
-                    metadata = parseinfdata(os.path.join(keymanpath, o, "kmp.inf"))
-                kbjson = os.path.join(keymanpath, o, o + ".json")
-                if os.path.isfile(kbjson):
-                    with open(kbjson, "r") as read_file:
-                        kbdata = json.load(read_file)
-                if kbdata:
-                    if 'description' in kbdata:
-                        description = kbdata['description']
-                    version = kbdata['version']
-                    name = kbdata['name']
-                if metadata[0]:
-                    info = metadata[0]
-                    md_version = info['version']['description']
-                    md_name = info['name']['description']
-                if not name:
-                    version = md_version
-                    name = md_name
+        if os.path.isdir(keymanpath):
+            for o in os.listdir(keymanpath):
+                if os.path.isdir(os.path.join(keymanpath,o)) and o != "icons":
+                    name = md_name = version = md_version = description = kbdata = None
+                    metadata = parsemetadata(os.path.join(keymanpath, o, "kmp.json"))
+                    if not metadata[0]:
+                        metadata = parseinfdata(os.path.join(keymanpath, o, "kmp.inf"))
+                    kbjson = os.path.join(keymanpath, o, o + ".json")
+                    if os.path.isfile(kbjson):
+                        with open(kbjson, "r") as read_file:
+                            kbdata = json.load(read_file)
+                    if kbdata:
+                        if 'description' in kbdata:
+                            description = kbdata['description']
+                        version = kbdata['version']
+                        name = kbdata['name']
+                    if metadata[0]:
+                        info = metadata[0]
+                        md_version = info['version']['description']
+                        md_name = info['name']['description']
+                    if not name:
+                        version = md_version
+                        name = md_name
 
-                installed_keyboards[o] = { "id" : o, "name" : name, "kmpname" : md_name, "version" : version, "kmpversion" : md_version, "path" : keymanpath, "description" : description}
+                    installed_keyboards[o] = { "id" : o, "name" : name, "kmpname" : md_name, "version" : version, "kmpversion" : md_version, "path" : keymanpath, "description" : description}
     return installed_keyboards
 
 
@@ -66,30 +127,3 @@ def get_kmp_version(keyboardid):
         return installed_kmp[keyboardid]['version']
     else:
         return None
-
-
-def main():
-    parser = argparse.ArgumentParser(description='Show installed keyman keyboards.')
-    parser.add_argument('-s', "--short", help='short format', action="store_true")
-
-    args = parser.parse_args()
-
-    installed_kmp = get_installed_kmp()
-    print("--- Installed keyboards ---")
-    for kmp in sorted(installed_kmp):
-        print(installed_kmp[kmp]['name'] + ", version:", installed_kmp[kmp]['version'] + ", id:", kmp)
-        if not args.short:
-            if installed_kmp[kmp]['version'] != installed_kmp[kmp]['kmpversion']:
-                logging.warning("Version mismatch. Installed keyboard is %s. Website says it is %s." % (installed_kmp[kmp]['kmpversion'], installed_kmp[kmp]['version']))
-            if installed_kmp[kmp]['name'] != installed_kmp[kmp]['kmpname']:
-                logging.warning("Name mismatch. Installed keyboard is %s. Website says it is %s." % (installed_kmp[kmp]['name'], installed_kmp[kmp]['kmpname']))
-
-            if installed_kmp[kmp]['description']:
-                print(installed_kmp[kmp]['description'])
-            else:
-                print("No description")
-            print(os.linesep)
-
-
-if __name__ == "__main__":
-    main()
