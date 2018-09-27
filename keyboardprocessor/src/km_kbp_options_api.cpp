7ac2bb5afc2ed0bb4bfe9e89c1db3068840ae3ba
Tim Eves <tim_eves@sil.org>
2018-09-27 15:35:16 +0700

    Initial commit of keyboardprocessor API
    Header's and start of null keyboard processor for testing.

---
+++ keyboardprocessor/src/km_kbp_options_api.cpp
@@ -0,0 +1,16 @@
+#include <cassert>
+#include <algorithm>
+#include <iterator>
+#include <list>
+#include <vector>
+
+#include <keyboardprocessor.h>
+
+#include <utfcodec.hpp>
+
+namespace km {
+namespace kbp
+{
+
+}
+}
