e406eb2d854f42336f547e6d95e88483a33bb4a6
Marc Durdin <marc@keyman.com>
2018-09-28 16:37:10 -0600

    [Developer] Avoid a crash in package editor when compiled keyboard file is missing

--- windows/src/global/delphi/packages/Keyman.System.PackageInfoRefreshKeyboards.pas
+++ windows/src/global/delphi/packages/Keyman.System.PackageInfoRefreshKeyboards.pas
@@ -233,7 +233,7 @@ begin
 
   // Need to test if the JS is a valid keyboard file.
   // This is a bit of a pain... but we have to stick with .js for compat
-  if IsKeyboardFileByContent(f) then
+  if not FileExists(f.FileName) or IsKeyboardFileByContent(f) then
     Exit(ftJavascript);
 
   Exit(ftOther);
@@ -297,6 +297,9 @@ class function TPackageInfoRefreshKeyboards.FillKeyboardDetails(f: TPackageConte
 var
   ki: TKeyboardInfo;
 begin
+  if not FileExists(f.FileName) then
+    Exit(False);
+
   Result := True;
   pki.ID := TKeyboardUtils.KeyboardFileNameToID(f.FileName);
   pki.RTL := False;
