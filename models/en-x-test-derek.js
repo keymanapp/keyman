a54280385514f33e7f6961c8cfce8ca9940d78d5
Eddie Antonio Santos <easantos@ualberta.ca>
2018-10-11 11:31:33 -0600

    Support new registerModel function.

--- models/en-x-test-derek.js
+++ models/en-x-test-derek.js
@@ -1,23 +1,21 @@
 /**
  * This is a dummy model that always predicts "Derek".
  */
-class Model {
-  constructor(configuration) {
-    this.configuration = {
-      leftContextCodeUnits: 32
-    };
-  }
+registerModel(function (_configuration) {
+  return new class Model {
+    constructor() {
+      this.configuration = {
+        leftContextCodeUnits: 32
+      };
+    }
 
-  predict(_) {
-    let transform = { insert: 'Derek', deleteLeft: 1, deleteRight: 0 };
+    predict(_) {
+      let transform = { insert: 'Derek', deleteLeft: 1, deleteRight: 0 };
 
-    return [
-      { transform, weight: 0 }
-    ];
-  }
-}
-
-// TODO: shouldn't this be running in the same scope as the original?
-if (typeof global !== 'undefined') {
-  global.Model = Model;
-}
+      return [
+        { transform, weight: 0 }
+      ];
+    }
+  };
+});
+/* global registerModel */
