e7fe39862df91fcde2ac8f499f9253b5606bc333
Eddie Antonio Santos <easantos@ualberta.ca>
2018-10-11 11:42:32 -0600

    Extract function: onMessageWhenReady().

--- lmlayer.js
+++ lmlayer.js
@@ -20,37 +20,12 @@ self.registerModel = function registerModel(modelFactory /* (c: Configuration) =
  * Handles messages from the keyboard.
  */
 self.onmessage = function (event) {
-  const {message, token} = event.data;
+  const {message} = event.data;
 
   if (message === 'initialize') {
     onMessageUninitialized(event);
   } else if (message === 'predict') {
-    // XXX: induce the other end to reject the promise, because of a
-    // token/message mismatch. This is for testing purposes.
-    if (token === null) {
-      cast('invalid', {token});
-      return;
-    }
-
-    // TODO: rip contexts out of message.
-    let rawSuggestions = model.predict();
-
-    // Sort in-place according to weight.
-    rawSuggestions.sort((a, b) => a.weight - b.weight);
-
-    // Convert the internal suggestion format to the one required by the keyboard.
-    let suggestions = rawSuggestions.map((internal) => {
-      let displayAs = internal.displayAs;
-
-      // Try to make up a display string.
-      if (displayAs === null || displayAs === undefined) {
-        displayAs = internal.transform.insert;
-      }
-
-      return { displayAs, ...internal.transform };
-    });
-
-    cast('suggestions', { token, suggestions });
+    onMessageWhenReady(event);
   } else {
     throw new Error('invalid message');
   }
@@ -76,6 +51,44 @@ function onMessageUninitialized(event) {
   // Ready! Send desired configuration.
   cast('ready', { configuration: model.configuration });
 }
+
+/**
+ * Handles messages when ready to predict.
+ */
+function onMessageWhenReady(event) {
+  const {message, token} = event.data;
+
+  if (message !== 'predict') {
+    throw new Error('invalid message');
+  }
+
+  // XXX: induce the other end to reject the promise, because of a
+  // token/message mismatch. This is for testing purposes.
+  if (token === null) {
+    cast('invalid', {token});
+    return;
+  }
+
+  // TODO: rip contexts out of message.
+  let rawSuggestions = model.predict();
+
+  // Sort in-place according to weight.
+  rawSuggestions.sort((a, b) => a.weight - b.weight);
+
+  // Convert the internal suggestion format to the one required by the keyboard.
+  let suggestions = rawSuggestions.map((internal) => {
+    let displayAs = internal.displayAs;
+
+    // Try to make up a display string.
+    if (displayAs === null || displayAs === undefined) {
+      displayAs = internal.transform.insert;
+    }
+
+    return { displayAs, ...internal.transform };
+  });
+
+  cast('suggestions', { token, suggestions });
+};
 
 /**
  * Send a message to the keyboard.
