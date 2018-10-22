e385ae72eb4b8a8f6a78d101c85552d280fb4e5e
Tim Eves <tim_eves@sil.org>
2018-10-22 18:46:59 +0700

    unit/context-api: Add more test cases.
    Check context_items_to_utf16 doesn't overrun it's buffer.
    Add tests for context_clear, context_shirnk and context_append.
    Add test including marker type context item.

--- keyboardprocessor/tests/unit/kmnkbd/context_api.cpp
+++ keyboardprocessor/tests/unit/kmnkbd/context_api.cpp
@@ -76,6 +76,9 @@ int main(int argc, char * argv[])
   n=km_kbp_context_items_to_utf16(ctxt2, ctxt_buffer,
                                   sizeof ctxt_buffer/sizeof(km_kbp_cp));
   if (initial_smp_context != ctxt_buffer) return __LINE__;
+  // Test buffer overrun protection.
+  n=km_kbp_context_items_to_utf16(ctxt2, ctxt_buffer, smp_ctxt_size/3);
+  if (n != smp_ctxt_size/3) return __LINE__;
 
   // Create a mock context object and set the items
   km_kbp_context mock_ctxt1, mock_ctxt2;
@@ -100,6 +103,39 @@ int main(int argc, char * argv[])
                                 sizeof ctxt_buffer/sizeof(km_kbp_cp));
   if (initial_smp_context != ctxt_buffer) return __LINE__;
 
+  // Call km_kbp_context_clear
+  km_kbp_context_clear(&mock_ctxt2);
+  if((n=km_kbp_context_length(&mock_ctxt2)) != 0) return __LINE__;
+
+  // Mutation tests
+  try_status(km_kbp_context_shrink(&mock_ctxt1, 42, nullptr));
+
+  // Append a character, a marker and & a string.
+  try_status(km_kbp_context_items_from_utf16(u" ", &ctxt1));
+  try_status(km_kbp_context_items_from_utf16(u"World!", &ctxt2));
+  try_status(km_kbp_context_append(&mock_ctxt1, ctxt1));
+  try_status(km_kbp_context_append(&mock_ctxt1, test_marker_ctxt));
+  try_status(km_kbp_context_append(&mock_ctxt1, ctxt2));
+
+  // Delete the items lists
+  km_kbp_context_items_dispose(ctxt1);
+  km_kbp_context_items_dispose(ctxt2);
+
+  // Check it matches. The marker will be elided during the conversion.
+  km_kbp_context_items_to_utf16(km_kbp_context_get(&mock_ctxt1),
+                                ctxt_buffer,
+                                sizeof ctxt_buffer/sizeof(km_kbp_cp));
+  if (std::u16string(u"Hello World!") != ctxt_buffer) return __LINE__;
+
+  // Test shrink and prepend, delete more than we provide to prepend.
+  try_status(km_kbp_context_items_from_utf16(u"Bye, ", &ctxt1));
+  // We delete 7 characters plus 1 marker hence 8 and not 7 as expected if you
+  //  go bye the test string above.
+  try_status(km_kbp_context_shrink(&mock_ctxt1, 8, ctxt1));
+  km_kbp_context_items_to_utf16(km_kbp_context_get(&mock_ctxt1),
+                                ctxt_buffer,
+                                sizeof ctxt_buffer/sizeof(km_kbp_cp));
+  if (std::u16string(u"Bye, Hello") != ctxt_buffer) return __LINE__;
 
   return 0;
 }
