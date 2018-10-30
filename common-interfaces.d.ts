61bf95e439d02739aca1181c49ce7fe2edd2678a
Eddie Antonio Santos <easantos@ualberta.ca>
2018-10-30 09:18:52 -0600

    Relicense as MIT (C) NRC & SIL.

--- common-interfaces.d.ts
+++ common-interfaces.d.ts
@@ -1,11 +1,33 @@
+/*
+ * Copyright (c) 2018 National Research Council Canada (author: Eddie A. Santos)
+ * Copyright (c) 2018 SIL International
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of
+ * this software and associated documentation files (the "Software"), to deal in
+ * the Software without restriction, including without limitation the rights to
+ * use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of
+ * the Software, and to permit persons to whom the Software is furnished to do so,
+ * subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS
+ * FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
+ * COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER
+ * IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+ * CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
+
 /**
  * A JavaScript string with the restriction that it must only
  * contain Unicode scalar values.
- * 
+ *
  * This means that any lone high surrogate must be paired with
  * a low surrogate, if it exists. Lone surrogate code units are
  * forbidden.
- * 
+ *
  * See also: https://developer.mozilla.org/en-US/docs/Web/API/USVString
  */
 type USVString = string;
