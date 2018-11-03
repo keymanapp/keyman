1fa8b584bd318196156e1fadbc9a4f51abda9280
Darcy Wong <darcy_wong@sil.org>
2018-11-03 21:30:22 +0700

    Address review comments

--- android/KMEA/app/src/main/java/com/tavultesoft/kmea/KMHardwareKeyboardInterpreter.java
+++ android/KMEA/app/src/main/java/com/tavultesoft/kmea/KMHardwareKeyboardInterpreter.java
@@ -14,98 +14,6 @@ public class KMHardwareKeyboardInterpreter implements KeyEvent.Callback {
    * keyboards.  Is conveniently based at 0, else we might need to HashMap it instead.
    */
 
-  /*
-  // Deprecated for scanCodeMap
-  private static final
-  int keyCodeMap[] = {
-    0,      //        public static final int KEYCODE_UNKNOWN = 0;
-    0,      //        public static final int KEYCODE_SOFT_LEFT = 1;
-    0,      //        public static final int KEYCODE_SOFT_RIGHT = 2;
-    0,      //        public static final int KEYCODE_HOME = 3;
-    0,      //        public static final int KEYCODE_BACK = 4;
-    0,      //        public static final int KEYCODE_CALL = 5;
-    0,      //        public static final int KEYCODE_ENDCALL = 6;
-    '0',    //        public static final int KEYCODE_0 = 7;
-    '1',    //        public static final int KEYCODE_1 = 8;
-    '2',    //        public static final int KEYCODE_2 = 9;
-    '3',    //        public static final int KEYCODE_3 = 10;
-    '4',    //        public static final int KEYCODE_4 = 11;
-    '5',    //        public static final int KEYCODE_5 = 12;
-    '6',    //        public static final int KEYCODE_6 = 13;
-    '7',    //        public static final int KEYCODE_7 = 14;
-    '8',    //        public static final int KEYCODE_8 = 15;
-    '9',    //        public static final int KEYCODE_9 = 16;
-    0,      //        public static final int KEYCODE_STAR = 17;
-    0,      //        public static final int KEYCODE_POUND = 18;
-    0,      //        public static final int KEYCODE_DPAD_UP = 19;
-    0,      //        public static final int KEYCODE_DPAD_DOWN = 20;
-    0,      //        public static final int KEYCODE_DPAD_LEFT = 21;
-    0,      //        public static final int KEYCODE_DPAD_RIGHT = 22;
-    0,      //        public static final int KEYCODE_DPAD_CENTER = 23;
-    0,      //        public static final int KEYCODE_VOLUME_UP = 24;
-    0,      //        public static final int KEYCODE_VOLUME_DOWN = 25;
-    0,      //        public static final int KEYCODE_POWER = 26;
-    0,      //        public static final int KEYCODE_CAMERA = 27;
-    0,      //        public static final int KEYCODE_CLEAR = 28;
-    'A',    //        public static final int KEYCODE_A = 29;
-    'B',    //        public static final int KEYCODE_B = 30;
-    'C',    //        public static final int KEYCODE_C = 31;
-    'D',    //        public static final int KEYCODE_D = 32;
-    'E',    //        public static final int KEYCODE_E = 33;
-    'F',    //        public static final int KEYCODE_F = 34;
-    'G',    //        public static final int KEYCODE_G = 35;
-    'H',    //        public static final int KEYCODE_H = 36;
-    'I',    //        public static final int KEYCODE_I = 37;
-    'J',    //        public static final int KEYCODE_J = 38;
-    'K',    //        public static final int KEYCODE_K = 39;
-    'L',    //        public static final int KEYCODE_L = 40;
-    'M',    //        public static final int KEYCODE_M = 41;
-    'N',    //        public static final int KEYCODE_N = 42;
-    'O',    //        public static final int KEYCODE_O = 43;
-    'P',    //        public static final int KEYCODE_P = 44;
-    'Q',    //        public static final int KEYCODE_Q = 45;
-    'R',    //        public static final int KEYCODE_R = 46;
-    'S',    //        public static final int KEYCODE_S = 47;
-    'T',    //        public static final int KEYCODE_T = 48;
-    'U',    //        public static final int KEYCODE_U = 49;
-    'V',    //        public static final int KEYCODE_V = 50;
-    'W',    //        public static final int KEYCODE_W = 51;
-    'X',    //        public static final int KEYCODE_X = 52;
-    'Y',    //        public static final int KEYCODE_Y = 53;
-    'Z',    //        public static final int KEYCODE_Z = 54;
-    188,    //        public static final int KEYCODE_COMMA = 55;
-    190,    //        public static final int KEYCODE_PERIOD = 56;
-    0,      //        public static final int KEYCODE_ALT_LEFT = 57;
-    0,      //        public static final int KEYCODE_ALT_RIGHT = 58;
-    0,      //        public static final int KEYCODE_SHIFT_LEFT = 59;
-    0,      //        public static final int KEYCODE_SHIFT_RIGHT = 60;
-    0,      //        public static final int KEYCODE_TAB = 61;
-    32,     //        public static final int KEYCODE_SPACE = 62;
-    0,      //        public static final int KEYCODE_SYM = 63;
-    0,      //        public static final int KEYCODE_EXPLORER = 64;
-    0,      //        public static final int KEYCODE_ENVELOPE = 65;
-    13,     //        public static final int KEYCODE_ENTER = 66;
-    0,      //        public static final int KEYCODE_DEL = 67;
-    192,    //        public static final int KEYCODE_GRAVE = 68;
-    189,    //        public static final int KEYCODE_MINUS = 69;
-    187,    //        public static final int KEYCODE_EQUALS = 70;
-    219,    //        public static final int KEYCODE_LEFT_BRACKET = 71;
-    221,    //        public static final int KEYCODE_RIGHT_BRACKET = 72;
-    220,    //        public static final int KEYCODE_BACKSLASH = 73;
-    186,    //        public static final int KEYCODE_SEMICOLON = 74;
-    222,    //        public static final int KEYCODE_APOSTROPHE = 75;
-    191,    //        public static final int KEYCODE_SLASH = 76;
-    0,      //        public static final int KEYCODE_AT = 77;
-    0,      //        public static final int KEYCODE_NUM = 78;
-    0,      //        public static final int KEYCODE_HEADSETHOOK = 79;
-    0,      //        public static final int KEYCODE_FOCUS = 80;
-    0,      //        public static final int KEYCODE_PLUS = 81;
-    0,      //        public static final int KEYCODE_MENU = 82;
-    0,      //        public static final int KEYCODE_NOTIFICATION = 83;
-    0       //        public static final int KEYCODE_SEARCH = 84;
-  };
-*/
-
   /**
    * From reference: https://source.android.com/devices/input#understanding-hid-usages-and-event-codes
    * The Android API sometimes refers to the Linux key code associated with a key as its "scan code".
@@ -133,8 +41,8 @@ public class KMHardwareKeyboardInterpreter implements KeyEvent.Callback {
     '0',    //        public static final int KEY_0 = 0x0B;
     189,    //        public static final int KEY_MINUS = 0x0C;
     187,    //        public static final int KEY_EQUALS = 0x0D;
-    0,      //        public static final int KEY_BACKSPACE = 0x0E;
-    0,      //        public static final int KEY_TAB = 0x0F;
+    8,      //        public static final int KEY_BACKSPACE = 0x0E;
+    9,      //        public static final int KEY_TAB = 0x0F;
     'Q',    //        public static final int KEY_Q = 0x10;
     'W',    //        public static final int KEY_W = 0x11;
     'E',    //        public static final int KEY_E = 0x12;
@@ -205,30 +113,7 @@ public class KMHardwareKeyboardInterpreter implements KeyEvent.Callback {
     0,      //        public static final int KEY_KPDOT = 0x53;
     0,      //        padding 0x54;
     0,      //        public static final int KEY_ZENKAKUHANKAKU = 0x55;
-    226,    //        public static final int KEY_102ND = 0x56;
-    0,      //        public static final int KEY_F11 = 0x57;
-    0,      //        public static final int KEY_F12 = 0x58;
-    0,      //        public static final int KEY_RO =  0x59;
-    0,      //        public static final int KEY_KATAKANA =  0x5A;
-    0,      //        public static final int KEY_HIRGANA =  0x5B;
-    0,      //        public static final int KEY_HENKAN =  0x5C;
-    0,      //        public static final int KEY_KATAKANAHIRAGANA =  0x5D;
-    0,      //        public static final int KEY_MUHENKAN =  0x5E;
-    0,      //        public static final int KEY_KPJPCOMMA =  0x5F;
-    0,      //        public static final int KEY_KPENTER = 0x60;
-    0,      //        public static final int KEY_RIGHTCTRL = 0x61;
-    0,      //        public static final int KEY_KPSLASH = 0x62;
-    0,      //        public static final int KEY_SYSRQ = 0x63;
-    0,      //        public static final int KEY_RIGHTALT = 0x64;
-    0,      //        padding 0x55;
-    0,      //        public static final int KEY_HOME = 0x66;
-    0,      //        public static final int KEY_UP = 0x67;
-    0,      //        public static final int KEY_PAGEUP = 0x68;
-    0,      //        public static final int KEY_LEFT = 0x69;
-    0,      //        public static final int KEY_RIGHT = 0x6A;
-    0,      //        public static final int KEY_END = 0x6B;
-    0,      //        public static final int KEY_DOWN = 0x6C;
-    0       //        public static final int KEY_PAGEDOWN = 0x6D;
+    226     //        public static final int KEY_102ND = 0x56;
 
     // Many more KEYS currently not used by KMW...
   };
@@ -243,11 +128,6 @@ public class KMHardwareKeyboardInterpreter implements KeyEvent.Callback {
 
   @Override
   public boolean onKeyDown(int keyCode, KeyEvent event) {
-
-    if (keyCode > KeyEvent.KEYCODE_SEARCH || keyCode < KeyEvent.KEYCODE_UNKNOWN) {
-      // The key is outside the range of keys we understand
-      return false;
-    }
 
     boolean isChiral = KMManager.getKMKeyboard(this.keyboardType).getChirality();
 
@@ -289,7 +169,7 @@ public class KMHardwareKeyboardInterpreter implements KeyEvent.Callback {
 
     // Range check scanCode. If it's beyond our expected range, just use "0"
     int scanCode = event.getScanCode();
-    int code = (scanCode < scanCodeMap.length) ? scanCodeMap[scanCode] : 0 ;
+    int code = (scanCode >= 0 && scanCode < scanCodeMap.length) ? scanCodeMap[scanCode] : 0 ;
     if (code == 0) {
       // Not an alphanumeric, punctuation or enter/tab/space key
       return false;
--- android/history.md
+++ android/history.md
@@ -11,8 +11,7 @@
 * Fix globe button when pausing WebBrowser (#1213)
 * Change system keyboard to "numeric" layer for digit/phone number text fields (#1218)
 * Add feature to vibrate device when Keyman Web calls `beep` (#1227)
-* Fix KMEA mapping of physical keys to use Linux Key Codes ("scan codes") 
-  which fixes handling of 102nd (K_oE2) key (#1291)
+* Add support for extra key found on European hardware keyboards (#1291)
 
 ## 2018-10-03 10.0.506 stable
 * Fix crash that can occur when displaying preview key (#1230, #1234)
