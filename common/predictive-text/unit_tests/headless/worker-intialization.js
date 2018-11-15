e9412a92264602a82e06ca33cdeb4578aa311274
Eddie Antonio Santos <easantos@ualberta.ca>
2018-11-15 14:30:23 -0700

    Test LMLayerWorker can mock postMessage.

--- common/predictive-text/unit_tests/headless/worker-intialization.js
+++ common/predictive-text/unit_tests/headless/worker-intialization.js
@@ -1,11 +1,25 @@
 var assert = require('chai').assert;
+var sinon = require('sinon');
 
-var worker = require('../../worker');
+let {LMLayerWorker} = require('../../worker');
 
 describe('LMLayerWorker', function() {
+
+  it('should allow for the mocking of postMessage()', function () {
+    var fakePostMessage = sinon.fake();
+    var worker = new LMLayerWorker({postMessage: fakePostMessage});
+    
+    // Sending it the initialize it should notify us that it's initialized!
+    worker.onMessage({
+      message: 'initialize',
+      model: 'en-x-dummy'
+    });
+    assert(fakePostMessage.called);
+  });
+
   describe('#constructor()', function() {
     it('should construct with zero arguments', function() {
-      assert.isOk(new worker.LMLayerWorker);
+      assert.isOk(new LMLayerWorker);
     });
   });
 });
