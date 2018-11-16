6583ce26dcd12605c8427966153fcf93194696f6
Eddie Antonio Santos <easantos@ualberta.ca>
2018-11-16 10:25:28 -0700

    Use an object instead of a new class instance.

--- common/predictive-text/worker/index.ts
+++ common/predictive-text/worker/index.ts
@@ -1,14 +1,14 @@
 // Dummy module for getting environment setup.
-class MyWorker {
-    public hello() {
+let EXPORTS = {
+    hello() {
         return 'hello';
     }
 }
 
 // The technique below allows code to work both in-browser and in Node.
-if(typeof module !== 'undefined' && typeof module.exports !== 'undefined') {
-    module.exports = new MyWorker();
+if (typeof module !== 'undefined' && typeof module.exports !== 'undefined') {
+    module.exports = EXPORTS;
 } else {
     //@ts-ignore
-    window.worker = new MyWorker();
+    window.worker = EXPORTS;
 }
