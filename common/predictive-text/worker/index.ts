e1e22e7bcf899dc5bbdcc429f793bd1d0e6378f5
Joshua A. Horton <joshua_horton@sil.org>
2018-11-16 09:38:53 +0700

    Node + browser-friendly index.ts.

--- common/predictive-text/worker/index.ts
+++ common/predictive-text/worker/index.ts
@@ -1,4 +1,14 @@
 // Dummy module for getting environment setup.
-module.exports ={
-    hello: () => 'hello'
+class MyWorker {
+    public hello() {
+        return 'hello';
+    }
+}
+
+// The technique below allows code to work both in-browser and in Node.
+if(typeof module !== 'undefined' && typeof module.exports !== 'undefined') {
+    module.exports = new MyWorker();
+} else {
+    //@ts-ignore
+    window.worker = new MyWorker();
 }
