43087a86fdcc4db8f0e1ad33fb52113b741d8ba0
glasseyes <dglassey@gmail.com>
2018-11-20 15:59:14 +0700

    dist for ibus-keyman and keyboardprocessor

--- linux/scripts/dist.sh
+++ linux/scripts/dist.sh
@@ -10,8 +10,8 @@
 set -e
 
 BASEDIR=`pwd`
-autotool_projects="kmflcomp libkmfl ibus-kmfl"
-extra_project="keyman-config"
+autotool_projects="kmflcomp libkmfl ibus-kmfl ibus-keyman"
+extra_projects="keyboardprocessor keyman-config"
 
 if [ "$1" == "origdist" ]; then
     if [ "$2" != "" ]; then
@@ -19,11 +19,12 @@ if [ "$1" == "origdist" ]; then
             echo "project $2 does not exist"
             exit 1
         fi
-        if [ "$2" == "keyman-config" ]; then
+        if [ "$2" == "keyman-config" || "$2" == "keyboardprocessor" ]; then
             autotool_projects=""
+            extra_projects="$2"
         else
             autotool_projects="$2"
-            extra_project=""
+            extra_projects=""
         fi
     fi
 else
@@ -32,11 +33,12 @@ else
             echo "project $1 does not exist"
             exit 1
         fi
-        if [ "$1" == "keyman-config" ]; then
+        if [ "$1" == "keyman-config" || "$1" == "keyboardprocessor" ]; then
             autotool_projects=""
+            extra_projects="$1"
         else
             autotool_projects="$1"
-            extra_project=""
+            extra_projects=""
         fi
     fi
 fi
@@ -50,7 +52,7 @@ for proj in ${autotool_projects}; do
     echo "configure $proj"
     cd $proj
     oldvers=`cat VERSION`
-    vers=`./configure -version|grep kmfl|grep -Po "${proj} configure \K[^a-z]*"`
+    vers=`./configure -version|grep -E 'kmfl|keyman'|grep -Po "${proj} configure \K[^a-z]*"`
     echo "$vers" > VERSION
     rm -rf ../build-$proj
     mkdir -p ../build-$proj
@@ -62,16 +64,25 @@ for proj in ${autotool_projects}; do
     echo "$oldvers" > VERSION
 done
 
-
-# dist for keyman-config
-if [ "${extra_project}" == "keyman-config" ]; then
-    cd keyman-config
-    rm -rf dist
-    python3 setup.py sdist
-    make man
-    cp dist/*.tar.gz ../dist
-fi
-cd $BASEDIR
+for proj in ${extra_projects}; do
+    # dist for keyman-config
+    if [ "${proj}" == "keyman-config" ]; then
+        cd keyman-config
+        rm -rf dist
+        python3 setup.py sdist
+        make man
+        cp dist/*.tar.gz ../dist
+    fi
+    if [ "${proj}" == "keyboardprocessor" ]; then
+        cd ../common/engine
+        vers=`grep -Po "\d.\d.\d" keyboardprocessor/meson.build`
+        cp -a keyboardprocessor keyman-keyboardprocessor-$vers
+        tar czf keyman-keyboardprocessor-$vers.tar.gz --exclude='debian/' --exclude='build/' keyman-keyboardprocessor-$vers
+        rm -rf keyman-keyboardprocessor-$vers
+        cp keyman-keyboardprocessor-$vers.tar.gz ../../linux/dist
+    fi
+    cd $BASEDIR
+done
 
 # create orig.tar.gz
 if [ "$1" == "origdist" ]; then
@@ -83,17 +94,25 @@ if [ "$1" == "origdist" ]; then
         mv $proj*.tar.gz ${origname}.orig.tar.gz
     done
 
-    if [ "${extra_project}" == "keyman-config" ]; then
-        tmp=`basename keyman\_config*.tar.gz .tar.gz`
-        origname=${tmp/keyman\_config-/keyman-config\_}
-        index=`expr index "${origname}" _`
-        kcversion=${tmp:$index}
+    for proj in ${extra_projects}; do
+        if [ "${proj}" == "keyman-config" ]; then
+            tmp=`basename keyman\_config*.tar.gz .tar.gz`
+            origname=${tmp/keyman\_config-/keyman-config\_}
+            index=`expr index "${origname}" _`
+            kcversion=${tmp:$index}
 
-        echo "keyman-config version: ${kcversion}"
+            echo "keyman-config version: ${kcversion}"
 
-        tar xf keyman_config-${kcversion}.tar.gz
-        mv keyman_config-${kcversion} keyman-config-${kcversion}
-        tar cfz keyman-config_${kcversion}.orig.tar.gz keyman-config-${kcversion}
-        rm -rf keyman-config-${kcversion} keyman_config-${kcversion}.tar.gz
-    fi
+            tar xf keyman_config-${kcversion}.tar.gz
+            mv keyman_config-${kcversion} keyman-config-${kcversion}
+            tar cfz keyman-config_${kcversion}.orig.tar.gz keyman-config-${kcversion}
+            rm -rf keyman-config-${kcversion} keyman_config-${kcversion}.tar.gz
+        fi
+        if [ "${proj}" == "keyboardprocessor" ]; then
+            tmp=`basename keyman-keyboardprocessor*.tar.gz .tar.gz`
+            origname=${tmp/keyman-keyboardprocessor-/keyman-keyboardprocessor\_}
+            index=`expr index "${origname}" _`
+            mv keyman-keyboardprocessor*.tar.gz ${origname}.orig.tar.gz
+        fi
+    done
 fi
