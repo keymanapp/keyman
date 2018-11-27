6cc20cfd8340dc14d1219c7eb64ead988e5bdb1d
Eddie Antonio Santos <easantos@ualberta.ca>
2018-11-27 14:28:31 -0700

    Make LMLayer.asBlobURI() test a bit more stringent.

--- common/predictive-text/unit_tests/in_browser/cases/lmlayer-integration.js
+++ common/predictive-text/unit_tests/in_browser/cases/lmlayer-integration.js
@@ -9,11 +9,20 @@ describe('LMLayer', function () {
   });
 
   describe('#asBlobURI', function () {
-    it('should take a function and convert it into a blob function', function () {
+    it('should take a function and convert it into a blob function', function (done) {
       let uri = LMLayer.asBlobURI(function dummyHandler() {
-        var dummy;
+        // Post something weird, so we can be reasonably certain it's not a fluke.
+        // WARNING: Do NOT factor out this string as a variable.
+        // It MUST remain a string in this function body, because it gets stringified!
+        postMessage('fhqwhgads');
       });
       assert.match(uri, /^blob:/);
+
+      let worker = new Worker(uri);
+      worker.onmessage = function thisShouldBeCalled(event) {
+        assert.propertyVal(event, 'data', 'fhqwhgads');
+        done();
+      };
     })
   })
 });
