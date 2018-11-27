d5da85fde4e40ebe9764852272bfc9fe6efdef83
Eddie Antonio Santos <easantos@ualberta.ca>
2018-11-27 14:01:11 -0700

    Test LMLayer.unwrap() utility function.

--- common/predictive-text/unit_tests/headless/lmlayer.js
+++ common/predictive-text/unit_tests/headless/lmlayer.js
@@ -5,8 +5,22 @@ let LMLayer = require('../../');
 
 describe('LMLayer', function() {
   describe('[[constructor]]', function () {
-    it.skip('should be take a URI to instantiate', function () {
+    it('should be take a URI to instantiate', function () {
       new LMLayer(uri);
+    });
+  });
+
+  /**
+   * .unwrap() is a static function that unwraps some function code.
+   */
+  describe('.unwrap', function () {
+    it('should return the inner code of a function', function () {
+      let text = LMLayer.unwrap(function hello() {
+        var hello;
+        var world;
+      });
+
+      assert.match(text, /^\s*var\s+hello;\s*var\s+world;\s*$/);
     });
   });
 });
