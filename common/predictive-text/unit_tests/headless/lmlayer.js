91e050f84d412978aab26482845fa47599b62b47
Eddie Antonio Santos <easantos@ualberta.ca>
2018-11-27 11:07:01 -0700

    Begin testing main LMLayer code.

---
+++ common/predictive-text/unit_tests/headless/lmlayer.js
@@ -0,0 +1,15 @@
+var assert = require('chai').assert;
+var sinon = require('sinon');
+
+let LMLayer = require('../../');
+
+describe('LMLayer', function() {
+  describe('[[constructor]]', function () {
+    it('should be given a Worker class to instantiate', function () {
+      let FakeWorker = sinon.fake();
+      new LMLayer(FakeWorker);
+      
+      assert.is(FakeWorker.callCount, 1);
+    });
+  });
+});
