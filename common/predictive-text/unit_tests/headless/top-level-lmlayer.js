c13ebe29ce741885057fff37fe003b87fb323b58
Eddie Antonio Santos <easantos@ualberta.ca>
2018-11-30 09:31:03 -0700

    Test basic usage of LMLayer#initialize()

--- common/predictive-text/unit_tests/headless/top-level-lmlayer.js
+++ common/predictive-text/unit_tests/headless/top-level-lmlayer.js
@@ -7,7 +7,7 @@ let LMLayer = require('../../build');
 // Note: these tests can only be run after BOTH stages of compilation are completed.
 describe('LMLayer', function() {
   describe('[[constructor]]', function () {
-    it('should be take a Worker to instantiate', function () {
+    it('should accept a Worker to instantiate', function () {
       let fakeWorker = {
         postMessage: sinon.fake(),
         onmessage: null
@@ -16,8 +16,31 @@ describe('LMLayer', function() {
     });
   });
 
-  // Since the Blob API is browser-specific, look for those tests
-  // for .asBlobURI() in the in_browser tests.
+  describe('#initialize()', function () {
+    it('should accept capabilities and model description', function () {
+      let fakeWorker = {
+        postMessage: sinon.fake(),
+        onmessage: null
+      };
+
+      let lmLayer = new LMLayer(fakeWorker);
+      lmLayer.initialize(
+        {
+          maxLeftContextCodeUnits: 32,
+        },
+        {
+          kind: 'wordlist',
+          words: ['foo', 'bar', 'baz', 'quux']
+        }
+      );
+
+      assert.isFunction(fakeWorker.onmessage, 'LMLayer failed to set a callback!');
+      assert.propertyVal(fakeWorker.postMessage, 'callCount', 1);
+    });
+  });
+
+  // Since the Blob API is limited to browsers, look for those
+  // tests for .asBlobURI() in the in_browser tests.
   describe('.unwrap', function () {
     it('should return the inner code of a function', function () {
       // Create a multi-line function body we can match in a RegExp.
