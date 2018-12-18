f3474dbaa5e194aa961341e338b2a9c562e03883
Tim Eves <tim_eves@sil.org>
2018-12-18 16:39:07 +0700

    Tidy up KMX_Option a bit and add persist action queuing to Save()

--- common/engine/keyboardprocessor/src/kmx/kmx_options.h
+++ common/engine/keyboardprocessor/src/kmx/kmx_options.h
@@ -20,6 +20,8 @@ private:
 
   void AddOptionsStoresFromXString(PKMX_WCHAR s);
 
+  int _GetIndex(std::u16string const &key) const;
+
 public:
   KMX_Options(LPINTKEYBOARDINFO kp) : _kp(kp) {}
   ~KMX_Options();
@@ -27,9 +29,30 @@ public:
   void Init(std::vector<option> &opts);
   void Load(options *options, std::u16string const &key);
   void Set(int nStoreToSet, int nStoreToRead);
+  void Set(int nStoreToSet, std::u16string const &value);
+  void Set(std::u16string const &key, std::u16string const &value);
   void Reset(options *options, int nStoreToReset);
   void Save(km_kbp_state *state, int nStoreToSave);
+
+  STORE const * begin() const;
+  STORE const * end() const;
 };
+
+inline
+void KMX_Options::Set(std::u16string const &key, std::u16string const &value) {
+  auto i = _GetIndex(key);
+  if (i != signed(_kp->Keyboard->cxStoreArray))   Set(i, value);
+}
+
+inline
+STORE const * KMX_Options::begin() const {
+  return _kp->Keyboard->dpStoreArray;
+}
+
+inline
+STORE const * KMX_Options::end() const {
+  return _kp->Keyboard->dpStoreArray + _kp->Keyboard->cxStoreArray;
+}
 
 } // namespace kmx
 } // namespace kbp
