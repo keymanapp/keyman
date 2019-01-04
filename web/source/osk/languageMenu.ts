8f020846c8d2dcf887597380541cd3b29620601d
Joshua A. Horton <joshua_horton@sil.org>
2019-01-04 15:14:15 +0700

    Formatting tweaks, extracts and preserves the language menu shim.

--- web/source/osk/languageMenu.ts
+++ web/source/osk/languageMenu.ts
@@ -14,6 +14,29 @@ namespace com.keyman.osk {
     constructor(keyman: KeymanBase) {
       this.keyman = keyman;
       this.scrolling = false;
+
+      this.shim = this.constructShim();
+    }
+
+    constructShim(): HTMLDivElement {
+      let languageMenu = this;
+      let shim = this.keyman.util._CreateElement('div');
+      let osk = this.keyman.osk;
+
+      shim.id='kmw-language-menu-background';
+      shim.addEventListener('touchstart', function(e) {
+        e.preventDefault();
+        languageMenu.hide();
+
+        // Display build only if touching menu, space *and* one other point on screen (build 369)
+        if(e.touches.length > 2) {
+          var sX=e.touches[1].pageX,sY=e.touches[1].pageY;
+          if(sX > osk.spaceBar.offsetLeft && sX < osk.spaceBar.offsetLeft+osk.spaceBar.offsetWidth &&
+            sY > osk.spaceBar.offsetTop && sY < osk.spaceBar.offsetTop+osk.spaceBar.offsetHeight) osk.showBuild();
+        }
+      },false);
+
+      return shim;
     }
 
     /**
@@ -34,21 +57,8 @@ namespace com.keyman.osk {
       // but allow the menu to be closed if anywhere else on screen is touched
 
       let osk = this.keyman.osk;
-      let lgList = this;
+      let languageMenu = this;
 
-      this.shim=util._CreateElement('div');
-      this.shim.id='kmw-language-menu-background';
-      this.shim.addEventListener('touchstart', function(e) {
-          e.preventDefault();
-          lgList.hide();
-
-          // Display build only if touching menu, space *and* one other point on screen (build 369)
-          if(e.touches.length > 2) {
-            var sX=e.touches[1].pageX,sY=e.touches[1].pageY;
-            if(sX > osk.spaceBar.offsetLeft && sX < osk.spaceBar.offsetLeft+osk.spaceBar.offsetWidth &&
-              sY > osk.spaceBar.offsetTop && sY < osk.spaceBar.offsetTop+osk.spaceBar.offsetHeight) osk.showBuild();
-          }
-        },false);
       document.body.appendChild(this.shim);
 
       // Add two nested DIVs to properly support iOS scrolling with momentum
@@ -68,8 +78,7 @@ namespace com.keyman.osk {
       // Add menu index strip
       var i,x,mx=util._CreateElement('div');
       mx.id='kmw-menu-index';
-      for(i=1; i<=26; i++)
-      {
+      for(i=1; i<=26; i++) {
         x=util._CreateElement('p');
         x.innerHTML=String.fromCharCode(i+64);
         mx.appendChild(x);
@@ -77,7 +86,7 @@ namespace com.keyman.osk {
 
       // Add index selection (for a large menu)
       mx.addEventListener('touchstart',function(e){
-        lgList.scrollToLanguage(e, m2, m3);
+        languageMenu.scrollToLanguage(e, m2, m3);
       }, false);
       mx.addEventListener('touchend',function(e){
         e.stopPropagation();
@@ -88,13 +97,17 @@ namespace com.keyman.osk {
       //TODO: not sure if either of these two handlers ar actually needed.  touchmove handler may be doing all that is necessary.
       // Add scroll end event handling to override body scroll
       menu.addEventListener('scroll',function(e){
-        lgList.scrolling=true;
-        },false);
+        languageMenu.scrolling=true;
+      },false);
       m2.addEventListener('scroll',function(e){
         //this.lgList.scrolling=true;
-        if(m2.scrollTop < 1)m2.scrollTop=1;
-        if(m2.scrollTop > m2.scrollHeight-m2.offsetHeight-1)m2.scrollTop=m2.scrollHeight-m2.offsetHeight-1;
-        },false);
+        if(m2.scrollTop < 1) {
+          m2.scrollTop=1;
+        }
+        if(m2.scrollTop > m2.scrollHeight-m2.offsetHeight-1) {
+          m2.scrollTop=m2.scrollHeight-m2.offsetHeight-1;
+        }
+      },false);
 
       // Add a list of keyboards to the innermost DIV
       this.activeLgNo=this.addLanguages(m3,kbdList);
@@ -109,19 +122,28 @@ namespace com.keyman.osk {
       document.body.appendChild(this.lgList);
 
       // Adjust size for viewport scaling (probably not needed for iOS, but check!)
-      if(util.device.OS == 'Android' && 'devicePixelRatio' in window)
+      if(util.device.OS == 'Android' && 'devicePixelRatio' in window) {
         this.lgList.style.fontSize=(2/window.devicePixelRatio)+'em';
+      }
 
 
       // Adjust width for pixel scaling on Android tablets
-      if(util.device.OS == 'Android' && util.device.formFactor == 'tablet' && 'devicePixelRatio' in window)
-      {
-        var w=parseInt(util.getStyleValue(menu,'width'),10),ms=menu.style;
-        if(!isNaN(w)) ms.width=ms.maxWidth=(2*w/window.devicePixelRatio)+'px';
-        w=parseInt(util.getStyleValue(m2,'width'),10); ms=m2.style;
-        if(!isNaN(w)) ms.width=ms.maxWidth=(2*w/window.devicePixelRatio)+'px';
-        w=parseInt(util.getStyleValue(m3,'width'),10); ms=m3.style;
-        if(!isNaN(w)) ms.width=ms.maxWidth=(2*w/window.devicePixelRatio)+'px';
+      if(util.device.OS == 'Android' && util.device.formFactor == 'tablet' && 'devicePixelRatio' in window) {
+        var w=parseInt(util.getStyleValue(menu,'width'),10),
+        ms=menu.style;
+        if(!isNaN(w)) {
+          ms.width=ms.maxWidth=(2*w/window.devicePixelRatio)+'px';
+        }
+        w=parseInt(util.getStyleValue(m2,'width'),10);
+        ms=m2.style;
+        if(!isNaN(w)) {
+          ms.width=ms.maxWidth=(2*w/window.devicePixelRatio)+'px';
+        }
+        w=parseInt(util.getStyleValue(m3,'width'),10);
+        ms=m3.style;
+        if(!isNaN(w)) {
+          ms.width=ms.maxWidth=(2*w/window.devicePixelRatio)+'px';
+        }
       }
 
       // Adjust initial top and height of menu
@@ -133,17 +155,15 @@ namespace com.keyman.osk {
           scale=Math.round(100.0*lineHeight/dy)/100.0,
           factor=(scale > 0.6 ? 1 : 2);
 
-      if(scale > 1.25) scale=1.25;
+      if(scale > 1.25) {
+        scale=1.25;
+      }
 
-      for(i=0;i<26;i++)
-      {
+      for(i=0;i<26;i++) {
         var qs=(<HTMLElement>mx.childNodes[i]).style;
-        if(factor == 2 && (i%2) == 1)
-        {
+        if(factor == 2 && (i%2) == 1) {
           qs.display='none';
-        }
-        else
-        {
+        } else {
           qs.fontSize=(scale*factor)+'em';
           qs.lineHeight=(lineHeight*factor)+'px';
         }
@@ -151,10 +171,11 @@ namespace com.keyman.osk {
 
       // Increase width of outer menu DIV by index, else hide index
       var menuWidth=m2.offsetWidth;
-      if(m2.scrollHeight > m2.offsetHeight+3)
+      if(m2.scrollHeight > m2.offsetHeight+3) {
         menuWidth = menuWidth+mx.offsetWidth;
-      else
+      } else {
         mx.style.display='none';
+      }
 
       menu.style.width=menuWidth+'px';
 
@@ -166,9 +187,13 @@ namespace com.keyman.osk {
       m2.scrollTop=top;
 
       // The scrollTop value is limited by the device, and must be limited to avoid dragging the document body
-      if(m2.scrollTop < top) m2.scrollTop=m2.scrollHeight-m2.offsetHeight;
-      if(m2.scrollTop > m2.scrollHeight-m2.offsetHeight-1)m2.scrollTop=m2.scrollHeight-m2.offsetHeight-1;
+      if(m2.scrollTop < top) {
+        m2.scrollTop=m2.scrollHeight-m2.offsetHeight;
+      }
 
+      if(m2.scrollTop > m2.scrollHeight-m2.offsetHeight-1) {
+        m2.scrollTop=m2.scrollHeight-m2.offsetHeight-1;
+      }
     }
 
     /**
@@ -201,7 +226,9 @@ namespace com.keyman.osk {
 
       // Explicitly set position and height
       s.left=util._GetAbsoluteX(osk.lgKey)+'px';
-      if(menuHeight > maxHeight) menuHeight=maxHeight;
+      if(menuHeight > maxHeight) {
+        menuHeight=maxHeight;
+      }
       s.height=menuHeight+'px';
 
       // Position menu at bottom of screen, but referred to top (works for both iOS and Firefox)
@@ -309,7 +336,7 @@ namespace com.keyman.osk {
           activeLanguageIndex=k;
         }
 
-        let lgMenu = this;
+        let languageMenu = this;
         // Several keyboards for this language
         if(lgBar.kList.length > 1) {
           lgBar.className='kbd-list';
@@ -323,7 +350,7 @@ namespace com.keyman.osk {
               this.parentNode.className=(this.parentNode.className=='kbd-list-closed'?'kbd-list-open':'kbd-list-closed');
 
             // Adjust top of menu to allow for expanded list
-            lgMenu.adjust(this.parentNode.className=='kbd-list-closed'?0:this.kList.length);
+            languageMenu.adjust(this.parentNode.className=='kbd-list-closed'?0:this.kList.length);
           }
           lgBar.addEventListener('touchstart',function(e){e.stopPropagation();},false);
           lgBar.addEventListener('touchmove',function(e){e.target.scrolled=true;e.stopPropagation();},false);
@@ -377,7 +404,7 @@ namespace com.keyman.osk {
       kb.innerHTML=unique?kbd['KL']:kbd['KN'].replace(' Keyboard',''); // Name
 
       // We're setting up a few events - this alias helps avoid scoping issues.
-      let lgList = this;
+      let languageMenu = this;
 
       // Touchstart (or mspointerdown) event highlights the touched list item
       var touchStart=function(e) {
@@ -385,8 +412,8 @@ namespace com.keyman.osk {
         if(this.className.indexOf('selected') <= 0) {
           this.className=this.className+' selected';
         }
-        lgList.scrolling=false;
-        lgList.y0=e.touches[0].pageY;//osk.lgList.childNodes[0].scrollTop;
+        languageMenu.scrolling=false;
+        languageMenu.y0=e.touches[0].pageY;//osk.lgList.childNodes[0].scrollTop;
         return true;
       };
 
@@ -394,7 +421,7 @@ namespace com.keyman.osk {
       // Touchmove drags the list and prevents release from selecting the language
       var touchMove=function(e: TouchEvent) {
         e.stopImmediatePropagation();
-        var scroller=<HTMLElement>lgList.lgList.childNodes[0],
+        var scroller=<HTMLElement>languageMenu.lgList.childNodes[0],
             yMax=scroller.scrollHeight-scroller.offsetHeight,
         y, dy;
 
@@ -406,19 +433,19 @@ namespace com.keyman.osk {
           return;
         }
 
-        dy=y-lgList.y0;
+        dy=y-languageMenu.y0;
 
         // Scroll up (show later listed languages)
         if(dy < 0) {
           if(scroller.scrollTop >= yMax-1) {
             e.preventDefault();
-            lgList.y0=y;
+            languageMenu.y0=y;
           }
           // Scroll down (show earlier listed languages)
         } else if(dy > 0) {
           if(scroller.scrollTop < 2) {
             e.preventDefault();
-            lgList.y0=y;
+            languageMenu.y0=y;
           }
           // Dont' scroll - can happen if changing scroll direction
         } else {
@@ -427,9 +454,9 @@ namespace com.keyman.osk {
 
         // Disable selected language if drag more than 5px
         if(dy < -5 || dy > 5) {
-          lgList.scrolling=true;
+          languageMenu.scrolling=true;
           this.className=this.className.replace(/\s*selected/,'');
-          lgList.y0=y;
+          languageMenu.y0=y;
         }
         return true;
       };
@@ -439,22 +466,19 @@ namespace com.keyman.osk {
         e.preventDefault();
         if(typeof(e.stopImmediatePropagation) != 'undefined') e.stopImmediatePropagation();else e.stopPropagation();
 
-        if(lgList.scrolling)
-        {
+        if(languageMenu.scrolling) {
           this.className=this.className.replace(/\s*selected/,'');
-        }
-        else
-        {
+        } else {
           com.keyman.DOMEventHandlers.states.setFocusTimer();
 
-          lgList.lgList.style.display='none'; //still allows blank menu momentarily on selection
-          lgList.keyman.keyboardManager._SetActiveKeyboard(this.kn,this.kc,true);
-          lgList.keyman.keyboardManager.doKeyboardChange(this.kn,this.kc);
-          lgList.keyman.domManager.focusLastActiveElement();
-          lgList.hide();
+          languageMenu.lgList.style.display='none'; //still allows blank menu momentarily on selection
+          languageMenu.keyman.keyboardManager._SetActiveKeyboard(this.kn,this.kc,true);
+          languageMenu.keyman.keyboardManager.doKeyboardChange(this.kn,this.kc);
+          languageMenu.keyman.domManager.focusLastActiveElement();
+          languageMenu.hide();
 
           // Update the OSK with the new keyboard
-          lgList.keyman.osk._Show();
+          languageMenu.keyman.osk._Show();
         }
         return true;
       };
@@ -473,16 +497,14 @@ namespace com.keyman.osk {
     hide() {
       let osk = this.keyman.osk;
 
-      let lgList = this;
+      let languageMenu = this;
       if(this.lgList) {
         osk.highlightKey(osk.lgKey.firstChild,false);
         this.lgList.style.visibility='hidden';
+
         window.setTimeout(function(){
-          if(lgList != null && typeof lgList != 'undefined') {
-            document.body.removeChild(lgList.shim);
-            document.body.removeChild(lgList.lgList);
-          }
-          osk.lgList=null;
+          document.body.removeChild(languageMenu.shim);
+          document.body.removeChild(languageMenu.lgList);
         },500);
       }
     }
