a2330c71153f98d1d9471873abef6950e4088312
Eddie Antonio Santos <easantos@ualberta.ca>
2019-01-10 09:11:18 -0700

    Create test for LMLayerWorker#predict().

---
+++ common/predictive-text/unit_tests/headless/worker-predict.js
@@ -0,0 +1,87 @@
+var assert = require('chai').assert;
+var sinon = require('sinon');
+
+let LMLayerWorker = require('../../build/intermediate');
+
+
+describe('LMLayerWorker', function () {
+  describe('#predict()', function () {
+    it('should send back a "ready" message', function () {
+      var suggestion = {
+        transform: {
+          insert: 'I ',
+          deleteLeft: 0
+        },
+        displayAs: 'I'
+      };
+
+      // Initialize the worker with a model that will produce one suggestion.
+      var fakePostMessage = sinon.fake();
+      var worker = new LMLayerWorker({ postMessage: fakePostMessage });
+      worker.onMessage(createMessageEventWithData({
+        message: 'initialize',
+        model: dummyModel([
+          [suggestion]
+        ]),
+        capabilities: defaultCapabilities()
+      }));
+      assert(fakePostMessage.calledOnceWith(sinon.match({
+        message: 'ready'
+      })));
+
+      // Now predict! We should get the suggestions back.
+      worker.onMessage(createMessageEventWithData({
+        message: 'predict',
+        transform: zeroTransform(),
+        context: emptyContext()
+      }));
+      assert(fakePostMessage.calledOnceWith(sinon.match({
+        message: 'suggestions',
+        suggestions: [suggestion]
+      })));
+    });
+  });
+
+  // Helpers (TODO: factor out!)
+
+  /**
+   * Creates a MessageEvent (for inter-worker communication), with the given data payload.
+   * @param {*} data 
+   */
+  function createMessageEventWithData(data) {
+    return { data };
+  }
+
+  /**
+   * A valid model that suggests exactly what you want it to suggest.
+   */
+  function dummyModel(futureSuggestions) {
+    return {
+      model: 'dummy',
+      futureSuggestions: futureSuggestions || []
+    };
+  }
+  /**
+   * Returns reasonable defaults for the default capabilities
+   * to initialize the LMLayer.
+   */
+  function defaultCapabilities() {
+    return {
+      maxLeftContextCodeUnits: 64
+    };
+  }
+
+  /**
+   * Context of an empty buffer; no text, at both the start and end of the buffer.
+   */
+  function emptyContext() {
+    return { left: '', startOfBuffer: true, endOfBuffer: true };
+  }
+
+  /**
+   * This transform, when applied, makes no changes to the buffer.
+   */
+  function zeroTransform() {
+    return { insert: '', deleteLeft: 0 };
+  }
+});
