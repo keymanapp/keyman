be789f05e91dd4c19202af056c37d5f27f0f6f9e
Eddie Antonio Santos <easantos@ualberta.ca>
2019-01-14 10:46:23 -0700

    Test PromiseStore#keep().

--- common/predictive-text/unit_tests/headless/promise-store.js
+++ common/predictive-text/unit_tests/headless/promise-store.js
@@ -12,9 +12,45 @@ describe('PromiseStore', function () {
 
       // Add one promise.
       new Promise(function (resolve, reject) {
-        promises.make(resolve, reject);
+        promises.make(randomToken(), resolve, reject);
       });
       assert.lengthOf(promises, 1);
     });
+
+    // TODO: test existing token
   });
+
+  describe('.keep()', function () {
+    it('should call the resolve() function', function () {
+      var promises = new PromiseStore();
+      var token = randomToken();
+      var promise = new Promise(function (resolve, reject) {
+        promises.make(token, resolve, reject);
+      });
+
+      // Resolve the promise asynchronously.
+      var randomPayload = randomToken();
+      doLater(function () {
+        assert.lengthOf(promises, 1);
+        // TODO: this API probably should not return a resolve function;
+        // it should call it immediately.
+        promises.keep(token)(randomPayload);
+      });
+
+      return promise.then(function (actual) {
+        assert.strictEqual(actual, randomPayload);
+        assert.lengthOf(promises, 0);
+      });
+    });
+  });
+
+  function randomToken() {
+    var range =  Number.MAX_SAFE_INTEGER - Number.MIN_SAFE_INTEGER;
+    return Math.random() * range + Number.MIN_SAFE_INTEGER;
+  }
+
+  // Do something asynchronously.
+  function doLater(callback) {
+    return setTimeout(callback, 0);
+  }
 });
