a40478e1c2e8f66188c9f70ac62c0676919479b4
Eddie Antonio Santos <easantos@ualberta.ca>
2019-01-14 10:27:41 -0700

    Complete PromiseStore#make() test.

--- common/predictive-text/unit_tests/headless/promise-store.js
+++ common/predictive-text/unit_tests/headless/promise-store.js
@@ -7,8 +7,14 @@ describe('PromiseStore', function () {
   describe('.make()', function () {
     it("should track a promise's callbacks", function () {
       var promises = new PromiseStore();
-      
+      // There should be no tracked promises.
       assert.lengthOf(promises, 0);
+
+      // Add one promise.
+      new Promise(function (resolve, reject) {
+        promises.make(resolve, reject);
+      });
+      assert.lengthOf(promises, 1);
     });
   });
 });
