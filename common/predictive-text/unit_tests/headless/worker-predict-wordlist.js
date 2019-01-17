bc3b22258581b80d23eb40ecd429588c2f83e584
Eddie Antonio Santos <easantos@ualberta.ca>
2019-01-17 16:01:43 -0700

    Test prediction within a word.

--- common/predictive-text/unit_tests/headless/worker-predict-wordlist.js
+++ common/predictive-text/unit_tests/headless/worker-predict-wordlist.js
@@ -18,8 +18,13 @@ describe('LMLayerWorker word list model', function() {
   });
 
   describe('prediction', function () {
-    it('should predict prefixes with a single letter transform', function () {
-      var MIN_SUGGESTIONS = 3;
+    var MIN_SUGGESTIONS = 3;
+
+    it('should predict prefixes with an empty context and a single letter transform', function () {
+      // Predicting when the user JUST typed 't' should result in something like this:
+      //
+      //   «t|                        » [Send]
+      //   [   to   ] [   the   ] [   this    ]
       var model = new WordListModel(
         defaultCapabilities(),
         jsonFixture('wordlists/english-1000')
@@ -38,6 +43,37 @@ describe('LMLayerWorker word list model', function() {
         suggestion = suggestions[i];
         suggestedWord = suggestion.transform.insert;
         assert.strictEqual(suggestedWord.substr(0, 1), 't');
+      }
+    });
+
+    it('should predict prefixes within an word and a single letter transform', function () {
+      // Predicting when the user JUST typed 'h', with the buffer having a 't' in it:
+      //
+      //   «th|                       » [Send]
+      //   [  this  ] [   the   ] [   there   ]
+      var model = new WordListModel(
+        defaultCapabilities(),
+        jsonFixture('wordlists/english-1000')
+      );
+
+      var suggestions = model.predict({
+        insert: 'h',
+        deleteLeft: 0,
+      }, {
+        left: 't',
+        startOfBuffer: false,
+        endOfBuffer: true
+      });
+      assert.isAtLeast(suggestions.length, MIN_SUGGESTIONS);
+
+      // Ensure all of the suggestions actually start with 'th'
+      var suggestion;
+      var suggestedWord;
+      for (var i = 0; i < MIN_SUGGESTIONS; i++) {
+        suggestion = suggestions[i];
+        suggestedWord = suggestion.transform.insert;
+        assert.strictEqual(suggestedWord.substr(0, 2), 'th');
+        assert.strictEqual(suggestion.transform.deleteLeft, 1);
       }
     });
   });
