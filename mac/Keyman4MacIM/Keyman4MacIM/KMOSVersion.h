90562cda57045959dc2865df7f0fe1066d69fc6c
Randy Boring <randy_boring@sil.org>
2019-02-14 01:51:29 -0600

    return a 16-bit representation of the OS Version much like Gestalt used to.

---
+++ mac/Keyman4MacIM/Keyman4MacIM/KMOSVersion.h
@@ -0,0 +1,19 @@
+//
+//  KMOSVersion.h
+//  Keyman
+//
+//  Created by Randy Boring on 2/13/19.
+//  Copyright © 2019 SIL International. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface KMOSVersion : NSObject
+
++(u_int16_t)SystemVersion;
+
+@end
+
+NS_ASSUME_NONNULL_END
