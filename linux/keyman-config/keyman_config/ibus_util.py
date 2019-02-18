677f9546a3833fde19386a4112dcab49ab04422e
darcywong00 <darcy_wong@sil.org>
2019-02-17 21:23:51 -0600

    Sanitize the keyboard id for ibus

--- linux/keyman-config/keyman_config/ibus_util.py
+++ linux/keyman-config/keyman_config/ibus_util.py
@@ -27,9 +27,10 @@ def install_to_ibus(bus, keyboard_id):
         # bus = IBus.Bus()
         logging.debug("installing to ibus")
         ibus_settings = Gio.Settings.new("org.freedesktop.ibus.general")
-        preload_engines = ibus_settings.get_strv("preload-engines")
+        preload_engines = ibus_settings.getr_strv("preload-engines")
         logging.debug(preload_engines)
         if keyboard_id not in preload_engines:
+            preload_engines.clear()
             preload_engines.append(keyboard_id)
         logging.debug(preload_engines)
         ibus_settings.set_strv("preload-engines", preload_engines)
