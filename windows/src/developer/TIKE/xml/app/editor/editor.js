d099bbf1897ff7d025ab4cbefb42c70154472dc8
Marc Durdin <marc@keyman.com>
2019-02-19 17:42:35 +1100

    [Developer] Fixes #1599. Ensure text editor resizes when window does

--- windows/src/developer/TIKE/xml/app/editor/editor.js
+++ windows/src/developer/TIKE/xml/app/editor/editor.js
@@ -86,6 +86,10 @@ async function loadSettings() {
     // Setup callbacks
     //
 
+    window.addEventListener('resize', function() {
+      editor.layout();
+    });
+
     const model = editor.getModel();
 
     context.reloadSettings();
