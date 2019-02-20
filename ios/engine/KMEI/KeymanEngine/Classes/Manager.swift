3b79a1266f27799cec2892c7a4029cf959131938
Randy Boring <randy_boring@sil.org>
2019-02-20 17:06:26 -0600

    process all the keyboard files so all the languages get added for later installation but only installs for the first language

--- ios/engine/KMEI/KeymanEngine/Classes/Manager.swift
+++ ios/engine/KMEI/KeymanEngine/Classes/Manager.swift
@@ -586,6 +586,7 @@ public class Manager: NSObject, HTTPDownloadDelegate, UIGestureRecognizerDelegat
               throw KMPError.fileSystem
             }
             
+            var haveInstalledOne = false
             for keyboard in installableKeyboards {
               let storedPath = Storage.active.keyboardURL(for: keyboard)
               
@@ -614,7 +615,10 @@ public class Manager: NSObject, HTTPDownloadDelegate, UIGestureRecognizerDelegat
                 log.error("Error saving the download: \(error)")
                 throw KMPError.copyFiles
               }
-              Manager.shared.addKeyboard(keyboard)
+              if !haveInstalledOne {
+                Manager.shared.addKeyboard(keyboard)
+                haveInstalledOne = true
+              }
             }
           }
         }
