89fbe0a2d01e9d7ac77a8009817095fd0c713530
Randy Boring <randy_boring@sil.org>
2019-03-04 19:23:24 -0600

    start on lexical model

---
+++ ios/engine/KMEI/KeymanEngine/Classes/Model/LexicalModelKeymanPackage.swift
@@ -0,0 +1,20 @@
+//
+//  LexicalModelKeymanPackage.swift
+//  KeymanEngine
+//
+//  Created by Randy Boring on 3/4/19.
+//  Copyright © 2019 SIL International. All rights reserved.
+//
+
+import Foundation
+
+public class LexicalModelKeymanPackage : KeymanPackage
+{
+    
+}
+
+// may be called parse()
+// returns a dictionary mapping language ID to lexical model ID
+public class processEntry() {
+    
+}
