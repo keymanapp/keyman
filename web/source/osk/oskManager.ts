7323a6a11050661e3b5faf2def19afdef67d5e11
Darcy Wong <darcy_wong@sil.org>
2019-03-21 09:03:12 +0700

    Adjust suggestion styling
    * Use full width
    * Remove "extra" blank region

--- web/source/osk/oskManager.ts
+++ web/source/osk/oskManager.ts
@@ -167,7 +167,7 @@ namespace com.keyman.osk {
 
       this._Visible = false;  // I3363 (Build 301)
       var s = this._Box.style;
-      s.zIndex='9999'; s.display='none'; s.width='auto';
+      s.zIndex='9999'; s.display='none';
       s.position = (device.formFactor == 'desktop' ? 'absolute' : 'fixed');
 
       // Use smaller base font size for mobile devices
@@ -301,9 +301,9 @@ namespace com.keyman.osk {
       var innerFrame=<HTMLDivElement> this._Box.firstChild,
         kbdClass = ' kmw-keyboard-' + (activeKeyboard ? activeKeyboard['KI'].replace('Keyboard_','') : '');
       if(innerFrame.id == 'keymanweb_title_bar') {
-        // Desktop order is title_bar, banner_bar, inner-frame
+        // Desktop order is title_bar, banner_container, inner-frame
         innerFrame=<HTMLDivElement> innerFrame.nextSibling.nextSibling;
-      } else if (innerFrame.id == 'keymanweb_banner_bar') {
+      } else if (innerFrame.id == 'keymanweb_banner_container') {
         innerFrame=<HTMLDivElement> innerFrame.nextSibling;
       }
       innerFrame.className = 'kmw-osk-inner-frame' + kbdClass;
