4c4071bdec355381b256ff5af5f1ac90df422a06
Marc Durdin <marc@keyman.com>
2019-04-01 11:54:57 +1100

    [Developer] compiler ladder unit tests are no longer relevant, rely instead on web regression test

--- windows/src/test/unit-tests/kmw-compiler-ladders/build.bat
+++
@@ -1,2 +0,0 @@
-@..\..\..\developer\kmcomp\kmcomp.exe -d simple_keyboard\simple_keyboard.kpj
-@..\..\..\developer\kmcomp\kmcomp.exe -d hieroglyphic\hieroglyphic.kpj
--- windows/src/test/unit-tests/kmw-compiler-ladders/simple_keyboard/HISTORY.md
+++
@@ -1,6 +0,0 @@
-Simple Keyboard Change History
-====================
-
-1.0 (2019-01-22)
-----------------
-* Created by Marc DUrdin
