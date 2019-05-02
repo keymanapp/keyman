64073fb02d8693975934f04fe3c0ac910a44bb90
Eddie Antonio Santos <easantos@ualberta.ca>
2019-05-02 15:11:03 -0600

    Clean up ASCII word breaker unit test.

--- common/predictive-text/unit_tests/headless/ascii-word-breaker.js
+++ common/predictive-text/unit_tests/headless/ascii-word-breaker.js
@@ -1,13 +1,11 @@
 var assert = require('chai').assert;
-var sinon = require('sinon');
 
-let LMLayer = require('../../build');
 var breakASCIIWords = require('../../build/intermediate').wordBreakers['ascii'];
 
-describe('The ASCIIWordBreaker', function() {
-    it('breaks simple English sentences', function () {
-        let breaks = breakASCIIWords('Look! -- The quick brown fox jumps... over the lazy dog!');
-        let words = breaks.map(span => span.text);
-        assert.deepEqual(words, ['Look', 'The', 'quick', 'brown', 'fox', 'jumps', 'over', 'the', 'lazy', 'dog']);
-    });
+describe('The ASCII word breaker', function () {
+  it('should break simple English sentences', function () {
+    let breaks = breakASCIIWords('Look! -- The quick brown fox jumps... over the lazy dog!');
+    let words = breaks.map(span => span.text);
+    assert.deepEqual(words, ['Look', 'The', 'quick', 'brown', 'fox', 'jumps', 'over', 'the', 'lazy', 'dog']);
+  });
 });
