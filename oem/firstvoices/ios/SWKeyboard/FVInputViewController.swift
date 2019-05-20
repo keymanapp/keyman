c6ee7eaef1cd0255d53498ca7202626d22e35e57
Marc Durdin <marc@keyman.com>
2019-05-21 07:08:09 +1000

    [iOS] FV Remove ObjC files, fixup banner -- until we have suggestions

--- oem/firstvoices/ios/SWKeyboard/FVInputViewController.swift
+++ oem/firstvoices/ios/SWKeyboard/FVInputViewController.swift
@@ -32,5 +32,54 @@ class FVInputViewController: InputViewController {
   required init?(coder aDecoder: NSCoder) {
     fatalError("init(coder:) has not been implemented")
   }
+
+  override func updateViewConstraints() {
+    super.updateViewConstraints()
+
+    if view.frame.size.width == 0 || self.view.frame.size.height == 0 {
+      return
+    }
+
+    setupTopBarImage(isPortrait: InputViewController.isPortrait)
+  }
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    setupTopBarImage(isPortrait: InputViewController.isPortrait)
+  }
+
+  override func viewWillTransition(to size: CGSize, with coordinator: UIViewControllerTransitionCoordinator) {
+    super.viewWillTransition(to: size, with: coordinator)
+    setupTopBarImage(isPortrait: UIDevice.current.orientation.isPortrait)
+  }
+
+  func getTopBarImage(isPortrait: Bool) -> String? {
+    if isPortrait {
+      return Bundle.main.path(forResource: "banner-Portrait@2x", ofType: "png")
+    }
+
+    // iPad
+    if UIDevice.current.userInterfaceIdiom != UIUserInterfaceIdiom.phone {
+      return Bundle.main.path(forResource: "banner-Landscape@2x", ofType: "png")
+    }
+
+    // iPhone
+    let screenRect = UIScreen.main.bounds
+    if CGFloat.maximum(screenRect.height, screenRect.width) >= 568.0 {
+      return Bundle.main.path(forResource: "banner-Landscape-568h@2x", ofType: "png")
+    } else {
+      return Bundle.main.path(forResource: "banner-Landscape@2x", ofType: "png")
+    }
+  }
+
+  func setupTopBarImage(isPortrait: Bool) {
+    let imgPath = getTopBarImage(isPortrait: isPortrait)
+    guard let path = imgPath else {
+      log.error("No image specified for the image banner!")
+      return
+    }
+
+    self.setBannerImage(to: path)
+  }
 }
 
