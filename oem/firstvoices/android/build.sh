92fd5d945cb147718719cb3218b06445ff4c7eb9
Marc Durdin <marc@keyman.com>
2019-05-27 09:32:58 +1000

    [Android] Add FV app to build scripts

--- oem/firstvoices/android/build.sh
+++ oem/firstvoices/android/build.sh
@@ -8,7 +8,7 @@ export KEYBOARDS_TARGET=app/src/main/assets/packages
 export KEYBOARDS_CSV_TARGET=app/src/main/assets/keyboards.csv
 
 # TODO: support passing -copy-keyboards, -debug, -clean etc in to build_keyboards
-./build_keyboards.sh
+./build_keyboards.sh -copy-keyboards
 
 # TODO: in the future build_common.sh should probably be shared with all oem products?
-./build_common.sh $*
+./build_common.sh "$@"
--- oem/firstvoices/android/build_common.sh
+++ oem/firstvoices/android/build_common.sh
@@ -13,6 +13,11 @@ display_usage ( ) {
     exit 1
 }
 
+verify_KMEA ( ) {
+    KMEA_BUILD_EXISTS=true
+    [ -f "$KEYMAN_ENGINE_DST" ] || KMEA_BUILD_EXISTS=false
+}
+
 echo Build $TARGET
 
 #
@@ -20,6 +25,9 @@ echo Build $TARGET
 #
 SHLVL=0
 
+DO_UPDATE=true
+FORCE_KMEA_BUILD=false
+ALLOW_KMEA_BUILD=true
 NO_DAEMON=false
 ONLY_DEBUG=false
 
@@ -33,6 +41,17 @@ while [[ $# -gt 0 ]] ; do
         -debug)
             ONLY_DEBUG=true
             ;;
+        # Settings relating to engine build
+        -no-update)
+            DO_UPDATE=false
+            ALLOW_KMEA_BUILD=false
+            ;;
+        -lib-build)
+            FORCE_KMEA_BUILD=true
+            ;;
+        -lib-nobuild)
+            ALLOW_KMEA_BUILD=false
+            ;;
         -h|-?)
             display_usage
             ;;
@@ -43,7 +62,47 @@ done
 echo
 echo "NO_DAEMON: $NO_DAEMON"
 echo "ONLY_DEBUG: $ONLY_DEBUG"
+echo "DO_UPDATE: $DO_UPDATE"
+echo "ALLOW_KMEA_BUILD: $ALLOW_KMEA_BUILD"
+echo "FORCE_KMEA_BUILD: $FORCE_KMEA_BUILD"
 echo
+
+# The KMEA build script moves the final .aar from KMEA to KMAPro...
+KMEA_BUILD_DIR=../../../android/KMEA/
+KEYMAN_ENGINE_SRC=../../../android/KMAPro/kMAPro/libs/keyman-engine.aar
+KEYMAN_ENGINE_DST=app/libs/keyman-engine.aar
+
+#
+# Build Keyman Engine
+#
+
+if [ $DO_UPDATE = true ]; then
+    # Does a prior build of KMEA exist?
+    verify_KMEA
+
+    if [ $ALLOW_KMEA_BUILD = true ] && [ $FORCE_KMEA_BUILD = false ] && [ $KMEA_BUILD_EXISTS = false ]; then
+        echo "Previous Keyman Engine build information is unavailable; rebuilding."
+        FORCE_KMEA_BUILD=true
+    fi
+
+    if [ $FORCE_KMEA_BUILD = true ]; then
+        echo "Building Keyman Engine..."
+        pushd $KMEA_BUILD_DIR
+        ./build.sh "$@"
+        popd
+    fi
+
+    verify_KMEA
+
+    if ! [ $KMEA_BUILD_EXISTS ]; then
+      echo "Build failed: Could not build required Keyman Engine resources."
+      exit 1
+    fi
+
+    # Copy Keyman Engine aar to 
+    cp "$KEYMAN_ENGINE_SRC" "$KEYMAN_ENGINE_DST"
+fi
+
 
 if [ "$NO_DAEMON" = true ]; then
   DAEMON_FLAG=--no-daemon
