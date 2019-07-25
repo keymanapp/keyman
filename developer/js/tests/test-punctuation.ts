9589778597f660d3f1fa78c347186c6d0931dd9a
Eddie Antonio Santos <easantos@ualberta.ca>
2019-07-25 11:26:39 -0600

    Test that the compiler adds the punctuation.

--- developer/js/tests/test-punctuation.ts
+++ developer/js/tests/test-punctuation.ts
@@ -6,24 +6,25 @@ const path = require('path');
 
 
 describe('LexicalModelCompiler', function () {
-  describe('#generateLexicalModelCode', function () {
-    const MODEL_ID = 'example.qaa.trivial';
+  describe('spefifying punctuation', function () {
+    const MODEL_ID = 'example.qaa.punctuation';
     const PATH = path.join(__dirname, 'fixtures', MODEL_ID)
+
     it('should compile a trivial word list', function () {
       let compiler = new LexicalModelCompiler;
       let code = compiler.generateLexicalModelCode(MODEL_ID, {
         format: 'trie-1.0',
-        sources: ['wordlist.tsv']
+        sources: ['wordlist.tsv'],
+        punctuation: {
+          quotesForKeepSuggestion: [`«`, `»`],
+          insertAfterWord: " " ,
+        }
       }, PATH) as string;
 
-      assert.doesNotThrow(function evalModelCode() {
-        eval(code);
-      }, SyntaxError);
-      // TODO: Mock LMLayerWorker.loadModel()
-  
-      // Sanity check: the word list has three total unweighted words, with a
-      // total weight of 3!
-      assert.match(code, /\btotalWeight\b["']?:\s*3\b/);
+      // Check that the punctuation actually made into the code:
+      assert.match(code, /«/);
+      assert.match(code, /»/);
+      // TODO: more robust assertions?
     });
   })
 });
