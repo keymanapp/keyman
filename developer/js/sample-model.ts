62f2e3f5e900b1040221dfa62ddab966ec1da471
Eddie Antonio Santos <easantos@ualberta.ca>
2019-08-12 15:34:25 -0600

    Rough draft of compilation of module.

---
+++ developer/js/sample-model.ts
@@ -0,0 +1,8 @@
+/// <reference path="./lexical-model-compiler/lexical-model.ts" />
+
+const source: LexicalModelSource = {
+  format: 'trie-1.0',
+  sources: ['tests/fixtures/example.qaa.trivial/wordlist.tsv'],
+};
+
+export default source;
