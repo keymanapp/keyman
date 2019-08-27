ddef37f7e6fc75b314fdf64ec74929f3e305390b
Eddie Antonio Santos <easantos@ualberta.ca>
2019-08-27 10:26:03 -0600

    Test a UTF-16LE file compiles properly.

--- developer/js/tests/test-compile-trie.ts
+++ developer/js/tests/test-compile-trie.ts
@@ -7,10 +7,10 @@ import {makePathToFixture, compileModelSourceCode} from './helpers';
 
 describe('LexicalModelCompiler', function () {
   describe('#generateLexicalModelCode', function () {
-    const MODEL_ID = 'example.qaa.trivial';
-    const PATH = makePathToFixture(MODEL_ID);
-
     it('should compile a trivial word list', function () {
+      const MODEL_ID = 'example.qaa.trivial';
+      const PATH = makePathToFixture(MODEL_ID);
+
       let compiler = new LexicalModelCompiler;
       let code = compiler.generateLexicalModelCode(MODEL_ID, {
         format: 'trie-1.0',
@@ -25,6 +25,26 @@ describe('LexicalModelCompiler', function () {
       // Sanity check: the word list has three total unweighted words, with a
       // total weight of 3!
       assert.match(code, /\btotalWeight\b["']?:\s*3\b/);
+    });
+
+    it('should compile a word list exported by Microsoft Excel', function () {
+      const MODEL_ID = 'example.qaa.utf16le';
+      const PATH = makePathToFixture(MODEL_ID);
+
+      let compiler = new LexicalModelCompiler;
+      let code = compiler.generateLexicalModelCode(MODEL_ID, {
+        format: 'trie-1.0',
+        sources: ['wordlist.txt']
+      }, PATH) as string;
+
+      let result = compileModelSourceCode(code);
+      assert.isFalse(result.hasSyntaxError);
+      assert.isNotNull(result.exportedModel);
+      assert.equal(result.modelConstructorName, 'TrieModel');
+
+      // Sanity check: the word list has three total unweighted words, with a
+      // total weight of 44,103!
+      assert.match(code, /\btotalWeight\b["']?:\s*44103\b/);
     });
   })
 });
