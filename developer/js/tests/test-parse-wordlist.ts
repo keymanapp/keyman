2a20b6bbc75d669081931420c894945bde99d6cf
Eddie Antonio Santos <easantos@ualberta.ca>
2019-08-27 10:29:52 -0600

    Test that the actual word list gets parsed properly (UTF-16/UTF-8).

--- developer/js/tests/test-parse-wordlist.ts
+++ developer/js/tests/test-parse-wordlist.ts
@@ -4,6 +4,18 @@ import 'mocha';
 import { makePathToFixture } from './helpers';
 
 const BOM = '\ufeff';
+const SENCOTEN_WORDLIST = [
+  ['TŦE', 13644],
+  ['E', 9134],
+  ['SEN', 4816],
+  ['Ȼ', 3479],
+  ['SW̱', 2621],
+  ['NIȽ', 2314],
+  ['U¸', 2298],
+  ['I¸', 1988],
+  ['ȻSE', 1925],
+  ['I', 1884]
+];
 
 describe('parseWordList', function () {
   it('should remove the UTF-8 byte order mark from files', function () {
@@ -20,11 +32,10 @@ describe('parseWordList', function () {
   });
 
   it('should read word lists in UTF-8', function () {
-    // N.B.: this is the format exported by MS Excel when selecting
-    // "UTF-16" text (tested on Excel for macOS).
+    // N.B.: this is the format exported by Google Drive when selecting "TSV".
     const filename = makePathToFixture('example.qaa.sencoten', 'wordlist.tsv');
     let wordlist = parseWordListFromFilename(filename);
-    assert.lengthOf(wordlist, 10);
+    assert.deepEqual(wordlist, SENCOTEN_WORDLIST);
   });
 
   it('should read word lists in UTF-16 little-endian (with BOM)', function () {
@@ -32,7 +43,7 @@ describe('parseWordList', function () {
     // "UTF-16" text (tested on Excel for macOS).
     const filename = makePathToFixture('example.qaa.utf16le', 'wordlist.txt');
     let wordlist = parseWordListFromFilename(filename);
-    assert.lengthOf(wordlist, 10);
+    assert.deepEqual(wordlist, SENCOTEN_WORDLIST);
   });
 
   it('should NOT read word lists in UTF-16 big-endian (with BOM)', function () {
