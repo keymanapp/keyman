ccc64987390b240d5b1db7aef9975ce3170da04f
Marc Durdin <marc@keyman.com>
2019-11-06 17:44:32 +1100

    fix(windows): Keyman was crashing sporadically
    Fixes #2147.
    
    This potentially addresses the issues reported in #2147 where Keyman
    would crash due to a race condition as a critical section was not
    deleted before attempting to recreate it.
    
    Some logging has been added for debugging these issues.
    
    Also fixes memory leaks and another race on startup relating to loading
    the taskbar icon.
    
    There are still some known memory leaks because DLL_THREAD_DETACH is
    not called for all threads before keyman32.dll is unloaded, so those
    threads don't get the opportunity to clean themsselves up. However, the
    key danger with the csGlobals critical section has been addressed.

--- windows/src/engine/keyman32/hookutils.cpp
+++ windows/src/engine/keyman32/hookutils.cpp
@@ -15,45 +15,44 @@ typedef BOOL
     IN CONST PMINIDUMP_CALLBACK_INFORMATION CallbackParam OPTIONAL
     );
 
-
-DWORD ExceptionMessage(LPSTR Proc, LPEXCEPTION_POINTERS ep)
-{
+DWORD ExceptionMessage(LPSTR Proc, LPEXCEPTION_POINTERS ep) {
   MINIDUMP_EXCEPTION_INFORMATION mei;
   char filename[MAX_PATH], temppath[MAX_PATH];
-  if(GetTempPath(MAX_PATH, temppath) == 0 ||
-      GetTempFileName(temppath, "kmc", 0, filename) == 0)
-    SendDebugMessageFormat(0, sdmGlobal, 0, "Minidump failed to generate temp file name");
-  else
-  {
+  if (GetTempPath(MAX_PATH, temppath) == 0 ||
+      GetTempFileName(temppath, "kmc", 0, filename) == 0) {
+    OutputThreadDebugString("Minidump failed to generate temp file name\n");
+  } 
+  else {
     HANDLE hFile = CreateFile(filename, GENERIC_WRITE, 0, NULL, CREATE_ALWAYS, 0, NULL);
-    if(!hFile)
-      SendDebugMessageFormat(0, sdmGlobal, 0, "Minidump failed to create file %s", filename);
-    else
-    {
+    if (!hFile) {
+      OutputThreadDebugString("Minidump failed to create file ");
+      OutputThreadDebugString(filename);
+    }
+    else {
       mei.ClientPointers = TRUE;
       mei.ExceptionPointers = ep;
       mei.ThreadId = GetCurrentThreadId();
       HMODULE hDbgHelp = LoadLibrary("dbghelp.dll");
-      if(!hDbgHelp)
-        SendDebugMessage(0, sdmGlobal, 0, "dbghelp.dll not available");
-      else
-      {
+      if (!hDbgHelp) {
+        OutputThreadDebugString("dbghelp.dll not available");
+      }
+      else {
         PMiniDumpWriteDump mdwd = (PMiniDumpWriteDump) GetProcAddress(hDbgHelp, "MiniDumpWriteDump");
-        if(!mdwd)
-          SendDebugMessage(0, sdmGlobal, 0, "MiniDumpWriteDump not available");
-        else
-        {
+        if (!mdwd) {
+          OutputThreadDebugString("MiniDumpWriteDump not available");
+        }
+        else {
           if (!(*mdwd)(GetCurrentProcess(), GetCurrentProcessId(), hFile,
-              (MINIDUMP_TYPE)(MiniDumpWithDataSegs | MiniDumpWithHandleData),
-              &mei, NULL, NULL))
+            (MINIDUMP_TYPE)(MiniDumpWithDataSegs | MiniDumpWithHandleData),
+            &mei, NULL, NULL)) {
             DebugLastError("MiniDumpWriteDump");
-          else
-          {
-            SendDebugMessageFormat(0, sdmGlobal, 0, "Minidump written to %s", filename);
+          } 
+          else {
+            OutputThreadDebugString("Minidump written to ");
+            OutputThreadDebugString(filename);
             HKEY hkey;
             if(RegCreateKeyEx(HKEY_CURRENT_USER, REGSZ_KeymanEngineDiag, 0, NULL, 0, KEY_ALL_ACCESS,
-              NULL, &hkey, NULL) == ERROR_SUCCESS)
-            {
+              NULL, &hkey, NULL) == ERROR_SUCCESS) {
               DWORD v = 0;
               RegSetValueEx(hkey, filename, 0, REG_DWORD, (PBYTE)&v, sizeof(DWORD));
               RegCloseKey(hkey);
@@ -66,23 +65,26 @@ DWORD ExceptionMessage(LPSTR Proc, LPEXCEPTION_POINTERS ep)
     }
   }
 
-  if(!ep || !ep->ExceptionRecord) 
-  {
-    SendDebugMessageFormat(0, sdmGlobal, 0, "CAUGHT UNKNOWN EXCEPTION");
+  if(!ep || !ep->ExceptionRecord) {
+    OutputThreadDebugString("CAUGHT UNKNOWN EXCEPTION");
     return EXCEPTION_CONTINUE_SEARCH;
   }
   LPEXCEPTION_RECORD er = ep->ExceptionRecord;
   
-  while(er != NULL)
-  {
-    if(er->ExceptionCode == EXCEPTION_ACCESS_VIOLATION)
-    	SendDebugMessageFormat(0, sdmGlobal, 0, "CAUGHT EXCEPTION %d (ACCESS VIOLATION) IN %s AT %x; attempted to %s %x",
-    	  er->ExceptionCode, Proc, er->ExceptionAddress,
-    	  er->ExceptionInformation[0] == 0 ? "read from" : "write to",
-    	  er->ExceptionInformation[1]);
-    else
-  	  SendDebugMessageFormat(0, sdmGlobal, 0, "CAUGHT EXCEPTION %d IN %s AT %x",
-    	  er->ExceptionCode, Proc, er->ExceptionAddress);
+  while(er != NULL) {
+    if (er->ExceptionCode == EXCEPTION_ACCESS_VIOLATION) {
+      OutputThreadDebugString("EXCEPTION_ACCESS_VIOLATION in ");
+      OutputThreadDebugString(Proc);
+      SendDebugMessageFormat(0, sdmGlobal, 0, "CAUGHT EXCEPTION %d (ACCESS VIOLATION) IN %s AT %x; attempted to %s %x",
+        er->ExceptionCode, Proc, er->ExceptionAddress,
+        er->ExceptionInformation[0] == 0 ? "read from" : "write to",
+        er->ExceptionInformation[1]);
+    }
+    else {
+      OutputThreadDebugString("CAUGHT EXCEPTION");
+      SendDebugMessageFormat(0, sdmGlobal, 0, "CAUGHT EXCEPTION %d IN %s AT %x",
+        er->ExceptionCode, Proc, er->ExceptionAddress);
+    }
     er = er->ExceptionRecord;
   }
 
