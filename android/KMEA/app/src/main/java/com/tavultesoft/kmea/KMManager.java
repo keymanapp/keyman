6fde9c1739c0dc7141d50d51d960144069e696da
Darcy Wong <darcy_wong@sil.org>
2019-11-13 08:31:54 +0700

    chore(android): move utility methods to new class
    Refactor charSequence utilities to CharSequenceUtil.java

--- android/KMEA/app/src/main/java/com/tavultesoft/kmea/KMManager.java
+++ android/KMEA/app/src/main/java/com/tavultesoft/kmea/KMManager.java
@@ -61,6 +61,7 @@ import com.tavultesoft.kmea.packages.JSONUtils;
 import com.tavultesoft.kmea.packages.LexicalModelPackageProcessor;
 import com.tavultesoft.kmea.packages.PackageProcessor;
 import com.tavultesoft.kmea.KMScanCodeMap;
+import com.tavultesoft.kmea.util.CharSequenceUtil;
 import com.tavultesoft.kmea.util.FileUtils;
 
 import org.json.JSONArray;
@@ -309,94 +310,6 @@ public final class KMManager {
     }
   }
 
-  // These aren't public for KMManager API. Just for unit testing:
-
-  /**
-   * Count the number of surrogate pairs starting from the end of a character sequence
-   * until we reach dn codepoints or searched the entire sequence.
-   * Doing this the hard way because we can't foreach charSequence in reverse.
-   * @param sequence - the character sequence to analyze
-   * @param dn - the number of code points to count up to
-   * @return int
-   */
-  public static int countSurrogatePairs(CharSequence sequence, int dn) {
-    if ((sequence == null) || (sequence.length() == 0) || (dn <= 0)) {
-      return 0;
-    }
-
-    int numPairs = 0, counter = 0;
-    int lastIndex = sequence.length()-1;
-    try {
-      do {
-        if (Character.isLowSurrogate(sequence.charAt(lastIndex - counter))) {
-          numPairs++;
-        }
-        counter++;
-      } while ((lastIndex - counter >= 0) && (counter < dn) && (numPairs < dn));
-      return numPairs;
-    } catch (Exception e) {
-      Log.e(TAG, "Error in countSurrogatePairs: " + e);
-      return numPairs;
-    }
-  }
-
-  /**
-   * Determine a character sequence that needs to be re-inserted. If currentContext is a
-   * subSequence of expectedChars, returns the character sequence that needs to be restored.
-   * @param expectedChars - expected character sequence
-   * @param currentContext - current character sequence
-   * @return CharSequence - Character sequence that will need to be appended to currentContext.
-   *                        Empty string if no characters need to be restored.
-   */
-  public static CharSequence restoreChars(CharSequence expectedChars, CharSequence currentContext) {
-    CharSequence charsToRestore = "";
-
-    try {
-      // Now see if we need to re-insert characters
-      if (expectedChars.length() != currentContext.length()) {
-        String expectedCharsString = expectedChars.toString();
-        String currentContextString = currentContext.toString();
-        int index = expectedCharsString.indexOf(currentContextString);
-        if (index > -1) {
-          // subSequence indices are start(inclusive) to end(exclusive)
-          charsToRestore = expectedChars.subSequence(index + currentContextString.length(), expectedChars.length());
-        }
-      }
-
-      return charsToRestore;
-    } catch (Exception e) {
-      Log.e(TAG, "Error in restoreChars: " + e);
-      return charsToRestore;
-    }
-  }
-
-  /**
-   * Adjust cursor position after insert the new text.
-   * @param charsBefore the character sequence of the inputconnection (2*s.length)
-   * @param s the inserted text
-   * @return int - number of characters to adjust the cursor
-   */
-  public static int adjustCursorPosition(CharSequence charsBefore, String s) {
-    if (charsBefore == null || s == null) {
-      return 0;
-    }
-
-    int _expected_start_index = charsBefore.length() - s.length();
-    int _move = 0;
-    while (_move < _expected_start_index) {
-
-      CharSequence _check = charsBefore.subSequence(
-        _expected_start_index - _move,charsBefore.length()-_move);
-      if(_check.equals(s)) {
-        break;
-      }
-      _move++;
-    }
-    return _move;
-  }
-
-  // End of public methods for unit testing
-
   /*
   // TODO: Chromium has a bug where deleteSurroundingText deletes an entire grapheme cluster
   // instead of one code-point.
@@ -423,7 +336,7 @@ public final class KMManager {
       return;
     }
 
-    int numPairs = countSurrogatePairs(charsBackup, dn);
+    int numPairs = CharSequenceUtil.countSurrogatePairs(charsBackup, dn);
 
     // Chop dn+numPairs code points from the end of charsBackup
     // subSequence indices are start(inclusive) to end(exclusive)
@@ -431,7 +344,7 @@ public final class KMManager {
     ic.deleteSurroundingText(dn + numPairs, 0);
     CharSequence newContext = ic.getTextBeforeCursor(originalBufferLength - dn, 0);
 
-    CharSequence charsToRestore = restoreChars(expectedChars, newContext);
+    CharSequence charsToRestore = CharSequenceUtil.restoreChars(expectedChars, newContext);
     if (charsToRestore.length() > 0) {
       // Restore expectedChars that Chromium deleted, and advance the cursor by expectedChars.length()
       ic.commitText(charsToRestore, charsToRestore.length());
@@ -2206,7 +2119,7 @@ public final class KMManager {
 
             //String userAgent = System.getProperty("http.agent");
             CharSequence charsBefore = ic.getTextBeforeCursor(s.length()*2, 0);
-            int move = adjustCursorPosition(charsBefore, s);
+            int move = CharSequenceUtil.adjustCursorPosition(charsBefore, s);
             if (move > 0) {
               Log.d(TAG, "adjusting cursor charsBefore: " + charsBefore.toString() + ", s: " + s + ", move: " + move);
               ic.commitText("", -move);
---
+++ android/KMEA/app/src/test/java/com/tavultesoft/kmea/util/CharSequenceUtilTest.java
@@ -0,0 +1,171 @@
+package com.tavultesoft.kmea.util;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.robolectric.RobolectricTestRunner;
+
+import com.tavultesoft.kmea.util.CharSequenceUtil;
+
+@RunWith(RobolectricTestRunner.class)
+public class CharSequenceUtilTest {
+  // Smiley emoji U+1F600 = D800 DC3D
+  private final String SMILEY = "\uD800\uDC3D";
+
+  // Winking emoji U+1F609 = D800 DC3C
+  private final String WINK = "\uD800\uDC3C";
+
+  private final String COMPOSING_DOT_ABOVE = "\u0307";
+  private final String COMPOSING_CIRCUMFLEX_ACCENT = "\u0302";
+
+  private final String P_COMPOSING_DOT_ABOVE = "p" + COMPOSING_DOT_ABOVE;
+  private final String P_COMPOSING_CIRCUMFLEX_ACCENT = "p" + COMPOSING_CIRCUMFLEX_ACCENT;
+
+  // Section countSurrogaetPairs Tests
+
+  @Test
+  public void test_countSurrogatePairs_invalid_input() {
+
+    // Test invalid input doesn't throw exception
+
+    // null sequence
+    CharSequence sequence = null;
+    int numPairs = CharSequenceUtil.countSurrogatePairs(sequence, 1);
+    Assert.assertEquals(0, numPairs);
+
+    // negative dn
+    sequence = SMILEY + " Day";
+    numPairs = CharSequenceUtil.countSurrogatePairs(sequence, -1);
+    Assert.assertEquals(0, numPairs);
+
+    // dn longer than the sequence length
+    numPairs = CharSequenceUtil.countSurrogatePairs(sequence, sequence.length()+5);
+    Assert.assertEquals(1, numPairs);
+  }
+
+  @Test
+  public void test_countSurrogatePairs_zero_pairs() {
+
+    // Test for scenarios that expect 0 surrogate pairs
+    CharSequence sequence = "Zero emojis";
+    int numPairs = CharSequenceUtil.countSurrogatePairs(sequence, sequence.length());
+    Assert.assertEquals(0, numPairs);
+
+    // dn doesn't reach the surrogate pair
+    sequence = "Zero emojis" + SMILEY + "!";
+    numPairs = CharSequenceUtil.countSurrogatePairs(sequence, 0);
+    Assert.assertEquals(0, numPairs);
+
+    numPairs = CharSequenceUtil.countSurrogatePairs(sequence, 1);
+    Assert.assertEquals(0, numPairs);
+  }
+
+  @Test
+  public void test_countSurrogatePairs_one_pair() {
+
+    // Test for scenarios that expect 1 surrogate pair
+    CharSequence sequence = "Have A " + SMILEY;
+    int numPairs = CharSequenceUtil.countSurrogatePairs(sequence, 1);
+    Assert.assertEquals(1, numPairs);
+
+    numPairs = CharSequenceUtil.countSurrogatePairs(sequence, 2);
+    Assert.assertEquals(1, numPairs);
+
+    numPairs = CharSequenceUtil.countSurrogatePairs(sequence, 6);
+    Assert.assertEquals(1, numPairs);
+
+    sequence = "Have A " + SMILEY + WINK + " Day";
+    numPairs = CharSequenceUtil.countSurrogatePairs(sequence, 5);
+    Assert.assertEquals(1, numPairs);
+
+    numPairs = CharSequenceUtil.countSurrogatePairs(sequence, 6);
+    Assert.assertEquals(1, numPairs);
+  }
+
+  @Test
+  public void test_countSurrogatePairs_two_pairs() {
+
+    // Test for scenarios that expect 2 surrogate pairs
+    CharSequence sequence = "Have A " + SMILEY + WINK + " Day";
+    int numPairs = CharSequenceUtil.countSurrogatePairs(sequence, 7);
+    Assert.assertEquals(2, numPairs);
+
+    numPairs = CharSequenceUtil.countSurrogatePairs(sequence, 8);
+    Assert.assertEquals(2, numPairs);
+  }
+
+
+  @Test
+  public void test_restoreChars_invalid_input() {
+    // Test invalid input doesn't throw exception
+    // null sequence
+    CharSequence expectedChars = null;
+    CharSequence currentContext = "qwerty";
+    CharSequence charsToRestore = CharSequenceUtil.restoreChars(expectedChars, currentContext);
+    Assert.assertEquals("", charsToRestore);
+
+    expectedChars = "qwerty";
+    currentContext = null;
+    charsToRestore = CharSequenceUtil.restoreChars(expectedChars, currentContext);
+    Assert.assertEquals("", charsToRestore);
+
+    expectedChars = WINK;
+    currentContext = "notamatch";
+    charsToRestore = CharSequenceUtil.restoreChars(expectedChars, currentContext);
+    Assert.assertEquals("", charsToRestore);
+  }
+
+  @Test
+  public void test_restoreChars_split_surrogate_pair() {
+    CharSequence expectedChars = "o" + P_COMPOSING_CIRCUMFLEX_ACCENT + P_COMPOSING_CIRCUMFLEX_ACCENT + WINK + "p";
+    CharSequence currentContext = P_COMPOSING_CIRCUMFLEX_ACCENT + "\uD800";
+    CharSequence charsToRestore = CharSequenceUtil.restoreChars(expectedChars, currentContext);
+    Assert.assertEquals("\uDC3C" + "p", charsToRestore);
+  }
+
+  @Test
+  public void test_restoreChars_expected_chars() {
+    CharSequence expectedChars = "qwertyp" + SMILEY + COMPOSING_DOT_ABOVE;
+    CharSequence currentContext = "qwertyp";
+    CharSequence charsToRestore = CharSequenceUtil.restoreChars(expectedChars, currentContext);
+    Assert.assertEquals(SMILEY + COMPOSING_DOT_ABOVE, charsToRestore);
+
+    expectedChars = "qwertyp" + COMPOSING_CIRCUMFLEX_ACCENT + "p" + COMPOSING_CIRCUMFLEX_ACCENT;
+    currentContext = "qwertyp";
+    charsToRestore = CharSequenceUtil.restoreChars(expectedChars, currentContext);
+    Assert.assertEquals(COMPOSING_CIRCUMFLEX_ACCENT + "p" + COMPOSING_CIRCUMFLEX_ACCENT, charsToRestore);
+
+    expectedChars = "qwertyp" + COMPOSING_CIRCUMFLEX_ACCENT + "p" + COMPOSING_CIRCUMFLEX_ACCENT;
+    currentContext = "qwerty";
+    charsToRestore = CharSequenceUtil.restoreChars(expectedChars, currentContext);
+    Assert.assertEquals("p" + COMPOSING_CIRCUMFLEX_ACCENT + "p" + COMPOSING_CIRCUMFLEX_ACCENT, charsToRestore);
+  }
+
+  @Test
+  public void test_adjustCursorPosition_invalid_input() {
+    CharSequence sequence = null;
+    String s = P_COMPOSING_CIRCUMFLEX_ACCENT + P_COMPOSING_CIRCUMFLEX_ACCENT;
+    int move = CharSequenceUtil.adjustCursorPosition(sequence, s);
+    Assert.assertEquals(0, move);
+
+    sequence = P_COMPOSING_CIRCUMFLEX_ACCENT + P_COMPOSING_CIRCUMFLEX_ACCENT;
+    s = null;
+    move = CharSequenceUtil.adjustCursorPosition(sequence, s);
+    Assert.assertEquals(0, move);
+  }
+
+  @Test
+  public void test_adjustCursorPosition_move_3() {
+    CharSequence charsBefore = "o" + P_COMPOSING_CIRCUMFLEX_ACCENT + P_COMPOSING_CIRCUMFLEX_ACCENT +
+      SMILEY + P_COMPOSING_DOT_ABOVE;
+    String s = P_COMPOSING_CIRCUMFLEX_ACCENT + P_COMPOSING_CIRCUMFLEX_ACCENT;
+
+    int move = CharSequenceUtil.adjustCursorPosition(charsBefore, s);
+    Assert.assertEquals(4, move);
+
+    charsBefore = "g" + P_COMPOSING_CIRCUMFLEX_ACCENT + P_COMPOSING_CIRCUMFLEX_ACCENT + "lyf";
+    move = CharSequenceUtil.adjustCursorPosition(charsBefore, s);
+    Assert.assertEquals(3, move);
+
+  }
+}
