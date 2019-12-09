9fb8e58f86dc879ae236ba1b9ec33dc53cb04920
Marc Durdin <marc@durdin.net>
2019-12-09 15:21:28 +0000

    chore: update comment

--- developer/js/tests/test-compile-trie.ts
+++ developer/js/tests/test-compile-trie.ts
@@ -70,4 +70,37 @@ describe('LexicalModelCompiler', function () {
     // Sanity check: the word breaker is a property of the object.
     assert.match(code, /\bwordBreaker\b["']?:\s+function\b/);
   });
+
+  it('should not generate unpaired surrogate code units', function () {
+    const MODEL_ID = 'example.qaa.smp';
+    const PATH = makePathToFixture(MODEL_ID);
+
+    let compiler = new LexicalModelCompiler;
+    let code = compiler.generateLexicalModelCode(MODEL_ID, {
+      format: 'trie-1.0',
+      sources: ['wordlist.tsv']
+    }, PATH) as string;
+
+    let result = compileModelSourceCode(code);
+    assert.isFalse(result.hasSyntaxError);
+    assert.isNotNull(result.exportedModel);
+    assert.equal(result.modelConstructorName, 'TrieModel');
+
+    // Test every character in the string to make sure we don't have
+    // unpaired surrogates which destroy everything.
+    // We can assume that the first and last chars are not SMP
+    for(var i = 1; i < code.length - 1; i++) {
+      if(code.charCodeAt(i) >= 0xD800 && code.charCodeAt(i) < 0xDC00) {
+        assert.isTrue((code.charCodeAt(i+1) >= 0xDC00 && code.charCodeAt(i+1) < 0xE000),
+          'Unpaired lead surrogate U+'+code.charCodeAt(i).toString(16)+' at position '+i+' of \''+code+'\'');
+      } else if(code.charCodeAt(i) >= 0xDC00 && code.charCodeAt(i) < 0xE000) {
+        assert.isTrue((code.charCodeAt(i-1) >= 0xD800 && code.charCodeAt(i-1) < 0xDC00),
+          'Unpaired trail surrogate U+'+code.charCodeAt(i).toString(16)+' at position '+i+' of \''+code+'\'');
+      }
+    }
+
+    // Sanity check: the word list has three total unweighted words, with a
+    // total weight of 27,596!
+    assert.match(code, /\btotalWeight\b["']?:\s*27596\b/);
+  });
 });
