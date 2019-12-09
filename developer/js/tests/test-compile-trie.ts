695a8ad4c65323fef53b1303ea6305732ff7934c
Marc Durdin <marc@durdin.net>
2019-12-09 18:48:36 +0000

    test: also test for REPLACEMENT CHARACTER

--- developer/js/tests/test-compile-trie.ts
+++ developer/js/tests/test-compile-trie.ts
@@ -90,13 +90,14 @@ describe('LexicalModelCompiler', function () {
     // unpaired surrogates which destroy everything.
     // We can assume that the first and last chars are not SMP
     for(var i = 1; i < code.length - 1; i++) {
+      assert.notEqual(0xFFFD, code.charCodeAt(i));
       if(code.charCodeAt(i) >= 0xD800 && code.charCodeAt(i) < 0xDC00) {
         assert.isTrue((code.charCodeAt(i+1) >= 0xDC00 && code.charCodeAt(i+1) < 0xE000),
           'Unpaired lead surrogate U+'+code.charCodeAt(i).toString(16)+' at position '+i+' of \''+code+'\'');
       } else if(code.charCodeAt(i) >= 0xDC00 && code.charCodeAt(i) < 0xE000) {
         assert.isTrue((code.charCodeAt(i-1) >= 0xD800 && code.charCodeAt(i-1) < 0xDC00),
           'Unpaired trail surrogate U+'+code.charCodeAt(i).toString(16)+' at position '+i+' of \''+code+'\'');
-      }
+      } 
     }
 
     // Sanity check: the word list has three total unweighted words, with a
