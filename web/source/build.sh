c1c3c1e6a1d797f2d8d6f648fdd28e9efc40004f
jahorton <jahorton@sil.org>
2019-12-16 12:23:16 +0700

    fix(android): Removes Closure's auto-polyfills, adds test case bypass

--- web/source/build.sh
+++ web/source/build.sh
@@ -83,7 +83,8 @@ minifier="$CLOSURECOMPILERPATH/compiler.jar"
 # `jsDocMissingType` prevents errors on type documentation Closure thinks is missing.  TypeScript may not
 # have the same requirements, and we trust TypeScript over Closure.
 minifier_warnings="--jscomp_error=* --jscomp_off=lintChecks --jscomp_off=unusedLocalVariables --jscomp_off=globalThis --jscomp_off=checkTypes --jscomp_off=checkVars --jscomp_off=jsdocMissingType --jscomp_off=uselessCode"
-minifycmd="$JAVA -jar $minifier --compilation_level WHITESPACE_ONLY $minifier_warnings --generate_exports"
+minifier_lang_specs="--language_in ECMASCRIPT5 --language_out ECMASCRIPT5"
+minifycmd="$JAVA -jar $minifier --compilation_level WHITESPACE_ONLY $minifier_warnings --generate_exports $minifier_lang_specs"
 
 if ! [ -f $minifier ];
 then
