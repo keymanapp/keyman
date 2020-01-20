b9a26909e3a2fcf4ac47b33509f6ec1a7e8eeb2a
jahorton <jahorton@sil.org>
2020-01-20 09:07:41 +0700

    feat(ios): adds KeyboardCalibrator tool

---
+++ ios/tools/KeyboardCalibrator/KeyboardCalibrator/DummyInputViewController.swift
@@ -0,0 +1,190 @@
+//
+//  DummyInputViewController.swift
+//  DefaultKeyboardHost
+//
+//  Created by Joshua Horton on 1/13/20.
+//  Copyright © 2020 Joshua Horton. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+private class CustomInputView: UIInputView {
+  var height: CGFloat
+  var inset: CGFloat
+  var innerView: UIView!
+  var insetView: UIView!
+
+  var heightLabel: UILabel!
+  var insetHeightLabel: UILabel!
+
+  init(height: CGFloat, inset: CGFloat) {
+    self.height = height
+    self.inset = inset
+    super.init(frame: CGRect.zero, inputViewStyle: .keyboard)
+  }
+
+  override var intrinsicContentSize: CGSize {
+    get {
+      return CGSize(width: UIScreen.main.bounds.width, height: height > 0 ? height + inset : 100)
+    }
+  }
+
+  required init?(coder: NSCoder) {
+    fatalError("init(coder:) has not been implemented")
+  }
+
+  func setConstraints() {
+    var guide: UILayoutGuide
+
+    if #available(iOS 11.0, *) {
+      guide = self.safeAreaLayoutGuide
+    } else {
+      guide = self.layoutMarginsGuide
+    }
+
+    if height != 0 {
+      innerView.heightAnchor.constraint(equalToConstant: height).isActive = true
+    } else {
+      innerView.heightAnchor.constraint(equalTo: guide.heightAnchor).isActive = true
+    }
+
+    if #available(iOS 11.0, *) {
+      innerView.widthAnchor.constraint(equalTo: guide.widthAnchor).isActive = true
+      innerView.leftAnchor.constraint(equalTo: guide.leftAnchor).isActive = true
+      insetView.widthAnchor.constraint(equalTo: guide.widthAnchor).isActive = true
+    } else {
+      innerView.widthAnchor.constraint(equalTo: self.widthAnchor).isActive = true
+      innerView.leftAnchor.constraint(equalTo: self.leftAnchor).isActive = true
+      insetView.widthAnchor.constraint(equalTo: self.widthAnchor).isActive = true
+    }
+
+    innerView.bottomAnchor.constraint(equalTo: insetView.topAnchor).isActive = true
+
+    insetView.heightAnchor.constraint(equalToConstant: inset).isActive = true
+    insetView.bottomAnchor.constraint(equalTo: guide.bottomAnchor).isActive = true
+  }
+
+  func load() {
+    innerView = UIView()
+    innerView.backgroundColor = .red
+    innerView.translatesAutoresizingMaskIntoConstraints = false
+
+    heightLabel = UILabel(frame: CGRect(x: 0, y: 0, width: 200, height: 21))
+    heightLabel.text = "Hello World"
+
+    innerView.addSubview(heightLabel)
+
+    self.addSubview(innerView)
+
+    insetView = UIView()
+    insetView.backgroundColor = .green
+    insetView.translatesAutoresizingMaskIntoConstraints = false
+
+    insetHeightLabel = UILabel(frame: CGRect(x: 0, y: 0, width: 200, height: 21))
+    insetHeightLabel.text = "(0.0, \(inset))"
+
+    insetView.addSubview(insetHeightLabel)
+
+    self.addSubview(insetView)
+  }
+
+  override func layoutSubviews() {
+    super.layoutSubviews()
+
+    if innerView.bounds.size != CGSize.zero {
+      heightLabel.text = "\(innerView.bounds.size)"
+    } else {
+      heightLabel.text = "\(intrinsicContentSize)"
+    }
+  }
+}
+
+class DummyInputViewController: UIInputViewController {
+  var kbdHeight: CGFloat
+  var insetHeight: CGFloat
+
+  // The app group used to access common UserDefaults settings.
+  static let appGroup = "group.horton.kmtesting"
+
+  static var keyboardHeightDefault: CGFloat {
+    get {
+      let userDefaults = UserDefaults(suiteName: DummyInputViewController.appGroup)!
+      return (userDefaults.object(forKey: "height") as? CGFloat) ?? 0
+    }
+
+    set(value) {
+      let userDefaults = UserDefaults(suiteName: DummyInputViewController.appGroup)!
+      userDefaults.set(value, forKey: "height")
+    }
+  }
+
+  @IBOutlet var nextKeyboardButton: UIButton!
+
+  var asSystemKeyboard: Bool = false
+
+  // This is the one used to initialize the keyboard as the app extension, marking "system keyboard" mode.
+  convenience init() {
+    // Retrieve kbd height from app group!
+    let userDefaults = UserDefaults(suiteName: DummyInputViewController.appGroup)!
+    let height = userDefaults.float(forKey: "height")
+
+    self.init(height: CGFloat(height))
+    asSystemKeyboard = true
+  }
+
+  init(height: CGFloat, inset: CGFloat = 0) {
+    kbdHeight = height
+    insetHeight = inset
+
+    super.init(nibName: nil, bundle: nil)
+  }
+
+  required init?(coder: NSCoder) {
+    fatalError("init(coder:) has not been implemented")
+  }
+
+  open override func loadView() {
+    let baseView = CustomInputView(height: kbdHeight, inset: insetHeight)
+
+    baseView.backgroundColor = .blue
+    baseView.translatesAutoresizingMaskIntoConstraints = false
+
+    self.inputView = baseView
+  }
+
+  open override func viewDidLoad() {
+    let baseView = self.inputView as! CustomInputView
+
+    // Perform custom UI setup here
+    baseView.load()
+    baseView.setConstraints()
+
+    // Adds a very basic "Next keyboard" button to ensure we can always swap keyboards, even on iPhone SE.
+    self.nextKeyboardButton = UIButton(type: .system)
+
+    self.nextKeyboardButton.setTitle(NSLocalizedString("Next Keyboard", comment: "Title for 'Next Keyboard' button"), for: [])
+    self.nextKeyboardButton.sizeToFit()
+    self.nextKeyboardButton.translatesAutoresizingMaskIntoConstraints = false
+
+    self.nextKeyboardButton.addTarget(self, action: #selector(handleInputModeList(from:with:)), for: .allTouchEvents)
+
+    self.view.addSubview(self.nextKeyboardButton)
+
+    var guide: UILayoutGuide
+    if #available(iOS 11.0, *) {
+      guide = self.view.safeAreaLayoutGuide
+      self.nextKeyboardButton.isHidden = !self.needsInputModeSwitchKey || !asSystemKeyboard
+    } else {
+      guide = self.view.layoutMarginsGuide
+      self.nextKeyboardButton.isHidden = false // Seems buggy to vary, and it matters greatly on older devices.
+    }
+
+    self.nextKeyboardButton.leftAnchor.constraint(equalTo: guide.leftAnchor).isActive = true
+    self.nextKeyboardButton.bottomAnchor.constraint(equalTo: guide.bottomAnchor).isActive = true
+  }
+
+  override func viewWillLayoutSubviews() {
+      super.viewWillLayoutSubviews()
+  }
+}
---
+++ ios/tools/KeyboardCalibrator/KeyboardCalibrator/ViewController.swift
@@ -0,0 +1,237 @@
+//
+//  ViewController.swift
+//  DefaultKeyboardHost
+//
+//  Created by Joshua Horton on 1/10/20.
+//  Copyright © 2020 Joshua Horton. All rights reserved.
+//
+
+import UIKit
+
+class OrientationSpec {
+  var predictRect: CGRect? = nil
+  var plainRect: CGRect? = nil
+  var dummyRect: CGRect? = nil
+
+  var insetBottom: CGFloat? = nil
+
+  public init() {
+  }
+
+  public var sufficient: Bool {
+    get {
+      return predictRect != nil && plainRect != nil && insetBottom != nil
+    }
+  }
+
+  public var complete: Bool {
+    get {
+      return sufficient && dummyRect != nil
+    }
+  }
+
+  public var bannerHeight: CGFloat {
+    get {
+      if let predictHeight = predictRect?.height, let plainHeight = plainRect?.height {
+        return predictHeight - plainHeight
+      } else {
+        return CGFloat.nan
+      }
+    }
+  }
+
+  public var keyboardHeight: CGFloat {
+    get {
+      if !sufficient {
+        return CGFloat.nan
+      } else {
+        return plainRect!.height - insetBottom! - inputServicesHeight
+      }
+    }
+  }
+
+  public var inputServicesHeight: CGFloat {
+    if let plainRect = plainRect {
+      if let dummyRect = dummyRect {
+        return dummyRect.height - plainRect.height
+      } else {
+        return 0
+      }
+    } else {
+      return CGFloat.nan
+    }
+  }
+}
+
+// Corresponds to indices within `operationalControl`
+enum CaptureOperation: Int {
+  case predict = 0
+  case plain = 1
+  case calibrate = 2
+  case check = 3
+}
+
+class ViewController: UIViewController {
+
+  @IBOutlet weak var bannerLabel: UILabel!
+  @IBOutlet weak var keyboardLabel: UILabel!
+  @IBOutlet weak var insetLabel: UILabel!
+  @IBOutlet weak var systemLabel: UILabel!
+  @IBOutlet weak var captureStatsView: UIScrollView!
+  @IBOutlet weak var uikitPointsLabel: UILabel!
+  
+  @IBOutlet weak var captureInput: UITextField!
+  @IBOutlet weak var operationControl: UISegmentedControl!
+  @IBOutlet weak var captureButton: UIButton!
+
+  var capture: OrientationSpec! = OrientationSpec()
+  var captureMode: CaptureOperation = .predict
+  var kbdFrame: NSValue? = nil
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+
+    NotificationCenter.default.addObserver(
+      self,
+      selector: #selector(keyboardWillShow),
+      name: UIResponder.keyboardWillShowNotification,
+      object: nil
+    )
+
+    captureStatsView.isHidden = true
+    let screen = UIScreen.main.bounds
+    uikitPointsLabel.text = "(\(screen.width), \(screen.height))"
+  }
+
+  @objc func keyboardWillShow(_ notification: Notification) {
+    if let keyboardFrame: NSValue = notification.userInfo?[UIResponder.keyboardFrameEndUserInfoKey] as? NSValue {
+
+        // We can only retrieve this during the notification, so we save it to a property
+        // for later capture.
+        kbdFrame = keyboardFrame
+    }
+  }
+
+  @objc func clearKeyboard() {
+    self.view.endEditing(true)
+  }
+
+  @IBAction func operationChanged(_ sender: Any) {
+    // Operation mode has been altered; update accordingly
+    captureMode = CaptureOperation(rawValue: operationControl.selectedSegmentIndex)!
+
+    switch captureMode {
+      case .predict:
+        captureInput.autocorrectionType = .yes
+        captureInput.inputView = nil
+        captureButton.setTitle("Capture", for: .normal)
+      case .plain:
+        captureInput.autocorrectionType = .no
+        captureInput.inputView = nil
+        captureButton.setTitle("Capture", for: .normal)
+      case .calibrate:
+        captureInput.autocorrectionType = .no
+        captureInput.inputView = nil
+        captureButton.setTitle("Calibrate", for: .normal)
+      case .check:
+        captureInput.autocorrectionType = .no
+
+        // Compute proper height
+        captureInput.inputView = DummyInputViewController(height: capture.keyboardHeight, inset: capture.inputServicesHeight).inputView
+
+        captureButton.setTitle("Clear keyboard", for: .normal)
+    }
+
+    captureInput.reloadInputViews()
+
+    //clearKeyboard()
+  }
+
+  @IBAction func performCapture() {
+    let rect = kbdFrame?.cgRectValue
+    if #available(iOS 11.0, *) {
+      capture.insetBottom = self.view.safeAreaInsets.bottom
+    } else {
+      capture.insetBottom = self.view.layoutMargins.bottom
+    }
+
+    // Determine correct capture mode
+    switch captureMode {
+      case .predict:
+        capture.predictRect = rect
+      case .plain:
+        capture.plainRect = rect
+      case .calibrate:
+        capture.dummyRect = rect
+      case .check:
+        break
+    }
+
+    // Update according to state of overall capture
+    if capture.sufficient {
+      operationControl.setEnabled(true, forSegmentAt: CaptureOperation.calibrate.rawValue)
+
+      // TODO: Update this thing
+      onCaptureSufficient()
+    }
+
+    if capture.complete {
+      operationControl.setEnabled(true, forSegmentAt: CaptureOperation.check.rawValue)
+    }
+
+    // Perform automatic operation-mode change where appropriate.
+    switch captureMode {
+      case .predict:
+        operationControl.selectedSegmentIndex = CaptureOperation.plain.rawValue
+        operationChanged(self) // is NOT automatically called otherwise!
+      case .plain:
+        operationControl.selectedSegmentIndex = CaptureOperation.calibrate.rawValue
+        operationChanged(self) // is NOT automatically called otherwise!
+      case .calibrate:
+        // May require multiple recalibrations.
+        break
+      case .check:
+        // Process is complete, so there's nothing automatic to do here.
+        break
+    }
+
+    clearKeyboard()
+  }
+
+  func onCaptureSufficient() {
+    let insetHeight = capture.insetBottom!
+    var totalInset = insetHeight
+
+    // Set the appropriate label's value!
+    bannerLabel.text = "Banner: \(capture.bannerHeight)"
+    insetLabel.text = "Inset: \(insetHeight)"
+
+    if let _ = capture.dummyRect {
+      systemLabel.text = "System inset: \(capture.inputServicesHeight)"
+      totalInset += capture.inputServicesHeight
+      systemLabel.isHidden = false
+    } else {
+      systemLabel.isHidden = true
+    }
+
+    // let keyboardHeight = capture.plainRect!.height - totalInset
+    keyboardLabel.text = "Keyboard: \(capture.keyboardHeight)"
+
+    DummyInputViewController.keyboardHeightDefault = capture.keyboardHeight
+
+    captureStatsView.isHidden = false
+  }
+
+  @IBAction func clearCapture() {
+    capture = OrientationSpec()
+
+    captureStatsView.isHidden = true
+
+    // Auto-reset to the default option
+    operationControl.selectedSegmentIndex = CaptureOperation.predict.rawValue
+    operationControl.setEnabled(false, forSegmentAt: CaptureOperation.calibrate.rawValue)
+    operationControl.setEnabled(false, forSegmentAt: CaptureOperation.check.rawValue)
+    operationChanged(self)
+  }
+}
+
