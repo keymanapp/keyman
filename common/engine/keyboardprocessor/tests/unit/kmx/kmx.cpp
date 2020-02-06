21246a516dddf89b6ce8c03f84b21a187ec36201
Darcy <darcy_wong@sil.org>
2020-02-06 11:12:30 +0700

    fix(common): Add unit test for testing long context  (>= 64 chars)
    Reproduces issue #2575 with a keyboardprcoessor unit test.
    
    * Also fix build warning in `kmx_options.cpp` where  unsigned int `idx` is always `>= 0`

--- common/engine/keyboardprocessor/tests/unit/kmx/kmx.cpp
+++ common/engine/keyboardprocessor/tests/unit/kmx/kmx.cpp
@@ -317,9 +317,9 @@ int run_test(const km::kbp::path & source, const km::kbp::path & compiled) {
   try_status(km_kbp_context_items_to_utf16(citems, buf, &n));
   km_kbp_context_items_dispose(citems);
 
-  std::cout << "expected: " << expected << std::endl;
+  std::cout << "expected  : " << expected << std::endl;
   std::cout << "text store: " << text_store << std::endl;
-  std::cout << "result: " << buf << std::endl;
+  std::cout << "result    : " << buf << std::endl;
 
   // Compare internal context with expected result
   if (buf != expected) return __LINE__;
