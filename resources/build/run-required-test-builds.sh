a1a70f7b72a6f4e77a408add0c03ced4c6b98840
Marc Durdin <marc@durdin.net>
2020-02-10 13:20:34 +1100

    fix(ci): don't include build-utils.sh
    It depends on 14.0 alpha version infrastructure; we
    shouldn't need it for triggering the test builds for
    beta.

--- resources/build/run-required-test-builds.sh
+++ resources/build/run-required-test-builds.sh
@@ -24,7 +24,11 @@ fi
 ## START STANDARD BUILD SCRIPT INCLUDE
 # adjust relative paths as necessary
 THIS_SCRIPT="$(greadlink -f "${BASH_SOURCE[0]}" 2>/dev/null || readlink -f "${BASH_SOURCE[0]}")"
-. "$(dirname "$THIS_SCRIPT")/build-utils.sh"
+
+# THIS DOESN'T WORK FOR 13.0 BETA
+# but we don't need it...
+# . "$(dirname "$THIS_SCRIPT")/build-utils.sh"
+
 ## END STANDARD BUILD SCRIPT INCLUDE
 
 . "$(dirname "$THIS_SCRIPT")/trigger-definitions.sh"
