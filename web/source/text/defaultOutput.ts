4e50387056eae54a7b4208b0323d7b7d165dedb0
jahorton <jahorton@sil.org>
2020-03-18 20:44:52 +0700

    refactor(web/engine): DefaultOutput.forUnicodeKeynames

--- web/source/text/defaultOutput.ts
+++ web/source/text/defaultOutput.ts
@@ -123,5 +123,30 @@ namespace com.keyman.text {
         return '';
       }
     }
+
+    public static forUnicodeKeynames(Lkc: KeyEvent) {
+      let quiet = Lkc.Ltarg instanceof Mock;
+      let keyName = Lkc.kName;
+
+      // Test for fall back to U_xxxxxx key id
+      // For this first test, we ignore the keyCode and use the keyName
+      if(!keyName || keyName.substr(0,2) != 'U_') {
+        return '';
+      }
+    
+      var codePoint = parseInt(keyName.substr(2,6), 16);
+      if (((0x0 <= codePoint) && (codePoint <= 0x1F)) || ((0x80 <= codePoint) && (codePoint <= 0x9F))) {
+        // Code points [U_0000 - U_001F] and [U_0080 - U_009F] refer to Unicode C0 and C1 control codes.
+        // Check the codePoint number and do not allow output of these codes via U_xxxxxx shortcuts.
+        if(!quiet) {
+          console.log("Suppressing Unicode control code: U_00" + codePoint.toString(16));
+        }
+        return '';
+      } else {
+        // String.fromCharCode() is inadequate to handle the entire range of Unicode
+        // Someday after upgrading to ES2015, can use String.fromCodePoint()
+        return String.kmwFromCharCode(codePoint);
+      }
+    }
   }
 }
--- web/source/text/processor.ts
+++ web/source/text/processor.ts
@@ -41,13 +41,10 @@ namespace com.keyman.text {
      * @return  {string}
      */
     defaultKeyOutput(Lkc: KeyEvent, keyShiftState: number, usingOSK: boolean): string {
-      let keyName = Lkc.kName;
       let n = Lkc.Lcode;
       let outputTarget = Lkc.Ltarg;
 
       let keyman = com.keyman.singleton;
-      let domManager = keyman.domManager;
-      let activeKeyboard = keyman.keyboardManager.activeKeyboard;
 
       let quiet = outputTarget instanceof Mock;
 
@@ -87,20 +84,9 @@ namespace com.keyman.text {
 
       // Test for fall back to U_xxxxxx key id
       // For this first test, we ignore the keyCode and use the keyName
-      if(keyName && keyName.substr(0,2) == 'U_') {
-        var codePoint = parseInt(keyName.substr(2,6), 16);
-        if (((0x0 <= codePoint) && (codePoint <= 0x1F)) || ((0x80 <= codePoint) && (codePoint <= 0x9F))) {
-          // Code points [U_0000 - U_001F] and [U_0080 - U_009F] refer to Unicode C0 and C1 control codes.
-          // Check the codePoint number and do not allow output of these codes via U_xxxxxx shortcuts.
-          if(!quiet) {
-            console.log("Suppressing Unicode control code: U_00" + codePoint.toString(16));
-          }
-          return ch;
-        } else {
-          // String.fromCharCode() is inadequate to handle the entire range of Unicode
-          // Someday after upgrading to ES2015, can use String.fromCodePoint()
-          ch=String.kmwFromCharCode(codePoint);
-        }
+      let unicodeMatch = DefaultOutput.forUnicodeKeynames(Lkc);
+      if(unicodeMatch) {
+        return unicodeMatch;
         // Hereafter, we refer to keyCodes.
       } else if(checkCodes) { // keyShiftState can only be '1' or '2'.
         try {
