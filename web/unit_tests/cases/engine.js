44e3bb3364d8e18a805c8369f63bce99e3d38d85
jahorton <jahorton@sil.org>
2020-03-24 11:36:46 +0700

    test(web/engine): mildly white-box variable-store unit test

--- web/unit_tests/cases/engine.js
+++ web/unit_tests/cases/engine.js
@@ -860,6 +860,49 @@ describe('Engine', function() {
     // TODO:  add a 'resetContext' test!
   })
 
+  describe('Variable Stores', function() {
+    this.timeout(kmwconfig.timeouts.scriptLoad + kmwconfig.timeouts.standard);
+
+    beforeEach(function(done) {
+      loadKeyboardFromJSON("/keyboards/options_with_save.json", done, kmwconfig.timeouts.scriptLoad);
+    });
+
+    it('Backing up and restoring (loadStore/saveStore)', function(done) {
+      // Keyboard's default value is 0, corresponding to "no foo."
+      var keyboardID = "options_with_save";
+      var prefixedKeyboardID = "Keyboard_" + keyboardID;
+      var storeName = "foo";
+
+      keyman.setActiveKeyboard(keyboardID, 'en').then(function() {
+        // Alas, saveStore itself requires the keyboard to be active!
+        KeymanWeb.saveStore(storeName, 1);
+
+        // First, ensure that we get the same thing if we load the value immediately.
+        var value = KeymanWeb.loadStore(prefixedKeyboardID, storeName, 0);
+        assert.equal(value, 1, "loadStore did not see the value saved to initialize the test before resetting keyboard");
+
+        // Reload the keyboard so that we can test its loaded value.
+        keyman.removeKeyboards(keyboardID);
+
+        // Now we can reload the keyboard and run the test.
+        var remainderOfTest = function() {
+          // This requires proper storage to a cookie, as we'll be on a new instance of the same keyboard.
+          var value = KeymanWeb.loadStore(prefixedKeyboardID, storeName, 0);
+          assert.equal(value, 1, "Did not properly save and reload variable store setting");
+
+          KeymanWeb.saveStore(storeName, 0);
+          
+
+          done();
+        }
+
+        loadKeyboardFromJSON("/keyboards/options_with_save.json", function() {
+          keyman.setActiveKeyboard(keyboardID, 'en').then(remainderOfTest);
+        }, kmwconfig.timeouts.scriptLoad);
+      });
+    });
+  })
+
   // Performs basic processing system checks/tests to ensure the sequence testing
   // is based on correct assumptions about the code.
   describe('Simulation Checks', function() {
