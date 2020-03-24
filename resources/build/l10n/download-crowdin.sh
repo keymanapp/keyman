e003fad5610771a78252fd0746847d534d10db46
Darcy Wong <darcy_wong@sil.org>
2020-03-24 15:27:39 +0700

    feat(common): Separate common crowdin utilities

--- resources/build/l10n/download-crowdin.sh
+++ resources/build/l10n/download-crowdin.sh
@@ -38,33 +38,12 @@ display_usage() {
   exit 1
 }
 
+# Include common paths and utilities
+. ./crowdin-utils.sh
+
 # Include scripts to process platforms
 . ./download-crowdin-android.sh
 . ./download-crowdin-ios.sh
-
-
-# If a source file $1 exists, copy it to a destination directory $2.
-# If destination doesn't exist, create it
-# copy_file [source file] [destination directory]
-function copy_file() {
-  if [ -f "$1" ]; then
-    if [ ! -d "$2" ]; then
-      echo "Creating $2 directory"
-      mkdir -p "$2"
-    fi
-    echo "Copying $1"
-    cp "$1" "$2"
-  fi
-}
-
-
-
-# Path definitions
-KMA_ROOT="$KEYMAN_ROOT/android"
-KMI_ROOT="$KEYMAN_ROOT/ios"
-CROWDIN_TMP_ROOT="$KEYMAN_ROOT/.crowdin-tmp"
-CROWDIN_ROOT="$KEYMAN_ROOT/crowdin"
-KEYMAN_ZIP="$CROWDIN_TMP_ROOT/Keyman.zip"
 
 if [[ $# -eq 0 ]] ; then
   display_usage
