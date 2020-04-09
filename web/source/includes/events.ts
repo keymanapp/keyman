c22c13bdf1bc6c2fb854a9b0c74434e0a5310006
jahorton <jahorton@sil.org>
2020-04-09 13:50:33 +0700

    change(web/engine): Fixes up base linkage for EventEmitter

--- web/source/includes/events.ts
+++ web/source/includes/events.ts
@@ -2,3 +2,6 @@
 // Implements Node's EventEmitter class and related module components in a near
 // browser-compatible way.  (Just requires a blank 'module' object on the window.)
 ///<reference path="../../node_modules/events/events.js" />
+
+declare class EventEmitter {}  // Unfortunately, I can't get it to recognize type information properly
+                               // because we can't use require statements.
