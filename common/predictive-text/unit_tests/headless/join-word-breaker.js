fd121ab3b5bc6ef0297d42c86cc6cb14c5bf2e10
Eddie Antonio Santos <easantos@ualberta.ca>
2020-04-23 09:59:46 -0600

    test(common/lmlayer): basic join word breaker functionality

--- common/predictive-text/unit_tests/headless/join-word-breaker.js
+++ common/predictive-text/unit_tests/headless/join-word-breaker.js
@@ -8,4 +8,22 @@ describe('The join word breaker decorator', function () {
     let breakWords = decorateWithJoin(defaultWordBreaker, ['-']);
     assert.isFunction(breakWords);
   });
+
+  it('should join spans at the given delimiter', function () {
+    let phrase = 'khui-chhùi';
+    let breakWords = decorateWithJoin(defaultWordBreaker, ['-']);
+
+    let undecoratedResult = defaultWordBreaker(phrase).map(onlyText);
+    let actualResult = breakWords(phrase).map(onlyText);
+    assert.deepEqual(undecoratedResult, ["khui", "-", "chhùi"]);
+    assert.deepEqual(actualResult, ["khui-chhùi"]);
+  });
+
+  /**
+   * Get just the text from a span.
+   * @param {Span} span 
+   */
+  function onlyText(span) {
+    return span.text;
+  }
 });
