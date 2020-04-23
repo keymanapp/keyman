791d4797b0317741fee410a360fda055c5ab56ab
Eddie Antonio Santos <Eddie.Santos@nrc-cnrc.gc.ca>
2020-04-23 16:22:00 -0600

    docs(common/lmlayer): correct Keyman version
    Co-Authored-By: Marc Durdin <marc@durdin.net>

--- developer/js/tests/fixtures/example.qaa.wordbreaker/example.qaa.wordbreaker.model.ts
+++ developer/js/tests/fixtures/example.qaa.wordbreaker/example.qaa.wordbreaker.model.ts
@@ -1,7 +1,7 @@
 const source: LexicalModelSource = {
   format: 'trie-1.0',
   sources: ['wordlist.tsv'],
-  /* Keyman 13.0+ word breaker specification: */
+  /* Keyman 14.0+ word breaker specification: */
   wordBreaker: {
     use: 'default'
   }
