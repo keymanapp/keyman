7415f24925fd253a91143921211ae30463b9c041
jahorton <jahorton@sil.org>
2020-04-29 12:56:31 +0700

    change(web): language-modeling-layer -> lexical-model-layer

--- web/source/includes/node-lmlayer.ts
+++ web/source/includes/node-lmlayer.ts
@@ -3,9 +3,9 @@
  */
 
 // Defines the main interface of the Language Modeling Layer (LMLayer) and its original typing information.
-///<reference path="../../node_modules/@keymanapp/language-modeling-layer/embedded_worker.d.ts" />
-///<reference path="../../node_modules/@keymanapp/language-modeling-layer/node-defaultWorker.ts" />
-///<reference path="../../node_modules/@keymanapp/language-modeling-layer/index.ts" />
+///<reference path="../../node_modules/@keymanapp/lexical-model-layer/embedded_worker.d.ts" />
+///<reference path="../../node_modules/@keymanapp/lexical-model-layer/node-defaultWorker.ts" />
+///<reference path="../../node_modules/@keymanapp/lexical-model-layer/index.ts" />
 
 // We DO need the embedded_worker.d.ts file - since we're directly linking into the LMLayer's code,
 // we need the typedef for the embedded worker to be linked.
