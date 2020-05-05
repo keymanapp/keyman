849cda09bb3d01ce232369d702455dbe7d6820c6
jahorton <jahorton@sil.org>
2020-05-05 14:47:32 +0700

    feat(web): starts base-level sentry linkage/testing setup

---
+++ common/core/web/tools/sentry-manager/src/index.ts
@@ -0,0 +1,69 @@
+///<reference path="../node_modules/@sentry/browser/build/bundle.min.js" />
+
+declare var com;
+
+(function() {
+  var ALIASABLE_FILES = {
+    'keymanweb.js':    'keymanweb.js',
+    'kmwuibutton.js':  'kmwuibutton.js',
+    'kmwuifloat.js':   'kmwuifloat.js',
+    'kmwuitoggle.js':  'kmwuitoggle.js',
+    'kmwuitoolbar.js': 'kmwuitoolbar.js'
+    // Also add entries for the naming system used by Android and iOS - and map them to the EMBEDDED upload, not the std 'native' one.
+  }
+
+  // If we've recognized one of our source files, 
+  function aliasFilename(filename: string) {
+    if(!ALIASABLE_FILES[filename]) {
+      return null;
+    }
+
+    filename = ALIASABLE_FILES[filename];
+
+    switch(location.protocol) {
+      case 'http:':
+        return 'http://' + location.host + '/' + filename;
+      case 'file:':
+        return 'file:///' + filename;
+      default:
+        return null;
+    }
+  }
+
+  // Filters all expected but unnecessary path prefixes, affixes, and suffixes reported by Sentry from our products.
+  // This allows us to mask all different sorts of installations with a single uploaded path.
+  function pathFilter(event: any) {
+    // Get the underlying JS error.
+    let exception = event.exception;
+
+    // Iterate through all wrapped exceptions.
+    for(let e of exception.values) {
+      for(let frame of e.stacktrace.frames) {
+        let URL = frame.filename as string;
+        let filename: string = '';
+        try {
+          filename = URL.substr(URL.lastIndexOf('/')+1);
+          if(ALIASABLE_FILES[filename]) {
+            // Make the file look like it's at the host's root so that the Sentry server will recognize it
+            // regardless of actual filepath position.
+            frame.filename = aliasFilename(filename) || frame.filename;
+          }
+        } catch {
+          // not sure what to do yet, but... don't make more errors in the error handler.  That's a bad idea.
+        }
+      }
+    }
+
+    return event;
+  }
+
+  // Do the actual Sentry initialization.
+  //@ts-ignore
+  Sentry.init({
+    beforeSend: pathFilter,
+    // FIXME:  DO NOT LEAVE IN PRODUCTION!
+    debug: true,
+    dsn: 'https://cf96f32d107c4286ab2fd82af49c4d3b@sentry.keyman.com/11', // keyman-web DSN
+    release: com.keyman.environment.SENTRY_RELEASE
+  });
+})()
