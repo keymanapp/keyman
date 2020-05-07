56193c2a83ce51028c1037ddbba8fdfcfd0549dd
jahorton <jahorton@sil.org>
2020-05-07 10:37:14 +0700

    feat(common): start of package-utils resource script

---
+++ resources/package-utils.sh
@@ -0,0 +1,85 @@
+#!/bin/bash
+
+display_usage ( ) {
+  echo ""
+  echo "package-utils.sh link-stash | link-unstash"
+  echo
+  echo "  clean-external-deps  to erase the node_modules folder without erasing links."
+  echo "                       Internally uses both link-stash and link-unstash"
+  echo ""
+  echo "  link-stash           to 'stash' the package's local links in a temporary"
+  echo "                       directory."
+  echo ""
+  echo "  link-unstash         reverses 'link-stash', restoring the links to their"
+  echo "                       original location and removing the temp directory."
+  echo "" 
+  exit 1
+}
+
+PACKAGE_SOURCE='node_modules'
+PACKAGE_STASH='__node_modules'
+
+# Stores all @keymanapp package dependency/dev-dependency names to the 'packages' array.
+get_local_dependencies() {
+  localDeps=`cat package.json | jq -r '.dependencies, .devDependencies | to_entries | map(select(.key | match("@keymanapp/";"i"))) | map(.key) | .[]'`
+
+  # Sadly, we can't rely on the bash script `readlines` command - not all shells  (eg: macOS) support it!
+  # This will have the same net effect.
+  IFS=$'\n'
+  packages=(${localDeps}); # Convert the output $localDeps string into a script-friendly array.
+  unset IFS
+}
+
+stash_package_links() {
+  get_local_dependencies
+
+  mkdir -p "$PACKAGE_STASH/@keymanapp"
+  for dep in "${packages[@]}"; do
+    if [ "${QUIET:-false}" = false ]; then
+      echo "Stashing link-based dependency: $dep"
+    fi
+    mv "$PACKAGE_SOURCE/$dep" "$PACKAGE_STASH/@keymanapp"
+  done
+}
+
+unstash_package_links() {
+  get_local_dependencies
+
+  mkdir -p "$PACKAGE_SOURCE/@keymanapp"
+  for dep in "${packages[@]}"; do
+    if [ "${QUIET:-false}" = false ]; then
+      echo "Restoring link-based dependency: $dep"
+    fi
+    mv "$PACKAGE_STASH/$dep" "$PACKAGE_SOURCE/@keymanapp"
+  done
+
+  rm -r "$PACKAGE_STASH"
+}
+
+# Parse args
+# If no argument was provided, print help.
+if [ $# -eq 0 ]; then
+  display_usage
+else
+  case $1 in
+    link-stash)
+      QUIET=false
+      stash_package_links
+      ;;
+    link-unstash)
+      QUIET=false
+      unstash_package_links
+      ;;
+    clean-external-deps)
+      QUIET=true
+
+      stash_package_links
+      rm -rf node_modules
+      unstash_package_links
+      ;;
+    # Default case - display help
+    *)
+      display_usage
+      ;;
+  esac
+fi
