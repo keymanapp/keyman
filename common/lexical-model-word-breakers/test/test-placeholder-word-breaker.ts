6aabd10c7090e6163f8676faf45550dfdccad02a
Eddie Antonio Santos <easantos@ualberta.ca>
2020-05-13 10:37:39 -0600

    test(common/lmlayer): add test for the placeholder

---
+++ common/lexical-model-word-breakers/test/test-placeholder-word-breaker.ts
@@ -0,0 +1,13 @@
+/// <reference types="@keymanapp/lexical-model-types" />
+
+import {assert} from 'chai';
+import {placeholder as breakWords} from '../'
+
+
+describe('The placeholder word breaker', function () {
+  it('should break simple English sentences', function () {
+    let breaks = breakWords('Look! -- The quick brown fox jumps... over the lazy dog!');
+    let words = breaks.map(span => span.text);
+    assert.deepEqual(words, ['Look', 'The', 'quick', 'brown', 'fox', 'jumps', 'over', 'the', 'lazy', 'dog']);
+  });
+});
