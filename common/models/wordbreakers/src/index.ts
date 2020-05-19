e98ae7d7a185a1bc283778fb7aa415e82fcb0b14
Eddie Antonio Santos <easantos@ualberta.ca>
2020-05-19 14:57:55 -0600

    fix(common/lmlayer): add @types/node to @keymanapp/models-wordbreaker to prevent failure

--- common/models/wordbreakers/src/index.ts
+++ common/models/wordbreakers/src/index.ts
@@ -1,7 +1,6 @@
 /// <reference path="./ascii.ts" />
 
 // Let the word breakers be available both in the browser and in Node.
-declare var module: unknown;
 if (typeof module !== "undefined" && (module as any).exports) {
   (module as any).exports['wordBreakers'] = wordBreakers;
 }
