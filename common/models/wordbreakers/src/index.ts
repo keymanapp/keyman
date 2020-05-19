7b1f86b39943d71dccd0ba4b8bcd18f98b04dcca
Eddie Antonio Santos <easantos@ualberta.ca>
2020-05-19 14:46:33 -0600

    fix(common/lmlayer): export wordBreakers namespace

--- common/models/wordbreakers/src/index.ts
+++ common/models/wordbreakers/src/index.ts
@@ -1,1 +1,7 @@
 /// <reference path="./ascii.ts" />
+
+// Let the word breakers be available both in the browser and in Node.
+declare var module: unknown;
+if (typeof module !== "undefined" && (module as any).exports) {
+  (module as any).exports['wordBreakers'] = wordBreakers;
+}
