374f0dfd8eb275eefd8fd23ded66ff047d645b4f
Eddie Antonio Santos <easantos@ualberta.ca>
2020-05-19 14:34:11 -0600

    Revert "refactor(common/lmlayer): move placeholder word breaker to @keymanapp/lexical-model-word-breakers"
    This reverts commit 73b16ae33eb62f08247fbcb1afe0fca032f3df97.

--- common/models/wordbreakers/src/index.ts
+++ common/models/wordbreakers/src/index.ts
@@ -1,6 +1,3 @@
 import {ascii} from "./ascii";
-import {placeholder} from './placeholder';
-import {default_} from "./default";
 
-export {ascii, placeholder};
-export {default_ as default};
+export {ascii};
--- common/models/wordbreakers/test/test-placeholder-word-breaker.ts
+++ common/models/wordbreakers/test/test-placeholder-word-breaker.ts
@@ -1,11 +1,11 @@
 import {assert} from 'chai';
-import {placeholder as breakWords} from '../src';
+import {placeholder as breakWords} from '../'
 
 
 describe.skip('The placeholder word breaker', function () {
   it('should break simple English sentences', function () {
     let breaks = breakWords('Look! -- The quick brown fox jumps... over the lazy dog!');
     let words = breaks.map(span => span.text);
-    assert.deepEqual(words, ['Look!', '--', 'The', 'quick', 'brown', 'fox', 'jumps...', 'over', 'the', 'lazy', 'dog!']);
+    assert.deepEqual(words, ['Look', 'The', 'quick', 'brown', 'fox', 'jumps', 'over', 'the', 'lazy', 'dog']);
   });
 });
