44ce9e8afcbba4f709e9fee9e17ed0204f1ff9dc
Eddie Antonio Santos <easantos@ualberta.ca>
2020-05-19 15:26:16 -0600

    test(common/lmlayer): let file only contain Trie/default WB integration tests

--- common/predictive-text/unit_tests/headless/default-word-breaker.js
+++ common/predictive-text/unit_tests/headless/default-word-breaker.js
@@ -1,5 +1,5 @@
 /**
- * Smoke-test the default 
+ * Smoke-test the default word breaker.
  */
 var assert = require('chai').assert;
 var TrieModel = require('../../build/intermediate').models.TrieModel;
@@ -7,29 +7,19 @@ var TrieModel = require('../../build/intermediate').models.TrieModel;
 var breakWords = require('../../build/intermediate').wordBreakers['default'];
 const SHY = '\u00AD';
 
+// The following are **integration tests** testing the interaction between the
+// Trie model "template" and the default word breaker.
+//
+// They exercise whether the default word breaker produces the correct word
+// breaks, and whether the Trie model can consume the word breaks properly.
 describe('The default word breaker', function () {
-  it('should break multilingual text', function () {
-    let breaks = breakWords(
-      `Добрый день! ᑕᐻ᙮ — after working on ka${SHY}wen${SHY}non:${SHY}nis,
-       let's eat phở! 🥣`
-    );
-    let words = breaks.map(span => span.text);
-    assert.deepEqual(words, [
-      'Добрый', 'день', '!', 'ᑕᐻ', '᙮', '—', 'after',
-      'working', 'on', `ka${SHY}wen${SHY}non:${SHY}nis`, ',',
-      "let's", 'eat', 'phở', '!', '🥣'
-    ]);
-  });
-
-  // The following tests are performed with model integration as an internal
-  // test for the wordbreaking API.
   it('recognizes a word at end of complete lefthand context', function () {
     var model = new TrieModel(jsonFixture('tries/english-1000'), {
       wordBreaker: breakWords // wordBreakers['default'] when fully integrated.
     });
 
     // Standard case - wordbreaking at the end of a word.
-    var context = { 
+    var context = {
       left: 'The quick brown fox jumped', startOfBuffer: true,
       right: ' over the lazy dog.', endOfBuffer: true
     };
@@ -47,7 +37,7 @@ describe('The default word breaker', function () {
     });
 
     // Standard case - wordbreaking at the end of a word.
-    var context = { 
+    var context = {
       left: 'The quick brown fox jumped', startOfBuffer: false,
       right: ' over the lazy dog.', endOfBuffer: false
     };
@@ -63,7 +53,7 @@ describe('The default word breaker', function () {
     });
 
     // Standard case - midword (xylophone) call
-    var context = { 
+    var context = {
       left: 'xyl', startOfBuffer: true,
       right: '', endOfBuffer: true
     };
@@ -72,14 +62,14 @@ describe('The default word breaker', function () {
 
     assert.strictEqual(broken, 'xyl');
   });
-    
+
   it('returns empty string when called without word text', function() {
     var model = new TrieModel(jsonFixture('tries/english-1000'), {
       wordBreaker: breakWords
     });
 
     // Wordbreaking on a empty space => no word.
-    context = { 
+    context = {
       left: 'The quick brown fox jumped ', startOfBuffer: true,
       right: 'over the lazy dog.', endOfBuffer: true
     };
@@ -95,7 +85,7 @@ describe('The default word breaker', function () {
     });
 
     // Wordbreaking on a empty space => no word.
-    context = { 
+    context = {
       left: '', startOfBuffer: true,
       right: '', endOfBuffer: true
     };
@@ -111,7 +101,7 @@ describe('The default word breaker', function () {
     });
 
     // Wordbreaking on a empty space => no word.
-    context = { 
+    context = {
       left: '', startOfBuffer: false,
       right: '', endOfBuffer: false
     };
@@ -128,13 +118,13 @@ describe('The default word breaker', function () {
 
     // A limitation of the current implementation; we should fix this before release.
     // Then again, when typing this is probably fine; just not when not typing.
-    context = { 
+    context = {
       left: 'The quick brown fox jum', startOfBuffer: true,
       right: 'ped over the lazy dog.', endOfBuffer: true
     };
 
     broken = model.wordbreak(context);
 
-    assert.strictEqual(broken, 'jumped');  // Current result:  'jum'.    
+    assert.strictEqual(broken, 'jumped');  // Current result:  'jum'.
   });
 });
