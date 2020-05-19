a4dae628178dae0e28c5e6b7cf500523d58b5422
Eddie Antonio Santos <easantos@ualberta.ca>
2020-05-19 15:07:06 -0600

    refactor(common/lmlayer): move default word breaker to models-wordbreakers package

--- common/models/wordbreakers/src/index.ts
+++ common/models/wordbreakers/src/index.ts
@@ -1,4 +1,5 @@
 /// <reference path="./ascii.ts" />
+/// <reference path="./default/index.ts" />
 /// <reference path="./placeholder.ts" />
 
 // Let the word breakers be available both in the browser and in Node.
