af343138cfbb1c679e631684c36f8e4353720dda
Eddie Antonio Santos <easantos@ualberta.ca>
2020-05-19 15:00:43 -0600

    test(common/lmlayer): placeholder word breaker

--- common/models/wordbreakers/test/test-placeholder-word-breaker.js
+++ common/models/wordbreakers/test/test-placeholder-word-breaker.js
@@ -1,8 +1,8 @@
-import {assert} from 'chai';
-import {placeholder as breakWords} from '../'
+const assert = require('chai').assert;
+const breakWords = require('..').wordBreakers['placeholder'];
 
 
-describe.skip('The placeholder word breaker', function () {
+describe('The placeholder word breaker', function () {
   it('should break simple English sentences', function () {
     let breaks = breakWords('Look! -- The quick brown fox jumps... over the lazy dog!');
     let words = breaks.map(span => span.text);
