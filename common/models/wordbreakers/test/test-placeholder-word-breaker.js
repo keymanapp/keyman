6af00a3358a0e2daf8b1b3aed5ce48e62c7610fd
Eddie Antonio Santos <easantos@ualberta.ca>
2020-05-19 15:03:43 -0600

    test(common/lmlayer): correct expected output of placeholder word breaker

--- common/models/wordbreakers/test/test-placeholder-word-breaker.js
+++ common/models/wordbreakers/test/test-placeholder-word-breaker.js
@@ -6,6 +6,6 @@ describe('The placeholder word breaker', function () {
   it('should break simple English sentences', function () {
     let breaks = breakWords('Look! -- The quick brown fox jumps... over the lazy dog!');
     let words = breaks.map(span => span.text);
-    assert.deepEqual(words, ['Look', 'The', 'quick', 'brown', 'fox', 'jumps', 'over', 'the', 'lazy', 'dog']);
+    assert.deepEqual(words, ['Look!', '--', 'The', 'quick', 'brown', 'fox', 'jumps...', 'over', 'the', 'lazy', 'dog!']);
   });
 });
