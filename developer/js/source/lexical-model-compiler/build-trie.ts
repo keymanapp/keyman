0740af1a10293566adbf15ba2e222e1f88cb2631
jahorton <jahorton@sil.org>
2020-07-13 11:46:15 +0700

    docs(developer): tweaks WordList type documentation

--- developer/js/source/lexical-model-compiler/build-trie.ts
+++ developer/js/source/lexical-model-compiler/build-trie.ts
@@ -1,8 +1,11 @@
 import { readFileSync } from "fs";
 
 /**
- * A word list is an array of pairs: the concrete word form itself, followed by
- * a non-negative count.
+ * A word list is (conceptually) an array of pairs: the concrete word form itself + a
+ * non-negative count.
+ * 
+ * Since each word should only appear once within the list, we represent it with
+ * an associative array pattern keyed by the wordform.
  */
 export type WordList = {[wordform: string]: number};
 
