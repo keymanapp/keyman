d65ecd1035988ff7cb38f37cc2eda4e78e7ea92c
jahorton <jahorton@sil.org>
2020-07-21 09:13:04 +0700

    refactor(ios/engine): useLocal support, other Keyman sites

--- ios/engine/KMEI/KeymanEngine/Classes/KeymanHosts.swift
+++ ios/engine/KMEI/KeymanEngine/Classes/KeymanHosts.swift
@@ -7,9 +7,73 @@
 
 import Foundation
 
+/**
+ * Defines constant-like definitions for Keyman's websites dependent upon the compile-time version
+ * of the KeymanEngine framework.
+ *
+ * Note that as a result, 'alpha', 'beta', and 'stable' versions of KeymanEngine may point to different
+ * sites.
+ */
+public enum KeymanHosts {
+  /**
+   * Used to enable '.local' variants of the endpoints for use in local development testing.
+   */
+  internal static let useLocal = false
 
-enum KeymanHosts {
-  static var API_KEYMAN_COM: URL {
-    return URL.init(string: "https://api.keyman.com")!
+  // This should never be used outside of the corresponding `static var`...
+  // save for use in automated testing.
+  internal static func getApiSiteURL(forTier: Version.Tier, useLocal: Bool) -> URL {
+    if useLocal {
+      return URL.init(string: "http://api.keyman.com.local")!
+    } else {
+      // TODO:  Once the staging-site changes are in place,
+      //        add switch-case here to select appropriate URL.
+      return URL.init(string: "https://api.keyman.com")!
+    }
+  }
+
+  /**
+   * Used for package-version and model queries.
+   */
+  public static var API_KEYMAN_COM: URL {
+    return getApiSiteURL(forTier: Version.currentTagged.tier ?? .stable, useLocal: useLocal)
+  }
+
+  // This should never be used outside of the corresponding `static var`...
+  // save for use in automated testing.
+  internal static func getHelpSiteURL(forTier: Version.Tier, useLocal: Bool) -> URL {
+    if useLocal {
+      return URL.init(string: "http://help.keyman.com.local")!
+    } else {
+      // TODO:  Once the staging-site changes are in place,
+      //        add switch-case here to select appropriate URL.
+      return URL.init(string: "https://help.keyman.com")!
+    }
+  }
+
+  /**
+   * Used for online help.
+   */
+  public static var HELP_KEYMAN_COM: URL {
+    return getHelpSiteURL(forTier: Version.currentTagged.tier ?? .stable, useLocal: useLocal)
+  }
+
+  // This should never be used outside of the corresponding `static var`...
+  // save for use in automated testing.
+  internal static func getMainSiteURL(forTier: Version.Tier, useLocal: Bool) -> URL {
+    if useLocal {
+      return URL.init(string: "http://keyman.com.local")!
+    } else {
+      // TODO:  Once the staging-site changes are in place,
+      //        add switch-case here to select appropriate URL.
+      return URL.init(string: "https://keyman.com")!
+    }
+  }
+
+  /**
+   * Used for keyboard searches and resource sharing links.
+   */
+  public static var KEYMAN_COM: URL {
+    return getMainSiteURL(forTier: Version.currentTagged.tier ?? .stable, useLocal: useLocal)
   }
 }
