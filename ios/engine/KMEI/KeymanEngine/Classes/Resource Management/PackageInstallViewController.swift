b1c54cacde19b1d367547b8ed0e8bfae0d953e6c
jahorton <jahorton@sil.org>
2020-07-27 16:09:56 +0700

    feat(ios): starts XIB integration

--- ios/engine/KMEI/KeymanEngine/Classes/Resource Management/PackageInstallViewController.swift
+++ ios/engine/KMEI/KeymanEngine/Classes/Resource Management/PackageInstallViewController.swift
@@ -12,6 +12,11 @@ import WebKit
 public class PackageInstallViewController<Resource: LanguageResource>: UIViewController {
   public typealias CompletionHandler = ([Resource.FullID]?) -> Void
 
+  // Needed to support iOS 9 + 10.
+  @IBOutlet weak var webViewContainer: UIView!
+  @IBOutlet weak var lblVersion: UILabel!
+  @IBOutlet weak var lblAuthor: UILabel!
+
   let package: Resource.Package
   var wkWebView: WKWebView?
   let completionHandler: CompletionHandler
@@ -21,7 +26,8 @@ public class PackageInstallViewController<Resource: LanguageResource>: UIViewCon
     self.package = package
     self.completionHandler = completionHandler
     self.isCustom = isCustom
-    super.init(nibName: nil, bundle: nil)
+
+    super.init(nibName: "PackageInstallView", bundle: Bundle.init(for: PackageInstallViewController.self))
 
     _ = view
   }
@@ -31,12 +37,20 @@ public class PackageInstallViewController<Resource: LanguageResource>: UIViewCon
   }
 
   override public func loadView() {
-    wkWebView = WKWebView.init(frame: .zero)
-    wkWebView!.backgroundColor = .white
-    view = wkWebView!
+    super.loadView()
 
-    // Ensure the web view fills its available space.
-    wkWebView?.autoresizingMask = [.flexibleWidth, .flexibleHeight]
+    wkWebView = WKWebView.init(frame: webViewContainer.frame)
+    wkWebView!.backgroundColor = .white
+    wkWebView!.translatesAutoresizingMaskIntoConstraints = false
+    webViewContainer.addSubview(wkWebView!)
+
+//    // Ensure the web view fills its available space.
+//    wkWebView?.autoresizingMask = [.flexibleWidth, .flexibleHeight]
+    wkWebView!.topAnchor.constraint(equalTo: webViewContainer.topAnchor).isActive = true
+    wkWebView!.leadingAnchor.constraint(equalTo: webViewContainer.leadingAnchor).isActive = true
+
+    wkWebView!.bottomAnchor.constraint(equalTo: webViewContainer.bottomAnchor).isActive = true
+    wkWebView!.trailingAnchor.constraint(equalTo: webViewContainer.trailingAnchor).isActive = true
 
     let cancelBtn = UIBarButtonItem(title: "Cancel", style: .plain,
                                     target: self,
@@ -48,6 +62,13 @@ public class PackageInstallViewController<Resource: LanguageResource>: UIViewCon
     navigationItem.leftBarButtonItem = cancelBtn
     navigationItem.rightBarButtonItem = installBtn
     navigationItem.title = package.name
+
+    lblVersion.text = "Version: \(package.version)"
+    if let author = package.metadata.info?.author?.description {
+      lblAuthor.text = "Author: \(author)"
+    } else {
+      lblAuthor.isHidden = true
+    }
   }
 
   override public func viewWillAppear(_ animated: Bool) {
