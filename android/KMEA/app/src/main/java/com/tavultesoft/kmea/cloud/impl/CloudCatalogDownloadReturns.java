396df9673d321d1bed26a1688e486d2cc6204a6d
Darcy Wong <darcy_wong@sil.org>
2020-11-02 14:48:22 +0700

    chore(android/engine): Remove callbacks for lexical model catalog

--- android/KMEA/app/src/main/java/com/tavultesoft/kmea/cloud/impl/CloudCatalogDownloadReturns.java
+++ android/KMEA/app/src/main/java/com/tavultesoft/kmea/cloud/impl/CloudCatalogDownloadReturns.java
@@ -8,23 +8,18 @@ import org.json.JSONObject;
 import java.util.List;
 
 /**
- * Result type for catalogue download.
+ * Result type for catalogue download. Only used for package updates.
  */
 public class CloudCatalogDownloadReturns {
-  public JSONArray lexicalModelJSON;
   public JSONObject packagesJSON;
 
   // Used by the CloudCatalogDownloadTask, as it fits well with doInBackground's param structure.
   public CloudCatalogDownloadReturns(List<CloudApiTypes.CloudApiReturns> returns) {
-    JSONArray lex = null;
     JSONObject pkg = null;
 
     //TODO: Seems to be wrong because only the last result for each type will be processed
     for(CloudApiTypes.CloudApiReturns ret: returns) {
       switch(ret.target) {
-        case LexicalModels:
-          lex = ret.jsonArray;
-          break;
         case PackageVersion:
           pkg = ret.jsonObject;
           break;
@@ -32,19 +27,16 @@ public class CloudCatalogDownloadReturns {
     }
 
     // Errors are thrown if we try to do this assignment within the loop.
-    this.lexicalModelJSON = lex;
     this.packagesJSON = pkg;
   }
 
   public CloudCatalogDownloadReturns(JSONObject keyboardJSON, JSONArray lexicalModelJSON, JSONObject packagesJSON) {
-    this.lexicalModelJSON = lexicalModelJSON;
     this.packagesJSON = packagesJSON;
   }
 
   public boolean isEmpty() {
-    boolean emptyLex = lexicalModelJSON == null || lexicalModelJSON.length() == 0;
     boolean emptyPkg = packagesJSON == null || packagesJSON.length() == 0;
 
-    return emptyLex && emptyPkg;
+    return emptyPkg;
   }
 }
