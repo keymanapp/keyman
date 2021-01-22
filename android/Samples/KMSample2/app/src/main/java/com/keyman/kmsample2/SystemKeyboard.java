3465e99271be44eabcf08db75d401f49ca38cf99
Darcy Wong <darcy_wong@sil.org>
2021-01-22 09:40:38 +0700

    chore(android/samples): Remove old sample keyboard loaded code

--- android/Samples/KMSample2/app/src/main/java/com/keyman/kmsample2/SystemKeyboard.java
+++ android/Samples/KMSample2/app/src/main/java/com/keyman/kmsample2/SystemKeyboard.java
@@ -193,9 +193,6 @@ public class SystemKeyboard extends InputMethodService implements OnKeyboardEven
   @Override
   public void onKeyboardLoaded(KeyboardType keyboardType) {
     // Handle Keyman keyboard loaded event here if needed
-    // We can set our custom keyboard here
-    int kbIndex = KMManager.getKeyboardIndex(this, "tamil99m", "ta");
-    KMManager.setKeyboard(this, kbIndex);
   }
 
   @Override
