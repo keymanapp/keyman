78ecbea855f2b55198be8955fe152d92d8726a5c
Eberhard Beilharz <ermshiperete@users.noreply.github.com>
2021-03-04 20:33:48 +0100

    Update linux/scripts/reconf.sh
    Co-authored-by: Marc Durdin <marc@durdin.net>

--- linux/scripts/reconf.sh
+++ linux/scripts/reconf.sh
@@ -60,10 +60,14 @@ for proj in ${extra_projects}; do
         cd keyman-config
         make clean
         cd keyman_config
-        sed -e "s/_VERSION_/${VERSION}/g" -e "s/_VERSIONWITHTAG_/${VERSION_WITH_TAG}/g" \
+        sed \
+            -e "s/_VERSION_/${VERSION}/g" \
+            -e "s/_VERSIONWITHTAG_/${VERSION_WITH_TAG}/g" \
             -e "s/_MAJORVERSION_/${VERSION_MAJOR}/g" \
-            -e "s/_RELEASEVERSION_/${VERSION_RELEASE}/g" -e "s/_TIER_/${TIER}/g" \
-            -e "s/_ENVIRONMENT_/${VERSION_ENVIRONMENT}/g" version.py.in > version.py
+            -e "s/_RELEASEVERSION_/${VERSION_RELEASE}/g" \
+            -e "s/_TIER_/${TIER}/g" \
+            -e "s/_ENVIRONMENT_/${VERSION_ENVIRONMENT}/g" \
+            version.py.in > version.py
 
         cd ../buildtools && python3 ./build-langtags.py
     fi
