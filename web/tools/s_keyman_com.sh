63ff7d403900e8388c752296439d3f292ef265e4
Darcy Wong <darcy_wong@sil.org>
2021-05-28 09:55:19 +0700

    chore(web): Move script to tools/

--- web/tools/s_keyman_com.sh
+++ web/tools/s_keyman_com.sh
@@ -10,7 +10,7 @@ set -eu
 ## START STANDARD BUILD SCRIPT INCLUDE
 # adjust relative paths as necessary
 THIS_SCRIPT="$(greadlink -f "${BASH_SOURCE[0]}" 2>/dev/null || readlink -f "${BASH_SOURCE[0]}")"
-. "$(dirname "$THIS_SCRIPT")/../resources/build/build-utils.sh"
+. "$(dirname "$THIS_SCRIPT")/../../resources/build/build-utils.sh"
 . "$KEYMAN_ROOT/resources/shellHelperFunctions.sh"
 ## END STANDARD BUILD SCRIPT INCLUDE
 
@@ -110,7 +110,7 @@ function remap_sourcemap_paths {
 
 function commit_and_push {
   echo "Committing and pushing KeymanWeb"
-  cd "$KEYMAN_ROOT/web/"
+  cd "$KEYMAN_ROOT/web/tools"
   pushd "$S_KEYMAN_COM"
 
   if [ ! -z "${TEAMCITY_VERSION-}" ]; then
