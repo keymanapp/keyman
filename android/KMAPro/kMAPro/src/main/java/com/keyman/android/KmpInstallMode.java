090556846fb8b2b139694c39ba3a944fc8c1f8f5
Marc Durdin <marc@durdin.net>
2021-06-09 10:47:00 +1000

    chore(android): add docs on KmpInstallMode

--- android/KMAPro/kMAPro/src/main/java/com/keyman/android/KmpInstallMode.java
+++ android/KMAPro/kMAPro/src/main/java/com/keyman/android/KmpInstallMode.java
@@ -1,7 +1,8 @@
 package com.keyman.android;
 
 public enum KmpInstallMode {
-  Silent,
-  WelcomeOnly,
-  Full
+  Silent,       // Show no UI while installing a package. Errors may be shown as toasts
+  WelcomeOnly,  // Show only the welcome screen after installing a package; language will
+                // be automatically selected if not provided as an input parameter
+  Full          // Show readme, language picker (if applicable), and welcome after install.
 }
