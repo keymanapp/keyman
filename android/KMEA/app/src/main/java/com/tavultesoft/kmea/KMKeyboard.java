c92d3970960278f5315b5fc606a13353e571da02
Marc Durdin <marc@durdin.net>
2021-08-02 08:56:45 +1000

    fix(android): avoid error with empty font data
    Fixes #5509.
    
    If a keyboard has empty font data string, then we should avoid
    attempting to parse it.

--- android/KMEA/app/src/main/java/com/tavultesoft/kmea/KMKeyboard.java
+++ android/KMEA/app/src/main/java/com/tavultesoft/kmea/KMKeyboard.java
@@ -1048,7 +1048,7 @@ final class KMKeyboard extends WebView {
    */
   private JSONObject makeFontPaths(String font) {
 
-    if(font == null) {
+    if(font == null || font.equals("")) {
       return null;
     }
 
