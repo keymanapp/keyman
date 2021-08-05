956293ad878ccbed3970b5371381ec47e55a8692
jahorton <jahorton@sil.org>
2021-08-05 14:03:06 +0700

    feat(ios): Keyman app - simulator build artifact

--- ios/kmbuild.sh
+++ ios/kmbuild.sh
@@ -24,7 +24,7 @@ cd "$(dirname "$THIS_SCRIPT")"
 verify_on_mac
 
 display_usage ( ) {
-    echo "build.sh [-clean] [-no-kmw] [-only-framework] [-no-codesign] [-no-archive] [-no-build] [-upload-sentry]"
+    echo "build.sh [-clean] [-no-kmw] [-only-framework] [-no-codesign] [-no-archive] [-all-artifacts] [-no-build] [-upload-sentry]"
     echo
     echo "  -clean                  Removes all previously-existing build products for KMEI and the Keyman app before building."
     echo "  -no-kmw                 Uses existing keyman.js, doesn't try to build"
@@ -37,6 +37,7 @@ display_usage ( ) {
     echo "  -upload-sentry          Uploads debug symbols, etc, to Sentry"
     echo "  -debug                  Sets the configuration to debug mode instead of release."
     echo "  -download-resources     Download up-to-date versions of the engine's default resources from downloads.keyman.com."
+    echo "  -all-artifacts          Produces all build artifacts, including a Simulator-installable copy of the .app"
 exit 1
 }
 
@@ -55,6 +56,7 @@ BUILD_PATH=$DERIVED_DATA/Build/Products
 DO_KMW_BUILD=true
 DO_KEYMANAPP=true
 DO_ARCHIVE=true
+DO_SIMULATOR_TARGET=false
 DO_CARTHAGE=true
 CLEAN_ONLY=false
 CONFIG=Release
@@ -102,6 +104,9 @@ while [[ $# -gt 0 ]] ; do
             ;;
         -download-resources)
             DO_KMP_DOWNLOADS=true
+            ;;
+        -all-artifacts)
+            DO_SIMULATOR_TARGET=true
             ;;
     esac
     shift # past argument
@@ -296,6 +301,16 @@ if [ $DO_KEYMANAPP = true ]; then
     xcodebuild $XCODEFLAGS -exportArchive -archivePath $ARCHIVE_PATH \
                 -exportOptionsPlist exportAppStore.plist \
                 -exportPath $BUILD_PATH/${CONFIG}-iphoneos -allowProvisioningUpdates
+  fi
+
+  if [ $DO_SIMULATOR_TARGET == true ]; then
+    echo "Preparing .app file for simulator-targeted artifact for testing"
+    xcodebuild $XCODEFLAGS_EXT $CODE_SIGN -scheme Keyman \
+                -sdk iphonesimulator \
+                VERSION=$VERSION \
+                VERSION_WITH_TAG=$VERSION_WITH_TAG \
+                VERSION_ENVIRONMENT=$VERSION_ENVIRONMENT \
+                UPLOAD_SENTRY=$UPLOAD_SENTRY 
   fi
 
   echo ""
