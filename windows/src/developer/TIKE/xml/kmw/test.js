6b3938374357585ae6dd4e1505ed58a9901163c5
Marc Durdin <marc@durdin.net>
2021-09-20 16:01:17 +1000

    feat(developer): touch layout testing
    Adds support for testing touch layouts without requiring use of device
    emulation. Uses new InlinedOSKView.
    
    Does not yet add a view within Keyman Developer; this will come shortly.

---
+++ windows/src/developer/TIKE/xml/kmw/test.js
@@ -0,0 +1,244 @@
+keyman.init({
+  ui:'button',
+  resources:'/resource/',
+  keyboards:'/keyboard/',
+  fonts:'/font/',
+  attachType:'auto'
+});
+
+var ta1 = document.getElementById('ta1');
+var charGrid = document.getElementById('character-grid');
+var lastContent = null;
+
+if(keyman.util.isTouchDevice()) {
+  document.body.className += ' touch-device';
+  if(keyman.util.device.OS == 'iOS') {
+    document.body.className += ' touch-device-ios';
+  } else if(keyman.util.device.OS == 'Android') {
+    document.body.className += ' touch-device-android';
+  }
+} else {
+  document.body.className += ' desktop-device';
+}
+
+function removeChildNodes(node) {
+  while (node.lastChild) {
+    node.removeChild(node.lastChild);
+  }
+}
+
+function addCharElements(text, code) {
+  var ebox = document.createElement('div'), echar = document.createElement('div'), ecode = document.createElement('div');
+  echar.textContent = text;
+  echar.className = 'char-char keymanweb-font';
+  ecode.textContent = code;
+  ecode.className = 'char-code';
+  ebox.appendChild(echar);
+  ebox.appendChild(ecode);
+  charGrid.appendChild(ebox);
+}
+
+function logContent() {
+  if(lastContent === ta1.value) {
+    updateLogCursor();
+    return;
+  }
+  removeChildNodes(charGrid);
+  if(ta1.value.length == 0) {
+    addCharElements('-','empty');
+  } else {
+    for(var i = 0; i < ta1.value.length; i++) {
+      //
+      var code = ta1.value.charCodeAt(i);
+      var text = ta1.value.charAt(i);
+      var slice = 4;
+      // Test for SMP
+      if(code >= 0xD800 && code < 0xDC00) {
+        if(i < ta1.value.length) {
+          var code2 = ta1.value.charCodeAt(i+1);
+          if(code2 >= 0xDC00 && code < 0xE000) {
+            code = (code - 0xD800) * 0x400 + (code2 - 0xDC00) + 0x10000;
+            text += ta1.value.charAt(i+1);
+            slice = 6;
+            i++;
+          }
+        }
+      }
+      addCharElements(text, ('000000'+(code).toString(16)).slice(-slice));
+    }
+  }
+  updateLogCursor();
+  lastContent = ta1.value;
+}
+
+var lastSelStart = -1;
+
+function calculateLengthByCodepoint(text, base, x)  {
+  var stop = base + x;
+  while(base < stop - 1) {
+    if(text.charCodeAt(base) >= 0xD800 && text.charCodeAt(base) < 0xDC00 &&
+       text.charCodeAt(base+1) >= 0xDC00 && text.charCodeAt(base+1) < 0xE000) {
+      // Decrement position by one for each surrogate pair
+      x--;
+    }
+    base++;
+  }
+  return x;
+}
+
+function updateLogCursor() {
+  var i, selStart, selLength, selDirection;
+
+  if(keyman.isPositionSynthesized()) { // this is an internal function
+    // For touch devices, we need to ask KMW
+    selStart = ta1.kmw_ip ? ta1.kmw_ip.getTextBeforeCaret().length : 0;
+    selLength = 0;
+    selDirection = 'forward';
+  } else {
+    // For desktop devices, we use the position reported by the textarea control
+    selStart = ta1.selectionStart;
+    selLength = ta1.selectionEnd - ta1.selectionStart;
+    selDirection = ta1.selectionDirection;
+  }
+
+  selLength = calculateLengthByCodepoint(ta1.value, selStart, selLength);
+  selStart = calculateLengthByCodepoint(ta1.value, 0, selStart);
+
+  //console.log('selStart='+selStart+', selLength='+selLength);
+  if(lastSelStart != selStart || lastSelLength != selLength) {
+    for(i = 0; i < charGrid.childNodes.length; i++) {
+      charGrid.childNodes[i].className = '';
+    }
+
+    var x = selDirection == 'backward' ? selStart-1 : selStart+selLength - 1;
+
+    if(x < 0) {
+      charGrid.className = 'cursor';
+    } else {
+      charGrid.className = '';
+      if(x >= 0 && x < charGrid.childNodes.length) {
+        charGrid.childNodes[x].className = 'cursor';
+      }
+      charGrid.childNodes[x].scrollIntoView();
+    }
+
+    for(i = selStart; i < selStart+selLength; i++) {
+      charGrid.childNodes[i].className += ' cursor-selected';
+    }
+    lastSelStart = selStart;
+    lastSelLength = selLength;
+  }
+}
+
+logContent();
+window.setInterval(logContent, 100);
+
+/* TODO: once KeymanWeb supports oninput signalling
+document.getElementById('ta1').addEventListener('input', logContent, false);
+*/
+
+window.onload = function() {
+  window.setTimeout(
+    function () {
+      //document.getElementById('ta1').focus();
+      keyman.moveToElement('ta1');
+    }, 10
+  );
+
+  let newOSK = null;
+  let deviceSelect = document.getElementById('device-select');
+  if(deviceSelect.value == '') deviceSelect.value = 'desktop';
+
+  deviceSelect.addEventListener('change', function() {
+    setOSK();
+    ta1.focus();
+  });
+
+
+  function setOSK() {
+    const devices = {
+      Windows:         { browser: 'chrome', formFactor: 'desktop', OS: 'windows', touchable: false, dimensions: [640, 300] },
+      macOS:           { browser: 'chrome', formFactor: 'desktop', OS: 'macosx',  touchable: false, dimensions: [640, 300] },
+      Linux:           { browser: 'chrome', formFactor: 'desktop', OS: 'linux',   touchable: false, dimensions: [640, 300] },
+      iPhone:          { browser: 'chrome', formFactor: 'phone',   OS: 'ios',     touchable: true,  dimensions: [320, 200] },
+      iPadMini:        { browser: 'chrome', formFactor: 'tablet',  OS: 'ios',     touchable: true,  dimensions: [640, 300] },
+      SamsungGalaxyS5: { browser: 'chrome', formFactor: 'phone',   OS: 'android', touchable: true,  dimensions: [320, 200] },
+      SamsungTablet:   { browser: 'chrome', formFactor: 'tablet',  OS: 'android', touchable: true,  dimensions: [640, 300] },
+    };
+
+    const targetDevice = devices[deviceSelect.value] || devices.Windows;
+
+    if(newOSK) {
+      document.getElementById('osk-host').removeChild(newOSK.element);
+      keyman.osk = null;
+    }
+
+    newOSK = new com.keyman.osk.InlinedOSKView(targetDevice, keyman.util.device.coreSpec);
+
+    if(document.body.offsetWidth < targetDevice.dimensions[0]) {
+      newOSK.setSize('320px', '200px');
+    } else {
+      newOSK.setSize(targetDevice.dimensions[0]+'px', targetDevice.dimensions[1]+'px');
+    }
+    document.getElementById('osk-host').appendChild(newOSK.element);
+
+    keyman.osk = newOSK;
+    newOSK.activeKeyboard = keyman.core.activeKeyboard;
+    document.getElementById('keyboard-name').innerText = keyman.core.activeKeyboard ? keyman.core.activeKeyboard.name : '(system keyboard)';
+    keyman.alignInputs();
+  }
+
+  setOSK();
+
+  keyman.addEventListener('keyboardchange', function(keyboardProperties) {
+    keyman.osk = newOSK;
+    newOSK.activeKeyboard = keyman.core.activeKeyboard;
+    document.getElementById('keyboard-name').innerText = keyman.core.activeKeyboard ? keyman.core.activeKeyboard.name : '(system keyboard)';
+    keyman.alignInputs();
+  });
+}
+
+/* Lexical models */
+
+var firstModel = true;
+var modelList = document.getElementById('model-list');
+
+/**
+ * Register a model for debugging. Called by keyboards.js. The
+ * first model registered will be activated automatically.
+ */
+function registerModel(model, src) {
+  var modelList = document.getElementById('model-list');
+  var opt = document.createElement('option');
+  opt.innerText = model;
+  opt.value = model;
+  opt['data-src'] = src;
+  modelList.appendChild(opt);
+
+  if(firstModel) {
+    keyman.modelManager.register({
+      id: model,
+      languages: ['en'],
+      path: 'http://'+location.host+'/model/'+src
+    });
+    document.getElementById('model-list-empty').style.display = 'none';
+    firstModel = false;
+  }
+}
+
+modelList.addEventListener('change', function() {
+  if(modelList.value == null) return;
+  let model = modelList.value;
+  if(modelList.selectedOptions.length == 0) return;
+  let opt = modelList.selectedOptions[0];
+
+  let lastModel = keyman.modelManager.activeModel;
+  if(lastModel) {
+    keyman.modelManager.deregister(lastModel.id);
+  }
+  keyman.modelManager.register({
+    id: model,
+    languages: ['en'],
+    path: 'http://'+location.host+'/model/'+opt['data-src']
+  });
+});
