65d1a3f9a024719c962d96f2d9b62f9761d01f58
Eberhard Beilharz <eb1@sil.org>
2021-11-05 17:43:30 +0100

    chore(linux): Improve `run-tests.sh` script
    Part of #5613.

--- linux/ibus-keyman/tests/run-tests.sh
+++ linux/ibus-keyman/tests/run-tests.sh
@@ -3,34 +3,69 @@
 BASEDIR=$(realpath $(dirname $0))
 TESTDIR=${XDG_DATA_HOME:-$HOME/.local/share}/keyman/test_kmx
 
-if [ "$DISPLAY" == ":0" ]; then
-  echo "Please start xephyr and set the DISPLAY environment variable before running these tests."
+if ! which Xvfb > /dev/null || ! which Xephyr > /dev/null || ! which metacity > /dev/null; then
+  echo "Please install Xvfb, Xephyr and metacity before running these tests!"
   exit 1
 fi
 
+function cleanup() {
+  if [ -f /tmp/ibus-keyman-test-pids ]; then
+    echo
+    echo "Shutting down processes..."
+    bash /tmp/ibus-keyman-test-pids > /dev/null 2>&1
+    rm /tmp/ibus-keyman-test-pids
+    echo "Finished shutdown of processes."
+  fi
+}
+
+echo > /tmp/ibus-keyman-test-pids
+
+trap cleanup EXIT SIGINT
+
+echo "Starting Xvfb..."
+Xvfb -screen 0 1024x768x24 :33 &> /dev/null &
+echo "kill -9 $!" >> /tmp/ibus-keyman-test-pids
+sleep 1
+echo "Starting Xephyr..."
+DISPLAY=:33 Xephyr :32 -screen 1024x768 &> /dev/null &
+echo "kill -9 $!" >> /tmp/ibus-keyman-test-pids
+sleep 1
+echo "Starting metacity"
+metacity --display=:32 &> /dev/null &
+echo "kill -9 $!" >> /tmp/ibus-keyman-test-pids
+
+export DISPLAY=:32
+
 if [ ! -d $TESTDIR ]; then
-  if [ -d $BASEDIR/../../../common/core/desktop/build/arch/debug ]; then
-    ln -sf $(realpath $BASEDIR/../../../common/core/desktop/build/arch/debug/tests/unit/kmx) $TESTDIR
-  elif [ -d $BASEDIR/../../../common/core/desktop/build/arch/release ]; then
-    ln -sf $(realpath $BASEDIR/../../../common/core/desktop/build/arch/release/tests/unit/kmx) $TESTDIR
+  KMXDIR=
+  [ -d $BASEDIR/../../../common/core/desktop/build/arch ] && KMXDIR=$BASEDIR/../../../common/core/desktop/build/arch
+  [ -d $BASEDIR/../../keyboardprocessor/arch ] && KMXDIR=$BASEDIR/../../keyboardprocessor/arch
+  if [[ $(wc -l $KMXDIR/*/tests/unit/kmx/*.kmx 2>/dev/null) > 0 ]]; then
+    mkdir -p $TESTDIR
+    ln -sf $(realpath $KMXDIR/*/tests/unit/kmx) $TESTDIR
   else
-    echo "Can't find kmx files in common/core/desktop/build/arch/*/tests/unit/kmx"
+    echo "Can't find kmx files in common/core/desktop/build/arch/*/tests/unit/kmx or linux/keyboardprocessor/arch/*/tests/unit/kmx"
     exit 2
   fi
 fi
 
 if [ $# -gt 0 ]; then
-  TESTFILES=$@
+  [ "$1" == "--" ] && shift
+  TESTFILES=($@)
 else
-  pushd $TESTDIR
+  pushd $TESTDIR > /dev/null
   TESTFILES=(*.kmx)
-  popd
+  popd > /dev/null
 fi
 
 export GSETTINGS_BACKEND=keyfile
 
-ibus-daemon --panel=disable --config=/usr/libexec/ibus-memconf > /tmp/ibus-daemon.log 2>&1 &
-../src/ibus-engine-keyman > /tmp/ibus-engine-keyman.log 2>&1 &
+ibus-daemon --panel=disable --config=/usr/libexec/ibus-memconf &> /tmp/ibus-daemon.log &
+echo "kill -9 $!" >> /tmp/ibus-keyman-test-pids
+../src/ibus-engine-keyman &> /tmp/ibus-engine-keyman.log &
+echo "kill -9 $!" >> /tmp/ibus-keyman-test-pids
 sleep 1s
 
+echo "Starting tests..."
 ./ibus-keyman-tests -k --tap --directory $TESTDIR "${TESTFILES[@]}"
+echo "Finished tests."
