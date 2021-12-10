058b73096c590fb649ca10628b1a59bc4f2d808d
Eberhard Beilharz <eb1@sil.org>
2021-12-10 16:38:00 +0100

    feat(linux): Add support for Ubuntu 22.04 Jammy
    Partially implements #5032.

--- linux/scripts/launchpad.sh
+++ linux/scripts/launchpad.sh
@@ -40,7 +40,7 @@ echo "ppa: ${ppa}"
 if [ "${DIST}" != "" ]; then
     distributions="${DIST}"
 else
-    distributions="bionic focal hirsute impish"
+    distributions="bionic focal hirsute impish jammy"
 fi
 
 if [ "${PACKAGEVERSION}" != "" ]; then
