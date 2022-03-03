04297aa47ca601814116ad28f1e2a7e576daa2f5
Darcy Wong <darcy_wong@sil.org>
2022-03-03 11:27:07 +0700

    Update oem/firstvoices/android/app/src/main/java/com/firstvoices/android/FVKmpInstallMode.java
    Co-authored-by: Marc Durdin <marc@durdin.net>

--- oem/firstvoices/android/app/src/main/java/com/firstvoices/android/FVKmpInstallMode.java
+++ oem/firstvoices/android/app/src/main/java/com/firstvoices/android/FVKmpInstallMode.java
@@ -3,6 +3,6 @@ package com.firstvoices.android;
 public enum FVKmpInstallMode {
   Silent,       // Show no UI while installing a package. Errors may be shown as toasts
   WelcomeOnly,  // Show only the welcome screen after installing a package; language will
-  // be automatically selected if not provided as an input parameter
+                // be automatically selected if not provided as an input parameter
   Full          // Show readme, language picker (if applicable), and welcome after install.
 }
