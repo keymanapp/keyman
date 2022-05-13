46c987bc7f6a40c91cadd94a379b39f9374752fd
Marc Durdin <marc@durdin.net>
2022-05-13 14:46:19 +1000

    chore(developer): build web-environment during js build

--- developer/js/build.sh
+++ developer/js/build.sh
@@ -94,6 +94,8 @@ type npm >/dev/null ||\
 
 if (( install_dependencies )) ; then
   verify_npm_setup
+  # We need to build web-environment with a script for now
+  "$KEYMAN_ROOT/resources/web-environment/build.sh" || fail "Could not build web-environment"
 fi
 
 build || fail "Compilation failed."
