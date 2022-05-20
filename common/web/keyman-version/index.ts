0489495e603fb00ee034960e38fcf8916d0fcef3
jahorton <jahorton@sil.org>
2022-05-20 10:50:09 +0700

    chore(web): declaration alias for module export use

--- common/web/keyman-version/index.ts
+++ common/web/keyman-version/index.ts
@@ -1,5 +1,8 @@
 /// <reference path="./version.inc.ts" />
 
+// Aliases the namespaced type with the non-namespaced module export.
+declare let KEYMAN_VERSION: typeof com.keyman.KEYMAN_VERSION;
+
 // Add all namespaces defined here to the global scope:
 if (typeof module != 'undefined' && typeof module.exports != 'undefined') {
   module.exports['KEYMAN_VERSION'] = com.keyman.KEYMAN_VERSION;
