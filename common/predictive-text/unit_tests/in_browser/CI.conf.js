e37e3aa88acc15627231482bc4119220e95767f8
Marc Durdin <marc@durdin.net>
2022-05-29 12:50:27 +1000

    chore(web): update BrowserStack to chrome 90 for Windows

--- common/predictive-text/unit_tests/in_browser/CI.conf.js
+++ common/predictive-text/unit_tests/in_browser/CI.conf.js
@@ -74,7 +74,7 @@ module.exports = function(config) {
 
   var CURRENT_WIN_LAUNCHERS = {
     // Currently, Firefox launcher is unstable; see https://github.com/karma-runner/karma-firefox-launcher/issues/93
-    // (in particular "not maintained" commentary). 
+    // (in particular "not maintained" commentary).
     //bs_firefox_win: {
     //  os: 'Windows',
     //  os_version: '10',
@@ -85,7 +85,7 @@ module.exports = function(config) {
       os: 'Windows',
       os_version: '10',
       browser: 'chrome',
-      browser_version: '70.0'
+      browser_version: '90.0'
     },
     // On recent versions of Edge, launcher fails to start and/or stop Edge successfully
     //bs_edge_win: {
