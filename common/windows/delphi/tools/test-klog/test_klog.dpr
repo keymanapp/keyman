4f233a3a0da8077d9b6b99429dfdb935ede13443
Marc Durdin <marc@durdin.net>
2022-06-10 12:00:47 +1000

    chore(windows): move test-klog to common and add call to Developer build

---
+++ common/windows/delphi/tools/test-klog/test_klog.dpr
@@ -0,0 +1,23 @@
+program test_klog;
+
+{$APPTYPE CONSOLE}
+
+uses
+  SysUtils,
+  klog in '..\..\..\..\..\windows\src\global\delphi\general\klog.pas',
+  VersionInfo in '..\..\..\..\..\windows\src\global\delphi\general\VersionInfo.pas',
+  ErrorControlledRegistry in '..\..\..\..\..\windows\src\global\delphi\vcl\ErrorControlledRegistry.pas',
+  Unicode in '..\..\..\..\..\windows\src\global\delphi\general\Unicode.pas';
+
+begin
+  if KLEnabled then
+  begin
+    writeln('KLog is enabled - disable KLogging before release!');
+    ExitCode := 1;
+  end
+  else
+  begin
+    writeln('KLog is disabled');
+    ExitCode := 0;
+  end;
+end.
