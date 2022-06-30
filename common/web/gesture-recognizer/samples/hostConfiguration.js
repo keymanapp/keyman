6e35523b762ada5a7ce97f3f1514ff96955bcc76
Joshua A. Horton <joshua_horton@sil.org>
2022-06-30 14:31:20 +0700

    feat(web): added popup modeling mode

--- common/web/gesture-recognizer/samples/hostConfiguration.js
+++ common/web/gesture-recognizer/samples/hostConfiguration.js
@@ -1,9 +1,10 @@
 function updateConfig() {
   let layout = document.config.screen;
   let bounds = document.config.bounds;
+  let receiver = document.config.receiver;
 
   let demoContainer = document.getElementById("demo-container");
-  demoContainer.className = layout.value + " " + bounds.value;
+  demoContainer.className = layout.value + " " + bounds.value + " " + receiver.value;
 }
 
 //////
@@ -11,6 +12,7 @@ function updateConfig() {
 window.addEventListener('load', function(ev) {
   let layoutGroup = document.config.screen;
   let boundsGroup = document.config.bounds;
+  let receiverGroup = document.config.receiver;
 
   for(entry of layoutGroup) {
     entry.addEventListener('change', updateConfig);
@@ -19,4 +21,10 @@ window.addEventListener('load', function(ev) {
   for(entry of boundsGroup) {
     entry.addEventListener('change', updateConfig);
   }
+
+  for(entry of receiverGroup) {
+    entry.addEventListener('change', updateConfig);
+  }
+
+  updateConfig();
 });
