53837778fa8d683994e32ff7a349740f1d18803e
Joshua A. Horton <joshua_horton@sil.org>
2022-07-13 09:13:35 +0700

    chore(web): addresses some PR comments/suggestions

--- common/web/gesture-recognizer/src/gestureRecognizer.ts
+++ common/web/gesture-recognizer/src/gestureRecognizer.ts
@@ -1,11 +1,8 @@
 /// <reference path="gestureRecognizerConfiguration.ts" />
+/// <reference path="mutable.ts" />
 /// <reference path="includes/events.ts" />
 
 namespace com.keyman.osk {
-  type Mutable<Type> = {
-    -readonly [Property in keyof Type]: Type[Property];
-  };
-
   export enum TrackedInputState {
     START = "start",
     END = "end",
@@ -18,12 +15,12 @@ namespace com.keyman.osk {
 
     protected readonly config: GestureRecognizerConfiguration;
 
-    private readonly mouseEngine?: MouseEventEngine;
-    private readonly touchEngine?: TouchEventEngine;
+    private readonly mouseEngine: MouseEventEngine;
+    private readonly touchEngine: TouchEventEngine;
 
     protected static preprocessConfig(config: GestureRecognizerConfiguration): GestureRecognizerConfiguration {
       // Allows configuration pre-processing during this method.
-      let processingConfig: Mutable<GestureRecognizerConfiguration> = Object.assign({}, config);
+      let processingConfig: Mutable<GestureRecognizerConfiguration> = {...config};
       processingConfig.mouseEventRoot = processingConfig.mouseEventRoot ?? processingConfig.targetRoot;
       processingConfig.touchEventRoot = processingConfig.touchEventRoot ?? processingConfig.targetRoot;
 
@@ -38,25 +35,26 @@ namespace com.keyman.osk {
       this.mouseEngine = new MouseEventEngine(config);
       this.touchEngine = new TouchEventEngine(config);
 
-      this.mouseEngine?.registerEventHandlers();
-      this.touchEngine?.registerEventHandlers();
+      this.mouseEngine.registerEventHandlers();
+      this.touchEngine.registerEventHandlers();
 
       const _this = this;
-      this.mouseEngine?.on(InputEventEngine.INPUT_UPDATE_EVENT_NAME, function(state, coord) {
-        _this.emit(GestureRecognizer.TRACKED_INPUT_UPDATE_EVENT_NAME, state, coord);
+      const forwardingUpdateHandler = (state, coord) => {
+        this.emit(GestureRecognizer.TRACKED_INPUT_UPDATE_EVENT_NAME, state, coord);
         return false;
-      });
+      }
 
-      this.touchEngine?.on(InputEventEngine.INPUT_UPDATE_EVENT_NAME, function(state, coord) {
-        _this.emit(GestureRecognizer.TRACKED_INPUT_UPDATE_EVENT_NAME, state, coord);
-        return false;
-      });
+      this.mouseEngine.on(InputEventEngine.INPUT_UPDATE_EVENT_NAME, forwardingUpdateHandler);
+      this.touchEngine.on(InputEventEngine.INPUT_UPDATE_EVENT_NAME, forwardingUpdateHandler);
     }
 
     public destroy() {
-      this.mouseEngine?.unregisterEventHandlers();
-      this.touchEngine?.unregisterEventHandlers();
+      this.mouseEngine.unregisterEventHandlers();
+      this.touchEngine.unregisterEventHandlers();
 
+      // Because these two fields are marked readonly, we can't directly delete them.
+      // Because they're private, we can't apply Mutable to make them deletable.
+      // So... awkward cast + assignment it is.
       (this.mouseEngine as any) = null;
       (this.touchEngine as any) = null;
     }
