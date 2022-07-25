70b2e2fec6cb43d841df85b013bf216e03cdf7e0
Joshua A. Horton <joshua_horton@sil.org>
2022-07-25 11:03:00 +0700

    change(web): TrackedPoint docs, clearer JSON serialization

--- common/web/gesture-recognizer/src/trackedPoint.ts
+++ common/web/gesture-recognizer/src/trackedPoint.ts
@@ -6,23 +6,49 @@ namespace com.keyman.osk {
     samples: TrackedPoint['path']; // ensures type match with public class property.
   }
 
+  /**
+   * Represents one 'tracked point' involved in a potential / recognized gesture as tracked over time.
+   * This 'tracked point' corresponds to one touch source as recognized by `Touch.identifier` or to
+   * one 'cursor-point' as represented by mouse-based motion.
+   */
   export class TrackedPoint {
+    /**
+     * Indicates whether or not this tracked point's original source is a DOM `Touch`.
+     */
     public readonly isFromTouch: boolean;
+
+    /**
+     * The numeric form of this point's identifier as seen in events (or as emulated for mouse events)
+     */
     public readonly rawIdentifier: number;
 
     private _initialTarget: EventTarget;
 
     private samples: InputSampleSequence = [];
 
+    /**
+     * Tracks the coordinates and timestamps of each update for the lifetime of this `TrackedPoint`.
+     */
     public get path(): readonly InputSample[] {
       return this.samples;
     }
 
-    constructor(identifier: number,
-                parsedObj: JSONTrackedPoint);
+    /**
+     * Constructs a new TrackedPoint instance for tracking updates to an active input point over time.
+     * @param identifier     The system identifier for the input point's events.
+     * @param initialTarget  The initiating event's original target element
+     * @param isFromTouch    `true` if sourced from a `TouchEvent`; `false` otherwise.
+     */
     constructor(identifier: number,
                 initialTarget: EventTarget,
                 isFromTouch: boolean);
+    /**
+     * Deserializes a TrackedPoint instance from its serialized-JSON form.
+     * @param identifier The unique identifier to assign to this instance.
+     * @param parsedObj  The JSON representation to deserialize.
+     */
+    constructor(identifier: number,
+      parsedObj: JSONTrackedPoint);
     constructor(identifier: number,
                 obj: EventTarget | JSONTrackedPoint,
                 isFromTouch?: boolean) {
@@ -42,6 +68,10 @@ namespace com.keyman.osk {
       return this._initialTarget;
     }
 
+    /**
+     * Gets a fully-unique string-based identifier, even for edge cases where both mouse and touch input
+     * are received simultaneously.
+     */
     public get identifier(): string {
       const prefix = this.isFromTouch ? 'touch' : 'mouse';
       return `${prefix}:${this.rawIdentifier}`;
@@ -54,7 +84,13 @@ namespace com.keyman.osk {
     toJSON(): JSONTrackedPoint {
       let jsonClone: JSONTrackedPoint = {
         isFromTouch: this.isFromTouch,
-        samples: [...this.samples.map((obj) => ({...obj} as InputSample))]
+        // Replicate array and its entries, but with certain fields of each entry missing.
+        // No .clientX, no .clientY.
+        samples: [...this.samples.map((obj) => ({
+          targetX: obj.targetX,
+          targetY: obj.targetY,
+          t:       obj.t
+        }))]
       }
 
       // Removes components of each sample that we don't want serialized.
