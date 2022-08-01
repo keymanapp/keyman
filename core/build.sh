f762776d5ca4b0a63f17814a501868d4d851b061
Marc Durdin <marc@durdin.net>
2022-08-01 14:15:52 +1000

    chore(core): get wasm core building again
    Updates search for emscripten if not on path, and fixes up unit test
    infrastructure for wasm builds for new tests.

--- core/build.sh
+++ core/build.sh
@@ -258,10 +258,32 @@ build_standard() {
   fi
 }
 
+#
+# We don't want to rely on emcc being on the path, because Emscripten puts far
+# too many things onto the path (in particular for us, node).
+#
+# The following comment suggests that we don't need emcc on the path.
+# https://github.com/emscripten-core/emscripten/issues/4848#issuecomment-1097357775
+#
+# So we try and locate emcc in common locations ourselves. The search pattern
+# is:
+#
+# 1. Look for $EMSCRIPTEN_BASE (our primary emscripten variable), which should
+#    point to the folder that emcc is located in
+# 2. Look for $EMCC which should point to the emcc executable
+# 3. Look for emcc on the path
+#
 locate_emscripten() {
-  local EMCC=`which emcc`
-  [ -z "$EMCC" ] && fail "Could not locate emscripten (emcc)"
-  EMSCRIPTEN_BASE="$(dirname "$EMCC")"
+  if [[ -z ${EMSCRIPTEN_BASE+x} ]]; then
+    if [[ -z ${EMCC+x} ]]; then
+      local EMCC=`which emcc`
+      [[ -z $EMCC ]] && fail "locate_emscripten: Could not locate emscripten (emcc) on the path or with \$EMCC or \$EMSCRIPTEN_BASE"
+    fi
+    [[ -x $EMCC ]] || fail "locate_emscripten: Variable EMCC ($EMCC) does not point to a valid executable emcc"
+    EMSCRIPTEN_BASE="$(dirname "$EMCC")"
+  fi
+
+  [[ -x ${EMSCRIPTEN_BASE}/emcc ]] || fail "locate_emscripten: Variable EMSCRIPTEN_BASE ($EMSCRIPTEN_BASE) does not point to emcc's folder"
 }
 
 build_meson_cross_file_for_wasm() {
