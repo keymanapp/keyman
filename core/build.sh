daf6a7970e405f5676cd9f80964a45b79be45efe
Marc Durdin <marc@durdin.net>
2022-08-26 15:12:38 +1000

    feat(core): use kmldmlc to build test keyboards
    Removes .kmx from repo and builds them from the source .xml files using
    kmldmlc (building that if necessary also with a poor fella's dependency
    check for now), and generally a lot of cleanup of the meson.build files
    for the ldml tests.
    
    Moves keyboards to a subfolder of the ldml unit test folder to keep them
    neat and tidy and avoid embedding huge files into ldml.js for test of
    WASM.
    
    Note: 000_null_keyboard and 002_null_invalid do build successfully, but
    the core ldml keyboardprocessor currently fails on them, because they
    are missing keys and vkey sections.

--- core/build.sh
+++ core/build.sh
@@ -285,6 +285,16 @@ if $CLEAN; then
   clean
 fi
 
+if $TESTS_CPP; then
+  # check dependency for ldml - kmldmlc
+  # TODO: in the future this should be part of the meson build, but
+  # it's too complicated at present due to old meson versions in
+  # debian packaging environments
+  if [[ ! -f "$KEYMAN_ROOT/developer/src/kmldmlc/build/kmldmlc.js" ]]; then
+    "$KEYMAN_ROOT/developer/src/kmldmlc/build.sh" configure build
+  fi
+fi
+
 if [[ $PLATFORM == native ]]; then
   case $os_id in
     "linux")
