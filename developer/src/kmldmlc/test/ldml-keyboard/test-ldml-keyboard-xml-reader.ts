918d261f99fc105993588d013a1ae75ce08bc6c7
Marc Durdin <marc@durdin.net>
2022-09-02 08:01:48 +1000

    feat(developer): add conformsTo validity unit test
    This is reasonably random, but wanted to add another test for content
    rather than structure, just to verify that the DTD-XSD-jsonschema
    translation is doing the right thing according to what we expect.
    
    Looking good.

--- developer/src/kmldmlc/test/ldml-keyboard/test-ldml-keyboard-xml-reader.ts
+++ developer/src/kmldmlc/test/ldml-keyboard/test-ldml-keyboard-xml-reader.ts
@@ -16,4 +16,15 @@ describe('ldml keyboard xml reader tests', function() {
     assert.equal(callbacks.messages.length, 1);
     assert.deepEqual(callbacks.messages[0], CompilerErrors.InvalidFile("data/keyboard must have required property 'names'"));
   });
+
+  it("should fail to load files with an invalid conformsTo", function() {
+    const inputFilename = makePathToFixture('invalid-conforms-to.xml');
+    const callbacks = new CompilerCallbacks();
+    let reader = new LDMLKeyboardXMLSourceFileReader(callbacks);
+    const source = reader.loadFile(inputFilename);
+    assert.isNull(source);
+    assert.equal(callbacks.messages.length, 1);
+    assert.deepEqual(callbacks.messages[0], CompilerErrors.InvalidFile("data/keyboard/conformsTo must be equal to one of the allowed values"));
+  });
+
 });
