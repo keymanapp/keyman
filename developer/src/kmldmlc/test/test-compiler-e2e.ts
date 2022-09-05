a708b2f85d0b6286b7f04641d21d2d523bad6842
Marc Durdin <marc@durdin.net>
2022-09-05 10:43:01 +1000

    chore(developer): add test for compiler messages and cleanup e2e test
    The purpose of these tests is to ensure that our slightly WET constant
    definitions for compiler errors are consistent in their usage:
    
      * that names are consistent between the message functions and
        constants
      * that constants have unique codes
      * that constants have the right error mask for their type
    
    This means we don't have to worry about DRYing out the constants, which
    would probably require us to have a preprocessing step, which has its
    own negatives.
    
    Also, added more reporting to e2e test in case of failures.

--- developer/src/kmldmlc/test/test-compiler-e2e.ts
+++ developer/src/kmldmlc/test/test-compiler-e2e.ts
@@ -7,25 +7,32 @@ import Compiler from '../src/keyman/compiler/compiler';
 import KMXBuilder from '../src/keyman/kmx/kmx-builder';
 import {CompilerCallbacks, makePathToFixture} from './helpers/index';
 
+function checkMessages(callbacks: CompilerCallbacks) {
+  if(callbacks.messages.length > 0) {
+    console.log(callbacks.messages);
+  }
+  assert.isEmpty(callbacks.messages);
+}
+
 function compileKeyboard(inputFilename: string): Uint8Array {
   const callbacks = new CompilerCallbacks();
   const k = new Compiler(callbacks);
-  let source = k.load(inputFilename);
-  if(!source) {
-    return null;
-  }
-  if(!k.validate(source)) {
-    return null;
-  }
-  let kmx = k.compile(source);
-  if(!kmx) {
-    return null;
-  }
+  const source = k.load(inputFilename);
+  checkMessages(callbacks);
+  assert.isNotNull(source, 'k.load should not have returned null');
+
+  const valid = k.validate(source);
+  checkMessages(callbacks);
+  assert.isTrue(valid, 'k.validate should not have failed');
+
+  const kmx = k.compile(source);
+  checkMessages(callbacks);
+  assert.isNotNull(kmx, 'k.compile should not have returned null');
 
   // Use the builder to generate the binary output file
-  let builder = new KMXBuilder(kmx, true);
-  let result = builder.compile();
-  assert(callbacks.messages.length == 0);
+  const builder = new KMXBuilder(kmx, true);
+  const result = builder.compile();
+  checkMessages(callbacks);
   return result;
 }
 
