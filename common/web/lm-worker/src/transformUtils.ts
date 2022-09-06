9be8839685ae8d65fbae37ff5eb9ab836f863804
Joshua A. Horton <joshua_horton@sil.org>
2022-09-06 10:47:51 +0700

    fix(common/models): undefined != 0

--- common/web/lm-worker/src/transformUtils.ts
+++ common/web/lm-worker/src/transformUtils.ts
@@ -16,10 +16,10 @@ class TransformUtils {
   }
 
   static isBackspace(transform: Transform): boolean {
-    return transform.insert == "" && transform.deleteLeft > 0 && transform.deleteRight == 0;
+    return transform.insert == "" && transform.deleteLeft > 0 && !transform.deleteRight;
   }
 
   static isEmpty(transform: Transform): boolean {
-    return transform.insert == '' && transform.deleteLeft == 0 && transform.deleteRight == 0;
+    return transform.insert == '' && transform.deleteLeft == 0 && !transform.deleteRight;
   }
 }
