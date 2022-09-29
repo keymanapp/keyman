f06578714e3e570f02ac21bd012128753477e532
Darcy Wong <darcy_wong@sil.org>
2022-09-29 10:34:25 +0700

    chore(common): Update standard example

--- resources/builder.inc.sh
+++ resources/builder.inc.sh
@@ -45,7 +45,7 @@ function _builder_findRepoRoot() {
 #  ## START STANDARD BUILD SCRIPT INCLUDE
 #  # adjust relative paths as necessary
 #  THIS_SCRIPT="$(greadlink -f "${BASH_SOURCE[0]}" 2>/dev/null || readlink -f "${BASH_SOURCE[0]}")"
-#  . "$(dirname "$THIS_SCRIPT")/resources/build/build-utils.sh"
+#  . "$(dirname "$THIS_SCRIPT")/resources/builder.inc.sh"
 #  ## END STANDARD BUILD SCRIPT INCLUDE
 function _builder_setBuildScriptIdentifiers() {
   if [ ! -z ${THIS_SCRIPT+x} ]; then
