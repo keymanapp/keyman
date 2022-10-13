90eb7c77ec3dc6b619cb506e0e6bc28e20cf2e47
Ross <writetoross@gmail.com>
2022-10-13 13:24:29 +1000

    fix(windows): ensure all modifier events go to seralized queue

--- windows/src/engine/keyman32/k32_globals.cpp
+++ windows/src/engine/keyman32/k32_globals.cpp
@@ -73,6 +73,7 @@ UINT
   //TODO: consolidate these messages -- they are probably not all required now
   wm_keyman = 0,						// user message - ignore msg   // I3594
   wm_keyman_keyevent = 0,   // for serialized input
+  wm_keyman_modifierevent = 0,
 	wm_kmdebug = 0,						//  " "  "  "   - debugging
 
 	wm_keymankeydown = 0,
