ee31f0063713657eeda19b302ba7cf30bb16f05a
Joshua A. Horton <joshua_horton@sil.org>
2022-10-17 14:51:21 +0700

    chore(web): reworks web/unit_tests/test.sh

--- web/unit_tests/test.sh
+++ web/unit_tests/test.sh
@@ -1,9 +1,6 @@
 #!/usr/bin/env bash
 
-# set -e: Terminate script if a command returns an error
-set -e
-
-WORKING_DIRECTORY=`pwd`
+set -eu
 
 ## START STANDARD BUILD SCRIPT INCLUDE
 # adjust relative paths as necessary
@@ -14,130 +11,38 @@ THIS_SCRIPT="$(greadlink -f "${BASH_SOURCE[0]}" 2>/dev/null || readlink -f "${BA
 . "$KEYMAN_ROOT/resources/build/build-utils-ci.inc.sh"
 . "$KEYMAN_ROOT/resources/shellHelperFunctions.sh"
 
-# A simple utility script to facilitate our different modes for unit-testing KMW.
-# It's rigged to be callable by NPM to facilitate testing during development when in other folders.
+# This script runs from its own folder
+cd "$(dirname "$THIS_SCRIPT")"
 
-display_usage ( ) {
-    echo "test.sh [-CI | -debug | -? | -h | -help] | [-log-level <level>] | [-reporter <reporter>]"
-    echo
-    echo "  -CI               to run unit tests in CI mode on BrowserStack."
-    echo "                    This script requires your credentials to be set in environment variables - see "
-    echo "                    https://stackoverflow.com/questions/32450546/hiding-browserstack-key-in-karma"
-    echo ""
-    echo "  -debug            to establish a Karma server that facilitates unit test debugging"
-    echo "                    Not compatible with -CI."
-    echo ""
-    echo "  -log-level        to set the logging level used by Karma."
-    echo "                    Valid options: 'debug', 'info', 'warn', 'error', 'disable'"
-    echo ""
-    echo "  -reporter         sets the test engine to utilize the specified <reporter>."
-    echo "                    Valid options:  BrowserStack, teamcity, dots, progress, mocha"
-    echo ""
-    echo "  -? | -h | -help   to display this help information"
-    echo ""
-    echo "                    Specifying no option will perform a simple, single run of the test cases on"
-    echo "                    the dominant set of browsers for the currently-detected active OS."
-    echo ""
-    exit 0
-}
+################################ Main script ################################
 
-# Designed to determine which set of browsers should be available for local testing,
-# based upon the current system OS.
-get_OS ( ) {
-  # Default value, since it's the most general case/configuration to detect.
-  os_id="linux"
+builder_check_color "$@"
 
-  # Subject to change with future improvements.
-  if [[ "${OSTYPE}" = "darwin"* ]]; then
-    os_id="mac"
-  elif [[ "${OSTYPE}" = "msys" ]]; then
-    os_id="win"
-  elif [[ "${OSTYPE}" = "cygwin" ]]; then
-    os_id="win"
+builder_describe "Builds the standalone, headless form of Keyman Engine for Web's input-processor module" \
+  "@../tools" \
+  "test+" \
+  ":engine               Runs the top-level Keyman Engine for Web unit tests" \
+  ":libraries            Runs all unit tests for KMW's submodules.  Currently excludes predictive-text tests" \
+  "--ci                  Set to utilize CI-based test configurations & reporting.  May not be set with ${BUILDER_TERM_START}--debug${BUILDER_TERM_END}." \
+  "--reporters=REPORTERS Set to override the 'reporters' used by the unit testing engines" \
+  "--browsers=BROWSERS   Set to override automatic browser selection for ${BUILDER_TERM_START}:engine${BUILDER_TERM_END} tests"
+
+builder_parse "$@"
+
+if builder_start_action test:libraries; then
+  HEADLESS_FLAGS=
+
+  if builder_has_option --ci; then
+    HEADLESS_FLAGS=--ci
   fi
-}
 
-get_browser_set_for_OS ( ) {
-    if [ $os_id = "mac" ]; then
-        BROWSERS="--browsers Firefox,Chrome,Safari"
-    elif [ $os_id = "win" ]; then
-        BROWSERS="--browsers Firefox,Chrome,Edge"
-    else
-        BROWSERS="--browsers Firefox,Chrome"
-    fi
-}
+  # No --reporter option exists yet for the headless modules.
 
-# Defaults
-get_OS
-get_browser_set_for_OS
+  $KEYMAN_ROOT/common/web/keyboard-processor/build.sh test $HEADLESS_FLAGS
+  $KEYMAN_ROOT/common/web/input-processor/build.sh build:tools test $HEADLESS_FLAGS
 
-CONFIG=manual.conf.js  # TODO - get/make OS-specific version
-DEBUG=false
-FLAGS=
-HEADLESS_FLAGS=
-
-# Parse args
-while [[ $# -gt 0 ]] ; do
-    key="$1"
-    case $key in
-        -CI)
-            CONFIG=CI.conf.js
-            HEADLESS_FLAGS="$HEADLESS_FLAGS --ci"
-            ;;
-        -log-level)
-            shift
-            FLAGS="--log-level=$1 $FLAGS"
-            ;;
-        -debug)
-            # Disables the default 'run once, then done' configuration needed for CI.
-            DEBUG=true
-            FLAGS="--no-single-run $FLAGS"
-            ;;
-        -reporter)
-            shift
-            FLAGS="--reporters $1 $FLAGS"
-            ;;
-        -h)
-            display_usage
-            ;;
-        -help)
-            display_usage
-            ;;
-        -\?)
-            display_usage
-            ;;
-    esac
-    shift # past argument
-done
-
-if [ $DEBUG = true ] && [ $CONFIG = CI.conf.js ]; then
-    echo "-CI and -debug are not compatible!"
-    exit 1
+  builder_finish_action success test:libraries
 fi
-
-if [ $CONFIG = CI.conf.js ]; then
-    # If doing a CI run, use the file's default browser selection.
-    BROWSERS=
-fi
-
-BASE_PATH=`dirname $BASH_SOURCE`
-cd $BASE_PATH/../source
-
-# Compile our testing dependencies; make sure the script fails if compilation fails!
-../tools/build.sh || fail "Dev resource compilation failed."
-
-# Run our headless tests first.
-
-# First:  Web-core tests.
-pushd "$KEYMAN_ROOT/common/web/keyboard-processor"
-./build.sh test $HEADLESS_FLAGS || fail "Tests failed by dependencies; aborting integration tests."
-# Once done, now we run the integrated (KeymanWeb) tests.
-popd
-
-pushd "$KEYMAN_ROOT/common/web/input-processor"
-./build.sh build:tools test $HEADLESS_FLAGS || fail "Tests failed by dependencies; aborting integration tests."
-# Once done, now we run the integrated (KeymanWeb) tests.
-popd
 
 # Browserstack or CI-based tests
 
@@ -161,14 +66,71 @@ if [[ $VERSION_ENVIRONMENT == test ]]; then
   fi
 fi
 
-CODE=0
+get_default_browser_set ( ) {
+  # Default value, since it's the most general case/configuration to detect.
+  local os_id="linux"
 
-if $DO_BROWSER_TEST_SUITE; then
-  echo_heading "Running KeymanWeb integration test suite"
+  # Subject to change with future improvements.
+  if [[ "${OSTYPE}" = "darwin"* ]]; then
+    os_id="mac"
+  elif [[ "${OSTYPE}" = "msys" ]]; then
+    os_id="win"
+  elif [[ "${OSTYPE}" = "cygwin" ]]; then
+    os_id="win"
+  fi
+
+  if [ $os_id = "mac" ]; then
+      BROWSERS="--browsers Firefox,Chrome,Safari"
+  elif [ $os_id = "win" ]; then
+      BROWSERS="--browsers Firefox,Chrome,Edge"
+  else
+      BROWSERS="--browsers Firefox,Chrome"
+  fi
+}
+
+if builder_start_action test:engine; then
+  if builder_has_option --ci && builder_has_option --debug; then
+    echo "${COLOR_RED}Options --ci and --debug are incompatible${COLOR_RESET}"
+    exit 1
+  fi
+
+  if [[ DO_BROWSER_TEST_SUITE == false ]]; then
+    echo "${COLOR_YELLOW}Skipping action test:engine - this CI build does not appear to be for a Web PR.${COLOR_YELLOW}"
+    builder_finish_action success test:engine
+    exit 0
+  fi
+
+  # Auto-select browsers if not specified as an option
+  if ! builder_has_option --browsers; then
+    get_default_browser_set
+  fi
+
+  # Select the right CONFIG file.
+  if builder_has_option --ci; then
+    CONFIG=CI.conf.js
+  else
+    CONFIG=manual.conf.js
+  fi
+
+  # Build modernizr module
   npm --no-color run modernizr -- -c unit_tests/modernizr.config.json -d unit_tests/modernizr.js
-  npm --no-color run karma -- start $FLAGS $BROWSERS unit_tests/$CONFIG
 
-  CODE=$?
+  # Prepare the flags for the karma command.
+  KARMA_FLAGS=
+
+  if builder_has_option --debug; then
+    KARMA_FLAGS="$KARMA_FLAGS --no-single-run"
+  fi
+
+  if builder_has_option --reporters; then
+    KARMA_FLAGS="$KARMA_FLAGS --reporters $REPORTERS"
+  fi
+
+  if ! builder_has_option --ci; then
+    KARMA_FLAGS="$KARMA_FLAGS --browsers $BROWSERS"
+  fi
+
+  npm --no-color run karma -- start $KARMA_FLAGS unit_tests/$CONFIG
+
+  builder_finish_action success test:engine
 fi
-
-exit $CODE
