44d759405f51701468338fa87471c164883804aa
Marc Durdin <marc@durdin.net>
2022-10-26 09:49:57 +1100

    fix(developer): Use US base layout for debugger
    Fixes #7529.
    
    We must always use the US base layout for the debugger, as positional
    keyboards are not affected by the active Windows system keyboard. This
    fix ensures that the behaviour in the debugger matches what the user
    experiences in Keyman for Windows.
    
    Note that this does not address support for mnemonic layouts, tracked in
    issue #1074.

--- windows/src/developer/TIKE/child/UfrmDebug.pas
+++ windows/src/developer/TIKE/child/UfrmDebug.pas
@@ -221,6 +221,7 @@ uses
   RegistryKeys,
   KeymanDeveloperOptions,
   KeymanDeveloperUtils,
+  ScanCodeMap,
   TextFileFormat,
   UfrmEditor,
   UfrmKeymanWizard,
@@ -450,18 +451,22 @@ function TfrmDebug.ProcessKeyEvent(var Message: TMessage): Boolean;
     end;
   end;
 var
-  modifier: uint16_t;
+  vkey, modifier: uint16_t;
 begin
   Assert(Assigned(FDebugCore));
+
+  // We always use the US virtual key code as a basis for our keystroke
+  // mapping; the best way to do this is to extract the scan code from
+  // the message data and work from that
+  vkey := MapScanCodeToUSVK((Message.LParam and $FF0000) shr 16);
   modifier := 0;
+
   if GetKeyState(VK_LCONTROL) < 0 then modifier := modifier or KM_KBP_MODIFIER_LCTRL;
   if GetKeyState(VK_RCONTROL) < 0 then modifier := modifier or KM_KBP_MODIFIER_RCTRL;
   if GetKeyState(VK_LMENU) < 0 then modifier := modifier or KM_KBP_MODIFIER_LALT;
   if GetKeyState(VK_RMENU) < 0 then modifier := modifier or KM_KBP_MODIFIER_RALT;
   if GetKeyState(VK_SHIFT) < 0 then modifier := modifier or KM_KBP_MODIFIER_SHIFT;
   if (GetKeyState(VK_CAPITAL) and 1) = 1 then modifier := modifier or KM_KBP_MODIFIER_CAPS;
-
-  // TODO: #7529 support translation of base layout to KBDUS layout
 
   if (modifier and (KM_KBP_MODIFIER_LCTRL or KM_KBP_MODIFIER_RALT)) = (KM_KBP_MODIFIER_LCTRL or KM_KBP_MODIFIER_RALT) then
   begin
@@ -473,19 +478,19 @@ begin
   if not SetKeyEventContext then
     Exit(False);
 
-  if km_kbp_process_event(FDebugCore.State, Message.WParam, modifier, 1) = KM_KBP_STATUS_OK then
+  if km_kbp_process_event(FDebugCore.State, vkey, modifier, 1) = KM_KBP_STATUS_OK then
   begin
     // Process keystroke -- true = swallow keystroke
     Result := True;
 
-    if IsModifierKey(Message.WParam) then
+    if IsModifierKey(vkey) then
     begin
       // We don't want to trigger the debugger on modifier keys
       Exit(True);
     end;
 
     FEvents.Clear;
-    FEvents.AddStateItems(FDebugCore.State, Message.WParam, modifier, debugkeyboard);
+    FEvents.AddStateItems(FDebugCore.State, vkey, modifier, debugkeyboard);
 
     if FSingleStepMode then
     begin
