ba7523f61fcb7ca3808fecd18979352eb0adf460
Ross <writetoross@gmail.com>
2022-11-10 07:57:27 +1000

    fix(windows): check kbd file is availabe to admin user

--- windows/src/desktop/kmshell/install/Keyman.Configuration.UI.InstallFile.pas
+++ windows/src/desktop/kmshell/install/Keyman.Configuration.UI.InstallFile.pas
@@ -162,7 +162,11 @@ begin
       FilenameBCP47 := Filenames[i].Split(['=']);
       Filename := FilenameBCP47[0];
       IsPackage := AnsiSameText(ExtractFileExt(FileName), '.kmp');
-
+      if not FileExists(FileName) then
+      begin
+        ShowMessage('File: ' + Filename + ' location not available to Admin User');
+        Exit;
+      end;
       if IsPackage then
       begin
         FPackage := (kmcom.Packages as IKeymanPackagesInstalled2).Install2(FileName, True);
--- windows/src/engine/kmcomapi/processes/package/kpinstallpackage.pas
+++ windows/src/engine/kmcomapi/processes/package/kpinstallpackage.pas
@@ -143,8 +143,6 @@ begin
   try
     if not IsAdministrator then
       Error(KMN_E_Install_KeyboardMustBeInstalledByAdmin);   // I3612
-    if not TZipFile.IsValid(FileName) then
-      Error(KMN_E_Install_KeyboardMustBeInstalledByAdmin); // #TODO: 6570 a new error message explaining issue
 
     FAutoApply := Context.Control.AutoApply;
     try
@@ -166,13 +164,7 @@ begin
 
       FZip := TZipFile.Create;
       try
-        // #TODO: 6570: Check we can access the file and display an error
-
-        // try
         FZip.Open(FileName, TZipMode.zmRead);
-         // except on EZipExeception do begin
-        //        ErrorFmt(KMN_E_Install_InvalidFile, VarArrayOf([ExtractFileName(FileName), E.Message]));
-        //    end;
         InfFile := '';
 
         for i := 0 to FZip.FileCount - 1 do
