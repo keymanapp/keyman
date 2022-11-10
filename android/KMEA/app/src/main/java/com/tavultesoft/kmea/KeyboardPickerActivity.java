b7fd1109f37264e6c8954dcf62b49f86bfdc3582
Darcy Wong <darcy_wong@sil.org>
2022-11-10 07:37:30 +0700

    fix(android/engine): Use IME package name if query permission denied

--- android/KMEA/app/src/main/java/com/tavultesoft/kmea/KeyboardPickerActivity.java
+++ android/KMEA/app/src/main/java/com/tavultesoft/kmea/KeyboardPickerActivity.java
@@ -29,6 +29,7 @@ import android.content.pm.PackageInfo;
 import android.content.pm.PackageManager;
 import android.graphics.Typeface;
 import android.inputmethodservice.InputMethodService;
+import android.os.Build;
 import android.os.Bundle;
 import android.view.MenuItem;
 import android.view.View;
@@ -537,6 +538,7 @@ public final class KeyboardPickerActivity extends BaseActivity {
     PackageManager packageManager = context.getPackageManager();
     for (InputMethodInfo imeInfo : imeList) {
       String id = imeInfo.getId();
+      String imeName = "";
       if (!id.startsWith(selfPackageName)) {
         // Attempt to get a readable name
         // Reference: https://stackoverflow.com/questions/62512501/get-human-readable-name-of-default-keyboard-not-package-name
@@ -544,16 +546,24 @@ public final class KeyboardPickerActivity extends BaseActivity {
         if (componentName != null) {
           String packageName = componentName.getPackageName();
           try {
-            //PackageInfo info = packageManager.getPackageInfo(packageName, 0);
             ApplicationInfo info = packageManager.getApplicationInfo(packageName, PackageManager.GET_META_DATA);
-            String imeName = (String)packageManager.getApplicationLabel(info);
+            imeName = (String)packageManager.getApplicationLabel(info);
+          } catch (PackageManager.NameNotFoundException e) {
+            // For Android 11+, this exception is thrown because we don't have QUERY_ALL_PACKAGES permission.
+            // We'll just display the package name instead.
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
+              imeName = packageName;
+            } else {
+              KMLog.LogException(TAG, "Name not found", e);
+              continue;
+            }
+          }
+
+          if (!imeName.isEmpty()) {
             HashMap<String, String> hashMap = new HashMap<String, String>();
             hashMap.put(titleKey, imeName);
             hashMap.put(subtitleKey, id);
             list.add(hashMap);
-
-          } catch (PackageManager.NameNotFoundException e) {
-            KMLog.LogException(TAG, "Name not found", e);
           }
         }
       }
