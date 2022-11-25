af9f74fe3d300f3014f3db08192cc0a331b5f77a
Joshua A. Horton <joshua_horton@sil.org>
2022-11-25 09:18:52 +0700

    feat(common/web): util bundling now makes cjs, mjs single-file modules

--- common/web/utils/build-bundler.js
+++ common/web/utils/build-bundler.js
@@ -1,22 +1,37 @@
 /*
- * Note:  while this file is not meant to exist long-term, it provides a nice
- * low-level proof-of-concept for esbuild bundling of the various Web submodules.
- *
- * Add some extra code at the end of src/index.ts and run it to verify successful bundling!
+ * Bundles @keymanapp/web-utils as single-file modules based upon the export list in src/index.ts.
  */
 
 import esbuild from 'esbuild';
 
+// Bundles to a compact ESModule
 esbuild.buildSync({
-  entryPoints: ['build/modules/index.js'],
+  entryPoints: ['build/obj/index.js'],
   bundle: true,
   sourcemap: true,
-  minify: true,
-  keepNames: true,
+  //minify: true,    // No need to minify a module.
+  //keepNames: true,
+  format: "esm",
   // Sets 'common/web' as a root folder for module resolution;
   // this allows the keyman-version import to resolve.
   nodePaths: ['..'],
-  outfile: "build/bundled/index.js",
+  outfile: "build/lib/index.mjs",
+  tsconfig: 'tsconfig.json',
+  target: "es5"
+});
+
+// Bundles to a compact CommonJS (classic Node) module
+esbuild.buildSync({
+  entryPoints: ['build/obj/index.js'],
+  bundle: true,
+  sourcemap: true,
+  //minify: true,    // No need to minify a module.
+  //keepNames: true,
+  format: "cjs",
+  // Sets 'common/web' as a root folder for module resolution;
+  // this allows the keyman-version import to resolve.
+  nodePaths: ['..'],
+  outfile: "build/lib/index.cjs",
   tsconfig: 'tsconfig.json',
   target: "es5"
 });
