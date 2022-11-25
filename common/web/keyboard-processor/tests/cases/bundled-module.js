43cdd783bb29625c6b1dd9e3f4158e6d4b076f48
Joshua A. Horton <joshua_horton@sil.org>
2022-11-25 14:35:51 +0700

    feat(common/web): bundled-module canary unit test

---
+++ common/web/keyboard-processor/tests/cases/bundled-module.js
@@ -0,0 +1,20 @@
+import { assert } from "chai";
+import * as Package from "../../build/lib/index.mjs";
+
+// A few small tests to ensure that the ES Module bundle was successfully constructed and is usable.
+
+describe('Bundled ES Module', function() {
+  describe('KeyboardProcessor', function () {
+    it('should initialize without errors', function () {
+      let kp = new Package.KeyboardProcessor();
+      assert.isNotNull(kp);
+    });
+  });
+
+  describe("Imported `utils`", function() {
+    it("should include `utils` package's Version class", () => {
+      let v16 = new Package.Version([16, 1]);
+      assert.equal(v16.toString(), "16.1");
+    });
+  })
+});
