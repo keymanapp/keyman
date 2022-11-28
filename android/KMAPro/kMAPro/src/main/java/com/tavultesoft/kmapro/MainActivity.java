adaaa3d2b9b1c9edeaaeb0fafe9a3cefce58c385
Darcy Wong <darcy_wong@sil.org>
2022-11-28 14:11:23 +0700

    fix(android/app): Toggle keyboard update notification

--- android/KMAPro/kMAPro/src/main/java/com/tavultesoft/kmapro/MainActivity.java
+++ android/KMAPro/kMAPro/src/main/java/com/tavultesoft/kmapro/MainActivity.java
@@ -353,6 +353,7 @@ public class MainActivity extends BaseActivity implements OnKeyboardEventListene
   @SuppressLint("RestrictedApi")
   @Override
   public boolean onPrepareOptionsMenu(final Menu menu) {
+    this.menu = menu;
     final MenuItem _overflowMenuItem = menu.findItem(R.id.action_overflow);
     if (_overflowMenuItem != null) {
       MenuItem updateKeyboards = this.menu.findItem(R.id.action_update_keyboards);
@@ -372,8 +373,8 @@ public class MainActivity extends BaseActivity implements OnKeyboardEventListene
     }
 
     final MenuItem _keyboardupdate = menu.findItem(R.id.action_update_keyboards);
-    if (_keyboardupdate != null) {
-      updateUpdateCountIndicator(_keyboardupdate, anUpdateCount, true);
+    if (_keyboardupdate != null && anUpdateCount > 0) {
+      _keyboardupdate.setVisible(true);
     }
   }
 
@@ -452,6 +453,11 @@ public class MainActivity extends BaseActivity implements OnKeyboardEventListene
         KMManager.getUpdateTool().executeOpenUpdates();
         // Dismiss icon
         updateUpdateCountIndicator(0);
+        final MenuItem _keyboardupdate = menu.findItem(R.id.action_update_keyboards);
+        if (_keyboardupdate != null && _keyboardupdate.isVisible()) {
+          _keyboardupdate.setVisible(false);
+        }
+
         return true;
       default:
         return super.onOptionsItemSelected(item);
