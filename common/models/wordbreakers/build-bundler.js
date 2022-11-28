3f4a7b22cde5abbfdb74106b6f182d74c366b52b
Joshua A. Horton <joshua_horton@sil.org>
2022-11-28 08:20:12 +0700

    feat(common/models): adds .cjs bundle, .d.ts bundle

--- common/models/wordbreakers/build-bundler.js
+++ common/models/wordbreakers/build-bundler.js
@@ -6,16 +6,42 @@
  */
 
 import esbuild from 'esbuild';
+import { spawn } from 'child_process';
 
 await esbuild.build({
   bundle: true,
   sourcemap: true,
-  minify: true,
-  keepNames: true,
   format: "esm",
   nodePaths: ['..'],
   entryPoints: ['build/obj/index.js'],
-  outfile: "build/lib/index.js",
+  outfile: "build/lib/index.mjs",
   tsconfig: 'tsconfig.json',
   target: "es5"
+});
+
+
+// Bundled CommonJS (classic Node) module version
+esbuild.buildSync({
+  bundle: true,
+  sourcemap: true,
+  format: "cjs",
+  nodePaths: ['..'],
+  entryPoints: ['build/obj/index.js'],
+  outfile: "build/lib/index.cjs",
+  tsconfig: 'tsconfig.json',
+  target: "es5"
+});
+
+const dtsBundleCommand = spawn('npx dts-bundle-generator --project tsconfig.json -o build/lib/index.d.ts src/index.ts', {
+  shell: true
+});
+
+dtsBundleCommand.stdout.on('data', data =>   console.log(data.toString()));
+dtsBundleCommand.stderr.on('data', data => console.error(data.toString()));
+
+// Forces synchronicity; done mostly so that the logs don't get jumbled up.
+dtsBundleCommand.on('exit', () => {
+  if(dtsBundleCommand.exitCode != 0) {
+    process.exit(dtsBundleCommand.exitCode);
+  }
 });
