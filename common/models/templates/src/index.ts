679652d7f340f27582d1677670efb9acfa23ab9d
Joshua A. Horton <joshua_horton@sil.org>
2022-11-28 09:36:03 +0700

    change(common/models): templates submodule conversion

--- common/models/templates/src/index.ts
+++ common/models/templates/src/index.ts
@@ -1,7 +1,17 @@
-/// <reference path="./trie-model.ts" />
-/// <reference path="./quote-behavior.ts" />
+import {
+  SENTINEL_CODE_UNIT, applyTransform, buildMergedTransform, isHighSurrogate, isLowSurrogate, isSentinel,
+  transformToSuggestion, defaultApplyCasing
+} from "./common.js";
+import PriorityQueue, { Comparator } from "./priority-queue.js";
+import QuoteBehavior from "./quote-behavior.js";
+import { Tokenization, tokenize, getLastPreCaretToken, wordbreak } from "./tokenization.js";
+import TrieModel, { TrieModelOptions } from "./trie-model.js";
 
-// Add all namespaces defined here to the global scope:
-if (typeof module != 'undefined' && typeof module.exports != 'undefined') {
-  module.exports['models'] = models;
-}
+export {
+  SENTINEL_CODE_UNIT, applyTransform, buildMergedTransform, isHighSurrogate, isLowSurrogate, isSentinel,
+  transformToSuggestion, defaultApplyCasing, // "common.ts"
+  PriorityQueue, Comparator, // "priority-queue.ts"
+  QuoteBehavior, // "quote-behavior.ts",
+  Tokenization, tokenize, getLastPreCaretToken, wordbreak, // "tokenization.ts"
+  TrieModel, TrieModelOptions // "trie-model.ts"
+};
