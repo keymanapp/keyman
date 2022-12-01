49ab4d4cef00c4907c844f9b7357a714607ad0e3
Joshua A. Horton <joshua_horton@sil.org>
2022-12-01 08:28:58 +0700

    chore(web): a bit more cleanup

--- web/build.sh
+++ web/build.sh
@@ -103,9 +103,9 @@ DOC_BUILD_EMBED_WEB="${BUILDER_TERM_START}build:embed${BUILDER_TERM_END} and ${B
 DOC_TEST_SYMBOL="actions -${BUILDER_TERM_START}test${BUILDER_TERM_END}, ${BUILDER_TERM_START}upload-symbols${BUILDER_TERM_END}"
 
 builder_describe "Builds Keyman Engine for Web." \
-  "@../common/web/keyman-version build:engine build:embed build:web build:ui" \
-  "@../common/web/input-processor build:engine build:embed build:web" \
-  "@tools/sourcemap-root build:engine build:embed build:web" \
+  "@../common/web/keyman-version build" \
+  "@../common/web/input-processor build" \
+  "@tools/sourcemap-root build" \
   "clean" \
   "configure" \
   "build" \
