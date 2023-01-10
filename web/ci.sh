6ffbb2a551cca7d7d014f87600f1dc30e7b455c3
Joshua A. Horton <joshua_horton@sil.org>
2023-01-10 09:17:03 +0700

    feat(web): implements publish-downloads action

--- web/ci.sh
+++ web/ci.sh
@@ -36,6 +36,10 @@ builder_parse "$@"
 
 ####
 
+TIER=`cat ../TIER.md`
+BUILD_NUMBER=`cat ../VERSION.md`
+S_KEYMAN_COM=../../s.keyman.com
+
 if builder_start_action build; then
   # Build step:  since CI builds start (and should start) from scratch, run the following
   # three actions:
@@ -89,10 +93,6 @@ if builder_start_action validate-size; then
 fi
 
 if builder_start_action publish-s.keyman; then
-  TIER=`cat ../TIER.md`
-  BUILD_NUMBER=`cat ../VERSION.md`
-  S_KEYMAN_COM=../../s.keyman.com
-
   # First phase: make sure the s.keyman.com repo is locally-available and up to date.
   pushd "$S_KEYMAN_COM"
   if builder_has_option --password; then
@@ -130,7 +130,86 @@ if builder_start_action publish-s.keyman; then
   builder_finish_action success publish-s.keyman
 fi
 
+# Note:  for now, this command is used to prepare the artifacts used by the download site, but
+#        NOT to actually UPLOAD them via rsync or to produce related .download_info files.
 if builder_start_action publish-downloads; then
-#
+  VERSION_BUILD_REGEX="^[0-9]+\.[0-9]+\.([0-9]+)$"
+
+  if [[ $BUILD_NUMBER =~ $VERSION_BUILD_REGEX ]]; then
+    BUILD_COUNTER="${BASH_REMATCH[1]}"
+  else
+    builder_finish_action failure publish-downloads
+    exit 0
+  fi
+
+  UPLOAD_PATH="build/upload/$BUILD_NUMBER"
+
+  # --- First action artifact - the KMW zip file ---
+  ZIP="$UPLOAD_PATH/keymanweb-$BUILD_NUMBER.zip"
+
+  # RSYNC_HOME should be pre-set environment variables.
+  # (7Z_HOME is illegal as a variable name in BASH b/c leading digit.)
+  mkdir -p "$UPLOAD_PATH"
+
+  # Nifty tidbit:  https://stackoverflow.com/questions/592620/how-can-i-check-if-a-program-exists-from-a-bash-script
+  # If we're fine with ensuring that the program is available via path, we can just use that on
+  # Win machines.  The decision was made to continue relying on an environment variable for 7-zip, though.
+
+  COMPRESS_CMD=
+  COMPRESS_ADD=
+
+  # Marc's preference; use $SEVEN_Z_HOME and have the BAs set up with THAT as an env var.
+  if [ -n "${SEVEN_Z_HOME+x}" ] &> /dev/null; then
+    echo "7z command available"
+    COMPRESS_CMD="$SEVEN_Z_HOME/7z"
+    COMPRESS_ADD="a -bd -bb0 -r" # add, hide progress, log level 0, recursive
+    COMPRESS_RENAME="rn"
+  fi
+
+  if [[ -z "${COMPRESS_CMD}" ]] ; then
+    if command -v zip &> /dev/null; then
+      echo "zip command available"
+      # Note:  does not support within-archive renames!
+      COMPRESS_CMD=zip
+      COMPRESS_ADD="-r"
+    else
+      echo "${COLOR_RED}Fallback approach failed: zip command unavailable${COLOR_RESET}" >&2
+      builder_finish_action failure publish-downloads
+      exit 1
+    fi
+  fi
+
+  pushd build/app/web/release
+  "${COMPRESS_CMD}" $COMPRESS_ADD ../../../../$ZIP *
+  cd ..
+  "${COMPRESS_CMD}" $COMPRESS_ADD ../../../$ZIP debug
+  popd
+
+  pushd build/app/ui/release
+  "${COMPRESS_CMD}" $COMPRESS_ADD ../../../../$ZIP *
+  cd ..
+  "${COMPRESS_CMD}" $COMPRESS_ADD ../../../$ZIP debug
+  popd
+
+  # --- Second action artifact - the 'static' folder (hosted user testing on downloads.keyman.com) ---
+
+  echo ""
+  echo "Building \`static/\` folder for long-term hosting of testing resources..."
+  STATIC="$UPLOAD_PATH/static"
+  mkdir -p "$STATIC"
+
+  mkdir -p "$STATIC/build"
+  cp -rf build/app    "$STATIC/build/app"
+  cp -rf build/engine "$STATIC/build/engine"
+  cp -rf build/tools  "$STATIC/build/tools"
+  # avoid build/upload, since that's the folder we're building!
+
+  cp -f index.html "$STATIC/index.html"
+
+  mkdir -p "$STATIC/src/tools"
+  cp -rf src/tools/testing "$STATIC/src/tools/testing"
+  cp -rf src/test          "$STATIC/src/test"
+  cp -rf src/samples       "$STATIC/src/samples"
+
   builder_finish_action success publish-downloads
 fi
