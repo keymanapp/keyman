88c7c03cefd6200f4b838ef65d0419e577ee0cce
Eberhard Beilharz <eb1@sil.org>
2023-01-20 18:24:34 +0100

    chore(linux): Build with meson instead of autotools
    This change introduces meson for building ibus-keyman instead of
    autotools.
    
    Closes #5981.

--- linux/ibus-keyman/tests/scripts/run-tests.sh
+++ linux/ibus-keyman/tests/scripts/run-tests.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 
-TOP_SRCDIR=${top_srcdir:-$(realpath $(dirname $0)/..)}
-BASEDIR=$(realpath $(dirname $0))
+TOP_SRCDIR=${top_srcdir:-$(realpath "$(dirname "$0")/..")}
 TESTDIR=${XDG_DATA_HOME:-$HOME/.local/share}/keyman/test_kmx
-PID_FILE=/tmp/ibus-keyman-test-pids
+
+. "$(dirname "$0")"/test-helper.sh
 
 if [ -v KEYMAN_PKG_BUILD ]; then
   # During package builds we skip these tests that require to start ibus because
@@ -21,12 +21,12 @@ if ! which Xvfb > /dev/null || ! which Xephyr > /dev/null || ! which metacity > 
 fi
 
 function cleanup() {
-  if [ -f $PID_FILE ]; then
+  if [ -f "$PID_FILE" ]; then
     echo
-    echo "Shutting down processes..."
-    bash $PID_FILE > /dev/null 2>&1
-    rm $PID_FILE
-    echo "Finished shutdown of processes."
+    echo "# Shutting down processes..."
+    bash "$PID_FILE" > /dev/null 2>&1
+    rm "$PID_FILE"
+    echo "# Finished shutdown of processes."
   fi
 }
 
@@ -50,48 +50,38 @@ function help() {
   exit 0
 }
 
-function can_run_wayland() {
-  local MUTTER_VERSION
-  MUTTER_VERSION=$(mutter --version | head -1 | cut -f2 -d' ' | cut -f1 -d'.')
-  if (( $MUTTER_VERSION < 40 )); then
-    return 1
-  else
-    return 0
-  fi
-}
-
 function run_tests() {
   DISPLAY_SERVER=$1
   shift
 
-  echo > $PID_FILE
+  echo > "$PID_FILE"
   TEMP_DATA_DIR=$(mktemp --directory)
-  echo "rm -rf ${TEMP_DATA_DIR}" >> $PID_FILE
+  echo "rm -rf ${TEMP_DATA_DIR}" >> "$PID_FILE"
 
   COMMON_ARCH_DIR=
-  [ -d ${TOP_SRCDIR}/../../core/build/arch ] && COMMON_ARCH_DIR=${TOP_SRCDIR}/../../core/build/arch
-  [ -d ${TOP_SRCDIR}/../keyboardprocessor/arch ] && COMMON_ARCH_DIR=${TOP_SRCDIR}/../keyboardprocessor/arch
+  [ -d "${TOP_SRCDIR}"/../../core/build/arch ] && COMMON_ARCH_DIR=${TOP_SRCDIR}/../../core/build/arch
+  [ -d "${TOP_SRCDIR}"/../keyboardprocessor/arch ] && COMMON_ARCH_DIR=${TOP_SRCDIR}/../keyboardprocessor/arch
 
-  if [ -d ${COMMON_ARCH_DIR}/release ]; then
+  if [ -d "${COMMON_ARCH_DIR}"/release ]; then
     COMMON_ARCH_DIR=${COMMON_ARCH_DIR}/release
-  elif [ -d ${COMMON_ARCH_DIR}/debug ]; then
+  elif [ -d "${COMMON_ARCH_DIR}"/debug ]; then
     COMMON_ARCH_DIR=${COMMON_ARCH_DIR}/debug
   else
     echo "Can't find neither ${COMMON_ARCH_DIR}/release nor ${COMMON_ARCH_DIR}/debug"
     exit 2
   fi
 
-  if [ ! -d $TESTDIR ] || ! [[ $(ls -x ${TESTDIR}/*.kmx 2>/dev/null | wc -l) > 0 ]]; then
-    if [[ $(ls -x ${COMMON_ARCH_DIR}/tests/unit/kmx/*.kmx 2>/dev/null | wc -l) > 0 ]]; then
-      mkdir -p $(realpath --canonicalize-missing $TESTDIR/..)
-      ln -sf $(realpath ${COMMON_ARCH_DIR}/tests/unit/kmx) $TESTDIR
+  if [ ! -d "$TESTDIR" ] || ! [[ $(find "${TESTDIR}/" -name \*.kmx 2>/dev/null | wc -l) -gt 0 ]]; then
+    if [[ $(find "${COMMON_ARCH_DIR}/tests/unit/kmx/" -name \*.kmx 2>/dev/null | wc -l) -gt 0 ]]; then
+      mkdir -p "$(realpath --canonicalize-missing "$TESTDIR"/..)"
+      ln -sf "$(realpath "${COMMON_ARCH_DIR}"/tests/unit/kmx)" "$TESTDIR"
     else
       echo "Can't find kmx files in ${COMMON_ARCH_DIR}/tests/unit/kmx"
       exit 3
     fi
   fi
 
-  echo "NOTE: When the tests fail check /tmp/ibus-engine-keyman.log and /tmp/ibus-daemon.log!"
+  echo "# NOTE: When the tests fail check /tmp/ibus-engine-keyman.log and /tmp/ibus-daemon.log!"
   echo ""
 
   if [ "$DISPLAY_SERVER" == "wayland" ]; then
@@ -100,26 +90,27 @@ function run_tests() {
       echo "ERROR: mutter doesn't support running headless. Can't run Wayland tests."
       exit 7
     fi
-    echo "Running on Wayland..."
+    echo "# Running on Wayland..."
     TMPFILE=$(mktemp)
     # mutter-Message: 18:56:15.422: Using Wayland display name 'wayland-1'
-    mutter --wayland --headless --no-x11 --virtual-monitor 1024x768 &> $TMPFILE &
-    echo "kill -9 $!" >> $PID_FILE
+    mutter --wayland --headless --no-x11 --virtual-monitor 1024x768 &> "$TMPFILE" &
+    echo "kill -9 $!" >> "$PID_FILE"
     sleep 1s
-    export WAYLAND_DISPLAY=$(cat $TMPFILE | grep "Using Wayland display" | cut -d"'" -f2)
-    rm $TMPFILE
+    export WAYLAND_DISPLAY
+    WAYLAND_DISPLAY=$(cat "$TMPFILE" | grep "Using Wayland display" | cut -d"'" -f2)
+    rm "$TMPFILE"
   else
-    echo "Starting Xvfb..."
+    echo "# Starting Xvfb..."
     Xvfb -screen 0 1024x768x24 :33 &> /dev/null &
-    echo "kill -9 $!" >> $PID_FILE
+    echo "kill -9 $!" >> "$PID_FILE"
     sleep 1
-    echo "Starting Xephyr..."
+    echo "# Starting Xephyr..."
     DISPLAY=:33 Xephyr :32 -screen 1024x768 &> /dev/null &
-    echo "kill -9 $!" >> $PID_FILE
+    echo "kill -9 $!" >> "$PID_FILE"
     sleep 1
-    echo "Starting metacity"
+    echo "# Starting metacity"
     metacity --display=:32 &> /dev/null &
-    echo "kill -9 $!" >> $PID_FILE
+    echo "kill -9 $!" >> "$PID_FILE"
 
     export DISPLAY=:32
   fi
@@ -128,16 +119,16 @@ function run_tests() {
   SCHEMA_DIR=$TEMP_DATA_DIR/glib-2.0/schemas
   export XDG_DATA_DIRS=$TEMP_DATA_DIR:$XDG_DATA_DIRS
 
-  mkdir -p $SCHEMA_DIR
-  cp ${TOP_SRCDIR}/../keyman-config/com.keyman.gschema.xml $SCHEMA_DIR/
-  glib-compile-schemas $SCHEMA_DIR
+  mkdir -p "$SCHEMA_DIR"
+  cp "${TOP_SRCDIR}"/../keyman-config/com.keyman.gschema.xml "$SCHEMA_DIR"/
+  glib-compile-schemas "$SCHEMA_DIR"
 
   if [ $# -gt 0 ]; then
     TESTFILES=($@)
   else
-    pushd $TESTDIR > /dev/null
+    pushd "$TESTDIR" > /dev/null || exit
     TESTFILES=(*.kmx)
-    popd > /dev/null
+    popd > /dev/null || exit
   fi
 
   export LD_LIBRARY_PATH=${COMMON_ARCH_DIR}/src:$LD_LIBRARY_PATH
@@ -150,20 +141,20 @@ function run_tests() {
     IBUS_CONFIG=--config=/usr/libexec/ibus-memconf
   fi
 
-  ibus-daemon ${ARG_VERBOSE-} --panel=disable ${IBUS_CONFIG-} &> /tmp/ibus-daemon.log &
-  echo "kill -9 $!" >> $PID_FILE
+  ibus-daemon "${ARG_VERBOSE-}" --panel=disable ${IBUS_CONFIG-} &> /tmp/ibus-daemon.log &
+  echo "kill -9 $!" >> "$PID_FILE"
   sleep 1s
 
-  ../src/ibus-engine-keyman ${ARG_VERBOSE-} &> /tmp/ibus-engine-keyman.log &
-  echo "kill -9 $!" >> $PID_FILE
+  ../src/ibus-engine-keyman "${ARG_VERBOSE-}" &> /tmp/ibus-engine-keyman.log &
+  echo "kill -9 $!" >> "$PID_FILE"
   sleep 1s
 
-  echo "Starting tests..."
+  echo "# Starting tests..."
   # Note: -k and --tap are consumed by the GLib testing framework
-  ./ibus-keyman-tests ${ARG_K-} ${ARG_TAP-} ${ARG_VERBOSE-} ${ARG_DEBUG-} \
-    ${ARG_SURROUNDING_TEXT-} ${ARG_NO_SURROUNDING_TEXT-} \
-    --directory $TESTDIR --${DISPLAY_SERVER} "${TESTFILES[@]}"
-  echo "Finished tests."
+  "${G_TEST_BUILDDIR:-.}"/ibus-keyman-tests "${ARG_K-}" "${ARG_TAP-}" \
+    "${ARG_VERBOSE-}" "${ARG_DEBUG-}" "${ARG_SURROUNDING_TEXT-}" "${ARG_NO_SURROUNDING_TEXT-}" \
+    --directory "$TESTDIR" --"${DISPLAY_SERVER}" "${TESTFILES[@]}"
+  echo "# Finished tests."
 
   cleanup
 }
@@ -190,7 +181,7 @@ done
 
 if ! can_run_wayland; then
   # support for --headless got added in mutter 40.x
-  echo "WARNING: mutter doesn't support running headless. Skipping Wayland tests."
+  echo "# WARNING: mutter doesn't support running headless. Skipping Wayland tests."
   USE_WAYLAND=0
   if [ "$USE_X11" == "0" ]; then
     echo "ERROR: no tests to run. Can't run Wayland tests, and --no-x11 is specified."
@@ -198,12 +189,12 @@ if ! can_run_wayland; then
   fi
 fi
 
-if [ "$USE_WAYLAND" == "0" -a "$USE_X11" == "0" ]; then
+if [ "$USE_WAYLAND" == "0" ] && [ "$USE_X11" == "0" ]; then
   echo "ERROR: I'll have to run somewhere. Specifying both --no-wayland and --no-x11 is not allowed."
   exit 6
 fi
 
-echo > $PID_FILE
+echo > "$PID_FILE"
 trap cleanup EXIT SIGINT
 
 if [ "$USE_WAYLAND" == "1" ]; then
