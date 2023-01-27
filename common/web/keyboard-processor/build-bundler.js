b171ff342e7ee71a1456f8323a830e3a2e8693db
Joshua A. Horton <joshua_horton@sil.org>
2023-01-27 11:45:30 +0700

    chore(common/web): cleans up cross-module imports, drops namespaced export setup

--- common/web/keyboard-processor/build-bundler.js
+++ common/web/keyboard-processor/build-bundler.js
@@ -8,25 +8,6 @@
 import esbuild from 'esbuild';
 import { spawn } from 'child_process';
 
-// Browser / namespace-targeted bundle
-esbuild.buildSync({
-  entryPoints: ['build/obj/index-namespaced.js'],
-  bundle: true,
-  sourcemap: true,
-  minify: true,
-  format: "iife",
-  keepNames: true,
-  // Sets 'common/web' as a root folder for module resolution;
-  // this allows the keyman-version and utils imports to resolve.
-  //
-  // We also need to point it at the nested build output folder to resolve in-project
-  // imports when compiled - esbuild doesn't seem to pick up on the shifted base.
-  nodePaths: ['..', "build/obj"],
-  outfile: "build/lib/index.namespaced.js",
-  tsconfig: 'tsconfig.json',
-  target: "es5"
-});
-
 // Bundled ES module version
 esbuild.buildSync({
   entryPoints: ['build/obj/index.js'],
@@ -68,22 +49,9 @@ const dtsBundleCommand = spawn('npx dts-bundle-generator --project tsconfig.json
 dtsBundleCommand.stdout.on('data', data =>   console.log(data.toString()));
 dtsBundleCommand.stderr.on('data', data => console.error(data.toString()));
 
-// Forces synchronicity; done mostly so that the logs don't get jumbled up.
-dtsBundleCommand.on('exit', () => {
-  if(dtsBundleCommand.exitCode != 0) {
-    process.exit(dtsBundleCommand.exitCode);
-  }
-
-  const namespacedDtsBundleCmd = spawn('npx dts-bundle-generator --project tsconfig.json -o build/lib/index.namespaced.d.ts src/index-namespaced.ts', {
-    shell: true
-  });
-
-  namespacedDtsBundleCmd.stdout.on('data', data =>   console.log(data.toString()));
-  namespacedDtsBundleCmd.stderr.on('data', data => console.error(data.toString()));
-
-  namespacedDtsBundleCmd.on('exit', () => {
-    if(namespacedDtsBundleCmd.exitCode != 0) {
-      process.exit(namespacedDtsBundleCmd.exitCode);
-    }
-  })
-});
+// // Forces synchronicity; done mostly so that the logs don't get jumbled up.
+// dtsBundleCommand.on('exit', () => {
+//   if(dtsBundleCommand.exitCode != 0) {
+//     process.exit(dtsBundleCommand.exitCode);
+//   }
+// });
