1b4f1b7a7efd1228fdb2642d5bd7baa8257b7e54
Joshua A. Horton <joshua_horton@sil.org>
2023-01-27 12:24:05 +0700

    chore(common/models): extra-minor cleanup

--- common/models/templates/src/index.ts
+++ common/models/templates/src/index.ts
@@ -1,5 +1,3 @@
-/// <reference types="@keymanapp/models-types" />
-
 import {
   SENTINEL_CODE_UNIT, applyTransform, buildMergedTransform, isHighSurrogate, isLowSurrogate, isSentinel,
   transformToSuggestion, defaultApplyCasing
