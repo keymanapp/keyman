0367df86520693672d0f6bd1162a53d532a08b0c
Eberhard Beilharz <eb1@sil.org>
2023-02-02 19:06:55 +0100

    chore(linux): Fix a few bugs in meson scripts

--- linux/ibus-keyman/tests/scripts/find-tests.sh
+++ linux/ibus-keyman/tests/scripts/find-tests.sh
@@ -1,6 +1,16 @@
 #!/usr/bin/env bash
-cd "$1" || exit
-while IFS= read -r -d '' file; do
-  testname=$(basename "$file" .kmx)
-  printf "$(basename "$file")\t${testname#k_}\n"
-done < <(find . -name \*.kmx -print0 | sort -z)
+set -eu
+while [ -n "$1" ]; do
+  if [ ! -d "$1" ]; then
+    shift
+    continue
+  fi
+  pushd "$1" > /dev/null
+  while IFS= read -r -d '' file; do
+    testname=$(basename "$file" .kmx)
+    #shellcheck disable=SC2059
+    printf "$(basename "$file")\t${testname#k_}\n"
+  done < <(find . -name \*.kmx -print0 | sort -z)
+  popd > /dev/null
+  exit 0
+done
