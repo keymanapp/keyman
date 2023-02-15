4a2bb6b3da7e84e091635080f006922775c6455e
Steven R. Loomis <srl295@gmail.com>
2023-02-15 09:49:54 -0600

    Review comments

--- core/src/kmx/kmx_plus.cpp
+++ core/src/kmx/kmx_plus.cpp
@@ -576,9 +576,9 @@ COMP_KMXPLUS_LAYR_Helper::getKey(KMX_DWORD key) const {
 bool
 COMP_KMXPLUS_KEY2::valid(KMX_DWORD _kmn_unused(length)) const {
   if (header.size < sizeof(*this)
-      + (keyCount  * sizeof(COMP_KMXPLUS_KEY2_KEY))
+      + (keyCount    * sizeof(COMP_KMXPLUS_KEY2_KEY))
       + (flicksCount * sizeof(COMP_KMXPLUS_KEY2_FLICK_LIST))
-      + (flickCount   * sizeof(COMP_KMXPLUS_KEY2_FLICK_ELEMENT))) {
+      + (flickCount  * sizeof(COMP_KMXPLUS_KEY2_FLICK_ELEMENT))) {
     DebugLog("header.size < expected size");
     assert(false);
     return false;
@@ -589,8 +589,9 @@ COMP_KMXPLUS_KEY2::valid(KMX_DWORD _kmn_unused(length)) const {
 
 bool
 COMP_KMXPLUS_LIST::valid(KMX_DWORD _kmn_unused(length)) const {
-  if (header.size <
-      sizeof(*this) + (sizeof(COMP_KMXPLUS_LIST_ITEM) * listCount) + (sizeof(COMP_KMXPLUS_LIST_INDEX) * indexCount)) {
+  if (header.size < sizeof(*this)
+      + (listCount  * sizeof(COMP_KMXPLUS_LIST_ITEM))
+      + (indexCount * sizeof(COMP_KMXPLUS_LIST_INDEX))) {
     DebugLog("header.size < expected size");
     assert(false);
     return false;
