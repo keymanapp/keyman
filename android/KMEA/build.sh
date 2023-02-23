f075ebe3aa5d0387e1f2c1ffab324c573c357b94
Darcy Wong <darcy_wong@sil.org>
2023-02-23 10:32:38 +0700

    refactor(android/engine): Fix --debug options

--- android/KMEA/build.sh
+++ android/KMEA/build.sh
@@ -27,13 +27,14 @@ cd "$THIS_SCRIPT_PATH"
 KMA_ROOT="$KEYMAN_ROOT/android"
 KMW_ROOT="$KEYMAN_ROOT/web"
 KMEA_ASSETS="$KMA_ROOT/KMEA/app/src/main/assets"
-ARTIFACT="app-release.aar"
-
 KMW_CONFIG=release
+BUILD_FLAGS="aR -x lint -x test"    # Gradle build w/o test
+TEST_FLAGS="-x aR lint testRelease" # Gradle test w/o build
+JUNIT_RESULTS="##teamcity[importData type='junit' path='keyman\android\KMEA\app\build\test-results\testReleaseUnitTest\']"
+ARTIFACT="app-release.aar"
 
 DEBUG="debug"
 RELEASE="release"
-
 
 builder_describe "Builds Keyman Engine for Android (KMEA)." \
   "@../../web configure" \
@@ -43,13 +44,33 @@ builder_describe "Builds Keyman Engine for Android (KMEA)." \
   "test             Runs lint and unit tests." \
   ":engine          Builds KMEA"
 
-builder_describe_outputs \
-  build:engine     ./app/build/outputs/aar/keyman-android.aar
-
+# parse before describe_outputs to check debug flags
 builder_parse "$@"
+
+if builder_has_option --debug; then
+  builder_heading "### Debug config ####"
+  KMW_CONFIG="$DEBUG"
+  BUILD_FLAGS="assembleDebug -x lintDebug -x test"
+  TEST_FLAGS="-x assembleDebug lintDebug testDebug"
+  JUNIT_RESULTS="##teamcity[importData type='junit' path='keyman\android\KMEA\app\build\test-results\testDebugUnitTest\']"
+  ARTIFACT="app-debug.aar"
+fi
+
+builder_describe_outputs \
+  build:engine     ./app/build/outputs/aar/${ARTIFACT}
 
 #### Build
 
+function _copy_artifacts() {
+  echo "Copying Keyman Engine for Android to KMAPro, Sample apps, and Tests"
+  mv $KMA_ROOT/KMEA/app/build/outputs/aar/$ARTIFACT $KMA_ROOT/KMAPro/kMAPro/libs/keyman-engine.aar
+  cp $KMA_ROOT/KMAPro/kMAPro/libs/keyman-engine.aar $KMA_ROOT/Samples/KMSample1/app/libs/keyman-engine.aar
+  cp $KMA_ROOT/KMAPro/kMAPro/libs/keyman-engine.aar $KMA_ROOT/Samples/KMSample2/app/libs/keyman-engine.aar
+  cp $KMA_ROOT/KMAPro/kMAPro/libs/keyman-engine.aar $KMA_ROOT/Tests/KeyboardHarness/app/libs/keyman-engine.aar
+  if [ ! -z ${RELEASE_OEM+x} ]; then
+    cp $KMA_ROOT/KMAPro/kMAPro/libs/keyman-engine.aar $KMA_ROOT/../oem/firstvoices/android/app/libs/keyman-engine.aar
+  fi
+}
 
 #
 # Prevents 'clear' on exit of mingw64 bash shell
@@ -68,18 +89,12 @@ if [[ $VERSION_ENVIRONMENT == "local" ]] && [[ $UPLOAD_SENTRY == true ]]; then
     KMWFLAGS="$KMWFLAGS -upload-sentry"
 fi
 
-#### Build action definitions ####
-
-if builder_has_option --debug; then
-  KMW_CONFIG=debug
-  ARTIFACT="app-debug.aar"
-fi
-
 DAEMON_FLAG=
 if builder_has_option --ci; then
   DAEMON_FLAG=--no-daemon
 fi
 
+#### Build action definitions ####
 
 if builder_start_action configure; then
 
@@ -128,20 +143,15 @@ if builder_start_action build:engine; then
   echo "Gradle Build of KMEA"
   cd $KMA_ROOT/KMEA
 
-  if builder_has_option --debug; then
-    BUILD_FLAGS="assembleDebug -x lintDebug -x test"
-    ARTIFACT="app-debug.aar"
-  else
-    BUILD_FLAGS="aR -x lint -x test"
-    ARTIFACT="app-release.aar"
-  fi
-
   echo "BUILD_FLAGS $BUILD_FLAGS"
   # Build without test
   ./gradlew $DAEMON_FLAG clean $BUILD_FLAGS
   if [ $? -ne 0 ]; then
-    die "ERROR: Build of KMEA failed"
+    builder_die "ERROR: Build of KMEA failed"
   fi
+
+  # TODO: remove _copy_artifacts() when all the Android projects have builder
+  _copy_artifacts
 
   builder_finish_action success build:engine
 fi
@@ -150,46 +160,16 @@ if builder_start_action test:engine; then
   echo "Gradle test of KMEA"
   cd $KMA_ROOT/KMEA
 
-  # Gradle flags to test w/o building
-  if builder_has_option --debug; then
-    TEST_FLAGS="-x assembleDebug lintDebug testDebug"
-    ARTIFACT="app-debug.aar"
-    echo "Building debug"
-    if builder_has_option --ci; then
-      # Report JUnit test results to CI
-      echo "##teamcity[importData type='junit' path='keyman\android\KMEA\app\build\test-results\testDebugUnitTest\']"
-    fi
-  else
-    TEST_FLAGS="-x aR lint testRelease"
-    ARTIFACT="app-release.aar"
-    echo "Building release"
-    if builder_has_option --ci; then
-      # Report JUnit test results to CI
-      echo "##teamcity[importData type='junit' path='keyman\android\KMEA\app\build\test-results\testReleaseUnitTest\']"
-    fi
+  if builder_has_option --ci; then
+    # Report JUnit test results to CI
+    echo "$JUNIT_RESULTS"
   fi
 
-    die "ERROR: Build of KMEA failed"
-  echo "TEST_FLAGS $TEST_FLAGS"
+  echo "TEST_FLAGS: $TEST_FLAGS"
   ./gradlew $DAEMON_FLAG $TEST_FLAGS
   if [ $? -ne 0 ]; then
-        builder_die "ERROR: KMEA test cases failed"
+    builder_die "ERROR: KMEA test cases failed"
   fi
 
   builder_finish_action success test:engine
 fi
-
-function _copy_artifacts() {
-  echo "Copying Keyman Engine for Android to KMAPro, Sample apps, and Tests"
-  mv $KMA_ROOT/KMEA/app/build/outputs/aar/$ARTIFACT $KMA_ROOT/KMAPro/kMAPro/libs/keyman-engine.aar
-  cp $KMA_ROOT/KMAPro/kMAPro/libs/keyman-engine.aar $KMA_ROOT/Samples/KMSample1/app/libs/keyman-engine.aar
-  cp $KMA_ROOT/KMAPro/kMAPro/libs/keyman-engine.aar $KMA_ROOT/Samples/KMSample2/app/libs/keyman-engine.aar
-  cp $KMA_ROOT/KMAPro/kMAPro/libs/keyman-engine.aar $KMA_ROOT/Tests/KeyboardHarness/app/libs/keyman-engine.aar
-  if [ ! -z ${RELEASE_OEM+x} ]; then
-    cp $KMA_ROOT/KMAPro/kMAPro/libs/keyman-engine.aar $KMA_ROOT/../oem/firstvoices/android/app/libs/keyman-engine.aar
-  fi
-  cd ..\
-}
-
-# TODO: why do we need this extra brace?
-}
