fa0a77f67e1cdf01634831259ef6b099d4bb63fd
Joshua A. Horton <joshua_horton@sil.org>
2023-02-27 09:11:13 +0700

    chore(web): drops a couple of build-script vars

--- web/common.inc.sh
+++ web/common.inc.sh
@@ -1,11 +1,6 @@
 #!/usr/bin/env bash
 #
 
-#### Build utility methods + definitions ####
-
-compiler="npm run tsc --"
-compilecmd="$compiler"
-
 # Compiles all build products corresponding to the specified target.
 # This should be called from the working directory of a child project's
 # build script.
@@ -26,13 +21,13 @@ compile ( ) {
 
   local COMPILE_TARGET=$1
 
-  $compilecmd -b src/$COMPILE_TARGET -v
+  npm run tsc -- -b src/$COMPILE_TARGET -v
 
   # COMPILE_TARGET entries are all prefixed with `engine`, so remove that.
   if [ -f "./build-bundler.js" ]; then
     node "./build-bundler.js"
 
     # So... tsc does declaration-bundling on its own pretty well, at least for local development.
-    $compilecmd --emitDeclarationOnly --outFile ./build/$COMPILE_TARGET/lib/index.d.ts -p src/$COMPILE_TARGET
+    npm run tsc -- --emitDeclarationOnly --outFile ./build/$COMPILE_TARGET/lib/index.d.ts -p src/$COMPILE_TARGET
   fi
 }
