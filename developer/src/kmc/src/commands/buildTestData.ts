8f02108579cbde4805e696d19d8384acb7a66738
Marc Durdin <marc@durdin.net>
2023-02-27 17:53:25 +1100

    Update developer/src/kmc/src/commands/buildTestData.ts
    Co-authored-by: Steven R. Loomis <srl295@gmail.com>

--- developer/src/kmc/src/commands/buildTestData.ts
+++ developer/src/kmc/src/commands/buildTestData.ts
@@ -7,7 +7,7 @@ export function declareBuildTestData(program: Command) {
   program
     .command('build-test-data <infile>')
     .description('Convert keyboard test .xml to .json')
-    .option('-o, --out-file <filename>', 'where to save the resulting .kmx file')
+    .option('-o, --out-file <filename>', 'where to save the resulting .json file')
     .action(buildTestData);
 }
 
