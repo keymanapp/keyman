0811282ccf5be5aa95e6dd37d2f963f5e3f0fa33
Marc Durdin <marc@durdin.net>
2023-03-14 13:05:27 +0700

    chore(common): add common/web/build.sh, small cleanup

--- resources/builder.inc.sh
+++ resources/builder.inc.sh
@@ -1,4 +1,12 @@
 #!/usr/bin/env bash
+
+# Note: these two lines can be uncommented for debugging and profiling build
+# scripts:
+#
+#   set -x
+#   PS4='+ $EPOCHREALTIME $0 $LINENO   '
+#
+
 #
 # This script contains utilities for builder_script calls
 #
@@ -1325,7 +1333,6 @@ _builder_parse_expanded_parameters() {
     # track which dependencies have been built, so they don't get built multiple
     # times.
     # TODO: consider printing expanded builder parameters instead of shorthand
-    echo "${_params[@]}"
     builder_echo setmark "build.sh parameters: <${_params[@]}>"
     if [[ ${#builder_extra_params[@]} -gt 0 ]]; then
       builder_echo grey "build.sh extra parameters: <${builder_extra_params[@]}>"
