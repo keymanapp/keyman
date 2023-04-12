7fb7f065f3c6aae7a9b8ecca566e2c2289da0821
Ross <writetoross@gmail.com>
2023-04-12 11:14:04 +1000

    feat(windows): keyman64 project files

---
+++ windows/src/engine/keyman64/pch.h
@@ -0,0 +1,8 @@
+#ifndef PCH_H
+#define PCH_H
+#define WIN32_LEAN_AND_MEAN             // Exclude rarely-used stuff from Windows headers
+// Windows Header Files
+#include <windows.h>
+
+
+#endif //PCH_H
