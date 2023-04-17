632a7762532d63fe1d3dd1b3da73cdace56c119f
Joshua A. Horton <joshua_horton@sil.org>
2023-04-17 13:28:49 +0700

    change(web): reverts script-source path detection mechanism in attempt to fix iOS releases

--- web/src/app/webview/src/debug-main.ts
+++ web/src/app/webview/src/debug-main.ts
@@ -1,7 +1,15 @@
 import KeymanEngine from './keymanEngine.js'
 import { SourcemappedWorker } from '@keymanapp/lexical-model-layer/web'
 
-var href = window.location.href;
+ /**
+  * Determine path and protocol of executing script, setting them as
+  * construction defaults.
+  *
+  * This can only be done during load when the active script will be the
+  * last script loaded.  Otherwise the script must be identified by name.
+ */
+ var scripts = document.getElementsByTagName('script');
+ var ss = scripts[scripts.length-1].src;
+ var sPath = ss.substr(0,ss.lastIndexOf('/')+1);
 
-var pwd = href.substr(0, href.lastIndexOf('/')+1);
-window['keyman'] = new KeymanEngine(SourcemappedWorker.constructInstance(), pwd);
+window['keyman'] = new KeymanEngine(SourcemappedWorker.constructInstance(), sPath);
