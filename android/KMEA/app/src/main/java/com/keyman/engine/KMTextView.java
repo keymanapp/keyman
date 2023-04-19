75d447cecc06aae697278e6894e015f9bd01c6e5
Darcy Wong <darcy_wong@sil.org>
2023-04-19 09:07:01 +0700

    fix(android/engine): Revert change to KMTextView

--- android/KMEA/app/src/main/java/com/keyman/engine/KMTextView.java
+++ android/KMEA/app/src/main/java/com/keyman/engine/KMTextView.java
@@ -167,7 +167,7 @@ public final class KMTextView extends AppCompatEditText {
   protected void onSelectionChanged(int selStart, int selEnd) {
     super.onSelectionChanged(selStart, selEnd);
     if (activeView != null && activeView.equals(this)) {
-      if (KMManager.updateSelectionRange(KMManager.KeyboardType.KEYBOARD_TYPE_INAPP, selStart, selEnd) && !KMManager.InAppKeyboardShouldIgnoreSelectionChange) {
+      if (KMManager.updateSelectionRange(KMManager.KeyboardType.KEYBOARD_TYPE_INAPP, selStart, selEnd)) {
         KMManager.resetContext(KeyboardType.KEYBOARD_TYPE_INAPP);
       }
     }
