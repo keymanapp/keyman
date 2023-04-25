88e2bd948931941f485c7fe1397baff31bc707c1
Joshua A. Horton <joshua_horton@sil.org>
2023-04-25 09:08:23 +0700

    chore(web): removes a couple already-modularized utils

--- web/src/engine/namespaced-main/kmwutils.ts
+++ web/src/engine/namespaced-main/kmwutils.ts
@@ -165,38 +165,6 @@ namespace com.keyman {
     }
 
     /**
-     * Get orientation of tablet or phone  display
-     *
-     * @return      {boolean}
-     */
-    portraitView() { // new for I3363 (Build 301)
-      return !this.landscapeView();
-    }
-
-    /**
-     * Get orientation of tablet or phone  display
-     *
-     * @return      {boolean}
-     */
-    landscapeView(): boolean	{ // new for I3363 (Build 301)
-      var orientation: number;
-
-      // Assume portrait mode if orientation undefined
-      if(typeof window.orientation != 'undefined') { // Used by iOS Safari
-        // Else landscape for +/-90, portrait for 0, +/-180
-        orientation = window.orientation as number;
-      } else if(typeof window.screen.orientation != 'undefined') { // Used by Firefox, Chrome
-        orientation = window.screen.orientation.angle;
-      }
-
-      if(orientation !== undefined) {
-        return (Math.abs(orientation/90) == 1);
-      } else {
-        return false;
-      }
-    }
-
-    /**
      * Return height of URL bar on mobile devices, if visible
      * TODO: This does not seem to be right, so is not currently used
      *
