c7be7a1bcbcc406e400521e762931a906d58f9f1
Joshua A. Horton <joshua_horton@sil.org>
2023-05-25 08:58:52 +0700

    feat(common/models): profile for lm-worker pre-polyfill

--- common/web/lm-worker/build.sh
+++ common/web/lm-worker/build.sh
@@ -56,9 +56,13 @@ if builder_start_action build; then
   # Build worker with tsc first
   tsc -b $builder_verbose || builder_die "Could not build worker."
 
+  EXT_FLAGS=
+  if builder_has_option --ci; then
+    EXT_FLAGS=--ci
+  fi
 
   echo "Bundling worker modules"
-  node build-bundler.js
+  node build-bundler.js "$EXT_FLAGS"
 
   # Declaration bundling.
   npm run tsc -- --emitDeclarationOnly --outFile ./build/lib/index.d.ts
