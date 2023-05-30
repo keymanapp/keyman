37e31530cd3d3504edf7a9359ca539fc7737daeb
Joshua A. Horton <joshua_horton@sil.org>
2023-05-30 09:13:31 +0700

    chore(web): removes post chain-merge artifact

--- web/build.sh
+++ web/build.sh
@@ -118,7 +118,3 @@ if builder_start_action test; then
 
   builder_finish_action success test
 fi
-
-if builder_has_action build:app/ui; then
-  builder_die "Modularization work is not yet complete; builds dependent on this will fail."
-fi
