91f985de03ac9a349a7d3686845dd56fcde04d10
Marc Durdin <marc@durdin.net>
2023-06-05 16:33:38 +0700

    chore(developer): cleanup build.sh

--- developer/src/kmc-kmw/build.sh
+++ developer/src/kmc-kmw/build.sh
@@ -1,31 +1,26 @@
 #!/usr/bin/env bash
-#
-# Compiles the kmc KeymanWeb keyboard compiler.
-#
-
-# Exit on command failure and when using unset variables:
-set -eu
-
 ## START STANDARD BUILD SCRIPT INCLUDE
 # adjust relative paths as necessary
-THIS_SCRIPT="$(greadlink -f "${BASH_SOURCE[0]}" 2>/dev/null || readlink -f "${BASH_SOURCE[0]}")"
-. "$(dirname "$THIS_SCRIPT")/../../../resources/build/build-utils.sh"
+THIS_SCRIPT="$(readlink -f "${BASH_SOURCE[0]}")"
+. "${THIS_SCRIPT%/*}/../../../resources/build/build-utils.sh"
 ## END STANDARD BUILD SCRIPT INCLUDE
 
 cd "$THIS_SCRIPT_PATH"
 
 . "$KEYMAN_ROOT/resources/shellHelperFunctions.sh"
+. "$KEYMAN_ROOT/resources/build/build-utils-ci.inc.sh"
 
 builder_describe "Build Keyman kmc KMW Keyboard Compiler module" \
-  "@/common/web/keyman-version" \
   "@/common/web/types" \
   "@/developer/src/kmc-kmn" \
   "configure" \
   "build" \
   "clean" \
   "test" \
+  "pack                      build a local .tgz pack for testing" \
   "publish                   publish to npm" \
   "--dry-run,-n              don't actually publish, just dry run"
+
 builder_describe_outputs \
   configure     /node_modules \
   build         build/src/main.js
@@ -34,42 +29,40 @@ builder_parse "$@"
 
 #-------------------------------------------------------------------------------------------------------------------
 
-if builder_start_action clean; then
-  rm -rf ./build/ ./tsconfig.tsbuildinfo
-  builder_finish_action success clean
-else
-  # We need the schema file at runtime and bundled, so always copy it for all actions except `clean`
+function copy_schema() {
+  # We need the schema file at runtime and bundled, so always copy it for all
+  # actions except `clean` and `configure`
   mkdir -p "$THIS_SCRIPT_PATH/build/src/"
   cp "$KEYMAN_ROOT/common/schemas/keyman-touch-layout/keyman-touch-layout.spec.json" "$THIS_SCRIPT_PATH/build/src/"
   cp "$KEYMAN_ROOT/common/schemas/kvks/kvks.schema.json" "$THIS_SCRIPT_PATH/build/src/"
-fi
+}
 
-#-------------------------------------------------------------------------------------------------------------------
-
-if builder_start_action configure; then
-  verify_npm_setup
-  builder_finish_action success configure
-fi
-
-#-------------------------------------------------------------------------------------------------------------------
-
-if builder_start_action build; then
+function do_build() {
+  copy_schema
   npm run build
-  builder_finish_action success build
-fi
+}
 
-#-------------------------------------------------------------------------------------------------------------------
-
-if builder_start_action test; then
+function do_test() {
   # TODO: add c8 for coverage
+  copy_schema
   npm test
-  builder_finish_action success test
-fi
+}
+
+function do_publish() {
+  copy_schema
+  builder_publish_to_npm
+}
+
+function do_pack() {
+  copy_schema
+  builder_publish_to_pack
+}
 
 #-------------------------------------------------------------------------------------------------------------------
 
-if builder_start_action publish; then
-  . "$KEYMAN_ROOT/resources/build/npm-publish.inc.sh"
-  npm_publish
-  builder_finish_action success publish
-fi
+builder_run_action clean       rm -rf ./build/ ./tsconfig.tsbuildinfo
+builder_run_action configure   verify_npm_setup
+builder_run_action build       do_build
+builder_run_action test        do_test
+builder_run_action pack        do_pack
+builder_run_action publish     do_publish
