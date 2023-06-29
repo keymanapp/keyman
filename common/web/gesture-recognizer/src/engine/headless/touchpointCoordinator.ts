327a9271a529aa2dc375f57e042e0ecd12fbbebe
Joshua A. Horton <joshua_horton@sil.org>
2023-06-29 13:40:59 +0700

    refactor(web): fully headless TrackedInput, top-level class equivalent

---
+++ common/web/gesture-recognizer/src/engine/headless/touchpointCoordinator.ts
@@ -0,0 +1,45 @@
+import EventEmitter from "eventemitter3";
+import { InputEngineBase } from "./inputEngineBase.js";
+import { TrackedInput } from "./trackedInput.js";
+import { TrackedPoint } from "./trackedPoint.js";
+
+interface EventMap<HoveredItemType> {
+  /**
+   * Indicates that a new potential gesture has begun.
+   * @param input
+   * @returns
+   */
+  'inputstart': (input: TrackedInput<HoveredItemType>) => void;
+}
+
+/**
+ * This class is responsible for interpreting the output of the various input-engine types
+ * and facilitating the detection of related gestures.  Its role is to serve as a headless
+ * version of the main `GestureRecognizer` class, avoiding its DOM and DOM-event dependencies.
+ *
+ * Of particular note: when a gesture involves multiple touchpoints - like a multitap - this class
+ * is responsible for linking related touchpoints together for the detection of that gesture.
+ */
+export class TouchpointCoordinator<HoveredItemType> extends EventEmitter<EventMap<HoveredItemType>> {
+  private inputEngines: InputEngineBase<HoveredItemType>[];
+
+  private _activeInputs: {[id: string]: TrackedInput<HoveredItemType>} = {};
+
+  public constructor() {
+    super();
+    this.inputEngines = [];
+  }
+
+  protected addEngine(engine: InputEngineBase<HoveredItemType>) {
+    engine.on('pointstart', this.onNewTrackedPath);
+    this.inputEngines.push(engine);
+  }
+
+  private readonly onNewTrackedPath = (touchpoint: TrackedPoint<HoveredItemType>) => {
+    const newInput = new TrackedInput<HoveredItemType>(touchpoint);
+    this._activeInputs[touchpoint.identifier] = newInput;
+
+    this.emit('inputstart', newInput);
+    return false;
+  }
+}
--- common/web/gesture-recognizer/src/engine/inputEventEngine.ts
+++ common/web/gesture-recognizer/src/engine/inputEventEngine.ts
@@ -1,16 +1,11 @@
-import EventEmitter from "eventemitter3";
 import { GestureRecognizerConfiguration } from "./configuration/gestureRecognizerConfiguration.js";
+import { InputEngineBase } from "./headless/inputEngineBase.js";
 import { InputSample } from "./headless/inputSample.js";
 import { Nonoptional } from "./nonoptional.js";
 import { TrackedPoint } from "./headless/trackedPoint.js";
 
-interface EventMap<HoveredItemType> {
-  'pointstart': (input: TrackedPoint<HoveredItemType>) => void
-}
-
-export abstract class InputEventEngine<HoveredItemType> extends EventEmitter<EventMap<HoveredItemType>> {
+export abstract class InputEventEngine<HoveredItemType> extends InputEngineBase<HoveredItemType> {
   protected readonly config: Nonoptional<GestureRecognizerConfiguration<HoveredItemType>>;
-  private _activeTouchpoints: TrackedPoint<HoveredItemType>[] = [];
 
   public constructor(config: Nonoptional<GestureRecognizerConfiguration<HoveredItemType>>) {
     super();
@@ -19,21 +14,6 @@ export abstract class InputEventEngine<HoveredItemType> extends EventEmitter<Eve
 
   abstract registerEventHandlers(): void;
   abstract unregisterEventHandlers(): void;
-
-  /**
-   * @param identifier The identifier number corresponding to the input sequence.
-   */
-  hasActiveTouchpoint(identifier: number) {
-    return this.getTouchpointWithId(identifier) !== undefined;
-  }
-
-  private getTouchpointWithId(identifier: number) {
-    return this._activeTouchpoints.find((point) => point.rawIdentifier == identifier);
-  }
-
-  public dropTouchpointWithId(identifier: number) {
-    this._activeTouchpoints = this._activeTouchpoints.filter((point) => point.rawIdentifier != identifier);
-  }
 
   protected buildSampleFor(clientX: number, clientY: number, target: EventTarget): InputSample<HoveredItemType> {
     const targetRect = this.config.targetRoot.getBoundingClientRect();
@@ -55,7 +35,7 @@ export abstract class InputEventEngine<HoveredItemType> extends EventEmitter<Eve
     const touchpoint = new TrackedPoint<HoveredItemType>(identifier, isFromTouch);
     touchpoint.update(sample);
 
-    this._activeTouchpoints.push(touchpoint);
+    this.addTouchpoint(touchpoint);
 
     // External objects may desire to directly terminate handling of
     // input sequences under specific conditions.
@@ -89,14 +69,14 @@ export abstract class InputEventEngine<HoveredItemType> extends EventEmitter<Eve
     touchpoint.path.terminate(true);
   }
 
-  protected onInputEnd(identifier: number) {
+  protected onInputEnd(identifier: number, target: EventTarget) {
     const touchpoint = this.getTouchpointWithId(identifier);
     if(!touchpoint) {
       return;
     }
 
     const lastEntry = touchpoint.path.coords[touchpoint.path.coords.length-1];
-    const sample = this.buildSampleFor(lastEntry.clientX, lastEntry.clientY);
+    const sample = this.buildSampleFor(lastEntry.clientX, lastEntry.clientY, target);
 
     /* While an 'end' event immediately follows a 'move' if it occurred simultaneously,
      * this is decidedly _not_ the case if the touchpoint was held for a while without
@@ -107,7 +87,7 @@ export abstract class InputEventEngine<HoveredItemType> extends EventEmitter<Eve
      * recording the _timing_ of the touchpoint's release.
      */
     if(sample.t != lastEntry.t) {
-      touchpoint.update(sample, touchpoint.currentHoveredItem);
+      touchpoint.update(sample);
     }
 
     this.getTouchpointWithId(identifier)?.path.terminate(false);
