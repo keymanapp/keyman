52b8dfb205bdca4b1d252a2803ad1c8a87cbdce2
Steven R. Loomis <srl295@gmail.com>
2023-07-04 10:26:50 -0500

    feat(common): preflighting on uset 🙀
    - update docs on UnicodeSet
    - uset parser: fix/clarify that the wasm interface takes a bufferSize, but the ts interface takes a range count. This was muddled before.
    
    For: #7377

--- common/web/types/src/ldml-keyboard/unicodeset-parser-api.ts
+++ common/web/types/src/ldml-keyboard/unicodeset-parser-api.ts
@@ -6,9 +6,9 @@ export interface UnicodeSetParser {
   /**
    * Parse a UnicodeSet into ranges
    * @param pattern string to parse such as `[a-z]`
-   * @param bufferSize number of ranges to allow for
+   * @param rangeCount number of ranges to allow for
    */
-  parseUnicodeSet(pattern: string, bufferSize: number) : UnicodeSet | null;
+  parseUnicodeSet(pattern: string, rangeCount: number) : UnicodeSet | null;
   /**
    * Calculate the number of ranges in a UnicodeSet
    * @param pattern string to parse such as `[a-z]`
@@ -18,9 +18,16 @@ export interface UnicodeSetParser {
 }
 
 /**
- * Represents a parsed UnicodeSet
+ * Parsed UnicodeSet, return value of of parseUnicodeSet()
  */
 export class UnicodeSet {
+  /**
+   * A UnicodeSet in range form.
+   * For example, `[ħa-z]` will parse to
+   * ranges = `[[0x41, 0x7A], [0x0127, 0x0127]]` meaning `[a,z], [ħ,ħ]`
+   * @param pattern the pattern per UnicodeSet syntax such as `[a-z]`
+   * @param ranges array of 2-element arrays, 'start' and 'end'.
+   */
   constructor(public pattern: string, public ranges: number[][]) {
   }
   /**
