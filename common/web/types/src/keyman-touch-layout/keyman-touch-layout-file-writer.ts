c11393b834a341be1474a0494492cdca2b2bbb32
Marc Durdin <marc@durdin.net>
2023-07-05 09:40:08 +0700

    fix(developer): strip default values when compiling touch layout
    Fixes #9178.
    
    The values `pad`, `width`, and `hint` should not be emitted in a
    compiled touch layout file, if they have default values of `0`, `0`,
    or `""`, respectively. This matches the legacy kmcomp pattern.

--- common/web/types/src/keyman-touch-layout/keyman-touch-layout-file-writer.ts
+++ common/web/types/src/keyman-touch-layout/keyman-touch-layout-file-writer.ts
@@ -50,7 +50,14 @@ export class TouchLayoutFileWriter {
     // currently expects
 
     const fixupKey = (key: TouchLayoutKey | TouchLayoutSubKey) => {
-      if(Object.hasOwn(key, 'pad')) (key.pad as any) = key.pad.toString();
+      if(Object.hasOwn(key, 'pad')) {
+        if(key.pad == 0) {
+          delete key.pad;
+        }
+        else {
+          (key.pad as any) = key.pad.toString();
+        }
+      }
       if(Object.hasOwn(key, 'sp')) {
         if(key.sp == 0) {
           delete key.sp;
@@ -59,9 +66,17 @@ export class TouchLayoutFileWriter {
           (key.sp as any) = key.sp.toString();
         }
       }
-      if(Object.hasOwn(key, 'width')) (key.width as any) = key.width.toString();
+      if(Object.hasOwn(key, 'width')) {
+        if(key.width == 0) {
+          delete key.width;
+        }
+        else {
+          (key.width as any) = key.width.toString();
+        }
+      }
       if(Object.hasOwn(key, 'text') && key.text === '') delete key.text;
       if(Object.hasOwn(key, 'id') && <string>key.id === '') delete key.id;
+      if(Object.hasOwn(key, 'hint') && (<any>key).hint === '') delete (<any>key).hint;
     };
 
     const fixupPlatform = (platform: TouchLayoutPlatform) => {
