c0b3a18f8df0963060d1f4f9569988113e74c7a3
Ross <writetoross@gmail.com>
2023-07-12 13:51:49 +1000

    feat(windows): address review comments step2

--- windows/src/engine/keyman64/keyman64_proxy.cpp
+++ windows/src/engine/keyman64/keyman64_proxy.cpp
@@ -5,6 +5,7 @@
 BOOL __stdcall DllMain(HINSTANCE hinstDll, DWORD fdwReason, LPVOID reserved) {
   UNREFERENCED_PARAMETER(reserved);
   UNREFERENCED_PARAMETER(hinstDll);
+  UNREFERENCED_PARAMETER(fdwReason);
 
   return TRUE;
 }
