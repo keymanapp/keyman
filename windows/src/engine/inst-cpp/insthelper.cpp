a0d9b772a9648d380cebc01bde8cf2c56d972dce
Jieen Chen <chenjieen2012@gmail.com>
2023-08-09 06:56:51 +0200

    First draft of converting EnginePostInstall()

--- windows/src/engine/inst-cpp/insthelper.cpp
+++ windows/src/engine/inst-cpp/insthelper.cpp
@@ -1,34 +1,81 @@
-// dllmain.cpp : Defines the entry point for the DLL application.
-#include "pch.h"
+#include "insthelper.h"
+#include "pch.h"  // use stdafx.h in Visual Studio 2017 and earlier
+#include <limits.h>
+#include <utility>
+#include <iostream>
+#include <msiquery.h>
 
-BOOL APIENTRY DllMain( HMODULE hModule,
-                       DWORD  ul_reason_for_call,
-                       LPVOID lpReserved
-                     )
-{
-    switch (ul_reason_for_call)
-    {
-    case DLL_PROCESS_ATTACH:
-    case DLL_THREAD_ATTACH:
-    case DLL_THREAD_DETACH:
-    case DLL_PROCESS_DETACH:
-        break;
-    }
+std::string SysErrorMessage(DWORD errorCode) {
+  // Declare a buffer to store the error message.
+  char messageBuffer[1024];
 
-    // Import functions from other units
-    extern "C" __declspec(dllexport) void __stdcall EnginePostInstall() {
-      // Implementation of EnginePostInstall function
-    }
+  // Get the error message from the system.
+  DWORD messageLength =
+      FormatMessage(FORMAT_MESSAGE_FROM_SYSTEM, nullptr, errorCode, 0, messageBuffer, sizeof(messageBuffer), nullptr);
 
-    extern "C" __declspec(dllexport) void __stdcall PreUninstall() {
-      // Implementation of PreUninstall function
-    }
-
-    int WINAPI DllEntryPoint(HINSTANCE hinst, unsigned long reason, void* lpReserved) {
-      // Entry point of the DLL
-      return 1;
-    }
-
-    return TRUE;
+  // If the error message is not empty, return it.
+  if (messageLength > 0) {
+    return std::string(messageBuffer);
+  } else {
+    return std::string("Unknown error");
+  }
 }
 
+std::wstring GetFolderPath(REFGUID folderId) {
+  LPWSTR folderPath;
+  if (SUCCEEDED(SHGetKnownFolderPath(folderId, 0, NULL, &folderPath))) {
+    std::wstring path(folderPath);
+    CoTaskMemFree(folderPath);
+    return path;
+  }
+  return L"";
+}
+
+unsigned int ReportFailure(MSIHANDLE hInstall, std::string func, unsigned int code) {
+  MsiSetProperty(
+      hInstall, "EnginePostInstall_Error",
+      (LPCWSTR)("Keyman Engine failed to set permissions on shared data in " + func + ": " + SysErrorMessage(code)));
+}
+
+unsigned int EnginePostInstall(MSIHANDLE hInstall) {
+  HANDLE hFile;
+  std::string Path;
+
+  try {
+    Path = GetFolderPath(CSIDL_COMMON_APPDATA) + SFolderKeymanRoot;
+    if (!DirectoryExists(Path)) {
+      if (!CreateDir(Path)) {
+        return ReportFailure(hInstall, "CreateDir", GetLastError());
+      }
+    }
+
+    hFile = CreateFile((LPCWSTR)Path.c_str(), READ_CONTROL | WRITE_DAC, 0, nullptr, OPEN_EXISTING, FILE_FLAG_BACKUP_SEMANTICS, 0);
+    if (hFile == INVALID_HANDLE_VALUE) {
+      return ReportFailure(hInstall, "CreateFile", GetLastError());
+    }
+
+    try {
+      try {
+        GrantPermissionToAllApplicationPackages(hFile, GENERIC_READ | GENERIC_EXECUTE, SE_FILE_OBJECT);
+      } catch (EOSError &E) {
+        return ReportFailure(hInstall, "GrantPermission", E.ErrorCode);
+      }
+
+      return ERROR_SUCCESS;
+
+    } finally {
+      if (!CloseHandle(hFile)) {
+        return ReportFailure(hInstall, "CloseHandle", GetLastError());
+      }
+    }
+  } catch (Exception &E) {
+    MsiSetProperty(
+        hInstall, "EnginePostInstall_Error", (LPCWSTR)("Keyman Engine failed to set permissions on shared data, " + E.what()));
+    // We don't have an error value so we'll just return !success
+    return ERROR_INVALID_FUNCTION;
+  }
+}
+
+unsigned int PreUninstall() {
+  return 1;
+}
--- windows/src/engine/inst-cpp/insthelper.h
+++ windows/src/engine/inst-cpp/insthelper.h
@@ -1,1 +1,10 @@
 #pragma once
+#include<Msi.h>
+
+#define INSTHELPER_API __declspec(dllimport)
+
+extern "C" INSTHELPER_API unsigned int ReportFailure(MSIHANDLE hInstall, std::string func, unsigned int code);
+
+extern "C" INSTHELPER_API unsigned int EnginePostInstall(MSIHANDLE hInstall);
+
+extern "C" INSTHELPER_API unsigned int PreUninstall();
