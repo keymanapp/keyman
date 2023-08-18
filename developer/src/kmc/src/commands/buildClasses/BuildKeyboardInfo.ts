c3eb2879d7ebe86f98ee5cca864c4afd75635fad
Marc Durdin <marc@durdin.net>
2023-08-18 14:57:48 +0700

    chore(developer): handle legacy folder specially for .keyboard_info

--- developer/src/kmc/src/commands/buildClasses/BuildKeyboardInfo.ts
+++ developer/src/kmc/src/commands/buildClasses/BuildKeyboardInfo.ts
@@ -1,4 +1,5 @@
 import * as fs from 'fs';
+import * as path from 'path';
 import { BuildActivity } from './BuildActivity.js';
 import { CompilerCallbacks, CompilerOptions, KeymanDeveloperProject, KeymanFileTypes } from '@keymanapp/common-types';
 import { KeyboardInfoCompiler } from '@keymanapp/kmc-keyboard-info';
@@ -12,7 +13,14 @@ export class BuildKeyboardInfo extends BuildActivity {
   public get sourceExtension(): KeymanFileTypes.Source { return KeymanFileTypes.Source.KeyboardInfo; }
   public get compiledExtension(): KeymanFileTypes.Binary { return KeymanFileTypes.Binary.KeyboardInfo; }
   public get description(): string { return 'Build a keyboard metadata file'; }
+
   public async build(infile: string, callbacks: CompilerCallbacks, options: CompilerOptions): Promise<boolean> {
+    const sourcePath = calculateSourcePath(infile);
+
+    if(sourcePath.startsWith('legacy/')) {
+      return this.buildLegacy(infile, callbacks);
+    }
+
     if(KeymanFileTypes.filenameIs(infile, KeymanFileTypes.Source.KeyboardInfo)) {
       // We are given a .keyboard_info but need to use the project file in the
       // same folder, so that we can find the related files. This also supports
@@ -30,14 +38,14 @@ export class BuildKeyboardInfo extends BuildActivity {
       return false;
     }
 
-    const metadata = findProjectFile(callbacks, project, KeymanFileTypes.Source.KeyboardInfo);
+    const metadata = this.findProjectFile(callbacks, project, KeymanFileTypes.Source.KeyboardInfo);
     if(!metadata) {
       // Project loader should always have added a metadata file
       return false;
     }
 
-    const keyboard = findProjectFile(callbacks, project, KeymanFileTypes.Source.KeymanKeyboard);
-    const kps = findProjectFile(callbacks, project, KeymanFileTypes.Source.Package);
+    const keyboard = this.findProjectFile(callbacks, project, KeymanFileTypes.Source.KeymanKeyboard);
+    const kps = this.findProjectFile(callbacks, project, KeymanFileTypes.Source.Package);
     if(!keyboard || !kps)  {
       return false;
     }
@@ -51,7 +59,7 @@ export class BuildKeyboardInfo extends BuildActivity {
       kmpFilename:  project.resolveOutputFilePath(kps, KeymanFileTypes.Source.Package, KeymanFileTypes.Binary.Package),
       kpsFilename: project.resolveInputFilePath(kps),
       jsFilename: fs.existsSync(jsFilename) ? jsFilename : undefined,
-      sourcePath: calculateSourcePath(infile),
+      sourcePath,
       lastCommitDate
     });
 
@@ -67,12 +75,39 @@ export class BuildKeyboardInfo extends BuildActivity {
 
     return true;
   }
-}
 
-function findProjectFile(callbacks: CompilerCallbacks, project: KeymanDeveloperProject, ext: KeymanFileTypes.Source) {
-  const file = project.files.find(file => file.fileType == ext);
-  if(!file) {
-    callbacks.reportMessage(InfrastructureMessages.Error_FileTypeNotFound({ext}));
+  /**
+   * Special case for legacy .keyboard_info files: we just copy the static data into
+   * the build/ folder.
+   */
+  private buildLegacy(infile: string, callbacks: CompilerCallbacks) {
+    if(!KeymanFileTypes.filenameIs(infile, KeymanFileTypes.Source.KeyboardInfo)) {
+      // We must have a .keyboard_info for legacy projects
+      callbacks.reportMessage(InfrastructureMessages.Error_FileTypeNotRecognized({
+        filename: infile,
+        extensions: KeymanFileTypes.Source.KeyboardInfo
+      }));
+      return false;
+    }
+
+    // For legacy projects, we are fine to hardcode the output path to build/
+    const outputFilename = path.join(path.dirname(infile), 'build', path.basename(infile));
+
+    try {
+      fs.copyFileSync(infile, outputFilename);
+    } catch(e) {
+      callbacks.reportMessage(InfrastructureMessages.Fatal_UnexpectedException({e}));
+      return false;
+    }
+
+    return true;
   }
-  return file;
+
+  findProjectFile(callbacks: CompilerCallbacks, project: KeymanDeveloperProject, ext: KeymanFileTypes.Source) {
+    const file = project.files.find(file => file.fileType == ext);
+    if(!file) {
+      callbacks.reportMessage(InfrastructureMessages.Error_FileTypeNotFound({ext}));
+    }
+    return file;
+  }
 }
