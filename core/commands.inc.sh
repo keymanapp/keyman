4c206e50a786b53a2b68b26d946505a163ebe6e2
Marc Durdin <marc@durdin.net>
2023-10-03 15:48:43 +0700

    fix(core): clean cached ICU in core
    Fixes #9636.

--- core/commands.inc.sh
+++ core/commands.inc.sh
@@ -11,7 +11,19 @@ do_clean() {
   # clean: note build/<target> will be left, but build/<target>/<configuration> should be gone
   local target=$1
   builder_start_action clean:$target || return 0
+
   rm -rf "$MESON_PATH"
+
+  # Removes ICU cached components so it will be re-downloaded and built. Note:
+  # we could use `git clean`, but this clarifies exactly what is deleted, and
+  # but we try not to use git commands in build scripts, to maintain clear
+  # responsibility
+  rm -rf \
+    "$THIS_SCRIPT_PATH/subprojects/packagecache" \
+    "$THIS_SCRIPT_PATH/subprojects/icu" \
+    "$THIS_SCRIPT_PATH/subprojects/*.tgz" \
+    "$THIS_SCRIPT_PATH/subprojects/*.zip"
+
   builder_finish_action success clean:$target
 }
 
