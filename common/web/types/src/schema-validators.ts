00484a0e0f39166dbec2cdff817a2aea62dc025a
Marc Durdin <marc@durdin.net>
2023-10-07 06:46:10 +0700

    chore: fixup js imports for schema validators

--- common/web/types/src/schema-validators.ts
+++ common/web/types/src/schema-validators.ts
@@ -1,28 +1,11 @@
-//@ts-expect-error
 import kpj from './schemas/kpj.schema.validator.mjs';
-
-//@ts-expect-error
 import kpj90 from './schemas/kpj-9.0.schema.validator.mjs';
-
-//@ts-expect-error
 import kvks from './schemas/kvks.schema.validator.mjs';
-
-//@ts-expect-error
 import ldmlKeyboard3 from './schemas/ldml-keyboard3.schema.validator.mjs';
-
-//@ts-expect-error
 import ldmlKeyboardTest3 from './schemas/ldml-keyboardtest3.schema.validator.mjs';
-
-//@ts-expect-error
 import displayMap from './schemas/displaymap.schema.validator.mjs';
-
-//@ts-expect-error
 import touchLayoutClean from './schemas/keyman-touch-layout.clean.spec.validator.mjs';
-
-//@ts-expect-error
 import touchLayout from './schemas/keyman-touch-layout.spec.validator.mjs';
-
-//@ts-expect-error
 import keyboard_info from './schemas/keyboard_info.schema.validator.mjs';
 
 // How to use: https://ajv.js.org/standalone.html#using-the-validation-function-s
