90f9baf4ddf19f1b8e0662eaaad48bff4a67948e
Joshua A. Horton <joshua_horton@sil.org>
2023-10-20 12:59:25 +0700

    feat(web): prototype flick preview styling

--- web/src/engine/osk/src/keyboard-layout/gesturePreviewHost.ts
+++ web/src/engine/osk/src/keyboard-layout/gesturePreviewHost.ts
@@ -1,9 +1,13 @@
 import { ActiveKey } from "@keymanapp/keyboard-processor";
 import { KeyElement } from "../keyElement.js";
 
+const FLICK_DIRS = ['n', 'ne', 'e', 'se', 's', 'sw', 'w', 'nw'] as const;
+
 export class GesturePreviewHost {
   private readonly div: HTMLDivElement;
   private readonly label: HTMLSpanElement;
+
+  private flickPreviews: HTMLDivElement[] = [];
 
   get element(): HTMLDivElement {
     return this.div;
@@ -24,5 +28,45 @@ export class GesturePreviewHost {
 
     // Re-use the text value from the base key's label.
     label.textContent = key.key.label.textContent;
+
+    for(const dir of FLICK_DIRS) {
+      const index = FLICK_DIRS.indexOf(dir);
+      const isDiag = (index % 2) == 1;
+
+      const arrowEle = document.createElement('div');
+      arrowEle.className = 'kmw-flick-preview';
+      let angle: number;
+
+      if(dir.includes('w')) {
+        arrowEle.style.left = isDiag ? '15%' : '5%';
+        arrowEle.textContent = '\u2329';
+        angle = (index - 6) * 45;
+      } else if(dir.includes('e')) {
+        arrowEle.style.right = isDiag ? '15%' : '5%';
+        arrowEle.textContent = '\u232a';
+        angle = (index - 2) * 45;
+      } else {
+        arrowEle.style.left = '50%';
+        arrowEle.style.transform = 'translateX(-50%)';
+        angle = 0;
+      }
+
+      if(dir.includes('n')) {
+        arrowEle.style.top = isDiag ? '5%' : '-5%';
+        arrowEle.textContent ||= '\ufe3f';
+      } else if(dir.includes('s')) {
+        arrowEle.style.bottom = isDiag ? '5%' : '-5%';
+        arrowEle.textContent ||= '\ufe40';
+      } else {
+        arrowEle.style.top = '50%';
+        arrowEle.style.transform = 'translateY(-50%) ';
+      }
+
+      // const angle = (index - 2) * 45;
+      arrowEle.style.transform = arrowEle.style.transform + `rotate(${angle}deg)`;
+
+      this.flickPreviews.push(arrowEle);
+      this.div.appendChild(arrowEle);
+    }
   }
 }
