61dd31e66f029fb3c07059655a687480315fded9
chenjieen <chenjieen2012@gmail.com>
2023-11-01 07:17:58 +0100

    Add entrypoint

--- windows/src/engine/inst-cpp/insthelper.h
+++ windows/src/engine/inst-cpp/insthelper.h
@@ -2,8 +2,17 @@
 #include <Windows.h>
 #include <Msi.h>
 
-#define INSTHELPER_API __declspec(dllimport)
+extern "C" __declspec(dllexport) unsigned int EnginePostInstall(MSIHANDLE hInstall);
 
-extern "C" INSTHELPER_API unsigned int EnginePostInstall(MSIHANDLE hInstall);
+extern "C" __declspec(dllexport) unsigned int PreUninstall();
 
-extern "C" INSTHELPER_API unsigned int PreUninstall();
+BOOL APIENTRY DllMain(HMODULE hModule, DWORD ul_reason_for_call, LPVOID lpReserved) {
+  switch (ul_reason_for_call) {
+  case DLL_PROCESS_ATTACH:
+  case DLL_THREAD_ATTACH:
+  case DLL_THREAD_DETACH:
+  case DLL_PROCESS_DETACH:
+    break;
+  }
+  return TRUE;
+}
