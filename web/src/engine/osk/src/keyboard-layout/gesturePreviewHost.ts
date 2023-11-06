1136b838fbde5e5cbd158b5eeb7a2c5bebbb9588
Joshua A. Horton <joshua_horton@sil.org>
2023-11-06 11:40:45 +0700

    fix(web): better consistency for flick-animation b/t phone and tablet

--- web/src/engine/osk/src/keyboard-layout/gesturePreviewHost.ts
+++ web/src/engine/osk/src/keyboard-layout/gesturePreviewHost.ts
@@ -3,6 +3,16 @@ import { KeyElement } from "../keyElement.js";
 import { FlickNameCoordMap, OrderedFlickDirections } from "../input/gestures/browser/flick.js";
 
 const FLICK_DIRS = ['n', 'ne', 'e', 'se', 's', 'sw', 'w', 'nw'] as const;
+
+
+
+/**With edge lengths of 1, to keep flick-text invisible at the start, the
+ * hypotenuse for an inter-cardinal path is sqrt(2).  To keep a perfect circle
+ * for all flicks, then, requires the straight-edge length for pure cardinal
+ * paths to match - sqrt(2).
+ */
+const FLICK_OVERFLOW_OFFSET = 1.4142;
+
 export class GesturePreviewHost {
   private readonly div: HTMLDivElement;
   private readonly label: HTMLSpanElement;
@@ -41,6 +51,8 @@ export class GesturePreviewHost {
     label.textContent = key.key.label.textContent;
 
     this.div.appendChild(this.previewImgContainer);
+    const width = Number.parseInt(getComputedStyle(this.div).width, 10) || this.flickEdgeLength;
+    const height = Number.parseInt(getComputedStyle(this.div).height, 10) || this.flickEdgeLength;
 
     if(keySpec.flick) {
       const flickSpec = keySpec.flick || {};
@@ -52,36 +64,30 @@ export class GesturePreviewHost {
 
         const ps /* preview style */ = flickPreview.style;
 
-        /* With edge lengths of 1, to keep flick-text invisible at the start, the
-         * hypotenuse for an inter-cardinal path is sqrt(2).  To keep a perfect circle
-         * for all flicks, then, requires the straight-edge length for pure cardinal
-         * paths to match - sqrt(2).
-         */
-        const OVERFLOW_OFFSET = 1.4142;
-
         // is in polar coords, origin toward north, clockwise.
         const coords = FlickNameCoordMap.get(dir);
         const x = -Math.sin(coords[0]); // Put 'e' flick at left
         const y =  Math.cos(coords[0]); // Put 'n' flick at bottom
 
+        ps.width = width + 'px';
+        ps.textAlign = 'center';
+
         if(x < 0) {
-          ps.right = (-x * OVERFLOW_OFFSET * edgeLength) + 'px';
+          ps.right = (-x * FLICK_OVERFLOW_OFFSET * edgeLength) + 'px';
         } else if(x > 0) {
-          ps.left  = ( x * OVERFLOW_OFFSET * edgeLength) + 'px';
+          ps.left  = ( x * FLICK_OVERFLOW_OFFSET * edgeLength) + 'px';
         } else {
           ps.left = '0px';
-          ps.right = '0px';
-          ps.textAlign = 'center';
         }
 
+        ps.height = height + 'px';
+        ps.lineHeight = height + 'px';
         if(y < 0) {
-          ps.bottom = (-y * OVERFLOW_OFFSET * edgeLength) + 'px';
+          ps.bottom = (-y * FLICK_OVERFLOW_OFFSET * edgeLength) + 'px';
         } else if(y > 0) {
-          ps.top    = ( y * OVERFLOW_OFFSET * edgeLength) + 'px';
+          ps.top    = ( y * FLICK_OVERFLOW_OFFSET * edgeLength) + 'px';
         } else {
           ps.top = '0px';
-          ps.bottom = '0px';
-          ps.lineHeight = '100%';
         }
 
         this.flickPreviews.set(dir, flickPreview);
@@ -116,7 +122,7 @@ export class GesturePreviewHost {
 
   public scrollFlickPreview(x: number, y: number) {
     const scrollStyle = this.previewImgContainer.style;
-    const edge = this.flickEdgeLength;
+    const edge = this.flickEdgeLength * FLICK_OVERFLOW_OFFSET;
 
     scrollStyle.marginLeft = `${edge * x}px`;
     scrollStyle.marginTop =  `${edge * y}px`;
