568e85b153f5ac5641d969949199427950fc1a6c
Joshua A. Horton <joshua_horton@sil.org>
2023-11-20 15:20:16 +0700

    chore(web): reverts prior commit

--- web/test.sh
+++ web/test.sh
@@ -41,9 +41,8 @@ if builder_start_action test:libraries; then
 
   # For now, we'll also link in the gesture-recognizer unit tests here.
   builder_heading "Running gesture-recognizer test suite"
-  # pushd "$KEYMAN_ROOT/common/web/gesture-recognizer"
-  # npm run test -- $HEADLESS_FLAGS
-  # popd
+  "$KEYMAN_ROOT/common/web/gesture-recognizer/build.sh" test $HEADLESS_FLAGS
+  popd
 
   builder_finish_action success test:libraries
 fi
