384ee3b189c7c5a9a2a3428acdc4aed22e7a6611
Steven R. Loomis <srl295@gmail.com>
2024-01-19 14:30:19 -0600

    feat(developer): ldml marker normalization in ts 🙀
    - C++ build fix
    
    For: #10317

--- core/src/ldml/ldml_markers.cpp
+++ core/src/ldml/ldml_markers.cpp
@@ -13,6 +13,7 @@
 #include <assert.h>
 
 #include "ldml_utils.hpp"
+#include <ldml/keyman_core_ldml.h>
 
 namespace km {
 namespace core {
@@ -24,7 +25,7 @@ const std::u32string REGEX_PREFIX       = U"\\uffff\\u0008";
 const std::u32string RAW_PREFIX         = U"\uffff\u0008";
 const std::u32string REGEX_ANY_MATCH    = U"[\\u0001-\\ud7fe]";
 const KMX_DWORD LDML_MARKER_NO_INDEX    = 0; // TODO: move to .ts
-static_assert(LDML_MARKER_NO_INDEX < LDML_MARKER_MIN);
+static_assert(LDML_MARKER_NO_INDEX < LDML_MARKER_MIN_INDEX);
 
 // string manipulation
 
