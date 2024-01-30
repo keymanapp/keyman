d8c232dc7c3e4d693db1f4bed08bd9029519c7d2
rc-swag <58423624+rc-swag@users.noreply.github.com>
2024-01-30 14:07:55 +1000

    feat(windows): refactor to use km_core_actions struct

--- windows/src/engine/keyman32/kmprocessactions.cpp
+++ windows/src/engine/keyman32/kmprocessactions.cpp
@@ -6,56 +6,77 @@
  */
 #include "pch.h"
 
-static BOOL processUnicodeChar(AITIP* app, const km_core_action_item* actionItem) {
-  if (Uni_IsSMP(actionItem->character)) {
-    app->QueueAction(QIT_CHAR, (Uni_UTF32ToSurrogate1(actionItem->character)));
-    app->QueueAction(QIT_CHAR, (Uni_UTF32ToSurrogate2(actionItem->character)));
-  }
-  else {
-    app->QueueAction(QIT_CHAR, actionItem->character);
-  }
-  return TRUE;
-}
-
-static BOOL processMarker(AITIP* app, const km_core_action_item* actionItem) {
-  app->QueueAction(QIT_DEADKEY, (DWORD)actionItem->marker);
-  return TRUE;
-}
-
-static BOOL processAlert(AITIP* app) {
-  app->QueueAction(QIT_BELL, 0);
-  return TRUE;
-}
-
-static BOOL processBack(AITIP* app, const km_core_action_item* actionItem) {
-  if (actionItem->backspace.expected_type == KM_CORE_BT_MARKER) {
-    app->QueueAction(QIT_BACK, BK_DEADKEY);
-  } else if(actionItem->backspace.expected_type == KM_CORE_BT_CHAR) {
-    // If this is a TSF-aware app we need to set the BK_SURROGATE flag to delete
-    // both parts of the surrogate pair. Legacy apps receive a BKSP WM_KEYDOWN event
-    // which results in deleting both parts in one action.
-    if (!app->IsLegacy() && Uni_IsSMP(actionItem->backspace.expected_value)) {
-      app->QueueAction(QIT_BACK, BK_DEFAULT | BK_SURROGATE);
+static void
+processUnicodeChar(AITIP* app, const km_core_usv* actionItem) {
+  while (*actionItem) {
+    if (Uni_IsSMP(*actionItem)) {
+      app->QueueAction(QIT_CHAR, (Uni_UTF32ToSurrogate1(*actionItem)));
+      app->QueueAction(QIT_CHAR, (Uni_UTF32ToSurrogate2(*actionItem)));
     } else {
-      app->QueueAction(QIT_BACK, BK_DEFAULT);
+      app->QueueAction(QIT_CHAR, *actionItem);
     }
-  } else { // KM_CORE_BT_UNKNOWN
-    app->QueueAction(QIT_BACK, BK_DEFAULT);
+    actionItem++;
   }
-  return TRUE;
 }
 
-static BOOL processPersistOpt(
-  const km_core_action_item* actionItem,
+static void processAlert(AITIP* app) {
+  app->QueueAction(QIT_BELL, 0);
+}
+
+static BOOL
+processBack(AITIP* app, const unsigned int code_points_to_delete) {
+
+  if (app->IsLegacy()) {
+    for (unsigned int i = 0; i < code_points_to_delete; i++) {
+        app->QueueAction(QIT_BACK, BK_DEFAULT);
+    }
+    return TRUE;
+  }
+
+  if (!app->IsLegacy()) {
+    WCHAR application_context[MAXCONTEXT];
+    DWORD cp_to_delete = code_points_to_delete;
+    if (!app->ReadContext(application_context)) {
+      SendDebugMessageFormat(0, sdmGlobal, 0, "processBack: Error reading context from application.");
+    } else {
+      // Find the length of the string
+      int length = 0;
+      while (application_context[length] != L'\0') {
+        length++;
+      }
+      // Read each character starting from caret
+      for (int i = length - 1; i >= 0 && cp_to_delete > 0; i--) {
+        // Need to consider malformed context where only one surrogate is present, at either the start or end of the context.
+        // In both these scenarios just perform one backspace one the same as if it was a non surrogate pair.
+        if ((i > 0) && (Uni_IsSurrogate1(application_context[i - 1]) && Uni_IsSurrogate2(application_context[i]))) {
+          app->QueueAction(QIT_BACK, BK_DEFAULT | BK_SURROGATE);
+          i--;
+          cp_to_delete--;
+          }
+        else {
+          app->QueueAction(QIT_BACK, BK_DEFAULT);
+          cp_to_delete--;
+        }
+      }
+    }
+    return TRUE;
+  }
+  return FALSE;
+}
+
+static void
+processPersistOpt(km_core_actions const* actions,
   km_core_state* keyboardState,
   LPINTKEYBOARDINFO activeKeyboard
 ) {
-  if (actionItem->option != NULL)
-  {
+
+
+  for (auto option = actions->persist_options; option->key; option++) {
+    // TODO: Do we need really to write the option back to the core?
     // Allocate for 1 option plus 1 pad struct of 0's for KM_CORE_IT_END
     km_core_option_item keyboardOpts[2] = { 0 };
-    keyboardOpts[0].key = actionItem->option->key;
-    keyboardOpts[0].value     = actionItem->option->value;
+    keyboardOpts[0].key                 = option->key;
+    keyboardOpts[0].value               = option->value;
     km_core_status eventStatus = (km_core_status_codes)km_core_state_options_update(keyboardState, keyboardOpts);
     if (eventStatus != KM_CORE_STATUS_OK)
     {
@@ -64,36 +85,26 @@ static BOOL processPersistOpt(
     }
 
     // Put the keyboard option into Windows Registry
-    if (actionItem->option != NULL && actionItem->option->key != NULL &&
-      actionItem->option->value != NULL)
-    {
-      // log"Saving keyboard option to registry");
-      SendDebugMessageFormat(0, sdmGlobal, 0, "ProcessHook: Saving option to registry for keyboard [%s].", activeKeyboard->Name);
-      LPWSTR value = new WCHAR[sizeof(actionItem->option->value) + 1];
-      wcscpy_s(value, sizeof(actionItem->option->value) + 1, reinterpret_cast<LPCWSTR>(actionItem->option->value));
-      SaveKeyboardOptionCoretoRegistry(activeKeyboard, reinterpret_cast<LPCWSTR>(actionItem->option->key), value);
-    }
+    // log"Saving keyboard option to registry");
+    SendDebugMessageFormat(0, sdmGlobal, 0, "ProcessHook: Saving option to registry for keyboard [%s].", activeKeyboard->Name);
+    size_t value_length = wcslen(reinterpret_cast<LPCWSTR>(option->value));
+    LPWSTR value = new WCHAR[value_length + 1];
+    wcscpy_s(value, value_length + 1, reinterpret_cast<LPCWSTR>(option->value));
+    SaveKeyboardOptionCoretoRegistry(activeKeyboard, reinterpret_cast<LPCWSTR>(option->key), value);
+    delete[] value;
+
   }
-  return TRUE;
 }
 
-static BOOL processInvalidateContext(
-  AITIP* app
-) {
-  app->ResetContext();
-  return TRUE;
-}
-
-static BOOL
-processCapsLock(const km_core_action_item* actionItem, BOOL isUp, BOOL Updateable, BOOL externalEvent) {
+static void processCapsLock(const km_core_caps_state caps_lock_state, BOOL isUp, BOOL Updateable, BOOL externalEvent) {
 
   // We only want to process the Caps Lock key event once --
   // in the first pass (!Updateable).
   if (Updateable){
-    return TRUE;
+    return;
   }
 
-  if (actionItem->capsLock) {
+  if (caps_lock_state == KM_CORE_CAPS_ON) {
     // This case would occur for the keyboard system store setting `store(&CapsOnOnly) '1'`
     if (isUp && !IsCapsLockOn()) {  // I267 - 24/11/2006 invert GetKeyState test
       SendDebugMessageFormat(0, sdmGlobal, 0, "processCapsLock: TURN CAPS ON: FIsUp=%d CapsState=%d", isUp, IsCapsLockOn());
@@ -109,8 +120,7 @@ processCapsLock(const km_core_action_item* actionItem, BOOL isUp, BOOL Updateabl
       keybd_event(VK_CAPITAL, SCAN_FLAG_KEYMAN_KEY_EVENT, KEYEVENTF_KEYUP, 0);
       keybd_event(VK_CAPITAL, SCAN_FLAG_KEYMAN_KEY_EVENT, 0, 0);
     }
-  }
-  else {
+  } else if (caps_lock_state == KM_CORE_CAPS_OFF) {
     // This case would occur for the keyboard system store setting `store(&ShiftFreesCaps) '1'`
     // OR selecting a keyboard with CAPs always off rule
     if ((!isUp && IsCapsLockOn()) || (externalEvent && IsCapsLockOn())) {
@@ -119,8 +129,6 @@ processCapsLock(const km_core_action_item* actionItem, BOOL isUp, BOOL Updateabl
       keybd_event(VK_CAPITAL, SCAN_FLAG_KEYMAN_KEY_EVENT, KEYEVENTF_KEYUP, 0);
     }
   }
-
-  return TRUE;
 }
 
 BOOL ProcessActions(BOOL* emitKeyStroke)
@@ -131,46 +139,20 @@ BOOL ProcessActions(BOOL* emitKeyStroke)
   _td->CoreProcessEventRun = FALSE;
   // Process the action items from the core. This actions will modify the windows context (AppContext).
   // Therefore it is not required to copy the context from the core to the windows context.
+  km_core_actions const* actions = km_core_state_get_actions(_td->lpActiveKeyboard->lpCoreKeyboardState);
 
-  for (auto act = km_core_state_action_items(_td->lpActiveKeyboard->lpCoreKeyboardState, nullptr); act->type != KM_CORE_IT_END; act++) {
-    BOOL continueProcessingActions = TRUE;
-    SendDebugMessageFormat(0, sdmGlobal, 0, "ProcessActions : act->type=%d", act->type);
-    switch (act->type) {
-    case KM_CORE_IT_CHAR:
-      continueProcessingActions = processUnicodeChar(_td->app, act);
-      break;
-    case KM_CORE_IT_MARKER:
-      continueProcessingActions = processMarker(_td->app, act);
-      break;
-    case KM_CORE_IT_ALERT:
-      continueProcessingActions = processAlert(_td->app);
-      break;
-    case KM_CORE_IT_BACK:
-      continueProcessingActions = processBack(_td->app, act);
-      break;
-    case KM_CORE_IT_PERSIST_OPT:
-      continueProcessingActions = processPersistOpt(act, _td->lpActiveKeyboard->lpCoreKeyboardState, _td->lpActiveKeyboard);
-      break;
-    case KM_CORE_IT_EMIT_KEYSTROKE:
-      *emitKeyStroke = TRUE;
-      continueProcessingActions = TRUE;
-      break;
-    case KM_CORE_IT_INVALIDATE_CONTEXT:
-      continueProcessingActions = processInvalidateContext(_td->app);
-      break;
-    case KM_CORE_IT_CAPSLOCK:
-      continueProcessingActions = processCapsLock(act, !_td->state.isDown, _td->TIPFUpdateable, FALSE);
-      break;
-    case KM_CORE_IT_END:
-      // fallthrough
-    default:
-      assert(false); // NOT SUPPORTED
-      break;
-    }
-    if (!continueProcessingActions) {
-      return FALSE;
-    }
+  processBack(_td->app, actions->code_points_to_delete);
+  processUnicodeChar(_td->app, actions->output);
+  if (actions->persist_options != NULL) {
+    processPersistOpt(actions, _td->lpActiveKeyboard->lpCoreKeyboardState, _td->lpActiveKeyboard);
   }
+  if (actions->do_alert) {
+    processAlert(_td->app);
+  }
+  if (actions->emit_keystroke) {
+    *emitKeyStroke = TRUE;
+  }
+  processCapsLock(actions->new_caps_lock_state, !_td->state.isDown, _td->TIPFUpdateable, FALSE);
   return TRUE;
 }
 
@@ -184,28 +166,14 @@ ProcessActionsNonUpdatableParse(BOOL* emitKeyStroke) {
   if (_td->TIPFUpdateable) {  // ensure only run when not updateable
     return FALSE;
   }
-
   _td->CoreProcessEventRun = TRUE;
 
-  BOOL continueProcessingActions = TRUE;
-  for (auto act = km_core_state_action_items(_td->lpActiveKeyboard->lpCoreKeyboardState, nullptr); act->type != KM_CORE_IT_END; act++) {
-    switch (act->type) {
-    case KM_CORE_IT_EMIT_KEYSTROKE:
-      *emitKeyStroke = TRUE;
-      SendDebugMessageFormat(0, sdmGlobal, 0, "ProcessActionsNonUpdatableParse EMIT_KEYSTROKE: act->type=[%d]", act->type);
-      continueProcessingActions = TRUE;
-      _td->CoreProcessEventRun = FALSE; // If we emit the key stroke on this parse we don't need the second parse
-      break;
-    case KM_CORE_IT_CAPSLOCK:
-      continueProcessingActions = processCapsLock(act, !_td->state.isDown, _td->TIPFUpdateable, FALSE);
-      break;
-    case KM_CORE_IT_INVALIDATE_CONTEXT:
-      continueProcessingActions = processInvalidateContext(_td->app);
-      break;
-    }
-    if (!continueProcessingActions) {
-      return FALSE;
-    }
+  km_core_actions const* acts    = km_core_state_get_actions(_td->lpActiveKeyboard->lpCoreKeyboardState);
+  processCapsLock(acts->new_caps_lock_state, !_td->state.isDown, _td->TIPFUpdateable, FALSE);
+  if (acts->emit_keystroke) {
+    *emitKeyStroke = TRUE;
+    SendDebugMessageFormat(0, sdmGlobal, 0, "ProcessActionsNonUpdatableParse EMIT_KEYSTROKE");
+    _td->CoreProcessEventRun  = FALSE;  // If we emit the key stroke on this parse we don't need the second parse
   }
   return TRUE;
 }
@@ -216,22 +184,7 @@ ProcessActionsExternalEvent() {
   if (!_td) {
     return FALSE;
   }
-  // Currently only a subset of actions are handled.
-  // Other actions will be added when needed.
-  BOOL continueProcessingActions = TRUE;
-  for (auto act = km_core_state_action_items(_td->lpActiveKeyboard->lpCoreKeyboardState, nullptr); act->type != KM_CORE_IT_END;
-       act++) {
-    switch (act->type) {
-    case KM_CORE_IT_CAPSLOCK:
-      continueProcessingActions = processCapsLock(act, !_td->state.isDown, FALSE, TRUE);
-      break;
-    case KM_CORE_IT_INVALIDATE_CONTEXT:
-      continueProcessingActions = processInvalidateContext(_td->app);
-      break;
-    }
-    if (!continueProcessingActions) {
-      return FALSE;
-    }
-  }
+  km_core_actions const* acts = km_core_state_get_actions(_td->lpActiveKeyboard->lpCoreKeyboardState);
+  processCapsLock(acts->new_caps_lock_state, !_td->state.isDown, FALSE, TRUE);
   return TRUE;
 }
