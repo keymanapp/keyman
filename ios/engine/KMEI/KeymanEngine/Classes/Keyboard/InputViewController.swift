42d59416337a929b69b7f208dd44e128428c70b5
Joshua Horton <joshua_horton@sil.org>
2024-02-06 13:37:58 +0700

    chore(ios): Update ios/engine/KMEI/KeymanEngine/Classes/Keyboard/InputViewController.swift
    Co-authored-by: Marc Durdin <marc@durdin.net>

--- ios/engine/KMEI/KeymanEngine/Classes/Keyboard/InputViewController.swift
+++ ios/engine/KMEI/KeymanEngine/Classes/Keyboard/InputViewController.swift
@@ -341,7 +341,7 @@ open class InputViewController: UIInputViewController, KeymanWebDelegate {
        * there's no guarantee (yet) that the times will be the same.
        * But something refresh-rate related is a fairly reasonable assumption.
        */
-      DispatchQueue.main.asyncAfter(deadline: .now() + 0.033) { //0.033) {  // contrast:  held backspace - every 0.1.
+      DispatchQueue.main.asyncAfter(deadline: .now() + 0.033) { // 33 msec; contrast: held backspace - every 100 msec.
 
         // Does NOT update after half a second if there's no context manipulation.
         let preCaretAsyncContext = self.textDocumentProxy.documentContextBeforeInput ?? ""
