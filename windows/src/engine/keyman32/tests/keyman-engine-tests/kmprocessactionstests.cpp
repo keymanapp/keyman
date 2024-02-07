a79830387115ce107352e32db4aaddccb337adc4
rc-swag <58423624+rc-swag@users.noreply.github.com>
2024-02-07 15:31:56 +1000

    feat: remove uneeded tests after core refactor

--- windows/src/engine/keyman32/tests/keyman-engine-tests/kmprocessactionstests.cpp
+++ windows/src/engine/keyman32/tests/keyman-engine-tests/kmprocessactionstests.cpp
@@ -24,44 +24,17 @@ public:
 };
 
 // KM_CORE_IT_CHAR - processUnicodeChar
+// Now that the AITIP no longer contains a context
+// this test is just reduced to checking the size of the
+// action queue
 TEST_F(KMPROCESSACTIONS, processUnicodeChartest) {
  // just check something has been added to the que
   WCHAR callbuf[MAXCONTEXT];
   AITIP testApp;
-  int expectedQueueSize           = 1;
-  km_core_action_item itemAddChar = { KM_CORE_IT_CHAR, {0,}, {'A'}};
-
-  processUnicodeChar(&testApp, &itemAddChar);
+  int expectedQueueSize           = 3; // 1 + surrogate pair
+  km_core_usv output_string[] = {0x0041, 0x010000, 0};
+  processUnicodeChar(&testApp, &output_string[0]);
   int queueSize = testApp.GetQueueSize();
   EXPECT_EQ(queueSize, expectedQueueSize);
-
-  //km_core_usv testSurrogateChar    = Uni_SurrogateToUTF32(0xD801, 0xDC37);  //𐐷';
-  //km_core_action_item itemAddChar2 = {KM_CORE_IT_CHAR, {0,}, {testSurrogateChar}};
-  //WCHAR expectedStringSurrogate[] = {'A', 0xD801, 0xDC37, 0};
-  //processUnicodeChar(&testApp, &itemAddChar2);
-  //contextBuf = testApp.ContextBufMax(MAXCONTEXT);
-  //EXPECT_STREQ(contextBuf, &expectedStringSurrogate[0]);
-}
-
-
-// KM_CORE_IT_BACK - processBack
-// First test processing a backspace for a deadkey
-TEST_F(KMPROCESSACTIONS, processBackDeadkeytest) {
-
-  WCHAR callbuf[MAXCONTEXT];
-  AITIP testApp;
-  int expectedQueueSize           = 1;
-  km_core_action_item itemAddChar = {KM_CORE_IT_CHAR, {0,}, {'A'}};
-
-  processUnicodeChar(&testApp, &itemAddChar);
-  uint32_t marker                 = 2;
-  km_core_action_item itemAddMarker = {KM_CORE_IT_MARKER, {0,}, {marker}};
-  processMarker(&testApp, &itemAddMarker);
-  km_core_action_item itemBackSpace = {KM_CORE_IT_BACK};
-  itemBackSpace.backspace.expected_type  = KM_CORE_IT_MARKER;
-  itemBackSpace.backspace.expected_value = marker;
-  processBack(&testApp, &itemBackSpace);
-  int queueSize = testApp.GetQueueSize();
-  EXPECT_EQ(expectedQueueSize, queueSize);
 }
 
