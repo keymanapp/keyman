9475cadfa48e0ae690f20a8d2fb9313fe02e5a82
Dr Mark C. Sinclair <markcsinclair@gmail.com>
2024-02-15 09:54:03 +0000

    chore(developer): rename fixture file in 'ERROR_FileTypeNotFound (BuildModelInfo; .kps)' test

--- developer/src/kmc/test/test-infrastructureMessages.ts
+++ developer/src/kmc/test/test-infrastructureMessages.ts
@@ -127,7 +127,7 @@ describe('InfrastructureMessages', function () {
 
   it('should generate ERROR_FileTypeNotFound if a project file does not contain a .kps file entry (BuildModelInfo)', async function() {
     const buildModelInfo = new BuildModelInfo();
-    const projectPath = makePathToFixture('invalid-projects', 'error_file_type_not_found_mdl_kps.kpj')
+    const projectPath = makePathToFixture('invalid-projects', 'error_file_type_not_found__model_kps.kpj')
     const ncb = new NodeCompilerCallbacks({logLevel: 'silent'});
     await buildModelInfo.build(projectPath, '', ncb, {});
     assert.isTrue(ncb.hasMessage(InfrastructureMessages.ERROR_FileTypeNotFound),
