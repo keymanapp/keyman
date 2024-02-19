3219754a6adcda188ce773de29431c5abe6eed24
Marc Durdin <marc@durdin.net>
2024-02-19 14:10:13 +0700

    chore(developer): reduce WARN_TouchLayoutUsesUnsupportedGesturesDownlevel to HINT
    Fixes #10741.

--- developer/src/kmc-kmn/src/kmw-compiler/kmw-compiler-messages.ts
+++ developer/src/kmc-kmn/src/kmw-compiler/kmw-compiler-messages.ts
@@ -4,8 +4,8 @@ import { kmnfile } from "./compiler-globals.js";
 
 const Namespace = CompilerErrorNamespace.KmwCompiler;
 // const SevInfo = CompilerErrorSeverity.Info | Namespace;
-// const SevHint = CompilerErrorSeverity.Hint | Namespace;
-const SevWarn = CompilerErrorSeverity.Warn | Namespace;
+const SevHint = CompilerErrorSeverity.Hint | Namespace;
+// const SevWarn = CompilerErrorSeverity.Warn | Namespace;
 const SevError = CompilerErrorSeverity.Error | Namespace;
 // const SevFatal = CompilerErrorSeverity.Fatal | Namespace;
 
@@ -107,7 +107,7 @@ export class KmwCompilerMessages extends KmnCompilerMessages {
     `Touch layout file ${o.filename} does not exist`);
   static ERROR_TouchLayoutFileDoesNotExist = SevError | 0x0004;
 
-  static Warn_TouchLayoutUsesUnsupportedGesturesDownlevel = (o:{keyId:string}) => m(this.WARN_TouchLayoutUsesUnsupportedGesturesDownlevel,
+  static Hint_TouchLayoutUsesUnsupportedGesturesDownlevel = (o:{keyId:string}) => m(this.HINT_TouchLayoutUsesUnsupportedGesturesDownlevel,
     `The touch layout uses a flick or multi-tap gesture on key ${o.keyId}, which is only available on version 17.0+ of Keyman`);
-  static WARN_TouchLayoutUsesUnsupportedGesturesDownlevel = SevWarn | 0x0005;
+  static HINT_TouchLayoutUsesUnsupportedGesturesDownlevel = SevHint | 0x0005;
 };
