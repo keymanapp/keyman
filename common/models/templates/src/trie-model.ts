ef7d87ace121465b6a069e7f77e7a864b9e73b15
Joshua A. Horton <joshua_horton@sil.org>
2024-03-10 05:48:28 +0700

    refactor(common/models): moves TS priority-queue implementation to web-utils

--- common/models/templates/src/trie-model.ts
+++ common/models/templates/src/trie-model.ts
@@ -26,12 +26,11 @@
 // Should probably make a 'lm-utils' submodule.
 
 // Allows the kmwstring bindings to resolve.
-import { extendString } from "@keymanapp/web-utils";
+import { extendString, PriorityQueue } from "@keymanapp/web-utils";
 import { default as defaultWordBreaker } from "@keymanapp/models-wordbreakers";
 
 import { applyTransform, isHighSurrogate, isSentinel, SENTINEL_CODE_UNIT, transformToSuggestion } from "./common.js";
 import { getLastPreCaretToken } from "./tokenization.js";
-import PriorityQueue from "./priority-queue.js";
 
 extendString();
 
