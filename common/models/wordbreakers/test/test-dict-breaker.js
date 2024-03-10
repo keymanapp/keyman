d40a09d58a4afb3118a53d492316d607f0aed06a
Joshua A. Horton <joshua_horton@sil.org>
2024-03-10 08:09:07 +0700

    feat(common/models/wordbreakers): dict-breaker helper unit tests (non-BMP)

--- common/models/wordbreakers/test/test-dict-breaker.js
+++ common/models/wordbreakers/test/test-dict-breaker.js
@@ -20,8 +20,16 @@ describe('dictionary-based wordbreaker', () => {
         assert.deepEqual(actualSplit, expectedSplit);
       });
 
-      it.skip('handles non-BMP text properly', () => {
+      it('handles non-BMP text properly', () => {
+        let smpA = String.fromCodePoint(0x1d5ba);
+        let smpP = String.fromCodePoint(0x1d5c9);
+        let smpE = String.fromCodePoint(0x1d5be);
 
+        const expectedSplit = [smpA, 'p', smpP, 'l', smpE];
+        const text = expectedSplit.join('');
+
+        const actualSplit = splitOnCodepoints(text);
+        assert.deepEqual(actualSplit, expectedSplit);
       });
     });
 
@@ -64,8 +72,33 @@ describe('dictionary-based wordbreaker', () => {
         }
       });
 
-      it.skip('properly indexes text with non-BMP chars', () => {
+      it('properly indexes text with non-BMP chars', () => {
+        let smpA = String.fromCodePoint(0x1d5ba);
+        let smpP = String.fromCodePoint(0x1d5c9);
+        let smpL = String.fromCodePoint(0x1d5c5);
+        let smpE = String.fromCodePoint(0x1d5be);
 
+        const apple = [smpA, 'p', smpP, 'l', smpE].join('');
+        const ale = ['a', smpL, smpE].join('');
+
+        const text = `${apple} and ${ale}`;
+        const expectedSplit = [apple, 'and', ale];
+
+        const actualSplit = splitOnWhitespace(text);
+        assert.deepEqual(actualSplit.map((entry) => entry.text), expectedSplit);
+
+        let index = 0;
+        for(let i=0; i < expectedSplit.length; i++) {
+          const start = text.indexOf(expectedSplit[i], index);
+
+          assert.equal(actualSplit[i].start, start);
+          // Note:  we're using code unit measurements here.  This is to
+          // uphold Span's specification.
+          assert.equal(actualSplit[i].end, start + expectedSplit[i].length);
+          assert.equal(actualSplit[i].length, expectedSplit[i].length);
+
+          index = start + actualSplit[i].length;
+        }
       });
 
       it('handles text with ZWNJs and mixed-whitespace sequences', () => {
