6308415ae46570ba39d1308d18d3cbdfa39e078a
Joshua A. Horton <joshua_horton@sil.org>
2024-03-10 18:27:25 +0700

    feat(web): dict-breaker demo/host page

--- web/src/test/manual/build.sh
+++ web/src/test/manual/build.sh
@@ -9,6 +9,10 @@ THIS_SCRIPT="$(readlink -f "${BASH_SOURCE[0]}")"
 
 builder_describe "Builds the Keyman Engine for Web's sample page setups." \
   "@/common/web/sentry-manager build" \
+  "@/web/src/engine/predictive-text build" \
+  "@/web/src/app/browser build" \
+  "@/web/src/app/ui build" \
+  "@/web/src/tools build" \
   "configure           Does nothing for this project" \
   "clean" \
   "build" \
@@ -20,6 +24,20 @@ DEST="web/build/test-resources"
 
 builder_describe_outputs \
   build       "/${DEST}/sentry-manager.js"
+
+#### Resource paths ####
+
+BUNDLE_CMD="node $KEYMAN_ROOT/common/web/es-bundling/build/common-bundle.mjs"
+
+TEMPLATES_SRC="$KEYMAN_ROOT/common/models/templates/build/lib/index.mjs"
+TEMPLATES_MAP="$KEYMAN_ROOT/common/models/templates/build/lib/index.mjs"
+WORDBREAKING_SRC="$KEYMAN_ROOT/common/models/wordbreakers/build/lib/index.mjs"
+WORDBREAKING_MAP="$KEYMAN_ROOT/common/models/wordbreakers/build/lib/index.mjs"
+
+SENTRY_MANAGER_SRC="$KEYMAN_ROOT/common/web/sentry-manager/build/lib/index.js"
+SENTRY_MANAGER_MAP="$KEYMAN_ROOT/common/web/sentry-manager/build/lib/index.js.map"
+SENTRY_SRC="$KEYMAN_ROOT/node_modules/@sentry/browser/build/bundle.min.js"
+SENTRY_MAP="$KEYMAN_ROOT/node_modules/@sentry/browser/build/bundle.min.js.map"
 
 #### Build action definitions ####
 
@@ -36,6 +54,20 @@ function do_copy() {
   cp -f "${KEYMAN_ROOT}/common/test/keyboards/platform-rules/platformtest.js" "${KEYMAN_ROOT}/${DEST}/keyboards/"
   cp -f "${KEYMAN_ROOT}/common/test/keyboards/test9469/build/test9469.js" "${KEYMAN_ROOT}/${DEST}/keyboards/"
   cp -f "${KEYMAN_ROOT}/common/test/resources/keyboards/"*.js "${KEYMAN_ROOT}/${DEST}/keyboards/"
+
+  WORDBREAK_DEMO_TARGET="$KEYMAN_ROOT/web/build/demos/wordbreaker-libs/"
+
+  mkdir -p "$WORDBREAK_DEMO_TARGET"
+
+  $BUNDLE_CMD    "${KEYMAN_ROOT}/web/src/predictive-text/templates/build/obj/index.js" \
+    --out        "${WORDBREAK_DEMO_TARGET}/templates.mjs" \
+    --format esm
+
+  # One of the functions (timedPromise) is quite helpful for automated testing, even in the DOM.
+  # So, to make sure it's easily-accessible for the DOM-based tests...
+  $BUNDLE_CMD    "${KEYMAN_ROOT}/web/src/predictive-text/wordbreakers/build/obj/index.js" \
+    --out        "${WORDBREAK_DEMO_TARGET}/wordbreakers.mjs" \
+    --format esm
 }
 
 builder_run_action clean   rm -rf "${KEYMAN_ROOT}/${DEST}"
