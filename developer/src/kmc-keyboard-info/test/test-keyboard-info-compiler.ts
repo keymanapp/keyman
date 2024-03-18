ca084c1e00508036f695e5a24339cf2a07c43cc4
Dr Mark C. Sinclair <markcsinclair@gmail.com>
2024-03-18 12:35:52 +0000

    chore(developer): add KeyboardInfoCompiler init test

--- developer/src/kmc-keyboard-info/test/test-keyboard-info-compiler.ts
+++ developer/src/kmc-keyboard-info/test/test-keyboard-info-compiler.ts
@@ -81,4 +81,23 @@ describe('keyboard-info-compiler', function () {
     assert.deepEqual((<any>unitTestEndpoints.langtagsByTag)['en-Latn'], ENLANGTAG);
     assert.deepEqual((<any>unitTestEndpoints.langtagsByTag)['en-US'], ENLANGTAG);
   });
+
+  it('check init initialises KeyboardInfoCompiler correctly', async function() {
+    const jsFilename = makePathToFixture('khmer_angkor', 'build', 'khmer_angkor.js');
+    const kpsFilename = makePathToFixture('khmer_angkor', 'source', 'khmer_angkor.kps');
+    const kmpFilename = makePathToFixture('khmer_angkor', 'build', 'khmer_angkor.kmp');
+
+    const sources = {
+      kmpFilename,
+      sourcePath: 'release/k/khmer_angkor',
+      kpsFilename,
+      jsFilename: jsFilename,
+      forPublishing: true,
+    };
+
+    const compiler = new KeyboardInfoCompiler();
+    assert.isTrue(await compiler.init(callbacks, {sources}));
+    assert.deepEqual(compiler['callbacks'], callbacks);
+    assert.deepEqual(compiler['options'], {sources});
+  });  
 });
