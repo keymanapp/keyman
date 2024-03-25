69fdd697d1efac80d9ca2d0b9adf09ae1d322b70
Marc Durdin <marc@durdin.net>
2024-03-25 10:27:19 +0700

    refactor(developer): polish fixup function

--- developer/src/kmc/src/util/kmcSentryOptions.ts
+++ developer/src/kmc/src/util/kmcSentryOptions.ts
@@ -1,15 +1,15 @@
-import Sentry from "@sentry/node";
+import { SentryNodeOptions } from "@keymanapp/developer-utils";
 
 /**
  * Rewrites sourcemap paths for the esbuild distribution of kmc (as used in
  * Keyman Developer itself) /<arbitrary-path>/kmc.mjs to /dist/kmc.mjs, so that
  * Sentry can pick up the mapping
  */
-export const kmcSentryOptions: Sentry.NodeOptions = {
+export const kmcSentryOptions: SentryNodeOptions = {
   beforeSend: function (event) {
-    const stacktrace = event?.exception?.values?.[0]?.stacktrace;
-    if (stacktrace?.frames) {
-      stacktrace.frames.forEach(function (frame) {
+    const frames = event?.exception?.values?.[0]?.stacktrace?.frames;
+    if (frames) {
+      frames.forEach(frame => {
         const filename = frame.filename.match(/[^/]+$/)[0];
         if (filename.endsWith('.mjs')) {
           frame.filename = `/dist/${filename}`;
