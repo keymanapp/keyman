d705589726a1fb553e7a8b4daf8b92617fc4a67d
Joshua A. Horton <joshua_horton@sil.org>
2024-04-19 09:25:08 +0700

    chore(android): restores try-catch, now with console.error err forwarding

--- android/KMEA/app/src/main/assets/android-host.js
+++ android/KMEA/app/src/main/assets/android-host.js
@@ -36,7 +36,11 @@ function init() {
   keyman.beepKeyboard = beepKeyboard;
 
   // Readies the keyboard stub for instant loading during the init process.
-  KeymanWeb.registerStub(JSON.parse(jsInterface.initialKeyboard()));
+  try {
+    KeymanWeb.registerStub(JSON.parse(jsInterface.initialKeyboard()));
+  } catch(error) {
+    console.error(error);
+  }
 
   keyman.init({
     'embeddingApp':device,
