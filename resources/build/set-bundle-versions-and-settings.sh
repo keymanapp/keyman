00e850c55724200880784b9f2cfc204e6fb651a2
sgschantz <shawn_schantz@sil.org>
2024-04-23 13:24:40 +0700

    add script to update version string in settings

--- resources/build/set-bundle-versions-and-settings.sh
+++ resources/build/set-bundle-versions-and-settings.sh
@@ -2,6 +2,10 @@
 
 # Calls script in xcode-utils to update the version
 # true:  applies VERSION_WITH_TAG to custom KeymanVersionWithTag plist member used for in-app display
+# updates the version string for Settings
 
 source "$KEYMAN_ROOT/resources/build/xcode-utils.sh"
+
 phaseSetBundleVersions true
+
+setSettingsBundleVersion
