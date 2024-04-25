812de690d8ea0233fa10c09be1b071f1fa5e2028
Joshua A. Horton <joshua_horton@sil.org>
2024-04-25 10:52:02 +0700

    chore(web): fixes pred-text karma config
    The upgrade to chai means that karma's 'framework' feature is no longer compatible, meaning a _little_ extra legwork is required to use it for karma browser tests.  It's just a little bit, though.
    
    Will need to replicate this for the other packages that use karma to test, as the pattern kinda proliferated.

--- common/predictive-text/unit_tests/in_browser/base.conf.cjs
+++ common/predictive-text/unit_tests/in_browser/base.conf.cjs
@@ -27,10 +27,11 @@ module.exports = {
 
   // frameworks to use
   // available frameworks: https://npmjs.org/browse/keyword/karma-adapter
-  frameworks: ['mocha', 'chai', 'fixture'],
+  frameworks: ['mocha', 'fixture'],
 
   // list of files / patterns to load in the browser
   files: [
+    { pattern: 'node_modules/chai/chai.js', served: true, watched: false, included: false, type: 'module'},
     // { pattern: 'node_modules/**/*.js', served: true, watched: false, included: false },
     // { pattern: 'node_modules/**/*.mjs', served: true, watched: false, included: false, type: "module" },
     // { pattern: 'node_modules/**/*.cjs', served: true, watched: false, included: false },
@@ -81,6 +82,9 @@ module.exports = {
 
   // enable / disable colors in the output (reporters and logs)
   colors: true,
+
+  // TEMP ENTRY!
+  reporters: ['teamcity'],
 
   // enable / disable watching file and executing tests whenever any file changes
   autoWatch: true,
