cd867ba80157ecb04f96a55dc45884fa8b74fe6c
Steven R. Loomis <srl295@gmail.com>
2024-04-25 08:58:47 -0500

    fix(core): update vkey_should_invalidate_context per comment
    Fixes #10955

--- core/src/vkey_to_contextreset.hpp
+++ core/src/vkey_to_contextreset.hpp
@@ -6,13 +6,13 @@
 namespace km {
 namespace core {
 
-static const auto vkey_to_contextreset_count = 256;
+static const auto vkey_should_invalidate_context_count = 256;
 /** true for any vkeys which require a context reset (such as frame keys) */
-extern bool vkey_to_contextreset[vkey_to_contextreset_count];
+extern bool vkey_should_invalidate_context[vkey_should_invalidate_context_count];
 
 /** @return true for any vkeys which require a context reset (such as frame keys) */
 inline bool vkey_should_contextreset(km_core_virtual_key vk) {
-  return ((vk < vkey_to_contextreset_count) && vkey_to_contextreset[vk]);
+  return ((vk < vkey_should_invalidate_context_count) && vkey_should_invalidate_context[vk]);
 }
 
 /** @return true for modifier states that are involved with context reset */
