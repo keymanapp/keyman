fa8d3a4a65598f915f825d343ac09ee3be467950
Dr Mark C. Sinclair <markcsinclair@gmail.com>
2024-05-06 16:14:45 +0100

    chore(developer): change IsValidKeyboardVersion to use more specific expects

--- developer/src/kmcmplib/tests/gtest-compiler-test.cpp
+++ developer/src/kmcmplib/tests/gtest-compiler-test.cpp
@@ -5,17 +5,17 @@ KMX_BOOL IsValidKeyboardVersion(KMX_WCHAR *dpString);
 
 TEST(Compiler, IsValidKeyboardVersion_test) {
     KMX_WCHAR ver_empty[] = { 0x0000 };
-    EXPECT_EQ(FALSE, IsValidKeyboardVersion(ver_empty));
+    EXPECT_FALSE(IsValidKeyboardVersion(ver_empty));
     KMX_WCHAR ver_valid[] = { 0x0031, 0x002E, 0x0031, 0x0000 }; // 1.1
-    EXPECT_EQ(TRUE, IsValidKeyboardVersion(ver_valid));
+    EXPECT_TRUE(IsValidKeyboardVersion(ver_valid));
     KMX_WCHAR ver_extra_zero[] = { 0x0031, 0x002E, 0x0030, 0x0000 }; // 1.0, should fail but doesn't
-    EXPECT_EQ(TRUE, IsValidKeyboardVersion(ver_extra_zero));
+    EXPECT_TRUE(IsValidKeyboardVersion(ver_extra_zero));
     KMX_WCHAR ver_trailing_point[] = { 0x0031, 0x002E, 0x0000 }; // 1.
-    EXPECT_EQ(FALSE, IsValidKeyboardVersion(ver_trailing_point));
+    EXPECT_FALSE(IsValidKeyboardVersion(ver_trailing_point));
     KMX_WCHAR ver_three_level[] = { 0x0031, 0x002E, 0x0032, 0x002E, 0x0033, 0x0000 }; // 1.2.3, should fail but doesn't
-    EXPECT_EQ(TRUE, IsValidKeyboardVersion(ver_three_level));
+    EXPECT_TRUE(IsValidKeyboardVersion(ver_three_level));
     KMX_WCHAR ver_letter[] = { 0x0061, 0x0000 }; // a
-    EXPECT_EQ(FALSE, IsValidKeyboardVersion(ver_letter));
+    EXPECT_FALSE(IsValidKeyboardVersion(ver_letter));
     KMX_WCHAR ver_trailing_letter[] = { 0x0031, 0x002E, 0x0061, 0x0000 }; // 1.a
-    EXPECT_EQ(FALSE, IsValidKeyboardVersion(ver_trailing_letter));
+    EXPECT_FALSE(IsValidKeyboardVersion(ver_trailing_letter));
 }
