c8a1c6aa39b70dcd0ae99a1900e33e37c74b6cbb
Marc Durdin <marc@durdin.net>
2024-05-09 13:21:06 +0700

    chore(developer): use builder.inc.sh instead of build-utils.sh in build scripts
    also, remove old Makefiles

--- developer/build.sh
+++ developer/build.sh
@@ -1,8 +1,9 @@
 #!/usr/bin/env bash
-## START STANDARD BUILD SCRIPT INCLUDE
+## START STANDARD UTILITY SCRIPT INCLUDE
 # adjust relative paths as necessary
 THIS_SCRIPT="$(readlink -f "${BASH_SOURCE[0]}")"
-. "${THIS_SCRIPT%/*}/../resources/build/builder.inc.sh"
-## END STANDARD BUILD SCRIPT INCLUDE
+. "${THIS_SCRIPT%/*}/../resources/build/build-utils.sh"
+## END STANDARD UTILITY SCRIPT INCLUDE
 
-./src/build.sh "$@"
+# This is not a builder script but calls a builder script
+"$THIS_SCRIPT_PATH/src/build.sh" "$@"
