330070fe910eb12edb75a77d67325d0942c7349a
Dr Mark C. Sinclair <markcsinclair@gmail.com>
2024-05-13 11:29:55 +0100

    chore(developer): add list of functions needing test (as comments)

--- developer/src/kmcmplib/tests/gtest-compiler-test.cpp
+++ developer/src/kmcmplib/tests/gtest-compiler-test.cpp
@@ -45,6 +45,18 @@ TEST_F(CompilerTest, ValidateMatchNomatchOutput_test) {
     EXPECT_EQ(CERR_None, ValidateMatchNomatchOutput((PKMX_WCHAR)sentinel));
 };
 
+// KMX_DWORD ParseLine(PFILE_KEYBOARD fk, PKMX_WCHAR str)
+// KMX_DWORD ProcessGroupLine(PFILE_KEYBOARD fk, PKMX_WCHAR p)
+// int kmcmp::cmpkeys(const void *key, const void *elem)
+// KMX_DWORD ProcessGroupFinish(PFILE_KEYBOARD fk)
+// KMX_DWORD ProcessStoreLine(PFILE_KEYBOARD fk, PKMX_WCHAR p)
+// bool resizeStoreArray(PFILE_KEYBOARD fk)
+// bool resizeKeyArray(PFILE_GROUP gp, int increment)
+// KMX_DWORD AddStore(PFILE_KEYBOARD fk, KMX_DWORD SystemID, const KMX_WCHAR * str, KMX_DWORD *dwStoreID)
+// KMX_DWORD AddDebugStore(PFILE_KEYBOARD fk, KMX_WCHAR const * str)
+// KMX_DWORD ProcessSystemStore(PFILE_KEYBOARD fk, KMX_DWORD SystemID, PFILE_STORE sp)
+// int GetCompileTargetsFromTargetsStore(const KMX_WCHAR* store)
+
 TEST_F(CompilerTest, IsValidKeyboardVersion_test) {
     EXPECT_FALSE(IsValidKeyboardVersion((KMX_WCHAR *)u""));
     EXPECT_FALSE(IsValidKeyboardVersion((KMX_WCHAR *)u" "));
@@ -57,3 +69,58 @@ TEST_F(CompilerTest, IsValidKeyboardVersion_test) {
     EXPECT_FALSE(IsValidKeyboardVersion((KMX_WCHAR *)u"a"));
     EXPECT_FALSE(IsValidKeyboardVersion((KMX_WCHAR *)u"1.a"));
 };
+
+// KMX_DWORD kmcmp::AddCompilerVersionStore(PFILE_KEYBOARD fk)
+// KMX_DWORD CheckStatementOffsets(PFILE_KEYBOARD fk, PFILE_GROUP gp, PKMX_WCHAR context, PKMX_WCHAR output, PKMX_WCHAR key)
+// KMX_BOOL CheckContextStatementPositions(PKMX_WCHAR context)
+// KMX_DWORD CheckUseStatementsInOutput(PKMX_WCHAR output)
+// KMX_DWORD CheckVirtualKeysInOutput(PKMX_WCHAR output)
+// KMX_DWORD InjectContextToReadonlyOutput(PKMX_WCHAR pklOut)
+// KMX_DWORD CheckOutputIsReadonly(const PFILE_KEYBOARD fk, const PKMX_WCHAR output)
+// KMX_DWORD ProcessKeyLine(PFILE_KEYBOARD fk, PKMX_WCHAR str, KMX_BOOL IsUnicode)
+// KMX_DWORD ProcessKeyLineImpl(PFILE_KEYBOARD fk, PKMX_WCHAR str, KMX_BOOL IsUnicode, PKMX_WCHAR pklIn, PKMX_WCHAR pklKey, PKMX_WCHAR pklOut)
+// KMX_DWORD ExpandKp_ReplaceIndex(PFILE_KEYBOARD fk, PFILE_KEY k, KMX_DWORD keyIndex, int nAnyIndex)
+// KMX_DWORD ExpandKp(PFILE_KEYBOARD fk, PFILE_KEY kpp, KMX_DWORD storeIndex)
+// PKMX_WCHAR GetDelimitedString(PKMX_WCHAR *p, KMX_WCHAR const * Delimiters, KMX_WORD Flags)
+// LinePrefixType GetLinePrefixType(PKMX_WCHAR *p)
+// int LineTokenType(PKMX_WCHAR *str)
+// KMX_BOOL StrValidChrs(PKMX_WCHAR q, KMX_WCHAR const * chrs)
+// KMX_DWORD GetXString(PFILE_KEYBOARD fk, PKMX_WCHAR str, KMX_WCHAR const * token,
+//  PKMX_WCHAR output, int max, int offset, PKMX_WCHAR *newp, int /*isVKey*/, int isUnicode
+// )
+// KMX_DWORD GetXStringImpl(PKMX_WCHAR tstr, PFILE_KEYBOARD fk, PKMX_WCHAR str, KMX_WCHAR const * token,
+//  PKMX_WCHAR output, int max, int offset, PKMX_WCHAR *newp, int isUnicode
+// )
+// KMX_DWORD process_baselayout(PFILE_KEYBOARD fk, PKMX_WCHAR q, PKMX_WCHAR tstr, int *mx)
+// KMX_DWORD process_platform(PFILE_KEYBOARD fk, PKMX_WCHAR q, PKMX_WCHAR tstr, int *mx)
+// KMX_DWORD process_if_synonym(KMX_DWORD dwSystemID, PFILE_KEYBOARD fk, PKMX_WCHAR q, PKMX_WCHAR tstr, int *mx)
+// KMX_DWORD process_if(PFILE_KEYBOARD fk, PKMX_WCHAR q, PKMX_WCHAR tstr, int *mx)
+// KMX_DWORD process_reset(PFILE_KEYBOARD fk, PKMX_WCHAR q, PKMX_WCHAR tstr, int *mx)
+// KMX_DWORD process_expansion(PFILE_KEYBOARD fk, PKMX_WCHAR q, PKMX_WCHAR tstr, int *mx, int max)
+// KMX_DWORD process_set_synonym(KMX_DWORD dwSystemID, PFILE_KEYBOARD fk, PKMX_WCHAR q, PKMX_WCHAR tstr, int *mx)
+// KMX_DWORD process_set(PFILE_KEYBOARD fk, PKMX_WCHAR q, PKMX_WCHAR tstr, int *mx)
+// KMX_DWORD process_save(PFILE_KEYBOARD fk, PKMX_WCHAR q, PKMX_WCHAR tstr, int *mx)
+// int xatoi(PKMX_WCHAR *p)
+// int GetGroupNum(PFILE_KEYBOARD fk, PKMX_WCHAR p)
+// KMX_DWORD ProcessEthnologueStore(PKMX_WCHAR p)
+// KMX_DWORD ProcessHotKey(PKMX_WCHAR p, KMX_DWORD *hk)
+// void SetChecksum(PKMX_BYTE buf, PKMX_DWORD CheckSum, KMX_DWORD sz)
+// KMX_BOOL kmcmp::CheckStoreUsage(PFILE_KEYBOARD fk, int storeIndex, KMX_BOOL fIsStore, KMX_BOOL fIsOption, KMX_BOOL fIsCall)
+// KMX_DWORD WriteCompiledKeyboard(PFILE_KEYBOARD fk, KMX_BYTE**data, size_t& dataSize)
+// KMX_DWORD ReadLine(KMX_BYTE* infile, int sz, int& offset, PKMX_WCHAR wstr, KMX_BOOL PreProcess)
+// KMX_DWORD GetRHS(PFILE_KEYBOARD fk, PKMX_WCHAR p, PKMX_WCHAR buf, int bufsize, int offset, int IsUnicode)
+// void safe_wcsncpy(PKMX_WCHAR out, PKMX_WCHAR in, int cbMax)
+// KMX_BOOL IsSameToken(PKMX_WCHAR *p, KMX_WCHAR const * token)
+// static bool endsWith(const std::string& str, const std::string& suffix)
+// KMX_DWORD ImportBitmapFile(PFILE_KEYBOARD fk, PKMX_WCHAR szName, PKMX_DWORD FileSize, PKMX_BYTE *Buf)
+// int atoiW(PKMX_WCHAR p)
+// KMX_DWORD kmcmp::CheckUTF16(int n)
+// KMX_DWORD kmcmp::UTF32ToUTF16(int n, int *n1, int *n2)
+// KMX_DWORD BuildVKDictionary(PFILE_KEYBOARD fk)
+// int GetVKCode(PFILE_KEYBOARD fk, PKMX_WCHAR p)
+// int GetDeadKey(PFILE_KEYBOARD fk, PKMX_WCHAR p)
+// void kmcmp::RecordDeadkeyNames(PFILE_KEYBOARD fk)
+// KMX_BOOL kmcmp::IsValidCallStore(PFILE_STORE fs)
+// bool hasPreamble(std::u16string result)
+// bool UTF16TempFromUTF8(KMX_BYTE* infile, int sz, KMX_BYTE** tempfile, int *sz16)
+// PFILE_STORE FindSystemStore(PFILE_KEYBOARD fk, KMX_DWORD dwSystemID)
