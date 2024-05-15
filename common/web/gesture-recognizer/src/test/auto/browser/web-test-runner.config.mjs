f70a171195be67480355f7f0bf5d5762c57eaf9b
Joshua A. Horton <joshua_horton@sil.org>
2024-05-15 15:01:09 +0700

    chore(web): gesture wtr config cleanup, explicit mobile device targets

--- common/web/gesture-recognizer/src/test/auto/browser/web-test-runner.config.mjs
+++ common/web/gesture-recognizer/src/test/auto/browser/web-test-runner.config.mjs
@@ -1,7 +1,8 @@
 // @ts-check
 import { devices, playwrightLauncher } from '@web/test-runner-playwright';
 import { defaultReporter, summaryReporter } from '@web/test-runner';
-import { LauncherWrapper, sessionStabilityReporter } from '@keymanapp/common-test-resources/test-runner-stability-reporter.mjs'
+import { LauncherWrapper, sessionStabilityReporter } from '@keymanapp/common-test-resources/test-runner-stability-reporter.mjs';
+import named from '@keymanapp/common-test-resources/test-runner-rename-browser.mjs'
 import { esbuildPlugin } from '@web/dev-server-esbuild';
 import { importMapsPlugin } from '@web/dev-server-import-maps';
 import { dirname, resolve } from 'path';
@@ -14,11 +15,15 @@ const KEYMAN_ROOT = resolve(dir, '../../../../../../../');
 export default {
   // debug: true,
   browsers: [
-    // These are the same type - and probably the same _instances_ - as are visible within the reporter!
-    // Probably a helpful fact to resolve name disambiguation.
     new LauncherWrapper(playwrightLauncher({ product: 'chromium' })),
     new LauncherWrapper(playwrightLauncher({ product: 'firefox' })),
-    new LauncherWrapper(playwrightLauncher({ product: 'webkit', concurrency: 1 }))
+    playwrightLauncher({ product: 'webkit', concurrency: 1}),
+    named(new LauncherWrapper(playwrightLauncher({ product: 'webkit', concurrency: 1, createBrowserContext({browser}) {
+      return browser.newContext({...devices['iPhone X'] });
+    }})), 'iOS Phone (emulated)'),
+    named(new LauncherWrapper(playwrightLauncher({ product: 'chromium' , createBrowserContext({browser}) {
+      return browser.newContext({...devices['Pixel 4'] })
+    }})), 'Android Phone (emulated)'),
   ],
   concurrency: 10,
   nodeResolve: true,
@@ -57,11 +62,6 @@ export default {
   /*
     Un-comment the next two lines for easy interactive debugging; it'll launch the
     test page in your preferred browser.
-
-    WARNING: https://github.com/modernweb-dev/web/issues/2721 may cause issues when
-    using manual mode.  Changing rootDir to the drive root (or similar) may provide
-    a decent workaround; it appears that Web Test Runner can do a little searching
-    for node_modules if and when necessary.
   */
   // open: true,
   // manual: true,
