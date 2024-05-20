9e0207ad871b16954a5161eb51b748e07d6c7f4d
Darcy Wong <darcy_wong@sil.org>
2024-05-20 10:58:31 +0700

    Update android/KMAPro/kMAPro/src/main/java/com/keyman/android/SystemKeyboard.java
    Co-authored-by: Marc Durdin <marc@durdin.net>

--- android/KMAPro/kMAPro/src/main/java/com/keyman/android/SystemKeyboard.java
+++ android/KMAPro/kMAPro/src/main/java/com/keyman/android/SystemKeyboard.java
@@ -169,6 +169,11 @@ public class SystemKeyboard extends InputMethodService implements OnKeyboardEven
     InputConnection ic = getCurrentInputConnection();
     if (ic != null) {
       ExtractedText icText = ic.getExtractedText(new ExtractedTextRequest(), 0);
+      /*
+        We do sometimes receive null `icText.text`, even though
+        getExtractedText() docs does not list this as a possible 
+        return value, so we test for that as well (#11479)
+      */
       if (icText != null && icText.text != null) {
         boolean didUpdateText = KMManager.updateText(KeyboardType.KEYBOARD_TYPE_SYSTEM, icText.text.toString());
         boolean didUpdateSelection = KMManager.updateSelectionRange(KeyboardType.KEYBOARD_TYPE_SYSTEM);
