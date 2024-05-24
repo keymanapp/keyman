4ed88d8fa6295104bd00a01bf7c9c3be1740c85c
Marc Durdin <marc@durdin.net>
2024-05-24 15:41:10 +0700

    refactor(developer): move developer-utils files into src/ and sub-folder
    In preparation for moving source file types into developer-utils, move
    the existing developer-utils files into src/ and src/utils subfolders.

---
+++ developer/src/common/web/utils/src/index.ts
@@ -0,0 +1,6 @@
+export { validateMITLicense } from './utils/validate-mit-license.js';
+export { KeymanSentry, SentryNodeOptions } from './utils/KeymanSentry.js';
+export { getOption, loadOptions, clearOptions } from './utils/options.js';
+export { escapeMarkdownChar } from './utils/markdown.js';
+export { KeymanUrls } from './utils/keyman-urls.js';
+
