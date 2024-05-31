76bd22b120b5ffca9c758908e7767c73123832a6
Marc Durdin <marc@durdin.net>
2024-05-31 14:54:09 +0700

    fix(developer): bundle ttfmeta library internally with kmc-keyboard-info
    Remove ttfmeta from package.json, and copy only the relevant files into
    ttfmeta/ under kmc-keyboard-info. This is a stopgap measure as we will
    probably attempt to identify an alternate library in the future.
    
    See also #11625 which is the driver for making this change now.
    
    Fixes: #11630

---
+++ developer/src/kmc-keyboard-info/src/ttfmeta/lib/index.js
@@ -0,0 +1,52 @@
+/**
+ * ttfmeta
+ * Copyright(c) 2021-2022 Khen Solomon Lethil
+ * MIT Licensed
+ * v1.0.9
+ */
+
+import * as fs from "fs";
+import * as ttf from "./main.js";
+
+/**
+ * @namespace
+ * @param {string | number | Buffer | URL} pathOrData
+ * @param {{(error:string|null,meta?:typeof ttf.result):void}} callback
+ */
+export function ttfInfo(pathOrData, callback) {
+  try {
+    if (pathOrData instanceof Buffer) {
+      ttf.ttfInfo(ttf.view(pathOrData), callback);
+    } else {
+      fs.readFile(pathOrData, function(error, data) {
+        if (error) {
+          callback(error.message || error.toString());
+        } else {
+          ttf.ttfInfo(ttf.view(data), callback);
+        }
+      });
+    }
+  } catch (/** @type {any}*/ error) {
+    callback(error.message || error.toString());
+  }
+}
+
+/**
+ * @param {string | number | Buffer | URL} pathOrData
+ * @returns {Promise<typeof ttf.result>}
+ */
+export function promise(pathOrData) {
+  return new Promise(function(res, rej) {
+    ttfInfo(pathOrData, function(e, d) {
+      if (d) {
+        res(d);
+      } else {
+        rej(e);
+      }
+    });
+  });
+}
+
+/** @namespace */
+export const ttfMeta = { ttfInfo, promise };
+export default ttfMeta;
