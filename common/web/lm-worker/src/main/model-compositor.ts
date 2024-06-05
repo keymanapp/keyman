d6f05323f8fdd94aed874dea3e563a311c251313
Joshua A. Horton <joshua_horton@sil.org>
2024-06-05 08:41:27 +0700

    fix(web): fix predictions after moving caret
    Fixes: #11684
    Fixes: KEYMAN-WEB-KR
    Fixes: #11620
    Fixes: KEYMAN-WEB-KT

--- common/web/lm-worker/src/main/model-compositor.ts
+++ common/web/lm-worker/src/main/model-compositor.ts
@@ -249,21 +249,11 @@ export default class ModelCompositor {
           // Worth considering:  extend Traversal to allow direct prediction lookups?
           // let traversal = match.finalTraversal;
 
-          // Find a proper Transform ID to map the correction to.
-          // Without it, we can't apply the suggestion.
-          let finalInput: Transform;
-          if(match.inputSequence.length > 0) {
-            // common case:  from the same keystroke `inputTransform`, with matching `.id`.
-            finalInput = match.inputSequence[match.inputSequence.length - 1].sample;
-          } else {
-            finalInput = inputTransform;  // A fallback measure.  Greatly matters for empty contexts.
-          }
-
           // Replace the existing context with the correction.
           let correctionTransform: Transform = {
             insert: correction,  // insert correction string
             deleteLeft: deleteLeft,
-            id: finalInput.id // The correction should always be based on the most recent external transform/transcription ID.
+            id: inputTransform.id // The correction should always be based on the most recent external transform/transcription ID.
           }
 
           let rootCost = match.totalCost;
