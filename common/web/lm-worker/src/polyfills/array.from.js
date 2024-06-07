ec50747d0993f61953775b373150998005c95d01
Joshua A. Horton <joshua_horton@sil.org>
2024-06-07 14:24:41 +0700

    change(android): removes unneeded conditionals

--- common/web/lm-worker/src/polyfills/array.from.js
+++ common/web/lm-worker/src/polyfills/array.from.js
@@ -1,18 +1,12 @@
 (function() {
   if(!Array.from) {
     function isHighSurrogate(codeUnit) {
-      if(typeof codeUnit == 'string') {
-        codeUnit = codeUnit.charCodeAt(0);
-      }
-
+      codeUnit = codeUnit.charCodeAt(0);
       return codeUnit >= 0xD800 && codeUnit <= 0xDBFF;
     }
 
     function isLowSurrogate(codeUnit) {
-      if(typeof codeUnit == 'string') {
-        codeUnit = codeUnit.charCodeAt(0);
-      }
-
+      codeUnit = codeUnit.charCodeAt(0);
       return codeUnit >= 0xDC00 && codeUnit <= 0xDFFF;
     }
 
