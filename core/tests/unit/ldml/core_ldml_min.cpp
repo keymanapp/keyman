3c823432187f8e5238cb0b0734420ca9be0f1c45
Steven R. Loomis <srl295@gmail.com>
2024-06-13 16:56:36 -0500

    test(core): Add a minimal test that exercises the core API
    - this test doesn't actually run well, it compiles but fails with an assertion.
    - the purpose is to exercise the linker.
    
    Fixes: #10968

---
+++ core/tests/unit/ldml/core_ldml_min.cpp
@@ -0,0 +1,38 @@
+/*
+  Copyright:    © SIL International.
+  Description:  Minimal, hermetic Keyman core test.
+  Create Date:  6 Jan 2024
+  Authors:      Steven R. Loomis
+
+  This is a different kind of a test. It's a very minimal test of the core API.
+  It does not have any file i/o other than console output and return code.
+
+  This test is expected to fail with an assertion failure. It's to exercise the API.
+*/
+
+#include <iostream>
+#include <assert.h>
+
+#include "keyman_core.h"
+
+int main(int argc, const char *argv[]) {
+
+    // "load" our "keyboard"
+    km_core_keyboard * test_kb = nullptr;
+
+    km_core_status status;
+    status = km_core_keyboard_load("", &test_kb);
+
+    std::cerr << "null km_core_keyboard_load = " << status << std::endl;
+    assert(status == KM_CORE_STATUS_INVALID_ARGUMENT);
+    assert(test_kb == nullptr);
+    km_core_keyboard_dispose(test_kb);
+    km_core_state_dispose(nullptr);
+    km_core_cu_dispose(nullptr);
+
+    status = km_core_process_event(nullptr, 0, 0, 0, 0);
+    /* NOTREACHED - assertion fails above. */
+    assert(status == KM_CORE_STATUS_INVALID_ARGUMENT);
+
+    return 0;
+}
