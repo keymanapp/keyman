801baf55d054c69b79f35aa6efb4fbe9ce2fc2bd
Sabine <Sabine_SIL@web.de>
2024-06-18 09:34:45 +0200

    feat(linux): remove comments

--- linux/mcompile/keymap/mc_import_rules.cpp
+++ linux/mcompile/keymap/mc_import_rules.cpp
@@ -408,11 +408,6 @@ bool KMX_ImportRules(LPKMX_KEYBOARD kp, vec_dword_3D& all_vector, GdkKeymap **ke
             continue;
           }
 
-        //_S2 TOP_6 TODO to compare win-lin kmn-files skip ss6+7; MUST BE removed later!!!!
-        if(ss == MenuCtrl|| ss == ShftMenuCtrl) {
-            continue;
-          }
-
           KMX_DWORD kc_us = (KMX_DWORD) KMX_get_KeyCodeUnderlying_From_VKUS(iKey);
 
           for(int caps = 0; caps <= 1; caps++) {
