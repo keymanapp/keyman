975b2edb98dc01741a8f36218bc94fbc68fb6db6
Dr Mark C. Sinclair <markcsinclair@gmail.com>
2024-06-19 14:19:59 +0100

    fix(developer): add ref to issue #11833 in relevant test case

--- developer/src/kmcmplib/tests/gtest-compiler-test.cpp
+++ developer/src/kmcmplib/tests/gtest-compiler-test.cpp
@@ -455,7 +455,7 @@ TEST_F(CompilerTest, GetXStringImpl_type_i_test) {
     const KMX_WCHAR tstr_index_comma_valid[] = { UC_SENTINEL, CODE_INDEX, 2, 4, 0 };
     EXPECT_EQ(0, u16cmp(tstr_index_comma_valid, tstr));
 
-    // index, space, valid
+    // index, space, valid ... should not be valid (see #11833)
     u16cpy(str, u"index(b 4)");
     fileKeyboard.cxStoreArray = 3u;
     fileKeyboard.dpStoreArray = file_store;
