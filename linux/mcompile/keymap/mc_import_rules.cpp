ade07bb298c58436765736b1a02dce960db55eba
Sabine <Sabine_SIL@web.de>
2024-06-19 12:18:54 +0200

    feat(linux): move CODE__SIZE and #include <../../../../common/include/kmx_file.h> instead of double #include "kmx_file.h"

--- linux/mcompile/keymap/mc_import_rules.cpp
+++ linux/mcompile/keymap/mc_import_rules.cpp
@@ -22,7 +22,6 @@
 #include <vector>
 #include <string>
 #include <stdio.h>
-//#include "km_types.h"
 #include "mc_kmxfile.h"
 #include "keymap.h"
 
@@ -617,31 +616,3 @@ bool KMX_ImportRules(LPKMX_KEYBOARD kp, vec_dword_3D& all_vector, GdkKeymap **ke
 
 return true;
 }
-
-const int CODE__SIZE[] = {
-   -1,   // undefined                0x00
-    1,   // CODE_ANY                 0x01
-    2,   // CODE_INDEX               0x02
-    0,   // CODE_CONTEXT             0x03
-    0,   // CODE_NUL                 0x04
-    1,   // CODE_USE                 0x05
-    0,   // CODE_RETURN              0x06
-    0,   // CODE_BEEP                0x07
-    1,   // CODE_DEADKEY             0x08
-   -1,  // unused                   0x09
-    2,   // CODE_EXTENDED            0x0A
-   -1,  // CODE_EXTENDEDEND         0x0B (unused)
-    1,   // CODE_SWITCH              0x0C
-   -1,  // CODE_KEY                 0x0D (never used)
-    0,   // CODE_CLEARCONTEXT        0x0E
-    1,   // CODE_CALL                0x0F
-   -1,  // UC_SENTINEL_EXTENDEDEND  0x10 (not valid with UC_SENTINEL)
-    1,   // CODE_CONTEXTEX           0x11
-    1,   // CODE_NOTANY              0x12
-    2,   // CODE_SETOPT              0x13
-    3,   // CODE_IFOPT               0x14
-    1,   // CODE_SAVEOPT             0x15
-    1,   // CODE_RESETOPT            0x16
-    3,   // CODE_IFSYSTEMSTORE       0x17
-    2    // CODE_SETSYSTEMSTORE      0x18
-};
