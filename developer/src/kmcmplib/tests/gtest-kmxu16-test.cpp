093041f6b26d3ee1ed96f24d5064c6fb0c3aacfc
Dr Mark C. Sinclair <markcsinclair@gmail.com>
2024-07-01 16:43:12 +0100

    fix(developer): add two test cases for u16tok_delim test

--- developer/src/kmcmplib/tests/gtest-kmxu16-test.cpp
+++ developer/src/kmcmplib/tests/gtest-kmxu16-test.cpp
@@ -21,3 +21,22 @@ TEST_F(KmxU16Test, u16chr_test) {
 	u16cpy(str, u"abc");
     EXPECT_EQ(3, u16chr(str, '\0') - str); // locate null terminator
 }
+
+TEST_F(KmxU16Test, u16tok_ch_test) {
+	EXPECT_TRUE(true);
+}
+
+// KMX_WCHAR * u16tok(KMX_WCHAR* p, const KMX_WCHAR* delim, KMX_WCHAR** ctx)
+TEST_F(KmxU16Test, u16tok_delim_test) {
+	KMX_WCHAR str[LINESIZE];
+	PKMX_WCHAR ctx = NULL;
+
+	// no string, no context
+	EXPECT_EQ(NULL, u16tok(nullptr, u"", &ctx));
+
+	// delimited string
+	u16cpy(str, u"abc|def");
+	EXPECT_EQ(str, u16tok(str, u"|", &ctx));
+	EXPECT_EQ(0, u16cmp(u"abc", str));
+	EXPECT_EQ(0, u16cmp(u"def", ctx));
+}
