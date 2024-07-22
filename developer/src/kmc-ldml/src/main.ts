cfa85d1cc81be88cfa2cdf5c61c9a7be78ce9b30
Marc Durdin <marc@durdin.net>
2024-07-22 15:58:39 +0700

    chore(developer): rename kmc-ldml export LdmlKeyboardCompilerMessages to LdmlCompilerMessages
    This makes the exported class name more consistent with the other
    compiler message classes and removes one more unnecessary alias.

--- developer/src/kmc-ldml/src/main.ts
+++ developer/src/kmc-ldml/src/main.ts
@@ -1,5 +1,4 @@
 
 export { LdmlKeyboardCompiler } from './compiler/compiler.js';
 export { LdmlCompilerOptions } from './compiler/ldml-compiler-options.js';
-export { LdmlCompilerMessages as LdmlKeyboardCompilerMessages } from './compiler/ldml-compiler-messages.js';
-// TODO: rename LdmlKeyboardCompilerMessages to LdmlCompilerMessages
+export { LdmlCompilerMessages } from './compiler/ldml-compiler-messages.js';
