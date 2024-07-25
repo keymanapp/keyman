aa5ad7af4acc2dab1889c1f77c757c22c241aaf4
Sabine <Sabine_SIL@web.de>
2024-07-25 13:56:31 +0200

    feat(mac): use new u16

--- mac/mcompile/u16.cpp
+++ mac/mcompile/u16.cpp
@@ -1,127 +1,94 @@
 /*
- * Keyman is copyright (C) SIL International. MIT License.
+ * Keyman is copyright 2024 (C) SIL International. MIT License.
  *
- * Keyboard reading and mapping for mac
+ * Functions for u16string
  */
 
 #include "u16.h"
 #include <codecvt>
 #include <locale>
 #include <stdarg.h>
-
-std::vector<std::u16string> convert_argv_to_Vector_u16str(int argc, char* argv[]) {
-  std::vector<std::u16string> vector_u16;
-
-  // for each arg convert to u16string and push to vector
-  for (char** arg = argv, i = 0; *arg; ++arg, i++) {
-    std::string s(*arg);
-    vector_u16.push_back(u16string_from_string(s));
-  }
-  return vector_u16;
-}
-
-std::vector<std::u16string> convert_argvW_to_Vector_u16str(int argc, wchar_t* argv[]) {
-  std::vector<std::u16string> vector_u16;
-
-  // for each arg convert to u16string and push to vector
-  for (wchar_t** arg = argv, i = 0; *arg; ++arg, i++) {
-    std::wstring S(*arg);
-    vector_u16.push_back(u16string_from_wstring(S));
-  }
-  return vector_u16;
-}
-
-// ToDo codecvt needs to be replaced !!
+#include "../../core/src/utfcodec.hpp"
 
 // String <- wstring
+/** @brief Obtain a std::string from a std::wstring */
 std::string string_from_wstring(std::wstring const str) {
-	std::wstring_convert<std::codecvt_utf8<wchar_t>, wchar_t> converter;
-	return converter.to_bytes(str);
+  return convert<wchar_t, char>((const std::wstring)str);
 }
 
 // wstring <- string
+/** @brief Obtain a std::wstring from a std::string */
 std::wstring wstring_from_string(std::string const str) {
-	std::wstring_convert<std::codecvt_utf8<wchar_t>, wchar_t> converter;
-	return converter.from_bytes(str);
-}
-
-// u16String <- wstring
-std::u16string u16string_from_wstring(std::wstring const wstr) {
-  std::string str = string_from_wstring(wstr);
-  std::u16string str16 = u16string_from_string(str);
-  return str16;
+  return convert<char, wchar_t>((const std::string)str);
 }
 
 // u16String <- string
+/** @brief  Obtain a std::string from a std::wstring */
 std::u16string u16string_from_string(std::string const str) {
-  std::wstring_convert<std::codecvt_utf8_utf16<char16_t>, char16_t> converter;
-  return converter.from_bytes(str);
+  return convert<char, char16_t>((const std::string)str);
 }
 
 // string <- u16string
+/** @brief  Obtain a std::string from a std::u16string */
 std::string string_from_u16string(std::u16string const str) {
-	std::wstring_convert<std::codecvt_utf8_utf16<char16_t>, char16_t> converter;
-	return converter.to_bytes(str);
+  return convert<char16_t, char>((const std::u16string)str);
 }
+
 // wstring <- u16string
-std::wstring wstring_from_u16string(std::u16string const str16) {
-	std::string str = string_from_u16string(str16);
-  std::wstring wstr = wstring_from_string(str);
-  return wstr;
+/** @brief  Obtain a std::wstring from a std::u16string */
+std::wstring wstring_from_u16string(std::u16string const str) {
+  std::string s   = convert<char16_t, char>((const std::u16string)str);
+  std::wstring ws = convert<char, wchar_t>((const std::string)s);
+  return ws;
 }
 
-void u16sprintf(KMX_WCHAR* dst, const size_t max_len, const wchar_t* fmt, ...) {
- // UTF16 (=const wchar_t*) -> -> std::string  -> std::u16string -> UTF16 ( = char16_t*)
-	wchar_t* wbuf = new wchar_t[max_len];
-	va_list args;
-	va_start(args, fmt);
-	vswprintf(wbuf, max_len, fmt, args);
-	va_end(args);
-
-	std::wstring_convert<std::codecvt_utf8<wchar_t>, wchar_t> convert_wstring;
-	std::wstring_convert<std::codecvt_utf8_utf16<char16_t>, char16_t> convert;
-
-   // UTF16 (=const wchar_t*) -> -> std::string  -> std::u16string -> UTF16 ( = char16_t*)
-	std::string utf8str = convert_wstring.to_bytes(wbuf);    // UTF16 ( = const wchar_t*)  -> std::string
-	std::u16string u16str = convert.from_bytes(utf8str);     // std::string -> std::u16string
-  u16ncpy(dst, u16str.c_str(), max_len);                   // std::u16string.c_str() -> char16_t*
-	delete[] wbuf;
+// u16string <- wstring
+/** @brief  Obtain a std::u16string from a std::wstring */
+std::u16string u16string_from_wstring(std::wstring const str) {
+  std::string s        = convert<wchar_t, char>((const std::wstring)str);
+  std::u16string utf16 = convert<char, char16_t>((const std::string)s);
+  return utf16;
 }
 
-std::wstring convert_pchar16T_To_wstr(KMX_WCHAR* name) {
-  //  convert char16_t*  -> std::u16string -> std::string -> std::wstring
-  //  char16_t* -> std::u16string
-  std::u16string u16str(name);
-  //  std::u16string -> std::string
-  std::string stri = string_from_u16string(u16str);
-  //  std::string -> std::wstring
-  std::wstring wstr = wstring_from_string(stri);
-  return wstr;
-  }
+// UTF16 (=const wchar_t*) -> -> std::string  -> std::u16string -> UTF16 ( = char16_t*)
+/** @brief @brief  Convert pointer to wchar_t to pointer to char16_t and copy sz elements into dst */
+void u16sprintf(KMX_WCHAR* dst, const size_t sz, const wchar_t* fmt, ...) {
+  wchar_t* wbuf = new wchar_t[sz];
+  va_list args;
+  va_start(args, fmt);
+  vswprintf(wbuf, sz, fmt, args);
+  va_end(args);
 
+  std::u16string u16str = u16string_from_wstring(wbuf);
+  u16ncpy(dst, u16str.c_str(), sz);
+  delete[] wbuf;
+}
+
+/** @brief @brief  Convert u16string to long integer */
 long int u16tol(const KMX_WCHAR* str, KMX_WCHAR** endptr, int base) {
   auto s = string_from_u16string(str);
   char* t;
-  size_t idx;
-  long int result = strtol(s, &idx, base);
-  if (endptr != nullptr) 
+  long int result = strtol(s.c_str(), &t, base);
+  if (endptr != nullptr)
     *endptr = (KMX_WCHAR*)str + (t - s.c_str());
   return result;
 }
 
+/** @brief  Append n characters from u16string */
 const KMX_WCHAR* u16ncat(KMX_WCHAR* dst, const KMX_WCHAR* src, size_t max) {
   KMX_WCHAR* o = dst;
   dst = (KMX_WCHAR*)u16chr(dst, 0);
-	// max -= (dst-o);
+  // max -= (dst-o);
   while (*src && max > 0) {
     *dst++ = *src++;
     max--;
   }
-	if (max > 0)
-  	*dst = 0;
+  if (max > 0)
+    *dst = 0;
   return o;
 }
 
+/** @brief  Append a '/' or '\\' to an array of char16_t */
 const KMX_WCHAR* u16rchr_slash(KMX_WCHAR const* name) {
   const KMX_WCHAR* cp = NULL;
   cp = u16rchr(name, '\\');
@@ -129,27 +96,31 @@ const KMX_WCHAR* u16rchr_slash(KMX_WCHAR const* name) {
     cp = u16rchr(name, '/');
   return cp;
 }
-/*
-KMX_CHAR* strrchr_slash(KMX_CHAR* name)
-{
+
+/** @brief  Append a '/' or '\\' to an array of char */
+KMX_CHAR* strrchr_slash(KMX_CHAR* name) {
   KMX_CHAR* cp = NULL;
   cp = strrchr(name, '\\');
   if (cp == NULL)
     cp = strrchr(name, '/');
   return cp;
 }
-*/
-// u16rchr returns last occurence of ch in p; It returns '\0'  if ch == '\0' and NULL if ch is not found
+
+/** @brief  Locate last occurrence of character in u16string */
 const KMX_WCHAR* u16rchr(const KMX_WCHAR* p, KMX_WCHAR ch) {
-  const KMX_WCHAR* p_end = p + u16len(p);
-	while (p_end >= p) {
-		if (*p_end == ch)
+  const KMX_WCHAR* p_end = p + u16len(p) - 1;
+
+  if (ch == '\0')
+    return p_end + 1;
+  while (p_end >= p) {
+    if (*p_end == ch)
       return p_end;
-		p_end--;
-	}
-	return NULL;
+    p_end--;
+  }
+  return NULL;
 }
 
+/** @brief @brief  Locate first occurrence of character in u16string */
 const KMX_WCHAR* u16chr(const KMX_WCHAR* p, KMX_WCHAR ch) {
   while (*p) {
     if (*p == ch)
@@ -159,6 +130,7 @@ const KMX_WCHAR* u16chr(const KMX_WCHAR* p, KMX_WCHAR ch) {
   return ch == 0 ? p : NULL;
 }
 
+/** @brief @brief  Copy the u16string pointed to by source into the array pointed to by destination */
 const KMX_WCHAR* u16cpy(KMX_WCHAR* dst, const KMX_WCHAR* src) {
   KMX_WCHAR* o = dst;
   while (*src) {
@@ -168,19 +140,20 @@ const KMX_WCHAR* u16cpy(KMX_WCHAR* dst, const KMX_WCHAR* src) {
   return o;
 }
 
+/** @brief  Copy n characters of the u16string pointed to by source into the array pointed to by destination */
 const KMX_WCHAR* u16ncpy(KMX_WCHAR* dst, const KMX_WCHAR* src, size_t max) {
   KMX_WCHAR* o = dst;
   while (*src && max > 0) {
     *dst++ = *src++;
     max--;
   }
-  while (max > 0) {
-    *dst++ = 0;
-    max--;
+  if (max > 0) {
+    *dst = 0;
   }
   return o;
 }
 
+/** @brief @brief  Return the length of the u16string str */
 size_t u16len(const KMX_WCHAR* p) {
   int i = 0;
   while (*p) {
@@ -190,20 +163,22 @@ size_t u16len(const KMX_WCHAR* p) {
   return i;
 }
 
+/** @brief @brief  Compare two u16strings */
 int u16cmp(const KMX_WCHAR* p, const KMX_WCHAR* q) {
   while (*p && *q) {
     if (*p != *q)
-     return *p - *q;
+      return *p - *q;
     p++;
     q++;
   }
   return *p - *q;
 }
 
+/** @brief @brief Case sensitive comparison of up to count characters in two strings */
 int u16nicmp(const KMX_WCHAR* p, const KMX_WCHAR* q, size_t count) {
   while (*p && *q && count) {
     if (toupper(*p) != toupper(*q))
-     return *p - *q;
+      return *p - *q;
     p++;
     q++;
     count--;
@@ -213,20 +188,22 @@ int u16nicmp(const KMX_WCHAR* p, const KMX_WCHAR* q, size_t count) {
   return 0;
 }
 
+/** @brief @brief Case sensitive comparison of two strings */
 int u16icmp(const KMX_WCHAR* p, const KMX_WCHAR* q) {
   while (*p && *q) {
     if (toupper(*p) != toupper(*q))
-     return *p - *q;
+      return *p - *q;
     p++;
     q++;
   }
   return *p - *q;
 }
 
+/** @brief Comparison of up to count characters in two strings */
 int u16ncmp(const KMX_WCHAR* p, const KMX_WCHAR* q, size_t count) {
   while (*p && *q && count) {
     if (*p != *q)
-     return *p - *q;
+      return *p - *q;
     p++;
     q++;
     count--;
@@ -236,11 +213,12 @@ int u16ncmp(const KMX_WCHAR* p, const KMX_WCHAR* q, size_t count) {
   return 0;
 }
 
-KMX_WCHAR* u16tok(KMX_WCHAR* p, KMX_WCHAR ch, KMX_WCHAR** ctx) {
+/** @brief Split u16string into tokens */
+KMX_WCHAR* u16tok(KMX_WCHAR* p, const KMX_WCHAR ch, KMX_WCHAR** ctx) {
   if (!p) {
     p = *ctx;
     if (!p)
-     return NULL;
+      return NULL;
   }
   KMX_WCHAR* q = p;
   while (*q && *q != ch) {
@@ -255,53 +233,52 @@ KMX_WCHAR* u16tok(KMX_WCHAR* p, KMX_WCHAR ch, KMX_WCHAR** ctx) {
   } else {
     *ctx = NULL;
   }
-	return *p ? p : NULL;
+  return *p ? p : NULL;
 }
 
-KMX_WCHAR* u16tok(KMX_WCHAR* p, KMX_WCHAR* delimiters, KMX_WCHAR** ctx) {
-	if (!p) {
-		p = *ctx;
-		if (!p)
-     return NULL;
-	}
+/** @brief Split u16string into tokens */
+KMX_WCHAR* u16tok(KMX_WCHAR* p, const KMX_WCHAR* delimiters, KMX_WCHAR** ctx) {
+  if (!p) {
+    p = *ctx;
+    if (!p)
+      return NULL;
+  }
 
   KMX_WCHAR* q = p;
-	while (*q && !u16chr(delimiters, *q)) {
-		q++;
-	}
-	if (*q) {
-		*q = 0;
-		q++;
-		while (*q && u16chr(delimiters, *q))
-     q++;
-		*ctx = q;
-	} else {
-		*ctx = NULL;
-	}
-	return *p ? p : NULL;
+  while (*q && !u16chr(delimiters, *q)) {
+    q++;
+  }
+  if (*q) {
+    *q = 0;
+    q++;
+    while (*q && u16chr(delimiters, *q))
+      q++;
+    *ctx = q;
+  } else {
+    *ctx = NULL;
+  }
+  return *p ? p : NULL;
 }
 
+/** @brief Convert a u16string to a double */
 double u16tof(KMX_WCHAR* str) {
-	double val = 0;
-	int offsetdot = 0;
-	char digit;
+  double val    = 0;
+  int offsetdot = 0;
+  char digit;
 
-	PKMX_WCHAR q = (PKMX_WCHAR)u16chr(str, '.');
-	size_t pos_dot = q - str;
+  PKMX_WCHAR q   = (PKMX_WCHAR)u16chr(str, '.');
+  size_t pos_dot = (q - str < 0) ? u16len(str) : q - str;
 
-	if (pos_dot < 0)
-		pos_dot = u16len(str);
+  for (size_t i = 0; i < u16len(str); i++) {
+    digit = static_cast<char>(towupper(*str));
 
-	for (size_t i = 0; i < u16len(str); i++) {
-		digit = static_cast<char>(towupper(*str));
+    if (i > pos_dot - 1)
+      offsetdot = 1;
 
-		if (i > pos_dot - 1)
-			offsetdot = 1;
+    if (digit != '.')
+      val = val + ((int(digit)) - 48) * pow(10, (pos_dot - 1 - i + offsetdot));
 
-		if (digit != '.')
-			val = val + ((int(digit)) - 48) * pow(10, (pos_dot - 1 - i + offsetdot));
-
-		str++;
-	}
-	return val;
+    str++;
+  }
+  return val;
 }
