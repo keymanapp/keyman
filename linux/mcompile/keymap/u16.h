03df66deb138c07cbbdebf1e20361dd54a53316a
Sabine <Sabine_SIL@web.de>
2024-07-30 18:51:22 +0200

    feat(linux): use u16.cpp/h from feat/linux/mcompile-comments

--- linux/mcompile/keymap/u16.h
+++ linux/mcompile/keymap/u16.h
@@ -1,45 +1,199 @@
 #ifndef U16_H
 #define U16_H
+#pragma once
 
-#include <iostream>
-#include <cmath>
-#include <vector>
-#include <ctype.h>
-#include <string>
 #include "km_types.h"
+#include <cmath>
+#include <ctype.h>
+#include <iostream>
+#include <string>
+#include <cstring>
+#include <vector>
 
 
-std::vector<std::u16string> convert_argvW_to_Vector_u16str(int argc, wchar_t* argv[]);
-std::vector<std::u16string> convert_argv_to_Vector_u16str(int argc, char* argv[]);
-
+/**
+ * @brief  Obtain a std::string from a std::wstring
+ * @param  str the std::wstring to be converted
+ * @return a std::string
+ */
 std::string string_from_wstring(std::wstring const str);
+
+/**
+ * @brief  Obtain a std::wstring from a std::string
+ * @param  str the std::string to be converted
+ * @return a std::wstring
+ */
 std::wstring wstring_from_string(std::string const str);
-std::wstring wstring_from_u16string(std::u16string const str16);
+
+/**
+ * @brief  Obtain a std::string from a std::wstring
+ * @param  str the std::wstring to be converted
+ * @return a std::string
+ */
 std::u16string u16string_from_string(std::string const str);
-std::u16string u16string_from_wstring(std::wstring const wstr);
+
+/**
+ * @brief  Obtain a std::string from a std::u16string
+ * @param  str the std::string to be converted
+ * @return a std::u16string
+ */
 std::string string_from_u16string(std::u16string const str);
 
-void u16sprintf(KMX_WCHAR * dst, const size_t sz, const wchar_t* fmt, ...);
+/**
+ * @brief  Obtain a std::wstring from a std::u16string
+ * @param  str the std::u16string to be converted
+ * @return a std::wstring
+ */
+std::wstring wstring_from_u16string(std::u16string const str);
 
-std::wstring  convert_pchar16T_To_wstr(const KMX_WCHAR *Name);
+/**
+ * @brief  Obtain a std::u16string from a std::wstring
+ * @param  str the std::wstring to be converted
+ * @return a std::u16string
+ */
+std::u16string u16string_from_wstring(std::wstring const str);
 
-size_t  u16len(const KMX_WCHAR *p);
-int  u16cmp(const KMX_WCHAR *p, const KMX_WCHAR *q);
-int  u16icmp(const KMX_WCHAR *p, const KMX_WCHAR *q);
-int  u16ncmp(const KMX_WCHAR *p, const KMX_WCHAR *q, size_t count);
-int  u16nicmp(const KMX_WCHAR *p, const KMX_WCHAR *q, size_t count);
-const KMX_WCHAR * u16ncpy(KMX_WCHAR *dst, const KMX_WCHAR *src, size_t max);
-const KMX_WCHAR * u16cpy(KMX_WCHAR *dst, const KMX_WCHAR *src);
-const KMX_WCHAR * u16rchr(const KMX_WCHAR *p, KMX_WCHAR ch);
-const KMX_WCHAR * u16chr(const KMX_WCHAR *p, KMX_WCHAR ch);
-const KMX_WCHAR * u16ncat(KMX_WCHAR *dst, const KMX_WCHAR *src, size_t max);
-KMX_WCHAR * u16tok(KMX_WCHAR *p,  KMX_WCHAR ch,  KMX_WCHAR **ctx);
-KMX_WCHAR * u16tok(KMX_WCHAR* p,  KMX_WCHAR* ch, KMX_WCHAR** ctx);
+/**
+ * @brief  Convert pointer to wchar_t to pointer to char16_t and copy sz elements into dst
+ * @param  dst destination
+ * @param  sz nr of characters to be copied
+ * @param  fmt source to convert and copy
+ * @return void
+ */
+void u16sprintf(KMX_WCHAR* dst, const size_t sz, const wchar_t* fmt, ...);
+
+/**
+ * @brief  Convert u16string to long integer
+ * @param  str u16string beginning with the representation of an integral number.
+ * @param  endptr Reference to the next character in str
+ * @param  base Numerical base (radix) that determines the valid characters and their interpretation
+ * @return a std::string
+ */
 long int u16tol(const KMX_WCHAR* str, KMX_WCHAR** endptr, int base);
-double u16tof( KMX_WCHAR* str);
 
-KMX_CHAR* strrchr_slash(KMX_CHAR* Name);
+/**
+ * @brief  Append n characters from u16string
+ * @param  dst Pointer to the destination array
+ * @param  src u16string to be appended
+ * @param  max Maximum number of characters to be appended.
+ * @return Pointer to the destination array
+ */
+const KMX_WCHAR* u16ncat(KMX_WCHAR* dst, const KMX_WCHAR* src, size_t max);
+
+/**
+ * @brief  Append a '/' or '\\' to an array of char16_t
+ * @param  Name Pointer to the source
+ * @return Pointer to the source with slash/backslash added
+ */
 const KMX_WCHAR* u16rchr_slash(KMX_WCHAR const* Name);
 
+/**
+ * @brief  Append a '/' or '\\' to an array of char
+ * @param  Name Pointer to the source
+ * @return Pointer to the source with slash/backslash added
+ */
+KMX_CHAR* strrchr_slash(KMX_CHAR* Name);
+/**
+ * @brief  Locate last occurrence of character in u16string
+ * @param  p Pointer to the source
+ * @param  ch The character to be found
+ * @return A pointer to the last occurrence of character in u16str
+ */
+const KMX_WCHAR* u16rchr(const KMX_WCHAR* p, KMX_WCHAR ch);
+
+/**
+ * @brief  Locate first occurrence of character in u16string
+ * @param  p Pointer to the source
+ * @param  ch The character to be found
+ * @return A pointer to the first occurrence of character in u16str
+ */
+const KMX_WCHAR* u16chr(const KMX_WCHAR* p, KMX_WCHAR ch);
+
+/**
+ * @brief  Copy the u16string pointed by source into the array pointed by destination
+ * @param  dst Pointer to the destination
+ * @param  src Pointer to the source to be copied
+ * @return Pointer to the destination
+ */
+
+const KMX_WCHAR* u16cpy(KMX_WCHAR* dst, const KMX_WCHAR* src);
+/**
+ * @brief  Copy n characters of the u16string pointed by source into the array pointed by destination
+ * @param  dst Pointer to the destination
+ * @param  src Pointer to the source to be copied
+ * @param  max Maximum number of characters to be copied
+ * @return Pointer to the destination
+ */
+const KMX_WCHAR* u16ncpy(KMX_WCHAR* dst, const KMX_WCHAR* src, size_t max);
+
+/**
+ * @brief  Return the length of the u16string str
+ * @param  p Pointer to the source
+ * @return The length of u16string
+ */
+size_t  u16len(const KMX_WCHAR* p);
+
+/**
+ * @brief  Compare two u16strings
+ * @param  p Pointer one u16string
+ * @param  q Pointer another u16string
+ * @return 0 if strings are eqaual
+ *         ! = 0 if unequal
+ */
+int  u16cmp(const KMX_WCHAR* p, const KMX_WCHAR* q);
+
+/**
+ * @brief Case sensitive comparison of up to count characters in two strings
+ * @param  p Pointer one u16string
+ * @param  q Pointer another u16string
+ * @param  count Maximum number of characters to compare
+ * @return 0 if strings are eqaual
+ *         ! = 0 if unequal
+ */
+int  u16nicmp(const KMX_WCHAR* p, const KMX_WCHAR* q, size_t count);
+
+/**
+ * @brief Case sensitive comparison of two strings
+ * @param  p Pointer one u16string
+ * @param  q Pointer another u16string
+ * @return 0 if strings are eqaual
+ *         ! = 0 if unequal
+ */
+int  u16icmp(const KMX_WCHAR* p, const KMX_WCHAR* q);
+
+/**
+ * @brief Comparison of up to count characters in two strings
+ * @param  p Pointer one u16string
+ * @param  q Pointer another u16string
+ * @param  count Maximum number of characters to compare
+ * @return 0 if strings are eqaual
+ *         ! = 0 if unequal
+ */
+int  u16ncmp(const KMX_WCHAR* p, const KMX_WCHAR* q, size_t count);
+
+/**
+ * @brief Split u16string into tokens
+ * @param  p Pointer to u16string to truncate.
+ * @param  ch the delimiter character
+ * @param  ctx the string until and without the delimiter
+ * @return Pointer to the destination or NULL
+ */
+KMX_WCHAR* u16tok(KMX_WCHAR* p, const KMX_WCHAR ch, KMX_WCHAR** ctx);
+
+/**
+ * @brief Split u16string into tokens
+ * @param  p Pointer to u16string to truncate.
+ * @param  delim the delimiter character
+ * @param  ctx the string until and without the delimiter
+ * @return Pointer to the destination or NULL
+ */
+KMX_WCHAR* u16tok(KMX_WCHAR* p, const KMX_WCHAR* delim, KMX_WCHAR** ctx);
+
+/**
+ * @brief Convert a u16string to a double
+ * @param  str Pointer to u16string
+ * @return double value equivalent to the string
+ */
+double u16tof(KMX_WCHAR* str);
 
 #endif /* U16_H */
