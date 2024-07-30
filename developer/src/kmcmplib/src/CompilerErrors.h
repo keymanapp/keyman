897697d208880ca216606ae320fc3f504ffedd53
Marc Durdin <marc@durdin.net>
2024-07-30 13:09:55 +0700

    chore(developer): support filename field in kmcmplib compiler messages
    Relates-to: #10866

--- developer/src/kmcmplib/src/CompilerErrors.h
+++ developer/src/kmcmplib/src/CompilerErrors.h
@@ -5,7 +5,8 @@
 
 namespace kmcmp {
   extern kmcmp_CompilerMessageProc msgproc;
+  extern void* msgprocContext;
+  extern std::string messageFilename;
 }
 
-extern void* msgprocContext;
 void ReportCompilerMessage(KMX_DWORD msg, const std::vector<std::string>& parameters = {});
