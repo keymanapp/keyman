1d6ea1aff05acc1bbdac1a78827b0469c20dbe2e
Joshua A. Horton <joshua_horton@sil.org>
2024-08-06 08:33:06 +0700

    docs(web): adds function doc-comments per review on ancestor PR #11998

--- common/web/lm-worker/src/main/correction/transform-tokenization.ts
+++ common/web/lm-worker/src/main/correction/transform-tokenization.ts
@@ -1,5 +1,26 @@
 import { applyTransform, type Tokenization } from "@keymanapp/models-templates";
 
+/**
+ * Determines a tokenization-aware sequence of (`Transform`) edits, one per
+ * token that would result after applying the incoming keystroke's `Transform`
+ * to its base `Context`.  This sequence reproduces the same net effect as the
+ * original incoming `transform` when applied in sequence.
+ *
+ * For example, using English and standard whitespace-based tokenization:
+ * - context: `the quick blue`
+ * - transform: `{ insert: 'rown fox', deleteLeft: 3 }`
+ * - Resulting context: `the quick brown fox`
+ * - Output: `[
+ *     { insert: 'rown', deleteLeft: 3 },
+ *     { insert: ' ',    deleteLeft: 0 },
+ *     { insert: 'fox',  deleteLeft: 0 }
+ *   ]`
+ * @param tokenize The tokenization function to utilize, as determined by the
+ * active lexical model or its settings.
+ * @param context  The original, unmodified context
+ * @param transform  A specification of incoming edits to `context`.
+ * @returns
+ */
 export function tokenizeTransform(
   tokenize: (context: Context) => Tokenization,
   context: Context,
@@ -35,8 +56,20 @@ export function tokenizeTransform(
   return tokenizedTransforms;
 }
 
-// If and when we look to do phrase-based suggestions and/or auto-correction on accidental
-// spaces, this function should prove useful.
+/**
+ * Given an incoming distribution of Transforms, this method applies
+ * `tokenizeTransform` for each, mapping each transform to its tokenized form in
+ * the returned distribution.
+ *
+ * It is believed that this may prove useful in the future for phrase-based
+ * suggestions and/or auto-correction of accidentally-typed whitespace.  For
+ * now, it already sees limited use in preventing replacement of word-adjacent
+ * punctuation marks.
+ * @param tokenize
+ * @param context
+ * @param transformDistribution
+ * @returns
+ */
 export function tokenizeTransformDistribution(
   tokenize: (context: Context) => Tokenization,
   context: Context,
