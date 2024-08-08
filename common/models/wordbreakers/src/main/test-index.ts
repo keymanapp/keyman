7016e5b40bdbff256641689a88ad2ffadda22cff
Joshua A. Horton <joshua_horton@sil.org>
2024-08-08 12:53:04 +0700

    chore(common/models): removes incomplete early check-in

--- common/models/wordbreakers/src/main/test-index.ts
+++ common/models/wordbreakers/src/main/test-index.ts
@@ -3,4 +3,3 @@ export * from './index.js';
 
 // Exposes some internal properties for unit-test accessibility
 export { WordBreakProperty } from './default/data.inc.js';
-export { searchForProperty } from './default/searchForProperty.js';
