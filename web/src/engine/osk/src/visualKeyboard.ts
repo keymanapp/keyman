4509805f58803c98142e2b4f1be16eb68be47dee
Joshua A. Horton <joshua_horton@sil.org>
2024-08-15 09:04:35 +0700

    fix(web): restore flick functionality

--- web/src/engine/osk/src/visualKeyboard.ts
+++ web/src/engine/osk/src/visualKeyboard.ts
@@ -151,7 +151,7 @@ export default class VisualKeyboard extends EventEmitter<EventMap> implements Ke
   /**
    * Tweakable gesture parameters referenced by supported gestures and the gesture engine.
    */
-  get gestureParams(): GestureParams<KeyElement> {
+  get gestureParams(): GestureParams {
     return this.config.gestureParams;
   };
 
@@ -421,11 +421,6 @@ export default class VisualKeyboard extends EventEmitter<EventMap> implements Ke
       */
       recordingMode: DEBUG_GESTURES,
       historyLength: DEBUG_HISTORY_COUNT
-    };
-
-    this.gestureParams.longpress.permitsFlick = (key) => {
-      const flickSpec = key?.key.spec.flick;
-      return !flickSpec || !(flickSpec.n || flickSpec.nw || flickSpec.ne);
     };
 
     const recognizer = new GestureRecognizer(gestureSetForLayout(this.kbdLayout, this.gestureParams), config);
