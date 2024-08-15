a766e4460e80a4ec9abffe4581f6e6d2884fc3a0
Marc Durdin <marc@durdin.net>
2024-08-15 17:20:33 +0200

    chore(developer): reduce kmc coverage threshold to 45%

--- developer/src/kmc/build.sh
+++ developer/src/kmc/build.sh
@@ -62,7 +62,7 @@ function do_api() {
 function do_test() {
   eslint .
   tsc --build test/
-  readonly C8_THRESHOLD=50
+  readonly C8_THRESHOLD=45
   c8 --reporter=lcov --reporter=text --lines $C8_THRESHOLD --statements $C8_THRESHOLD --branches $C8_THRESHOLD --functions $C8_THRESHOLD mocha
   builder_echo warning "Coverage thresholds are currently $C8_THRESHOLD%, which is lower than ideal."
   builder_echo warning "Please increase threshold in build.sh as test coverage improves."
