98787afbdcacf7a8e1a4f9e64e929fd3948f7007
rc-swag <58423624+rc-swag@users.noreply.github.com>
2024-08-20 11:15:50 +1000

    feat(windows): restore removal of klogging

--- common/windows/delphi/tools/test-klog/test_klog.dpr
+++ common/windows/delphi/tools/test-klog/test_klog.dpr
@@ -7,17 +7,13 @@ uses
   klog in '..\..\..\..\..\common\windows\delphi\general\klog.pas',
   VersionInfo in '..\..\..\..\..\common\windows\delphi\general\VersionInfo.pas',
   ErrorControlledRegistry in '..\..\..\..\..\common\windows\delphi\vcl\ErrorControlledRegistry.pas',
-  Unicode in '..\..\..\..\..\common\windows\delphi\general\Unicode.pas',
-  DebugPaths in '..\..\..\..\..\common\windows\delphi\general\DebugPaths.pas',
-  RegistryKeys in '..\..\..\..\..\common\windows\delphi\general\RegistryKeys.pas',
-  KeymanVersion in '..\..\..\..\..\common\windows\delphi\general\KeymanVersion.pas',
-  KeymanPaths in '..\..\..\..\..\common\windows\delphi\general\KeymanPaths.pas';
+  Unicode in '..\..\..\..\..\common\windows\delphi\general\Unicode.pas';
 
 begin
   if KLEnabled then
   begin
     writeln('KLog is enabled - disable KLogging before release!');
-    ExitCode := 0;
+    ExitCode := 1;
   end
   else
   begin
