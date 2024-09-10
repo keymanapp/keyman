178325c019eace4d73339c409adfeefad9f691b8
Marc Durdin <marc@durdin.net>
2024-09-10 15:30:54 +0700

    chore(developer): use new npm publish for kmc-generate

--- developer/src/kmc-generate/build.sh
+++ developer/src/kmc-generate/build.sh
@@ -15,14 +15,10 @@ builder_describe "Build Keyman kmc-generate module" \
   "@/common/web/keyman-version" \
   "@/common/web/types" \
   "@/developer/src/common/web/test-helpers" \
-  "configure" \
-  "build" \
-  "api                       analyze API and prepare API documentation" \
-  "clean" \
-  "test" \
-  "pack                      build a local .tgz pack for testing" \
-  "publish                   publish to npm" \
+  clean configure build api test publish \
+  "--npm-publish+            For publish, do a npm publish, not npm pack (only for CI)" \
   "--dry-run,-n              don't actually publish, just dry run"
+
 builder_describe_outputs \
   configure     /node_modules \
   build         /developer/src/kmc-generate/build/src/main.js \
@@ -52,5 +48,4 @@ builder_run_action configure  verify_npm_setup
 builder_run_action build      do_build
 builder_run_action api        api-extractor run --local --verbose
 builder_run_action test       do_test
-builder_run_action publish    builder_publish_to_npm
-builder_run_action pack       builder_publish_to_pack
+builder_run_action publish    builder_publish_npm
