fd629c574e6227706db1b172b892ba911d9e6bfe
Eberhard Beilharz <eb1@sil.org>
2024-10-16 14:53:37 +0200

    feat(core): rename `core.js` to `km-core.js`

--- web/src/engine/core-processor/src/core-processor.ts
+++ web/src/engine/core-processor/src/core-processor.ts
@@ -1,17 +1,17 @@
-type km_core_attr = import('./import/core/core-interface.js').km_core_attr;
+type km_core_attr = import('./import/core/km-core-interface.js').km_core_attr;
 
 export class CoreProcessor {
   private instance: any;
 
   /**
-     * Initialize Core Processor
-     * @param baseurl - The url where core.js is located
-     */
+   * Initialize Core Processor
+   * @param baseurl - The url where km-core.js is located
+   */
   public async init(baseurl: string): Promise<boolean> {
 
     if (!this.instance) {
       try {
-        const module = await import(baseurl + '/core.js');
+        const module = await import(baseurl + '/km-core.js');
         this.instance = await module.default({
           locateFile: function (path: string, scriptDirectory: string) {
             return baseurl + '/' + path;
