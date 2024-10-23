246efb4f39d8d41de271513a10332d0e4fd838e0
Shawn Schantz <shawn_schantz@sil.org>
2024-10-23 19:14:26 +0700

    add resizeKeyboard logging

--- ios/engine/KMEI/KeymanEngine/Classes/Keyboard/KeymanWebViewController.swift
+++ ios/engine/KMEI/KeymanEngine/Classes/Keyboard/KeymanWebViewController.swift
@@ -768,6 +768,7 @@ extension KeymanWebViewController {
     // the first time.
     setOskWidth(Int(kbSize.width))
     setOskHeight(Int(kbSize.height))
+    os_log("KeymanWebViewController resizeKeyboard to kbSize %{public}d", log:KeymanEngineLogger.ui, type: .default, NSCoder.string(for:kbSize))
   }
 
   func resetKeyboardState() {
