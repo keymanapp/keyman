8695112c82739a13f5c508d5ab16f90d27216ac5
sgschantz <shawn_schantz@sil.org>
2024-11-01 11:22:09 +0700

    fix(ios): add subviews for keyboard height adjustment

--- ios/engine/KMEI/KeymanEngine/Classes/KeyboardHeightViewController.swift
+++ ios/engine/KMEI/KeymanEngine/Classes/KeyboardHeightViewController.swift
@@ -12,6 +12,7 @@ import os.log
 
 class KeyboardHeightViewController: UIViewController {
   var label: UILabel = UILabel()
+  var keyboardImage: UIImageView = UIImageView()
 
   public init() {
     super.init(nibName: nil, bundle: nil)
@@ -21,6 +22,18 @@ class KeyboardHeightViewController: UIViewController {
   override func viewDidLoad() {
     super.viewDidLoad()
 
+    let deviceOrientation = UIDevice.current.orientation
+    let isPortrait = deviceOrientation.isPortrait
+ 
+    let keyboardScale = KeyboardScaleMap.getDeviceDefaultKeyboardScale(forPortrait: isPortrait)
+    let scalemessage = "KeyboardHeightViewController viewDidLoad, isPortrait: \(isPortrait) keyboardScale: \(String(describing: keyboardScale))"
+    os_log("%{public}s", log:KeymanEngineLogger.ui, type: .info, scalemessage)
+
+    let kbHeight = keyboardScale?.keyboardHeight ?? 216 // default for ancient devices
+
+    let message = "KeyboardHeightViewController viewDidLoad,  boundsRect: \(self.view.bounds) keyboardHeight from scale: \(kbHeight)"
+    os_log("%{public}s", log:KeymanEngineLogger.ui, type: .info, message)
+
     //title = NSLocalizedString("menu-settings-spacebar-title", bundle: engineBundle, comment: "")
     title = "Adjust Keyboard Height"
     navigationItem.setHidesBackButton(false, animated: true)
@@ -28,21 +41,23 @@ class KeyboardHeightViewController: UIViewController {
 
     navigationController?.toolbar?.barTintColor = Colors.statusToolbar
 
-    let firstFrame = CGRect(x: 160, y: 240, width: 200, height: 150)
-    let firstView = UIView(frame: firstFrame)
-    firstView.backgroundColor = UIColor.blue
-    view.addSubview(firstView)
+    let contentView = UIView(frame: self.view.bounds)
+    contentView.backgroundColor = UIColor.systemBlue
+    view.addSubview(contentView)
     
-    label.frame = CGRectMake(10, 10, 180, 100)
+    label.frame = CGRectMake(10, 50, 180, 100)
     label.backgroundColor=UIColor.white
     label.textAlignment = NSTextAlignment.center
-    label.text = "adjust keyboard height"
+    label.text = "Drag the top edge of the keyboard to adjust its height./nSeparate values are saved for portrait and landscape keyboard height."
     //label.isHidden=true
-    firstView.addSubview(label)
-  }
-  
-  override open func viewWillAppear(_ animated: Bool) {
-    super.viewWillAppear(animated)
+    contentView.addSubview(label)
+    
+    // calculate frame for keyboard image
+    let keyboardFrame = CGRectMake(0, self.view.frame.height-kbHeight, self.view.frame.width, kbHeight)
+    keyboardImage.frame = keyboardFrame
+    keyboardImage.contentMode = UIView.ContentMode.scaleToFill
+    keyboardImage.image = UIImage(named:"portrait-osk")
+    contentView.addSubview(keyboardImage)
   }
   
   required public init?(coder aDecoder: NSCoder) {
