2cb2e8c4fb8a272e38cd7744aad2b9d5a39d8dc0
Dr Mark C. Sinclair <markcsinclair@gmail.com>
2024-11-26 08:08:15 +0000

    chore(developer): initial commit of test-string-lists.ts

---
+++ common/web/types/test/ldml-keyboard/test-string-list.ts
@@ -0,0 +1,11 @@
+import 'mocha';
+import { assert } from 'chai';
+//import { ListIndex, ListItem } from '../../src/ldml-keyboard/string-list.js';
+
+describe('Test of String-List', () => {
+  describe('should test ultimate truth', () => {
+    it('the truth', () => {
+      assert.isTrue(true);
+    });
+  });
+});
