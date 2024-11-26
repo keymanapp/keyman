fa4f1e5d0fc7acb8c41c4787c68a4df13f8b1bd2
Dr Mark C. Sinclair <markcsinclair@gmail.com>
2024-11-26 08:50:36 +0000

    chore(developer): add one ListItem test case

--- common/web/types/test/ldml-keyboard/test-string-list.ts
+++ common/web/types/test/ldml-keyboard/test-string-list.ts
@@ -1,11 +1,14 @@
 import 'mocha';
 import { assert } from 'chai';
 //import { ListIndex, ListItem } from '../../src/ldml-keyboard/string-list.js';
+import { ListItem } from '../../src/ldml-keyboard/string-list.js';
 
 describe('Test of String-List', () => {
-  describe('should test ultimate truth', () => {
-    it('the truth', () => {
-      assert.isTrue(true);
+  describe('should test ListItem', () => {
+    it('fromStrings returns an empty ListItem if source is null', () => {
+      const actual   = ListItem.fromStrings(null, null, null);
+      const expected = new ListItem();
+      assert.deepEqual(actual, expected);
     });
   });
 });
