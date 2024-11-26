f3e5c1bc77f09a552fc90e9ddc41cacdeb42a8f0
rc-swag <58423624+rc-swag@users.noreply.github.com>
2024-11-26 15:03:58 +1000

    feat(windows): add exiterror goto plus extra checks
    Adds ExitError goto as there where too many paths requiring similar
    clean up. Checks around reading the file.

--- windows/src/engine/keyman32/K32_load.cpp
+++ windows/src/engine/keyman32/K32_load.cpp
@@ -84,40 +84,52 @@ BOOL LoadlpKeyboard(int i)
     return_SendDebugExit(FALSE);
   }
 
-  FILE* kmx_file;
+  FILE* kmx_file = nullptr;
+  PWCHAR keyboardPath = nullptr;
+  void* buffer = nullptr;
   errno_t status = fopen_s(&kmx_file, kmx_filename, "rb");
+
   if (!status) {
     SendDebugMessageFormat("Problem reading opening kmx_file %s status [%d].", kmx_filename, status);
     return_SendDebugExit(FALSE);
   }
 
-  fseek(kmx_file, 0, SEEK_END);
+  if (fseek(kmx_file, 0, SEEK_END) < 0 ) {
+    SendDebugMessageFormat("Problem seeking to end of kmx_file %s.", kmx_filename);
+    goto ExitError;
+  }
+
   size_t length = ftell(kmx_file);
-  fseek(kmx_file, 0, SEEK_SET);
-  void* buffer = malloc(length);
+
+  if (length < 0)
+  {
+    SendDebugMessageFormat("Problem determining length of kmx_file %s.", kmx_filename);
+    goto ExitError;
+  }
+
+  rewind(kmx_file);
+
+  buffer = malloc(length);
 
   if (!buffer) {
-    fclose(kmx_file);
-    return_SendDebugExit(FALSE);
+    SendDebugMessageFormat("Problem allocating buffer for reading kmx_file %s.", kmx_filename);
+    goto ExitError;
   }
 
   if (fread(buffer, 1, length, kmx_file) != length) {
     SendDebugMessageFormat("Problem reading entire kmx_file %s.", kmx_filename);
-    fclose(kmx_file);
-    free(buffer);
-    return_SendDebugExit(FALSE);
+    goto ExitError;
   }
 
   fclose(kmx_file);
-  PWCHAR keyboardPath = strtowstr(kmx_filename);
+  keyboardPath = strtowstr(kmx_filename);
   km_core_status err_status = km_core_keyboard_load_from_blob(keyboardPath, buffer, length, &_td->lpKeyboards[i].lpCoreKeyboard);
 
   free(buffer);
 
   if (err_status != KM_CORE_STATUS_OK) {
     SendDebugMessageFormat("km_core_keyboard_load failed for %ls with error status [%d]", keyboardPath, err_status);
-    delete[] keyboardPath;
-    return_SendDebugExit(FALSE);
+    goto ExitError;
   }
   delete[] keyboardPath;
 
@@ -134,22 +146,30 @@ BOOL LoadlpKeyboard(int i)
 
   if (err_status != KM_CORE_STATUS_OK) {
     SendDebugMessageFormat("km_core_state_create failed with error status [%d]", err_status);
-    // Dispose of the keyboard to leave us in a consistent state
-    ReleaseKeyboardMemoryCore(&_td->lpKeyboards[i].lpCoreKeyboard);
-    return_SendDebugExit(FALSE);
+    goto ExitError;
   }
   // Register callback?
   err_status = km_core_keyboard_get_imx_list(_td->lpKeyboards[i].lpCoreKeyboard, &_td->lpKeyboards[i].lpIMXList);
   if (err_status != KM_CORE_STATUS_OK) {
     SendDebugMessageFormat("km_core_keyboard_get_imx_list failed with error status [%d]", err_status);
-    // Dispose of the keyboard to leave us in a consistent state
-    ReleaseKeyboardMemoryCore(&_td->lpKeyboards[i].lpCoreKeyboard);
-    return_SendDebugExit(FALSE);
+    goto ExitError;
   }
 
   LoadDLLs(&_td->lpKeyboards[i]);
-
   LoadKeyboardOptionsRegistrytoCore(&_td->lpKeyboards[i], _td->lpKeyboards[i].lpCoreKeyboardState);
 
   return_SendDebugExit(TRUE);
+ExitError:
+  if (kmx_file) {
+    fclose(kmx_file);
+  }
+  if (buffer) {
+    free(buffer);
+  }
+  if (keyboardPath != nullptr) {
+    delete[] keyboardPath;
+  }
+  // Dispose of the keyboard to leave us in a consistent state
+  ReleaseKeyboardMemoryCore(&_td->lpKeyboards[i].lpCoreKeyboard);
+  return_SendDebugExit(FALSE);
 }
