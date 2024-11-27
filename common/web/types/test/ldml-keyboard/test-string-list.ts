a5c80243e742746dbc29995464959ef3982b926d
Dr Mark C. Sinclair <markcsinclair@gmail.com>
2024-11-27 02:24:11 +0000

    chore(developer): add six test cases for ListIndex

--- common/web/types/test/ldml-keyboard/test-string-list.ts
+++ common/web/types/test/ldml-keyboard/test-string-list.ts
@@ -1,9 +1,48 @@
 import 'mocha';
 import { assert } from 'chai';
-//import { ListIndex, ListItem } from '../../src/ldml-keyboard/string-list.js';
-import { ListItem } from '../../src/ldml-keyboard/string-list.js';
+import { StrsItem } from '../../src/kmx/kmx-plus/kmx-plus.js';
+import { ListIndex, ListItem } from '../../src/ldml-keyboard/string-list.js';
 
 describe('Test of String-List', () => {
+  describe('should test ListIndex', () => {
+    it('can construct a ListIndex', () => {
+      const strsItem = new StrsItem("abc");
+      const actual   = new ListIndex(strsItem);
+      assert.deepEqual(actual.value, strsItem);
+    });
+    it('can check two ListIndex for equality', () => {
+      const strsItemOne = new StrsItem("abc");
+      const strsItemTwo = new StrsItem("abc");
+      const listItemOne = new ListIndex(strsItemOne);
+      const listItemTwo = new ListIndex(strsItemTwo);
+      assert.isTrue(listItemOne.isEqual(listItemTwo));
+    });
+    it('can check two different ListIndex are not equal', () => {
+      const strsItemOne = new StrsItem("abc");
+      const strsItemTwo = new StrsItem("def");
+      const listItemOne = new ListIndex(strsItemOne);
+      const listItemTwo = new ListIndex(strsItemTwo);
+      assert.isFalse(listItemOne.isEqual(listItemTwo));
+    });
+    it('can check a ListIndex and string for equality', () => {
+      const strsItem = new StrsItem("abc");
+      const listItem = new ListIndex(strsItem);
+      const aString = "abc";
+      assert.isTrue(listItem.isEqual(aString));
+    });
+    it('can check a ListIndex and string for inequality', () => {
+      const strsItem = new StrsItem("abc");
+      const listItem = new ListIndex(strsItem);
+      const aString = "def";
+      assert.isFalse(listItem.isEqual(aString));
+    });
+    it('can provide a correct string representation', () => {
+      const strsItem = new StrsItem("abc");
+      const listItem = new ListIndex(strsItem);
+      const expected = "abc";
+      assert.deepEqual(listItem.toString(), expected);
+    });
+  });
   describe('should test ListItem', () => {
     it('fromStrings returns an empty ListItem if source is null', () => {
       const actual   = ListItem.fromStrings(null, null, null);
