acf156b965a92faf4bfcf8cded7cf9effd370379
Dr Mark C. Sinclair <markcsinclair@gmail.com>
2024-11-27 07:02:44 +0000

    chore(common/web): add test-unicodeset-parser.ts to test UnicodeSet

---
+++ common/web/types/test/ldml-keyboard/test-unicodeset-parser-api.ts
@@ -0,0 +1,24 @@
+import 'mocha';
+import { assert } from 'chai';
+import { UnicodeSet } from '../../src/ldml-keyboard/unicodeset-parser-api.js';
+
+class MockUnicodeSet extends UnicodeSet {
+  constructor(public pattern: string, public ranges: number[][]) {
+    super(pattern, ranges); // does nothing
+    this.pattern = pattern;
+    this.ranges  = ranges;
+  }
+}
+
+describe('Test of Unicode-Parser-API', () => {
+  describe('Test UnicodeSet', () => {
+    it('can provide a correct ranges length', () => {
+      const unicodeSet = new MockUnicodeSet("[ħa-z]", [[0x41, 0x7A], [0x0127, 0x0127]]);
+      assert.equal(unicodeSet.length, 2);
+    });
+    it('can provide a correct string representation', () => {
+      const unicodeSet = new MockUnicodeSet("[ħa-z]", [[0x41, 0x7A], [0x0127, 0x0127]]);
+      assert.deepEqual(unicodeSet.toString(), "[ħa-z]");
+    });
+  });
+});
