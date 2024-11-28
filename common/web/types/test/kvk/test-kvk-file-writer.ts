33056f0ec1c5fdab74cbbeac0fecc3738e3fe734
Dr Mark C. Sinclair <markcsinclair@gmail.com>
2024-11-28 08:56:23 +0000

    chore(common/web): incomplete test case for build() ... keys still needed

--- common/web/types/test/kvk/test-kvk-file-writer.ts
+++ common/web/types/test/kvk/test-kvk-file-writer.ts
@@ -9,9 +9,15 @@
 import 'mocha';
 import { assert } from 'chai';
 import { KvkFileWriter } from '../../src/main.js';
-//import { VisualKeyboard, VisualKeyboardKey, DEFAULT_KVK_FONT } from '../../src/kvk/visual-keyboard.js';
-//import { BUILDER_KVK_HEADER_FLAGS } from '../../src/kvk/kvk-file.js';
-import { BUILDER_KVK_STRING } from '../../src/kvk/kvk-file.js';
+import { VisualKeyboard, VisualKeyboardKey, DEFAULT_KVK_FONT } from '../../src/kvk/visual-keyboard.js';
+import { BUILDER_KVK_FILE,
+         BUILDER_KVK_HEADER_FLAGS,
+         BUILDER_KVK_STRING,
+         BUILDER_KVK_HEADER_IDENTIFIER,
+         BUILDER_KVK_HEADER_VERSION,
+} from '../../src/kvk/kvk-file.js';
+
+const VISUAL_KEYBOARD_TEXT_COLOR = 0xFF000008;
 
 describe('Test of KVK-File-Writer', () => {
   describe('Test of write()', () => {
@@ -21,6 +27,27 @@ describe('Test of KVK-File-Writer', () => {
     //   const file   = writer.write(vk);
     //   assert.isNotNull(file);
     // });
+  });
+  describe('Test of build()', () => {
+    it('can build a BUILDER_KVK_FILE', () => {
+      const vk     = initVisualKeyboard();
+      const writer = new KvkFileWriter;
+      const binary: BUILDER_KVK_FILE = writer['build'](vk);
+      assert.equal(binary.header.identifier, BUILDER_KVK_HEADER_IDENTIFIER);
+      assert.equal(binary.header.version, BUILDER_KVK_HEADER_VERSION);
+      assert.deepEqual(binary.header.associatedKeyboard.str, vk.header.associatedKeyboard);
+      assert.equal(binary.header.flags, BUILDER_KVK_HEADER_FLAGS.kvkhNone);
+      assert.deepEqual(binary.header.ansiFont, {
+        color: VISUAL_KEYBOARD_TEXT_COLOR,
+        size: vk.header.ansiFont.size,
+        name: { len: vk.header.ansiFont.name.length + 1, str: vk.header.ansiFont.name },
+      });
+      assert.deepEqual(binary.header.unicodeFont, {
+        color: VISUAL_KEYBOARD_TEXT_COLOR,
+        size: vk.header.unicodeFont.size,
+        name: { len: vk.header.ansiFont.name.length + 1, str: vk.header.ansiFont.name },
+      });
+    });
   });
   describe('Test of setString()', () => {
     it('can set a BUILDER_KVK_STRING', () => {
@@ -34,17 +61,17 @@ describe('Test of KVK-File-Writer', () => {
   });
 });
 
-// function initVisualKeyboard(): VisualKeyboard {
-//   const vkh = {
-//     // version?: number,
-//     flags: BUILDER_KVK_HEADER_FLAGS.kvkhNone,
-//     // associatedKeyboard?: string,
-//     ansiFont: DEFAULT_KVK_FONT,
-//     unicodeFont: DEFAULT_KVK_FONT,
-//     // underlyingLayout?: string,
-//   };
-//   const vkks: VisualKeyboardKey[] = [];
-//   const vk: VisualKeyboard = { header: vkh, keys: vkks };
-//   return vk;
-// };
+function initVisualKeyboard(): VisualKeyboard {
+  const vkh = {
+    // version?: number,
+    flags: BUILDER_KVK_HEADER_FLAGS.kvkhNone,
+    associatedKeyboard: "associatedKeyboard",
+    ansiFont: DEFAULT_KVK_FONT,
+    unicodeFont: DEFAULT_KVK_FONT,
+    // underlyingLayout?: string,
+  };
+  const vkks: VisualKeyboardKey[] = [];
+  const vk: VisualKeyboard = { header: vkh, keys: vkks };
+  return vk;
+};
 
