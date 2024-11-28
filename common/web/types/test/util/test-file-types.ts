324c57f793d3b6e1283ed8cc48b01a514bfcd192
Dr Mark C. Sinclair <markcsinclair@gmail.com>
2024-11-28 02:19:19 +0000

    chore(common/web): test Source file extension removal

--- common/web/types/test/util/test-file-types.ts
+++ common/web/types/test/util/test-file-types.ts
@@ -10,11 +10,23 @@ import 'mocha';
 import { assert } from 'chai';
 import { removeExtension } from '../../src/util/file-types.js';
 
+const sourceExts = [ // can't use Source, as it is const enum
+  '.model.ts',
+  '.kpj',
+  '.kmn',
+  '.xml',
+  '.kvks',
+  '.keyman-touch-layout',
+];
+
 describe('Test of File-Types', () => {
   describe('Test of removeExtension()', () => {
-    it('can remove file extension', () => {
-      const filename = removeExtension("file.kmn");
-      assert.deepEqual(filename, "file");
+    it('can remove Source file extension', () => {
+      sourceExts.forEach((ext) => {
+        const filename = `file${ext}`;
+        const actual   = removeExtension(filename);
+        assert.deepEqual(actual, "file");
+      });
     });
     it('can handle no file extension', () => {
       const filename = removeExtension("file");
