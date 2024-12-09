13b463aedf8ef29cd1e6356662b34b6242116c49
Dr Mark C. Sinclair <markcsinclair@gmail.com>
2024-12-09 16:16:31 +0000

    chore(common/web): add initElemElement and first true test case for isEqual()

--- common/web/types/tests/kmx/kmx-plus/element-string.tests.ts
+++ common/web/types/tests/kmx/kmx-plus/element-string.tests.ts
@@ -8,12 +8,40 @@
 
 import 'mocha';
 import { assert } from 'chai';
+import { ElemElementFlags, ElemElement } from '../../../src/kmx/kmx-plus/element-string.js';
+import { StrsItem, UsetItem } from '../../../src/kmx/kmx-plus/kmx-plus.js';
+import { UnicodeSet } from '../../../src/ldml-keyboard/unicodeset-parser-api.js';
+
+const GOTHIC_A = new StrsItem("𐌰", 0x10330);
+const GOTHIC_A_SET = new UsetItem(
+  new UnicodeSet("[𐌰-𐍊]", [[0x10330,0x1034A]]),
+  GOTHIC_A
+);
 
 describe('Test of ElementString', () => {
   describe('Test of ElemElement', () => {
     describe('Test of isEqual()', () => {
-      assert.isTrue(true);
-      assert.isFalse(false);
+      it('returns true when elems identical', () => {
+        const one = initElemElement();
+        const two = initElemElement()
+        assert.isTrue(one.isEqual(two));
+      });
     });
   });
 });
+
+function initElemElement(
+  value: StrsItem = GOTHIC_A,
+  uset: UsetItem = GOTHIC_A_SET,
+  order: number = 0,
+  tertiary: number = 1,
+  flags: ElemElementFlags = ElemElementFlags.none,
+): ElemElement {
+  const ee  = new ElemElement();
+  ee.value = value;
+  ee.uset = uset;
+  ee.order = order;
+  ee.tertiary = tertiary;
+  ee.flags = flags;
+  return ee;
+}
