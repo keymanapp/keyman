fea40e2689b8f185568a2b42f6575dcae846077f
Dr Mark C. Sinclair <markcsinclair@gmail.com>
2024-12-09 11:47:54 +0000

    chore(common/web): initial test boilerplate

---
+++ common/web/types/tests/kmx/kmx-plus/element-string.tests.ts
@@ -0,0 +1,19 @@
+/*
+ * Keyman is copyright (C) SIL Global. MIT License.
+ * 
+ * Created by Dr Mark C. Sinclair on 2024-12-09
+ * 
+ * Test code for element-string.ts
+ */
+
+import 'mocha';
+import { assert } from 'chai';
+
+describe('Test of ElementString', () => {
+  describe('Test of ElemElement', () => {
+    describe('Test of isEqual()', () => {
+      assert.isTrue(true);
+      assert.isFalse(false);
+    });
+  });
+});
