6bef09f68b81290a69b7a6efb7252bde86669d9e
Eberhard Beilharz <eb1@sil.org>
2024-12-20 18:00:25 +0100

    fix(core): work around alignment problem when using Emscripten
    The data we read from the KMX file is not in places 32-bit aligned, e.g.
    `dpGroupArray` will often start at a 16-bit boundary. Emscripten doesn't
    like this and will abort the program if `SAFE_HEAP=1` is defined.
    
    This change works around the problem by calling `CopyKeyboard` and thus
    expanding the structure.
    
    See #12844.

--- core/src/kmx/kmx_processevent.h
+++ core/src/kmx/kmx_processevent.h
@@ -57,7 +57,7 @@ private:
   KMX_BOOL LoadKeyboardFromBlob(PKMX_BYTE buf, size_t sz, LPKEYBOARD* lpKeyboard);
   KMX_BOOL VerifyKeyboard(PKMX_BYTE filebase, size_t sz);
   KMX_BOOL VerifyChecksum(PKMX_BYTE buf,  size_t sz);
-#ifdef KMX_64BIT
+#if defined(KMX_64BIT) || defined(__EMSCRIPTEN__)
   LPKEYBOARD CopyKeyboard(PKMX_BYTE bufp, PKMX_BYTE base);
 #else
   LPKEYBOARD FixupKeyboard(PKMX_BYTE bufp, PKMX_BYTE base);
