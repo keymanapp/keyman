09eb489db2d0ea906d1def66d0c4f2d29bb7ddd8
Joshua Horton <jahorton@sil.org>
2025-01-13 11:43:36 +0700

    chore: update macOS environment-variable shell script

--- resources/devbox/macos/keyman.macos.env.sh
+++ resources/devbox/macos/keyman.macos.env.sh
@@ -5,7 +5,6 @@ export ANT_HOME=/usr/local/opt/ant
 export MAVEN_HOME=/usr/local/opt/maven
 export GRADLE_HOME=/usr/local/opt/gradle
 export ANDROID_HOME=~/.android
-export ANDROID_SDK_ROOT="$HOME/Library/Android/sdk"
 
 export PATH=$ANT_HOME/bin:$PATH
 export PATH=$MAVEN_HOME/bin:$PATH
@@ -13,10 +12,11 @@ export PATH=$GRADLE_HOME/bin:$PATH
 export PATH=$ANDROID_HOME/tools:$PATH
 export PATH=$ANDROID_HOME/tools/bin:$PATH
 export PATH=$ANDROID_HOME/platform-tools:$PATH
+export PATH=$ANDROID_HOME/cmdline-tools/latest/bin:$PATH
 export PATH=$ANDROID_HOME/build-tools/30.0.3:$PATH
-export PATH="/usr/local/opt/openjdk@8/bin:$PATH"
+export PATH="/opt/homebrew/opt/java11/bin:$PATH"
 export PATH="$HOMEBREW_PREFIX/opt/coreutils/libexec/gnubin:$PATH"
-export JAVA_HOME=/usr/local/opt/openjdk@8
+export JAVA_HOME=/opt/homebrew/opt/java11
 
 # Python 2.7
 eval "$(pyenv init --path)"
