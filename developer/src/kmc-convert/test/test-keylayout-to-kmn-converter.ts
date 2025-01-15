958373e310e061ea687880ff68f2c0836986191b
Sabine <Sabine_SIL@web.de>
2025-01-15 09:30:25 +0100

    feat(developer): kmc-convert use object for writing out all rules

--- developer/src/kmc-convert/test/test-keylayout-to-kmn-converter.ts
+++ developer/src/kmc-convert/test/test-keylayout-to-kmn-converter.ts
@@ -7,19 +7,19 @@ cd developer/src/kmc-convert/
 
 
 import 'mocha';
-import {assert} from 'chai';
-import {compilerTestCallbacks, compilerTestOptions} from './helpers/index.js';
-import {KeylayoutToKmnConverter} from '../src/keylayout-to-kmn/keylayout-to-kmn-converter.js';
+import { assert } from 'chai';
+import { compilerTestCallbacks, compilerTestOptions } from './helpers/index.js';
+import { KeylayoutToKmnConverter } from '../src/keylayout-to-kmn/keylayout-to-kmn-converter.js';
 
 import { makePathToFixture } from './helpers/index.js';       // _S2 my imports
 
-describe('KeylayoutToKmnConverter', function() {
+describe('KeylayoutToKmnConverter', function () {
 
-    before(function() {
-      compilerTestCallbacks.clear();
-    });
+  before(function () {
+    compilerTestCallbacks.clear();
+  });
 
-    // _S2 first test
+  // _S2 first test
   /*  it('should throw on null inputs', async function () {
       // const inputFilename = makePathToFixture('file.keylayout');
       const converter = new KeylayoutToKmnConverter(compilerTestCallbacks, compilerTestOptions);
@@ -34,34 +34,44 @@ describe('KeylayoutToKmnConverter', function() {
     });*/
 
 
-    // _S2 My tests... // later throws on NOT all elements loaded
-    it('should throw on all elements loaded', async function () {
+  // _S2 My tests... // later throws on NOT all elements loaded
+  it('should throw on all elements loaded', async function () {
 
-      // some keys, no deadkeys
-       //const inputFilename  = makePathToFixture('../data/Mysample.keylayout');
-      // all keys, some deadkeys
-      //const inputFilename  = makePathToFixture('../data/US_complete.keylayout');
+    // some keys, no deadkeys
+    //const inputFilename  = makePathToFixture('../data/Mysample.keylayout');
 
-      //const inputFilename  = makePathToFixture('../data/German_complete.keylayout');
-      const inputFilename  = makePathToFixture('../data/Italian_copy.keylayout');
-      //const inputFilename  = makePathToFixture('../data/My_dk_Keyboard.keylayout');
+    // all keys, some deadkeys
+    // copy into C:\Projects\keyman\keyman\developer\src\kmc-convert\test\DATA then they run OK
 
-      const outputFilename = makePathToFixture('../data/MyResult.kmn');
-      
-      // TODO check filename if correct
-      console.log('        inputFilename', inputFilename)
-      console.log('        outputFilename', outputFilename)
+    //const inputFilename  = makePathToFixture('../data/US_complete.keylayout');          // OK
+    //const inputFilename  = makePathToFixture('../data/German_complete.keylayout');      // OK
+    const inputFilename  = makePathToFixture('../data/Italian_copy.keylayout');         // OK
+    //const inputFilename  = makePathToFixture('../data/German_Standard_copy.keylayout');         //OK
+    //const inputFilename  = makePathToFixture('../data/German_Standard2.keylayout');         //OK
+    //const inputFilename = makePathToFixture('../data/German_StandardTweaked.keylayout');         //NO C3
+    //const inputFilename  = makePathToFixture('../data/Spanish_copy.keylayout');         // OK
+    //const inputFilename  = makePathToFixture('../data/Latin_American_copy.keylayout');  // OK
+    //const inputFilename  = makePathToFixture('../data/My_dk_Keyboard.keylayout');       // No
+    //const inputFilename  = makePathToFixture('../data/Swiss_French_copy.keylayout');    // OK
+    //const inputFilename  = makePathToFixture('../data/Swiss_German_copy.keylayout');    // OK
+    //const inputFilename  = makePathToFixture('../data/US_copy.keylayout');              // OK
 
-      // _S2 create obj of derived class-> use derived functions
-      const converter = new KeylayoutToKmnConverter(compilerTestCallbacks, compilerTestOptions);
+    const outputFilename = makePathToFixture('../data/MyResult.kmn');
 
-      let threw = false;
-      try {
-        await converter.run(inputFilename, outputFilename);
-      } catch {
-        threw = true;
-      }
-      assert.isTrue(threw);
-    });
+    // TODO check filename if correct
+    console.log('        inputFilename', inputFilename)
+    console.log('        outputFilename', outputFilename)
 
+    // _S2 create obj of derived class-> use derived functions
+    const converter = new KeylayoutToKmnConverter(compilerTestCallbacks, compilerTestOptions);
+
+    let threw = false;
+    try {
+      await converter.run(inputFilename, outputFilename);
+    } catch {
+      threw = true;
+    }
+    assert.isTrue(threw);
   });
+
+});
