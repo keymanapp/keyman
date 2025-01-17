aaaa9341f1afc8291b3b0a056dc6d96f6716fcdf
Joshua Horton <jahorton@sil.org>
2025-01-17 13:05:03 +0700

    feat(web): integrate new predictive-text helpers

--- web/src/engine/predictive-text/worker-thread/src/main/model-compositor.ts
+++ web/src/engine/predictive-text/worker-thread/src/main/model-compositor.ts
@@ -3,7 +3,7 @@ import * as correction from './correction/index.js'
 import APPLIED_SUGGESTION_COMPONENT = correction.APPLIED_SUGGESTION_COMPONENT;
 
 import TransformUtils from './transformUtils.js';
-import { correctAndEnumerate, dedupeSuggestions, finalizeSuggestions, predictionAutoSelect, processSimilarity, toAnnotatedSuggestion, tupleDisplayOrderSort } from './predict-helpers.js';
+import { correctAndEnumerate, dedupeSuggestions, finalizeSuggestions, isContextAtAcceptedSuggestionEdge, predictionAutoSelect, processSimilarity, suggestFromPriorContext, toAnnotatedSuggestion, tupleDisplayOrderSort } from './predict-helpers.js';
 import { detectCurrentCasing, determineModelTokenizer, determineModelWordbreaker, determinePunctuationFromModel } from './model-helpers.js';
 import { LexicalModelTypes } from '@keymanapp/common-types';
 import CasingForm = LexicalModelTypes.CasingForm;
@@ -131,6 +131,11 @@ export class ModelCompositor {
     const timer = this.activeTimer = new correction.ExecutionTimer(this.testMode ? Number.MAX_VALUE : SEARCH_TIMEOUT, this.testMode ? Number.MAX_VALUE : SEARCH_TIMEOUT * 1.5);
 
     const { postContextState, rawPredictions } = await correctAndEnumerate(this.contextTracker, this.lexicalModel, timer, transformDistribution, context);
+
+    // Check:  did we just reach the tail of a prior token via BKSP?
+    if(isContextAtAcceptedSuggestionEdge(postContextState)) {
+      return suggestFromPriorContext(postContextState, inputTransform, this.punctuation);
+    }
 
     if(this.activeTimer == timer) {
       this.activeTimer = null;
