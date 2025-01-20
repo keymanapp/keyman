2bf58bc0b9b13a2a9b7ae0efdbe0068589763d78
Eberhard Beilharz <eb1@sil.org>
2025-01-20 15:33:08 +0100

    feat(web): make properties of `KMXKeyboard` read-only
    Addresses code review comment.

--- web/src/engine/keyboard/src/keyboards/kmxKeyboard.ts
+++ web/src/engine/keyboard/src/keyboards/kmxKeyboard.ts
@@ -6,12 +6,12 @@ import { km_core_keyboard } from 'keyman/engine/core-processor';
 export class KMXKeyboard {
 
   constructor(id: string, keyboard: km_core_keyboard) {
-    this.id = id;
-    this.keyboard = keyboard;
+    this._id = id;
+    this._keyboard = keyboard;
   }
 
-  id: string;
-  keyboard: km_core_keyboard;
+  private _id: string;
+  private _keyboard: km_core_keyboard;
 
   get isMnemonic(): boolean {
     return false;
@@ -20,5 +20,13 @@ export class KMXKeyboard {
   get version(): string {
     // TODO-web-core: get version from `km_core_keyboard_get_attrs`
     return '';
+  }
+
+  get id(): string {
+    return this._id;
+  }
+
+  get keyboard(): km_core_keyboard {
+    return this._keyboard;
   }
 }
