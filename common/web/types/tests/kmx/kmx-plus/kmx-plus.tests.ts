bdfa21128ed1ebeeadd8c17c4fbd8289858a2032
Dr Mark C. Sinclair <markcsinclair@gmail.com>
2025-02-03 11:57:05 +0000

    fix(common/web): initial boiler plate for kmx-plus-tests.ts

---
+++ common/web/types/tests/kmx/kmx-plus/kmx-plus.tests.ts
@@ -0,0 +1,20 @@
+/*
+ * Keyman is copyright (C) SIL Global. MIT License.
+ * 
+ * Created by Dr Mark C. Sinclair on 2025-02-03
+ * 
+ * Test code for kmx-plus.ts
+ */
+
+import 'mocha';
+import { assert } from 'chai';
+
+describe('Test of KMX Plus file', () => {
+  describe('Test of Strs', () => {
+    describe('Test of allocString()', () => {
+      it('can allocate a StrsItem', () => {
+        assert.isTrue(true);
+      });
+    });
+  });
+});
