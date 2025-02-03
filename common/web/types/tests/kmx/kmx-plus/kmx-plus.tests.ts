0fabe6614a9640b20a75c857e0acd21b2a3ce737
Dr Mark C. Sinclair <markcsinclair@gmail.com>
2025-02-03 12:47:58 +0000

    fix(common/web): add test cases for Strs implicit constructor and allocString for a one-character string

--- common/web/types/tests/kmx/kmx-plus/kmx-plus.tests.ts
+++ common/web/types/tests/kmx/kmx-plus/kmx-plus.tests.ts
@@ -8,13 +8,31 @@
 
 import 'mocha';
 import { assert } from 'chai';
+import { Strs, StrsItem, StrsOptions, DependencySections } from '../../../src/kmx/kmx-plus/kmx-plus.js';
+
+const GOTHIC_A = new StrsItem("𐌰", 0x10330);
 
 describe('Test of KMX Plus file', () => {
   describe('Test of Strs', () => {
+    describe('Test of implicit constructor', () => {
+      it('can construct a Strs', () => {
+        const strs = new Strs();
+        assert.isNotNull(strs);
+        assert.deepEqual(strs.strings, [ new StrsItem('') ]);
+        assert.deepEqual(strs.allProcessedStrings, new Set<string>());
+      });
+    });
     describe('Test of allocString()', () => {
-      it('can allocate a StrsItem', () => {
-        assert.isTrue(true);
+      it('can allocate a one-character StrsItem', () => {
+        const strs = new Strs();
+        strs['processString'] = stubStrsProcessString;
+        const csi = strs.allocString("𐌰", { singleOk: true });
+        assert.deepEqual(csi, GOTHIC_A);
       });
     });
   });
 });
+
+function stubStrsProcessString(s: string, opts: StrsOptions, sections: DependencySections) {
+  return s;
+}
