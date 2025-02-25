ad8f2c25bf760755ddc2ddc3770517c77c3887fc
Dr Mark C. Sinclair <markcsinclair@gmail.com>
2025-02-25 17:49:01 +0000

    feat(developer): add two further test cases, one for Token, one for lexer

--- developer/src/kmc-kmn/test/ng-compiler/lexer.tests.ts
+++ developer/src/kmc-kmn/test/ng-compiler/lexer.tests.ts
@@ -8,13 +8,26 @@
 
 import 'mocha';
 import { assert } from 'chai';
-import { ScanRecogniser, TokenTypes } from '../../src/ng-compiler/lexer.js'
+import { lexer, ScanRecogniser, Token, TokenTypes } from '../../src/ng-compiler/lexer.js'
 
 describe("Lexer Tests", () => {
   describe("ScanRecogniser", () => {
     it("can construct a ScanRecogniser", () => {
       const sr = new ScanRecogniser(TokenTypes.TT_STORE, new RegExp("^store"),      true);
       assert.deepEqual(sr.toString(), '[TT_STORE,/^store/,true]');
+    });
+  });
+  describe("Token", () => {
+    it("can construct a Token", () => {
+      const token = new Token(TokenTypes.TT_STORE, 'store');
+      assert.deepEqual(token.toString(), '[TT_STORE,store]');
+    });
+  });
+  describe("lexer", () => {
+    it("can recognise a TT_STORE token", () => {
+      const actual   = lexer(new String('store'));
+      const expected = [new Token(TokenTypes.TT_STORE, 'store')];
+      assert.deepEqual(actual, expected);
     });
   });
 });
