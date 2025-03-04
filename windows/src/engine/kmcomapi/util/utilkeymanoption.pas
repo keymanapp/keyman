95f748888a29356712352bbcb0ae01c9cd9a1457
rc-swag <58423624+rc-swag@users.noreply.github.com>
2025-03-04 14:45:24 +1000

    fix(windows): sets options values in firstrun
    Previously it only set the startwithwindows, checkforupdats and
    autmaticallyreportusage values in firstrun if the flag was true.
    It was only true if the argument was passed to the install executable on
    the command line. However, it is valid to treat its non existance on the
    command line as false for these 3 values.

--- windows/src/engine/kmcomapi/util/utilkeymanoption.pas
+++ windows/src/engine/kmcomapi/util/utilkeymanoption.pas
@@ -133,7 +133,7 @@ const KeymanOptionInfo: array[0..16] of TKeymanOptionInfo = (  // I3331   // I36
   (opt: koCheckForUpdates;                   RegistryName: SRegValue_CheckForUpdates;                 OptionType: kotBool; BoolValue: True;  GroupName: 'kogGeneral'),
 
   (opt: koAutomaticallyReportErrors;         RegistryName: SRegValue_AutomaticallyReportErrors;        OptionType: kotBool; BoolValue: True;  GroupName: 'kogGeneral'),   // I4393
-  (opt: koAutomaticallyReportUsage;          RegistryName: SRegValue_AutomaticallyReportUsage;         OptionType: kotBool; BoolValue: False; GroupName: 'kogGeneral'),   // I4393
+  (opt: koAutomaticallyReportUsage;          RegistryName: SRegValue_AutomaticallyReportUsage;         OptionType: kotBool; BoolValue: True; GroupName: 'kogGeneral'),   // I4393
 
   // OSK options
 
@@ -143,7 +143,7 @@ const KeymanOptionInfo: array[0..16] of TKeymanOptionInfo = (  // I3331   // I36
 
   // Startup options
 
-  (opt: koStartWithWindows;                  RegistryName: '';                                         OptionType: kotBool; BoolValue: False; GroupName: 'kogStartup'),
+  (opt: koStartWithWindows;                  RegistryName: '';                                         OptionType: kotBool; BoolValue: True; GroupName: 'kogStartup'),
   (opt: koShowStartup;                       RegistryName: SRegValue_ShowStartup;                      OptionType: kotBool; BoolValue: True;  GroupName: 'kogStartup'),
   (opt: koTestKeymanFunctioning;             RegistryName: SRegValue_TestKeymanFunctioning;            OptionType: kotBool; BoolValue: True;  GroupName: 'kogStartup'),
 
