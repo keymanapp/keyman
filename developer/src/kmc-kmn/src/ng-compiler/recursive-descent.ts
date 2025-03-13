3e3e0977b4f997a56849d4c7d32fb2beb6dd2c49
Dr Mark C. Sinclair <markcsinclair@gmail.com>
2025-03-13 17:03:16 +0000

    feat(developer): add OptionalRule, ManyRule and OneOrManyRule

--- developer/src/kmc-kmn/src/ng-compiler/recursive-descent.ts
+++ developer/src/kmc-kmn/src/ng-compiler/recursive-descent.ts
@@ -26,14 +26,14 @@ export class Rule {
 export class SequenceRule extends Rule {
   private rules: Rule[];
 
-  public constructor (tokenBuffer: TokenBuffer, rules: Rule[]) {
+  public constructor(tokenBuffer: TokenBuffer, rules: Rule[]) {
     super(tokenBuffer);
     this.rules = rules;
   }
 
   public parse(node: ASTNode): boolean {
     let parseSuccess: boolean = true;
-    let save: number = this.tokenBuffer.currentPosition;
+    const save: number = this.tokenBuffer.currentPosition;
     const tmp: ASTNode = new ASTNode(NodeTypes.TMP);
 
     for (const rule of this.rules) {
@@ -52,3 +52,57 @@ export class SequenceRule extends Rule {
   }
 }
 
+export class OptionalRule extends Rule {
+  public constructor(tokenBuffer: TokenBuffer, rule: Rule) {
+    super(tokenBuffer, rule);
+  }
+
+  public parse(node: ASTNode): boolean {
+    let parseSuccess: boolean = true;
+    const save: number = this.tokenBuffer.currentPosition;
+    const tmp: ASTNode = new ASTNode(NodeTypes.TMP);
+    parseSuccess = this.rule.parse(tmp);
+
+    if (parseSuccess) {
+      node.addChildren(tmp.getChildren());
+    } else {
+      this.tokenBuffer.resetCurrentPosition(save);
+      // TODO generate warning
+    }
+
+    return true;
+  }
+}
+
+export class ManyRule extends Rule {
+  public constructor(tokenBuffer: TokenBuffer, rule: Rule) {
+    super(tokenBuffer, rule);
+  }
+
+  public parse(node: ASTNode): boolean {
+    while (this.rule.parse(node));
+    return true;
+  }
+}
+
+export class OneOrManyRule extends Rule {
+  public constructor(tokenBuffer: TokenBuffer, rule: Rule) {
+    super(tokenBuffer, rule);
+  }
+
+  public parse(node: ASTNode): boolean {
+    let parseSuccess: boolean = false;
+    let save: number = this.tokenBuffer.currentPosition;
+    const tmp: ASTNode = new ASTNode(NodeTypes.TMP);
+
+    if (this.rule.parse(tmp)) {
+      parseSuccess = true;
+      while (this.rule.parse(tmp));
+      node.addChildren(tmp.getChildren());
+    } else {
+      this.tokenBuffer.resetCurrentPosition(save);
+    }
+
+    return parseSuccess;
+  }
+}
