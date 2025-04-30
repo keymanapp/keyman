a29568744c3278a66df2e49df7923ee58392c776
Joshua Horton <joshua_horton@sil.org>
2025-04-30 15:05:40 +0700

    maint(web): run LMLayer tests during standard Web module tests
    Fixes: #12872
    
    Test-bot: skip

--- web/src/engine/predictive-text/worker-main/build.sh
+++ web/src/engine/predictive-text/worker-main/build.sh
@@ -57,13 +57,9 @@ function do_build() {
 function do_test() {
   local TEST_OPTIONS=
   if builder_has_option --ci; then
-    # We'll test the included libraries here for now.  At some point, we may wish
-    # to establish a ci.sh script for predictive-text to handle this instead.
-    ./unit_tests/test.sh test:libraries test:headless test:browser --ci
-  else
-    # If we're not in --ci mode, then this doesn't need to trigger the sibling projects' tests.
-    ./unit_tests/test.sh test:headless test:browser
+    TEST_OPTIONS=--ci
   fi
+    ./unit_tests/test.sh test:headless test:browser ${TEST_OPTIONS}
 }
 
 builder_run_action configure  do_configure
