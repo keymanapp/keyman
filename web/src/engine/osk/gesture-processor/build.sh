843221c46672cbf0b44432636564dd9c24345828
Joshua Horton <joshua_horton@sil.org>
2025-04-30 12:18:59 +0700

    chore(web): improve clean actions
    A few cases weren't properly handled, while a few others simply weren't called by the top-level script.  This PR seeks to make the top-level clean action able to fully clean all built components, perhaps save for sample and test pages.
    
    Test-bot: skip

--- web/src/engine/osk/gesture-processor/build.sh
+++ web/src/engine/osk/gesture-processor/build.sh
@@ -58,7 +58,7 @@ function do_test_tools() {
 }
 
 builder_run_action configure     do_configure
-builder_run_action clean         rm -rf build/ intermediate/
+builder_run_action clean         rm -rf build/
 builder_run_action build:module  do_build_module
 builder_run_action build:tools   src/tools/build.sh build
 builder_run_action test:module   test-headless-typescript "${SUBPROJECT_NAME}"
