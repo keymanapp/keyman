bc318915bb8d51bede82d0e911f298b7595bc821
Darcy Wong <darcy_wong@sil.org>
2025-05-02 08:26:28 +0700

    Apply suggestions from code review
    Co-authored-by: Marc Durdin <marc@durdin.net>

--- resources/shellHelperFunctions.sh
+++ resources/shellHelperFunctions.sh
@@ -138,13 +138,13 @@ write_download_info() {
   FILE_EXTENSION="${BASE_FILE##*.}"
 
   # stat flags to get filesize in bytes
-  if [[ "$BUILDER_OS" == mac ]]; then
+  if [[ "$BUILDER_OS" == mac ]] && [[ $(which stat) == /usr/bin/stat ]]; then
     STAT_FLAGS="-f%z"
   else
     STAT_FLAGS="-c%s"
   fi
   FILE_SIZE=$(/usr/bin/stat ${STAT_FLAGS} "${BASE_PATH}/${BASE_FILE}")
-  MD5_HASH=($(md5sum "${BASE_PATH}/${BASE_FILE}")) # hash is the first element returned
+  MD5_HASH=$(md5sum "${BASE_PATH}/${BASE_FILE}" | cut -d" " -f 1 -) # hash is the first element returned
 
   if [[ -f "$DOWNLOAD_INFO_FILEPATH" ]]; then
     builder_warn "Overwriting $DOWNLOAD_INFO_FILEPATH"
