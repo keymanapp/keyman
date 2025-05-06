397b5bdf93ff511b61a3a4cd5a69177de2706234
Eberhard Beilharz <eb1@sil.org>
2025-05-06 11:49:59 +0200

    maint(linux): streamline use of env variables to detect running on TC
    This changes the variable we use to detect if we're running on TC to
    be the same as the one used on #13810.

--- linux/keyman-config/run-tests.sh
+++ linux/keyman-config/run-tests.sh
@@ -12,14 +12,14 @@ if [[ "$1" == "--coverage" ]]; then
   coverage="-m coverage run --source=. --data-file=build/.coverage"
 fi
 
-if [[ -n "${TEAMCITY_VERSION}" ]]; then
+if [[ -n "${TEAMCITY_GIT_PATH}" ]]; then
   if ! pip3 list --format=columns | grep -q teamcity-messages; then
       if [[ -n "${TEAMCITY_PLATFORM}" ]] || [[ -n "${DOCKER_RUNNING}" ]]; then
         # Ubuntu 24.04+ prevents mixing pip and system packages and wants us
         # to use a virtualenv. However, that causes other problems, so we
         # override this check only if we're running in a TC agent or docker
         # so that we don't accidentally break the local system.
-        # Note: it is intentional that we use TEAMCITY_VERSION as well
+        # Note: it is intentional that we use TEAMCITY_GIT_PATH as well
         # as TEAMCITY_PLATFORM here so that it is possible to simulate
         # running in a TC agent and still not break your local system
         # (run in a docker container or explicitly set TEAMCITY_PLATFORM
