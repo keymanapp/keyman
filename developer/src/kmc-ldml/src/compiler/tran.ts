25ddb324f2f05e85dc3b2c29369337e01ab41242
Steven R. Loomis <srl295@gmail.com>
2025-06-02 13:33:00 -0500

    chore(developer): ldml: bring strs/tran compiler messages in line with design
    - fix a case where context wasn't being passed
    
    Fixes: #14099

--- developer/src/kmc-ldml/src/compiler/tran.ts
+++ developer/src/kmc-ldml/src/compiler/tran.ts
@@ -151,7 +151,7 @@ export abstract class TransformCompiler<T extends TransformCompilerType, TranBas
 
     // check for incorrect \uXXXX escapes. Do this before substituting markers or sets.
     // We run this first because it's more helpful than the ABNF.
-    cookedFrom = this.checkEscapes(cookedFrom); // check for \uXXXX escapes before normalizing
+    cookedFrom = this.checkEscapes(cookedFrom, transform); // check for \uXXXX escapes before normalizing
 
     if (cookedFrom === '') {
       this.callbacks.reportMessage(LdmlCompilerMessages.Error_TransformFromMatchesNothing({ from: cookedFrom }, transform));
@@ -301,10 +301,10 @@ export abstract class TransformCompiler<T extends TransformCompilerType, TranBas
     result._before = reorder.before;
     result._from = reorder.from;
     result._order = reorder.order;
-    if (reorder.from && this.checkEscapes(reorder.from) === null) {
+    if (reorder.from && this.checkEscapes(reorder.from, reorder) === null) {
       return null; // error'ed
     }
-    if (reorder.before && this.checkEscapes(reorder.before) === null) {
+    if (reorder.before && this.checkEscapes(reorder.before, reorder) === null) {
       return null; // error'ed
     }
     result.elements = sections.elem.allocElementString(sections, {x:reorder}, reorder.from, reorder.order, reorder.tertiary, reorder.tertiaryBase, reorder.preBase);
@@ -344,7 +344,7 @@ export abstract class TransformCompiler<T extends TransformCompilerType, TranBas
    * @param cookedFrom the original string
    * @returns the original string, or null if an error was reported
    */
-  private checkEscapes(cookedFrom: string, transform?: LKTransform): string | null {
+  private checkEscapes(cookedFrom: string, transform?: ObjectWithMetadata): string | null {
     if (!cookedFrom) return cookedFrom;
 
     // should not follow marker prefix, nor marker prefix with range
