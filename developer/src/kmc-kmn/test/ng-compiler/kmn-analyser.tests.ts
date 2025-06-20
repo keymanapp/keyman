52302b2a86c666e28e2dad8badfe6f5f8e29c447
Dr Mark C. Sinclair <markcsinclair@gmail.com>
2025-06-20 14:58:35 +0100

    feat(developer): able to complete syntax analysis round trip for 12 repository keyboards

--- developer/src/kmc-kmn/test/ng-compiler/kmn-analyser.tests.ts
+++ developer/src/kmc-kmn/test/ng-compiler/kmn-analyser.tests.ts
@@ -2179,7 +2179,7 @@ describe("KMN Analyser Tests", () => {
         'release/b/bangla_probhat/source/bangla_probhat',
         'release/b/banne/source/banne',
         'release/b/bassa_vah/source/bassa_vah',
-      ].splice(0, 10).forEach((name) => {
+      ].splice(0, 12).forEach((name) => {
         const buffer: String = new String(readFileSync(`../../../../keyboards/${name}.kmn`));
         Rule.tokenBuffer = stringToTokenBuffer(buffer);
         const kmnTreeRule: Rule = new KmnTreeRule();
