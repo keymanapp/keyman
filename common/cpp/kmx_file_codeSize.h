47e2fc593fe7760ed60280dfa81c1bcb653b8058
Sabine <Sabine_SIL@web.de>
2025-07-11 16:02:15 +0200

    chore(common): add files for code_size[]

---
+++ common/cpp/kmx_file_codeSize.h
@@ -0,0 +1,10 @@
+/*
+  Copyright:        Copyright (C) 2003-2018 SIL International.
+  Authors:          mcdurdin
+*/
+
+#pragma once
+
+extern const int my_CODE__SIZE[];
+#define CODE__SIZE_MAX 5
+
