b79595d143d5361825341142ebd2cf9d93fbe8fd
Eberhard Beilharz <eb1@sil.org>
2025-07-11 08:52:11 +0200

    maint(windows): replace use of `wzzip` with `add_zip_files`
    This makes use of the new functionality refactored in the previous
    commit by replacing the use of the `wzzip` function with `add_zip_files`.
    
    Test-bot: skip

--- developer/src/inst/build.sh
+++ developer/src/inst/build.sh
@@ -22,7 +22,6 @@ builder_parse "$@"
 
 . "${KEYMAN_ROOT}/resources/build/win/environment.inc.sh"
 . "${KEYMAN_ROOT}/resources/build/win/wix.inc.sh"
-. "${KEYMAN_ROOT}/resources/build/win/zip.inc.sh"
 . "${KEYMAN_ROOT}/resources/build/zip.inc.sh"
 
 # In dev environments, we'll hack the tier to alpha; CI sets this for us in real builds.
@@ -234,7 +233,7 @@ function make-installer() {
   echo "Version=$KEYMAN_VERSION" >> setup.inf
   echo "MSIFileName=keymandeveloper.msi" >> setup.inf
   echo "Title=Keyman Developer ${KEYMAN_VERSION_WITH_TAG}" >>setup.inf
-  wzzip setup.zip keymandeveloper.msi setup.inf
+  add_zip_files setup.zip keymandeveloper.msi setup.inf
   cat "$DEVELOPER_PROGRAM/setup.exe" setup.zip > "keymandeveloper-$KEYMAN_VERSION.exe"
   wrap-signcode //d "Keyman Developer" "keymandeveloper-$KEYMAN_VERSION.exe"
 
