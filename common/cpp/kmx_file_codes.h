4f354f8ef46da1165477bee81f7c47c6c07c1d6d
Sabine <Sabine_SIL@web.de>
2025-07-14 15:57:25 +0200

    chore(common): remove new line

---
+++ common/cpp/kmx_file_codes.h
@@ -0,0 +1,9 @@
+/*
+  Copyright:        Copyright (C) 2003-2018 SIL International.
+  Authors:          SabineSchmitt
+*/
+
+#pragma once
+
+extern const int CODE__SIZE_all[];
+#define CODE__SIZE_MAX 5
