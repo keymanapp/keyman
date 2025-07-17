c04ef63db632f679033f23227a91eaeaf6874f1a
Sabine <Sabine_SIL@web.de>
2025-07-17 12:16:24 +0200

    chore(common): undo changes for kmdecomp

--- developer/src/kmdecomp/kmdecomp.cpp
+++ developer/src/kmdecomp/kmdecomp.cpp
@@ -24,10 +24,6 @@
 #include "../../../common/windows/cpp/include/keymanversion.h"
 #include "../../../common/windows/cpp/include/legacy_kmx_memory.h"
 #include "../../../common/windows/cpp/include/legacy_kmx_file.h"
-#include "../../../common/include/keyman_vkey.h"
-
-using namespace km_vk;
-
 
 BOOL LoadKeyboard(LPSTR fileName, LPKEYBOARD *lpKeyboard, LPBYTE *lpBitmap, DWORD *cbBitmap);
 
