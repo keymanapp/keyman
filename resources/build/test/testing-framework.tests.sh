1ed4af1f7fb8c7885d20f07289e5a018e99e9ead
Eberhard Beilharz <eb1@sil.org>
2025-07-21 16:34:15 +0200

    test(common): add unit test for reporting test failures
    I added a unit test as part of #14345 but then forgot to commit it.
    
    Follow-up-of: #14345
    Build-bot: skip
    Test-bot: skip

--- resources/build/test/testing-framework.tests.sh
+++ resources/build/test/testing-framework.tests.sh
@@ -3,6 +3,8 @@
 #
 # Unit tests for locate_emscripten.inc.sh
 
+# shellcheck disable=SC2317
+
 shopt -s inherit_errexit
 
 ## START STANDARD BUILD SCRIPT INCLUDE
@@ -29,7 +31,7 @@ teardown() {
   alias xyz=
 }
 
-function test_create_mock_function_mockfunc() {
+function test__create_mock__function_mockfunc() {
   create_mock  myfunc  mocked_func
   status=$?
 
@@ -37,7 +39,7 @@ function test_create_mock_function_mockfunc() {
   assert-equal "${status}" "0"
 }
 
-function test_create_mock_function_defaultmock() {
+function test__create_mock__function_defaultmock() {
   create_mock  myfunc
   status=$?
 
@@ -45,7 +47,7 @@ function test_create_mock_function_defaultmock() {
   assert-equal "${status}" "0"
 }
 
-function test_create_mock_builtin_mockfunc() {
+function test__create_mock__builtin_mockfunc() {
   create_mock  command  mocked_func
   status=$?
 
@@ -53,7 +55,7 @@ function test_create_mock_builtin_mockfunc() {
   assert-equal "${status}" "0"
 }
 
-function test_create_mock_builtin_defaultmock() {
+function test__create_mock__builtin_defaultmock() {
   create_mock  command
   status=$?
 
@@ -61,7 +63,7 @@ function test_create_mock_builtin_defaultmock() {
   assert-equal "${status}" "0"
 }
 
-function test_create_mock_file_mockfunc() {
+function test__create_mock__file_mockfunc() {
   create_mock  more  mocked_func
   status=$?
 
@@ -69,7 +71,7 @@ function test_create_mock_file_mockfunc() {
   assert-equal "${status}" "0"
 }
 
-function test_create_mock_file_defaultmock() {
+function test__create_mock__file_defaultmock() {
   create_mock  more
   status=$?
 
@@ -77,7 +79,7 @@ function test_create_mock_file_defaultmock() {
   assert-equal "${status}" "0"
 }
 
-function test_create_mock_alias() {
+function test__create_mock__alias() {
   alias xyz=ls
 
   result=$(create_mock  xyz 2>&1)
@@ -88,7 +90,7 @@ function test_create_mock_alias() {
   assert-equal "${status}" "1"
 }
 
-function test_create_mock_keyword() {
+function test__create_mock__keyword() {
   result=$(create_mock if 2>&1)
   status=$?
 
@@ -96,7 +98,7 @@ function test_create_mock_keyword() {
   assert-equal "${status}" "1"
 }
 
-function test_create_mock_nonexisting() {
+function test__create_mock__nonexisting() {
   result=$(create_mock nonexisting 2>&1)
   status=$?
 
@@ -104,7 +106,7 @@ function test_create_mock_nonexisting() {
   assert-equal "${status}" "1"
 }
 
-function test_unmock_function() {
+function test__unmock__function() {
   create_mock  myfunc
   assert-contains "$(declare -f myfunc)" "myfunc called"
 
@@ -114,7 +116,7 @@ function test_unmock_function() {
   assert-contains "$(declare -f myfunc)" "world"
 }
 
-function test_unmock_builtin() {
+function test__unmock__builtin() {
   create_mock  command
   assert-contains "$(declare -f command)" "command called"
 
@@ -124,7 +126,7 @@ function test_unmock_builtin() {
   assert-contains "$(declare -f command)" "builtin command"
 }
 
-function test_unmock_file() {
+function test__unmock__file() {
   create_mock  more
   assert-contains "$(declare -f more)" "more called"
 
@@ -134,4 +136,55 @@ function test_unmock_file() {
   assert-contains "$(declare -f more)" "/usr/bin/more"
 }
 
-run_tests
+function test__run_tests__reports_failures() {
+  local test_output test_output_contents
+  test_output=$(mktemp)
+  LAST_LINE="$(wc -l < "${BASH_ARGV0}")"
+
+  # Start a new shell that will run the tests
+  bash - > "${test_output}" <<EOF
+    . "$0" --recursive
+
+    # ${LAST_LINE} and ${BASH_ARGV0} intentionally not escaped!
+    run_tests "\${START_LAST_SECTION}" "${LAST_LINE}" "${BASH_ARGV0}"
+    status=\$?
+
+    unmock builder_echo
+    exit ${status}
+EOF
+  status=$?
+
+  test_output_contents="$(cat "${test_output}")"
+  rm -f "${test_output}"
+
+  assert-equal "${status}" 2
+  assert-contains "${test_output_contents}" "Total of 3 tests run, 1 passed, 2 failed"
+  assert-contains "${test_output_contents}" "FAIL: test_failing1"
+  assert-contains "${test_output_contents}" "FAIL: test_failing2"
+  assert-contains "${test_output_contents}" "PASS: test_succeeding1"
+}
+
+
+# shellcheck disable=SC2031
+if [[ "${1:-}" != "--recursive" ]]; then
+  run_tests 1 "${LINENO}"
+  exit
+fi
+
+#------------------------------------------------------------------------------
+# Tests that are run as part of 'test__run_tests__reports_failures'
+
+# shellcheck disable=SC2034
+START_LAST_SECTION="${LINENO}"
+
+function test_failing1() {
+  assert-failed 0
+}
+
+function test_failing2() {
+  assert-succeeded 1
+}
+
+function test_succeeding1() {
+  assert-succeeded 0
+}
