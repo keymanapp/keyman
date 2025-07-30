f8606ba2d03fe2fb307852bebce6925e951f3fd3
Dr Mark C. Sinclair <markcsinclair@gmail.com>
2025-07-30 15:11:10 +0100

    feat(developer): add several replace...() rules to simplify code to BNF form

--- developer/src/kmc-kmn/test/ng-compiler/bnf-verify.tests.ts
+++ developer/src/kmc-kmn/test/ng-compiler/bnf-verify.tests.ts
@@ -15,18 +15,14 @@ interface Dictionary {
 }
 
 describe("Verify Parser Against BNF Tests", () => {
-  beforeEach(() => {
-  });
-  describe("Parser Tests", () => {
-    it("contains all the BNF grammar rules", () => {
-      const bnfBuffer: string = readFileSync('../../src/kmc-kmn/src/ng-compiler/kmn-file.bnf').toString();
-      const bnfRules: Dictionary = getBnfRules(bnfBuffer);
-      const sourceBuffer: string = readFileSync('../../src/kmc-kmn/src/ng-compiler/kmn-analyser.ts').toString();
-      const sourceRules: Dictionary = getSourceRules(sourceBuffer);
-      assert.equal(sourceRules, {});
-      assert.equal(bnfRules, {});
-      assert.isTrue(true);
-    });
+  it("matches the BNF grammar rules", () => {
+    const bnfBuffer: string = readFileSync('../../src/kmc-kmn/src/ng-compiler/kmn-file.bnf').toString();
+    const bnfRules: Dictionary = getBnfRules(bnfBuffer);
+    const sourceBuffer: string = readFileSync('../../src/kmc-kmn/src/ng-compiler/kmn-analyser.ts').toString();
+    const sourceRules: Dictionary = getSourceRules(sourceBuffer);
+    assert.equal(sourceRules, {});
+    assert.equal(bnfRules, {});
+    assert.isTrue(true);
   });
 });
 
@@ -58,6 +54,13 @@ function getSourceRules(buffer:string): Dictionary {
     for (let match of matches) {
       const name = lowerCaseFirstLetter(match[1]);
       rules[name] = removeWhiteSpace(match[2]);
+      rules[name] = replaceSequenceRule(rules[name]);
+      rules[name] = replaceAlternateRule(rules[name]);
+      rules[name] = replaceOneOrManyRule(rules[name]);
+      rules[name] = replaceManyRule(rules[name]);
+      rules[name] = replaceOptElements(rules[name]);
+      rules[name] = replaceOneOrManyElements(rules[name]);
+      rules[name] = replaceManyElements(rules[name]);
     }
     return rules;
 }
@@ -69,4 +72,67 @@ function lowerCaseFirstLetter(str: string): string {
 
 function removeWhiteSpace(str: string): string {
   return str.replaceAll(/\s/g, '');
+}
+
+function replaceSequenceRule(str: string): string {
+  if (str != null && str.includes('SequenceRule')) {
+    str = str.replace(/^SequenceRule\(\[/, '');
+    str = str.replace(/,?\]\)$/, '');
+    str = str.replaceAll(/,/g, ' ');
+  }
+  return str;
+}
+
+function replaceAlternateRule(str: string): string {
+  if (str != null && str.includes('AlternateRule')) {
+    str = str.replace(/^AlternateRule\(\[/, '');
+    str = str.replace(/,?\]\)$/, '');
+    str = str.replaceAll(/,/g, '|');
+  }
+  return str;
+}
+
+function replaceOneOrManyRule(str: string): string {
+  if (str != null && str.includes('OneOrManyRule')) {
+    str = str.replace(/^OneOrManyRule\(/, '');
+    str = str.replace(/\)$/, '');
+    str = str + '+';
+  }
+  return str;
+}
+
+function replaceManyRule(str: string): string {
+  if (str != null && str.includes('ManyRule')) {
+    str = str.replace(/ManyRule\(/, '');
+    str = str.replace(/\)$/, '');
+    str = str + '*';
+  }
+  return str;
+}
+
+function replaceOptElements(str: string): string {
+  if (str != null && str.includes('opt')) {
+    str = str.replaceAll(/(?:opt)(\S)(\S*)/g,
+      (match, p1, p2, offset, string, groups) =>
+        { return p1.toLowerCase()+p2+'?'; });
+  }
+  return str;
+}
+
+function replaceOneOrManyElements(str: string): string {
+  if (str != null && str.includes('oneOrMany')) {
+    str = str.replaceAll(/(?:oneOrMany)(\S)(\S*)/g,
+      (match, p1, p2, offset, string, groups) =>
+        { return p1.toLowerCase()+p2+'+'; });
+  }
+  return str;
+}
+
+function replaceManyElements(str: string): string {
+  if (str != null && str.includes('many')) {
+    str = str.replaceAll(/(?:many)(\S)(\S*)/g,
+      (match, p1, p2, offset, string, groups) =>
+        { return p1.toLowerCase()+p2+'*'; });
+  }
+  return str;
 }
