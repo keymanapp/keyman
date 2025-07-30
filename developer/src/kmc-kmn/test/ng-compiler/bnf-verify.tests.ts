3dae937b2bdf4be599acd5b18e8c6e36b9202918
Dr Mark C. Sinclair <markcsinclair@gmail.com>
2025-07-30 10:34:31 +0100

    feat(developer): add wrapLines() and initial bnfRules()

--- developer/src/kmc-kmn/test/ng-compiler/bnf-verify.tests.ts
+++ developer/src/kmc-kmn/test/ng-compiler/bnf-verify.tests.ts
@@ -8,13 +8,38 @@
 
 import 'mocha';
 import { assert } from 'chai';
+import { readFileSync } from 'fs';
+
+interface Dictionary {
+  [key: string]: string;
+}
 
 describe("Verify Parser Against BNF Tests", () => {
   beforeEach(() => {
   });
   describe("Parser Tests", () => {
     it("contains all the BNF grammar rules", () => {
+      const buffer: string  = readFileSync('../../src/kmc-kmn/src/ng-compiler/kmn-file.bnf').toString();
+      const rules: Dictionary = bnfRules(buffer);
+      assert.equal(rules, {});
       assert.isTrue(true);
     });
   });
 });
+
+function bnfRules(buffer:string): Dictionary {
+  const rules: Dictionary = {};
+  buffer = wrapLines(buffer);
+  const lines: string[] = buffer.split(/\r\n|\n|\r/g);
+  lines.forEach((line) => {
+    const match = line.match(/(.*)\s*:\s*(.*)/)
+    if (match) {
+      rules[match[1]] = match[2];
+    }
+  });
+  return rules;
+}
+
+function wrapLines(buffer:string): string {
+  return buffer.replaceAll(/[^\S\r\n]*(\r\n|\n|\r)[^\S\r\n]+/g, '');
+}
