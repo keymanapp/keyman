12e17034eeddcd838a93b04c2f93af2491047811
Dr Mark C. Sinclair <markcsinclair@gmail.com>
2025-07-30 09:31:35 +0100

    feat(developer): add initial bnf-verify.tests.ts file

---
+++ developer/src/kmc-kmn/test/ng-compiler/bnf-verify.tests.ts
@@ -0,0 +1,20 @@
+/*
+ * Keyman is copyright (C) SIL Global. MIT License.
+ *
+ * Created by Dr Mark C. Sinclair on 2025-07-30
+ *
+ * Tests for KMC KMN Next Generation Parser (Recursive Descent/Verify against BNF)
+ */
+
+import 'mocha';
+import { assert } from 'chai';
+
+describe("Verify Parser Against BNF Tests", () => {
+  beforeEach(() => {
+  });
+  describe("Parser Tests", () => {
+    it("contains all the BNF grammar rules", () => {
+      assert.isTrue(true);
+    });
+  });
+});
