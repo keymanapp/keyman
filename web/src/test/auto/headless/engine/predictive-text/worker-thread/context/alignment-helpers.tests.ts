15e8c1acfba1cb4b1bc90d228128918e219febbd
Joshua Horton <joshua_horton@sil.org>
2025-08-14 13:13:27 -0500

    docs(web): add header comments to new files

--- web/src/test/auto/headless/engine/predictive-text/worker-thread/context/alignment-helpers.tests.ts
+++ web/src/test/auto/headless/engine/predictive-text/worker-thread/context/alignment-helpers.tests.ts
@@ -1,3 +1,13 @@
+/*
+ * Keyman is copyright (C) SIL Global. MIT License.
+ *
+ * Created by jahorton on 2025-07-30
+ *
+ * This file contains low-level tests designed to validate helper functions
+ * used when aligning cached context states to incoming contexts and when
+ * validating potential substitution edit operations.
+ */
+
 import { assert } from 'chai';
 import { EditOperation, getEditPathLastMatch, isSubstitutionAlignable } from '@keymanapp/lm-worker/test-index';
 
