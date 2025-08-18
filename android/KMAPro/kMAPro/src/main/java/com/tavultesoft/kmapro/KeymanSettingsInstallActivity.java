84c6d72e8ffc0c374a1937ebac537ccff485f684
Darcy Wong <darcy_wong@sil.org>
2025-08-18 14:14:09 +0700

    chore(android): Remove old import

--- android/KMAPro/kMAPro/src/main/java/com/tavultesoft/kmapro/KeymanSettingsInstallActivity.java
+++ android/KMAPro/kMAPro/src/main/java/com/tavultesoft/kmapro/KeymanSettingsInstallActivity.java
@@ -7,7 +7,6 @@ import java.util.ArrayList;
 import java.util.HashMap;
 
 import androidx.appcompat.app.AlertDialog;
-import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
 import android.content.Context;
