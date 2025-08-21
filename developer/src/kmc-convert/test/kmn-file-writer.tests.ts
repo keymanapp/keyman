0c26f4cadab4c140f0c5fb92c06a2c2fc150388d
Sabine <Sabine_SIL@web.de>
2025-08-21 17:46:07 +0200

    feat(developer): new kmcConvertUtil module and tests moved from util.ts and kmn-file-writer.tests.ts

--- developer/src/kmc-convert/test/kmn-file-writer.tests.ts
+++ developer/src/kmc-convert/test/kmn-file-writer.tests.ts
@@ -203,66 +203,6 @@ describe('KmnFileWriter', function () {
     });
   });
 
-  /*describe('convertToUnicodeCodePoint ', function () {
-    [
-      ["&#x10F601;", 'U+10F601'],
-      ["&#x1F601;", 'U+1F601'],
-      ["&#x9;", 'U+0009'],
-      ["&#x99;", 'U+0099'],
-      ["&#x999;", 'U+0999'],
-      ["&#x9999;", 'U+9999'],
-      ["&#x99999;", 'U+99999'],
-      ["&#1111553;", 'U+10F601'],
-      ["&#128513;", 'U+1F601'],
-      ["&#9;", 'U+0009'],
-      ["&#99;", 'U+0063'],
-      ["&#999;", 'U+03E7'],
-      ["&#9999;", 'U+270F'],
-      ["&#99999;", 'U+1869F'],
-      ['0000;', '0000;'],
-      ['X;', 'X;'],
-      ['123;', '123;'],
-      [';', ';'],
-      [' ;', ' ;']
-    ].forEach(function (values) {
-      it(('should convert "' + values[0] + '"').padEnd(25, " ") + 'to "' + values[1] + '"', async function () {
-        const result = kmcConvertUtil.convertToUnicodeCodePoint(values[0] as string);
-        assert.equal(result, values[1]);
-      });
-    });
-  });*/
-
-  /*describe('convertToUnicodeCharacter ', function () {
-    [
-      ["&#x61;", 'a'],
-      ["&#x1234;", 'ሴ'],
-      ["&#x1F60E;", '😎'],
-      ["&#x1000000;", undefined],
-      ["&#97;", 'a'],
-      ["&#4660;", 'ሴ'],
-      ["&#128518;", '😆'],
-      ["&#1000000;", undefined],
-      ["U+0061;", 'a'],
-      ["U+1234;", 'ሴ'],
-      ["U+1F60E", '😎'],
-      ["U+1000000;", undefined],
-      ["&commat;", undefined],
-      ["a", 'a'],
-      ["ሴ", 'ሴ'],
-      ['😎', '😎'],
-      ["W̊", "W̊"],
-      ["ab", undefined],
-      ["", ''],
-      [undefined, undefined],
-      [null, undefined]
-    ].forEach(function (values) {
-      it(('should convert "' + values[0] + '"').padEnd(25, " ") + 'to "' + values[1] + '"', async function () {
-        const result = kmcConvertUtil.convertToUnicodeCharacter(values[0] as string);
-        assert.equal(result, values[1]);
-      });
-    });
-  });*/
-
   describe('reviewRules messages', function () {
     const sut_w = new KmnFileWriter(compilerTestCallbacks, compilerTestOptions);
 
