7fcdfb8f658e03a13b7146dd81f8fc0a24af5466
Marc Durdin <marc@durdin.net>
2025-09-12 10:52:31 +0200

    fix(developer): restrict invalid characters in identifiers in kmcmplib
    The compiler has always been very ambiguous on which characters were
    accepted in group and store names, even to the point of accepting
    things like comma in a store name, which would then make it impossible
    to reference in an `index` statement!
    
    This commit clarifies the allowable characters in an identifier. While
    it would have been possible to use UAX#31 for this, that would have
    extended the requirements for this change substantially, and may have
    caused us more trouble with legacy keyboards. Given kmcmplib is
    end-of-life (see epic/ng-compiler), I have chosen a lower friction
    approach. There are certainly other characters that could be excluded,
    but in general I have chosen to exclude only those that will definitely
    be problematic.
    
    The set of allowable characters for deadkeys has actually been expanded
    in this release to match the store and group name rules.
    
    It is expected that there may be some impacted keyboards, but addressing
    this change will be relatively straightforward, so I consider this to be
    an acceptable back-compatibility trade-off, see
    https://github.com/keymanapp/keyman/wiki/Principles-of-Keyman-Code-Changes#4-source-backward-compatibility-keyboard-model-and-package-source-file-formats-should-be-backward-compatible
    
    Fixes: #14604
    Test-bot: skip
    Build-bot: skip build:developer

--- developer/src/kmcmplib/src/CompilerErrors.h
+++ developer/src/kmcmplib/src/CompilerErrors.h
@@ -2,6 +2,19 @@
 
 #include <string>
 #include <vector>
+#include "kmcmplibapi.h"
+#include "kmn_compiler_errors.h"
+
+class CompilerMessage {
+public:
+  virtual void report(enum KmnCompilerMessages::KmnCompilerMessages msg, const std::vector<std::string>& parameters) = 0;
+  virtual void report(enum KmnCompilerMessages::KmnCompilerMessages msg) = 0;
+};
+
+class DefaultCompilerMessage : public CompilerMessage {
+  virtual void report(enum KmnCompilerMessages::KmnCompilerMessages msg, const std::vector<std::string>& parameters) ;
+  virtual void report(enum KmnCompilerMessages::KmnCompilerMessages msg);
+};
 
 namespace kmcmp {
   extern kmcmp_CompilerMessageProc msgproc;
