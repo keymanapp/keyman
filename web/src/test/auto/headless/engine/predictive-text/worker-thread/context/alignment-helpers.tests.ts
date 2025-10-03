824de55819a9c611876db8f65883a99cb7010444
Joshua Horton <joshua_horton@sil.org>
2025-10-03 08:48:01 -0500

    change(web): trying to re-jig merge conflict detection via doc change

--- web/src/test/auto/headless/engine/predictive-text/worker-thread/context/alignment-helpers.tests.ts
+++ web/src/test/auto/headless/engine/predictive-text/worker-thread/context/alignment-helpers.tests.ts
@@ -2,10 +2,6 @@
  * Keyman is copyright (C) SIL Global. MIT License.
  *
  * Created by jahorton on 2025-07-30
- *
- * This file contains low-level tests designed to validate helper functions
- * used when aligning cached context states to incoming contexts and when
- * validating potential substitution edit operations.
  */
 
 // No longer needed.
