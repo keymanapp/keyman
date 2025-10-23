d57274b26d8145fcc4f98480ced0629ac80513e6
Christoph Völker <christov@gmx.de>
2025-10-23 20:58:46 +0200

    test(developer): add unit test for Warn_PreviousMapFileCouldNotBeLoaded and raise coverage threshold to 75%

--- developer/src/kmc-analyze/test/analyzer-messages.tests.ts
+++ developer/src/kmc-analyze/test/analyzer-messages.tests.ts
@@ -1,11 +1,13 @@
 /*
  * Keyman is copyright (C) SIL Global. MIT License.
  */
-// import { assert } from 'chai';
+import { assert } from 'chai';
 import 'mocha';
 import { AnalyzerMessages } from '../src/analyzer-messages.js';
 import { TestCompilerCallbacks, verifyCompilerMessagesObject } from '@keymanapp/developer-test-helpers';
 import { CompilerErrorNamespace } from '@keymanapp/developer-utils';
+import { AnalyzeOskCharacterUse } from '../src/osk-character-use/index.js';
+
 
 describe('AnalyzerMessages', function () {
 
@@ -26,4 +28,20 @@ describe('AnalyzerMessages', function () {
   });
 
   // TODO: test each message
+  it('should generate Warn_PreviousMapFileCouldNotBeLoaded when previous map file is missing or invalid', function () {
+    const analyzer = new AnalyzeOskCharacterUse(callbacks);
+
+    //const invalidFile = makePathToFixture('projects/invalid-map/does-not-exist.json');
+    const invalidFile = 'nonexistent-file.json';
+
+    // internal method; using 'as any' to access
+    (analyzer as any).loadPreviousMap(invalidFile);
+
+    assert.isTrue(
+      callbacks.hasMessage(AnalyzerMessages.WARN_PreviousMapFileCouldNotBeLoaded),
+      'Expected warning when previous map file could not be loaded'
+    );
+  });
+
 });
+
