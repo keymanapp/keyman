e3ce615b9d9d5e070ca07e978bc4e376070cf937
Joshua Horton <joshua_horton@sil.org>
2025-11-10 16:05:12 -0600

    change(web): enhance SearchQuotientSpur.split() unit tests per new inputSplitIndex field

--- web/src/test/auto/headless/engine/predictive-text/worker-thread/correction-search/search-quotient-spur.tests.ts
+++ web/src/test/auto/headless/engine/predictive-text/worker-thread/correction-search/search-quotient-spur.tests.ts
@@ -1359,7 +1359,13 @@ describe('SearchQuotientSpur', () => {
       assert.isTrue(tail instanceof LegacyQuotientSpur);
       assert.deepEqual((head as LegacyQuotientSpur).inputs, headTarget.inputs);
       assert.deepEqual((tail as LegacyQuotientSpur).inputs, tailTarget.inputs);
-      assert.deepEqual((head as LegacyQuotientSpur).inputSource, headTarget.inputSource);
+      assert.deepEqual((head as LegacyQuotientSpur).inputSource, {
+        ...headTarget.inputSource,
+        segment: {
+          ...headTarget.inputSource.segment,
+          end: 2
+        }
+      });
       assert.deepEqual((tail as LegacyQuotientSpur).inputSource, tailTarget.inputSource);
     });
   });
