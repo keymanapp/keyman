a0bd1091742d80816f8ff6e6f139b938c27d6434
Joshua Horton <joshua_horton@sil.org>
2025-11-12 13:41:43 -0600

    fix(web): do not preserve taillessTrueKeystroke when receiving empty input

--- web/src/engine/predictive-text/worker-thread/src/main/correction/context-tokenization.ts
+++ web/src/engine/predictive-text/worker-thread/src/main/correction/context-tokenization.ts
@@ -139,7 +139,7 @@ export class ContextTokenization {
     } else {
       const priorToClone = param1;
       this.tokens = priorToClone.tokens.map((entry) => new ContextToken(entry));
-      this.transitionEdits = {...priorToClone.transitionEdits};
+      this.transitionEdits = priorToClone.transitionEdits ? {...priorToClone.transitionEdits} : null;
       this.taillessTrueKeystroke = priorToClone.taillessTrueKeystroke;
     }
   }
