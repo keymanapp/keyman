fc04e2445b1ec12aca48234fa64525488ade9c98
Eberhard Beilharz <eb1@sil.org>
2025-11-14 12:35:48 +0100

    fix(web): address code review comments
    Deal with `currentScript` being null/undefined.

--- web/src/app/webview/src/debug-main.ts
+++ web/src/app/webview/src/debug-main.ts
@@ -5,8 +5,8 @@ import { SourcemappedWorker } from '@keymanapp/lexical-model-layer/web'
 * Determine path and protocol of executing script, setting them as
 * construction defaults.
 */
-const ss = (document.currentScript as HTMLScriptElement).src;
-const sPath = ss.substring(0, ss.lastIndexOf('/') + 1);
+const ss = (document.currentScript as HTMLScriptElement)?.src;
+const sPath = ss ? ss.substring(0, ss.lastIndexOf('/') + 1) : './';
 
 // @ts-ignore
 window['keyman'] = new KeymanEngine(SourcemappedWorker, sPath);
--- web/src/app/webview/src/release-main.ts
+++ web/src/app/webview/src/release-main.ts
@@ -5,8 +5,8 @@ import { Worker } from '@keymanapp/lexical-model-layer/web'
 * Determine path and protocol of executing script, setting them as
 * construction defaults.
 */
-const ss = (document.currentScript as HTMLScriptElement).src;
-const sPath = ss.substring(0, ss.lastIndexOf('/') + 1);
+const ss = (document.currentScript as HTMLScriptElement)?.src;
+const sPath = ss ? ss.substring(0, ss.lastIndexOf('/') + 1) : './';
 
 // @ts-ignore
 window['keyman'] = new KeymanEngine(Worker, sPath);
