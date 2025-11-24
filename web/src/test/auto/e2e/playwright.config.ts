12d56453a43769656ee96f8b51b91a6e12e005af
Eberhard Beilharz <eb1@sil.org>
2025-11-24 20:02:17 +0100

    test(web): add e2e tests for KMX keyboard with web-core
    Also add similar tests for a JS keyboard.
    
    Test-bot: skip

---
+++ web/src/test/auto/e2e/playwright.config.ts
@@ -0,0 +1,84 @@
+import { dirname, resolve } from 'node:path';
+import { fileURLToPath } from 'node:url';
+import { defineConfig, devices } from '@playwright/test';
+
+const dir = dirname(fileURLToPath(import.meta.url));
+const KEYMAN_ROOT = resolve(dir, '../../../../../');
+
+/**
+ * See https://playwright.dev/docs/test-configuration.
+ */
+export default defineConfig({
+  rootDir: KEYMAN_ROOT,
+  testDir: '.',
+  testMatch: '*.tests.ts',
+  /* Run tests in files in parallel */
+  fullyParallel: true,
+  /* Fail the build on CI if you accidentally left test.only in the source code. */
+  forbidOnly: !!process.env.KEYMAN_IS_CI_BUILD,
+  /* Retry on CI only */
+  retries: process.env.KEYMAN_IS_CI_BUILD ? 2 : 0,
+  /* Opt out of parallel tests on CI. */
+  workers: process.env.KEYMAN_IS_CI_BUILD ? 1 : undefined,
+  /* Reporter to use. See https://playwright.dev/docs/test-reporters */
+  // reporter: process.env.KEYMAN_IS_CI_BUILD ? [
+  //   [KEYMAN_ROOT + '/common/test/resources/playwright-TC-reporter.ts', { parentFlow: 'web_tests' }]
+  // ] : [
+  reporter: [
+    ['list'],
+    ['html', { open: 'never' }],
+  ],
+  /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
+  use: {
+    /* Base URL to use in actions like `await page.goto('')`. */
+    // baseURL: 'http://localhost:3000',
+
+    /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
+    trace: 'on-first-retry',
+  },
+
+  /* Configure projects for major browsers */
+  projects: [
+    {
+      name: 'chromium',
+      use: { ...devices['Desktop Chrome'] },
+    },
+
+    // {
+    //   name: 'firefox',
+    //   use: { ...devices['Desktop Firefox'] },
+    // },
+
+    // {
+    //   name: 'webkit',
+    //   use: { ...devices['Desktop Safari'] },
+    // },
+
+    /* Test against mobile viewports. */
+    // {
+    //   name: 'Mobile Chrome',
+    //   use: { ...devices['Pixel 5'] },
+    // },
+    // {
+    //   name: 'Mobile Safari',
+    //   use: { ...devices['iPhone 12'] },
+    // },
+
+    /* Test against branded browsers. */
+    // {
+    //   name: 'Microsoft Edge',
+    //   use: { ...devices['Desktop Edge'], channel: 'msedge' },
+    // },
+    // {
+    //   name: 'Google Chrome',
+    //   use: { ...devices['Desktop Chrome'], channel: 'chrome' },
+    // },
+  ],
+
+  /* Run your local dev server before starting the tests */
+  webServer: {
+    command: `${KEYMAN_ROOT}/web/build.sh start`,
+    url: 'http://localhost:3000',
+    reuseExistingServer: !process.env.KEYMAN_IS_CI_BUILD,
+  },
+});
