8bcf03cb1ec2d7912ec54d14615bda03090ab57a
Matthew Lee <matthew_lee@sil.org>
2025-11-25 02:35:58 -0600

    Update android/KMEA/app/src/main/java/com/keyman/engine/KMManager.java
    Co-authored-by: Marc Durdin <marc@durdin.net>

--- android/KMEA/app/src/main/java/com/keyman/engine/KMManager.java
+++ android/KMEA/app/src/main/java/com/keyman/engine/KMManager.java
@@ -2563,10 +2563,8 @@ public final class KMManager {
   public static int getKeyboardHeightMax(Context context, int orientation) {
     if (orientation == Configuration.ORIENTATION_LANDSCAPE) {
       return (int) KMManager.KeyboardHeight_Context_Landscape_Default * 2;
-    } else if (orientation == Configuration.ORIENTATION_PORTRAIT) {
+    } else /* orientation == Configuration.ORIENTATION_PORTRAIT */ {
       return (int) KMManager.KeyboardHeight_Context_Portrait_Default * 2;
-    } else {
-      return KMManager.KeyboardHeight_Reset;
     }
   }
 
