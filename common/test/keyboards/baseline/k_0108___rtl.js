22ab28b4cca3de5d23914e67a2a9ca2e4e6e1d20
Eberhard Beilharz <eb1@sil.org>
2026-01-20 16:16:29 +0100

    feat(web): add RTL baseline test
    Part-of: #15288
    Test-bot: skip

---
+++ common/test/keyboards/baseline/k_0108___rtl.js
@@ -0,0 +1,48 @@
+if(typeof keyman === 'undefined') {
+  console.log('Keyboard requires KeymanWeb 10.0 or later');
+  if(typeof tavultesoft !== 'undefined') tavultesoft.keymanweb.util.alert("This keyboard requires KeymanWeb 10.0 or later");
+} else {
+KeymanWeb.KR(new Keyboard_k_0108___rtl());
+}
+function Keyboard_k_0108___rtl()
+{
+  var modCodes = keyman.osk.modifierCodes;
+  var keyCodes = keyman.osk.keyCodes;
+
+  this._v=(typeof keyman!="undefined"&&typeof keyman.version=="string")?parseInt(keyman.version,10):9;
+  this.KI="Keyboard_k_0108___rtl";
+  this.KN="0108 - rtl";
+  this.KMINVER="10.0";
+  this.KV=null;
+  this.KDU=0;
+  this.KH='';
+  this.KM=0;
+  this.KBVER="1.0";
+  this.KMBM=0 /* 0x0000 */;
+  this.KRTL=1;
+  this.KVS=[];
+  this.gs=function(t,e) {
+    return this.g_Main_0(t,e);
+  };
+  this.gs=function(t,e) {
+    return this.g_Main_0(t,e);
+  };
+  this.g_Main_0=function(t,e) {
+    var k=KeymanWeb,r=0,m=0;
+    if(k.KKM(e, modCodes.VIRTUAL_KEY /* 0x4000 */, keyCodes.K_A /* 0x41 */)) {
+      if(1){
+        r=m=1;   // Line 14
+        k.KDC(0,t);
+        k.KO(-1,t,"ش");
+      }
+    }
+    else if(k.KKM(e, modCodes.VIRTUAL_KEY /* 0x4000 */, keyCodes.K_B /* 0x42 */)) {
+      if(1){
+        r=m=1;   // Line 15
+        k.KDC(0,t);
+        k.KO(-1,t,"ال");
+      }
+    }
+    return r;
+  };
+}
