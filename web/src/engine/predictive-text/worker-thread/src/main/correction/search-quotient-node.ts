e5231ff435db7454ae6592e068c7d031e359ed71
Joshua Horton <joshua_horton@sil.org>
2026-01-28 16:04:26 -0600

    change(web): improve .transitionId typing

--- web/src/engine/predictive-text/worker-thread/src/main/correction/search-quotient-node.ts
+++ web/src/engine/predictive-text/worker-thread/src/main/correction/search-quotient-node.ts
@@ -51,7 +51,7 @@ export interface InputSegment {
   /**
    * The transform / transition ID of the corresponding input event.
    */
-  transitionId: number,
+  transitionId: number | undefined,
 
   /**
    * Marks the initial index (inclusive) within the insert strings for the
