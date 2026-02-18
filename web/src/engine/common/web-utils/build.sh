5c445d470ce410d43fce39a17d4d752ac2b2cd47
Joshua Horton <joshua_horton@sil.org>
2026-02-18 12:59:17 +0700

    change(web): remove temp debug shell-script echo statements

--- web/src/engine/common/web-utils/build.sh
+++ web/src/engine/common/web-utils/build.sh
@@ -35,13 +35,11 @@ function do_build() {
   # May be useful one day, for building a mass .d.ts for KMW as a whole.
   # So... tsc does declaration-bundling on its own pretty well, at least for local development.
   tsc --emitDeclarationOnly --outFile "${KEYMAN_ROOT}/${BUILD_DIR}/lib/index.d.ts"
-  echo 'a'
   # One of the functions (timedPromise) is quite helpful for automated testing, even in the DOM.
   # So, to make sure it's easily-accessible for the DOM-based tests...
   node "${LIB_BUNDLER}"  "${KEYMAN_ROOT}/${BUILD_DIR}/obj/index.js" \
     --out                "${KEYMAN_ROOT}/${BUILD_DIR}/lib/index.mjs" \
     --format esm
-  echo 'b'
 }
 
 function do_test() {
