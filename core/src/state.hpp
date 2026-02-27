67468c4dce64329659ffd2acf319fda3af7cf808
Eberhard Beilharz <eb1@sil.org>
2026-02-27 18:21:48 +0100

    fix(core): address code review comments
    Co-authored-by: Marc Durdin <marc@durdin.net>

--- core/src/state.hpp
+++ core/src/state.hpp
@@ -176,9 +176,11 @@ public:
     );
     void apply_actions_and_merge_app_context();
 
-    // This is used to track whether the backspace key was handled internally
-    // during the keydown event. This is needed so that we can return the same
-    // value from the keyup event. Only used when processing KM_CORE_VKEY_BKSP.
+    /**
+     * This is used to track whether the backspace key was handled internally
+     * during the keydown event. This is needed so that we can return the same
+     * value from the keyup event. Only used when processing KM_CORE_VKEY_BKSP.
+     */
     void set_backspace_handled_internally(bool handled) { _backspace_handled_internally = handled; }
     bool backspace_handled_internally() const { return _backspace_handled_internally; }
   };
