816fac7a47156083db791b13080467208669255d
Joshua Horton <joshua_horton@sil.org>
2026-03-04 09:17:20 -0600

    feat(web): implement dedicated substitution-spur class, tests
    Per the search quotient graph documentation, we should no longer have one node type handle all edit operation types.  Our current ("legacy") implementation and unit tests are most tailored for the needs of 'substitution' edit types, so this PR starts there.
    
    Build-bot: skip build:web
    Test-bot: skip

--- web/src/test/auto/headless/engine/predictive-text/helpers/buildCantLinearFixture.ts
+++ web/src/test/auto/headless/engine/predictive-text/helpers/buildCantLinearFixture.ts
@@ -1,4 +1,4 @@
-import { LegacyQuotientRoot, LegacyQuotientSpur, models } from "@keymanapp/lm-worker/test-index";
+import { SearchQuotientRoot, SubstitutionQuotientSpur, models } from "@keymanapp/lm-worker/test-index";
 import { jsonFixture } from "@keymanapp/common-test-resources/model-helpers.mjs";
 
 import TrieModel = models.TrieModel;
@@ -8,31 +8,31 @@ const testModel = new TrieModel(jsonFixture('models/tries/english-1000'));
 // Needs legacy vs 'new' style implementation!
 
 export function buildCantLinearFixture() {
-  const rootPath = new LegacyQuotientRoot(testModel);
+  const rootPath = new SearchQuotientRoot(testModel);
 
   const distrib1 = [
     { sample: {insert: 'c', deleteLeft: 0, id: 11}, p: 0.5 },
     { sample: {insert: 'r', deleteLeft: 0, id: 11}, p: 0.4 },
     { sample: {insert: 't', deleteLeft: 0, id: 11}, p: 0.1 }
   ];
-  const path1 = new LegacyQuotientSpur(rootPath, distrib1, distrib1[0]);
+  const path1 = new SubstitutionQuotientSpur(rootPath, distrib1, distrib1[0]);
 
   const distrib2 = [
     { sample: {insert: 'a', deleteLeft: 0, id: 12}, p: 0.7 },
     { sample: {insert: 'e', deleteLeft: 0, id: 12}, p: 0.3 }
   ];
-  const path2 = new LegacyQuotientSpur(path1, distrib2, distrib2[0]);
+  const path2 = new SubstitutionQuotientSpur(path1, distrib2, distrib2[0]);
 
   const distrib3 = [
     { sample: {insert: 'n', deleteLeft: 0, id: 13}, p: 0.8 },
     { sample: {insert: 'r', deleteLeft: 0, id: 13}, p: 0.2 }
   ];
-  const path3 = new LegacyQuotientSpur(path2, distrib3, distrib3[0]);
+  const path3 = new SubstitutionQuotientSpur(path2, distrib3, distrib3[0]);
 
   const distrib4 = [
     { sample: {insert: 't', deleteLeft: 0, id: 14}, p: 1 }
   ];
-  const path4 = new LegacyQuotientSpur(path3, distrib4, distrib4[0]);
+  const path4 = new SubstitutionQuotientSpur(path3, distrib4, distrib4[0]);
 
   return {
     paths: [null, path1, path2, path3, path4],
---
+++ web/src/test/auto/headless/engine/predictive-text/helpers/buildQuotientDocFixture.tests.ts
@@ -0,0 +1,32 @@
+import { assert } from "chai";
+
+import { buildQuotientDocFixture } from "./buildQuotientDocFixture.js";
+
+describe('buildQuotientDocFixture() fixture', () => {
+    it('constructs paths properly', () => {
+      const {searchRoot, nodes} = buildQuotientDocFixture();
+
+      [searchRoot /*, nodes.sc1, nodes.sc2*/].forEach((n) => {
+        assert.equal(n.inputCount, 0);
+      });
+      [/*nodes.k1c0,*/ nodes.k1c1, nodes.k1c2 /*, nodes.k1c3*/].forEach((n) => {
+        assert.equal(n.inputCount, 1);
+      });
+      [/*nodes.k2c0, nodes.k2c1,*/ nodes.k2c2, nodes.k2c3].forEach((n) => {
+        assert.equal(n.inputCount, 2);
+      });
+
+      [searchRoot/*, nodes.k1c0, nodes.k2c0*/].forEach((n) => {
+        assert.equal(n.codepointLength, 0);
+      });
+      [/*nodes.sc1, */nodes.k1c1/*, nodes.k2c1*/].forEach((n) => {
+        assert.equal(n.codepointLength, 1);
+      });
+      [/*nodes.sc2,*/ nodes.k1c2, nodes.k2c2].forEach((n) => {
+        assert.equal(n.codepointLength, 2);
+      });
+      [/*nodes.k1c3,*/ nodes.k2c3].forEach((n) => {
+        assert.equal(n.codepointLength, 3);
+      });
+    });
+});
