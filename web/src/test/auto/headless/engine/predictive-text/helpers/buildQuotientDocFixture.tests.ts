4816729d0b1c3857ab1ca631235314c922b98505
Joshua Horton <joshua_horton@sil.org>
2026-03-10 16:02:56 -0500

    feat(web): add dedicated DeletionQuotientSpur type for handling deletion-edit search-graph edges
    Build-bot: skip build:web
    Test-bot: skip

--- web/src/test/auto/headless/engine/predictive-text/helpers/buildQuotientDocFixture.tests.ts
+++ web/src/test/auto/headless/engine/predictive-text/helpers/buildQuotientDocFixture.tests.ts
@@ -9,17 +9,17 @@ describe('buildQuotientDocFixture() fixture', () => {
       [searchRoot, nodes.sc1, nodes.sc2].forEach((n) => {
         assert.equal(n.inputCount, 0);
       });
-      [/*nodes.k1c0,*/ nodes.k1c1, nodes.k1c2, nodes.k1c3].forEach((n) => {
+      [nodes.k1c0, nodes.k1c1, nodes.k1c2, nodes.k1c3].forEach((n) => {
         assert.equal(n.inputCount, 1);
       });
-      [/*nodes.k2c0, nodes.k2c1,*/ nodes.k2c2, nodes.k2c3].forEach((n) => {
+      [nodes.k2c0, nodes.k2c1, nodes.k2c2, nodes.k2c3].forEach((n) => {
         assert.equal(n.inputCount, 2);
       });
 
-      [searchRoot/*, nodes.k1c0, nodes.k2c0*/].forEach((n) => {
+      [searchRoot, nodes.k1c0, nodes.k2c0].forEach((n) => {
         assert.equal(n.codepointLength, 0);
       });
-      [nodes.sc1, nodes.k1c1/*, nodes.k2c1*/].forEach((n) => {
+      [nodes.sc1, nodes.k1c1, nodes.k2c1].forEach((n) => {
         assert.equal(n.codepointLength, 1);
       });
       [nodes.sc2, nodes.k1c2, nodes.k2c2].forEach((n) => {
