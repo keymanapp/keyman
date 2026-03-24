15e0ff268e35a2a222161cf299f3931115f086ac
Shawn Schantz <shawn_schantz@sil.org>
2026-03-24 15:38:43 -0400

    feat(mac): update swift-tools-version to 6.2

--- mac/KeymanSettings/Package.swift
+++ mac/KeymanSettings/Package.swift
@@ -1,6 +1,5 @@
-// swift-tools-version: 6.0
+// swift-tools-version: 6.2
 // The swift-tools-version declares the minimum version of Swift required to build this package.
-// TODO: Update to 6.2 after Xcode 26 update is complete
 
 import PackageDescription
 
