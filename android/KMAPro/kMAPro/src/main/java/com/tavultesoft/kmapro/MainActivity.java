717889a7ec63d08ecb3d30ff87cbef45eef1feb0
kongchanlina <kongchanlina3@gmail.com>
2026-04-03 22:40:22 +0700

    dark-mode-on-new-updated-ui

--- android/KMAPro/kMAPro/src/main/java/com/tavultesoft/kmapro/MainActivity.java
+++ android/KMAPro/kMAPro/src/main/java/com/tavultesoft/kmapro/MainActivity.java
@@ -16,6 +16,7 @@ import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.OutputStream;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
