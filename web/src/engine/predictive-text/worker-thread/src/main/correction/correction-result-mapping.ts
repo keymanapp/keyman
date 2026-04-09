6084bbe687d7682e9df6e2f6ce1fd40ea6793b94
Joshua Horton <joshua_horton@sil.org>
2026-04-09 16:48:18 -0500

    docs(web): add documentation for new search-related interfaces

--- web/src/engine/predictive-text/worker-thread/src/main/correction/correction-result-mapping.ts
+++ web/src/engine/predictive-text/worker-thread/src/main/correction/correction-result-mapping.ts
@@ -1,7 +1,29 @@
+/*
+ * Keyman is copyright (C) SIL Global. MIT License.
+ *
+ * Created by jahorton on 2026-04-02
+ *
+ * This file defines the type used for tracking critical graph-search properties
+ * utilized during correction-search by any type compatible with the
+ * `getBestMatches` algorithm.
+ */
+
 import { CorrectionSearchable } from "./correction-searchable.js";
 
+/**
+ * Any return value from `.handleNextNode()` designed for use with the
+ * `getBestMatches` method must adhere to this type interface for representing
+ * completed search paths.
+ */
 export interface CorrectionResultMapping<ResultType> {
+  /**
+   * Represents the "searchable" object through which the completed search path last traversed.
+   */
   readonly matchingSpace: CorrectionSearchable<ResultType, CorrectionResultMapping<ResultType>>;
+
+  /**
+   * The object representing the search path completed at the current search step.
+   */
   readonly matchedResult: ResultType;
 
   /**
