2f861b962805f8dfa5db3e6769d4a35bbbff75ad
Marc Durdin <marc@durdin.net>
2026-04-16 08:28:15 +0200

    chore(core): disable api-header-extractor build for Ubuntu packaging
    As Ubuntu packaging does not currently have a node environment, we'll
    disable api-header-extractor; the output files are not used in this
    context anyway.

--- core/tools/api-header-extractor/build.sh
+++ core/tools/api-header-extractor/build.sh
@@ -5,6 +5,20 @@ THIS_SCRIPT="$(readlink -f "${BASH_SOURCE[0]}")"
 . "${THIS_SCRIPT%/*}/../../../resources/build/builder-full.inc.sh"
 ## END STANDARD BUILD SCRIPT INCLUDE
 
+################################ Main script ################################
+
+builder_describe "Extract documentation from header files" clean configure build
+builder_parse "$@"
+
+if [[ -v KEYMAN_PKG_BUILD ]]; then
+  # KEYMAN_PKG_BUILD is defined in linux/debian/rules when running
+  # a packaging build
+  echo "Disabled for Ubuntu packaging as node environment is not available"
+  exit 0
+fi
+
+#----------------------------------------------------------------------------
+
 . "$KEYMAN_ROOT/resources/build/node.inc.sh"
 . "$KEYMAN_ROOT/resources/build/typescript.inc.sh"
 
@@ -12,11 +26,6 @@ CORE_ROOT="$KEYMAN_ROOT/core"
 CORE_H_FILE="$CORE_ROOT/include/keyman/keyman_core_api.h"
 CORE_H_DOC="index.md"
 OUTPATH="$CORE_ROOT/docs/api"
-
-################################ Main script ################################
-
-builder_describe "Extract documentation from header files" clean configure build
-builder_parse "$@"
 
 do_build_and_run() {
   tsc --build
