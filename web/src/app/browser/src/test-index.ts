1bc83f9fcf7a6a984b29eafe2d79b3e80cf29ea8
Eberhard Beilharz <eb1@sil.org>
2026-04-30 17:46:55 +0200

    chore(web): cleanup `web/src/app/browser/src/test-index.ts` exports
    The export of `test-index.ts` were intended to be used by tests only,
    but over time they contained also exports that were consumed else where,
    e.g. in UI classes like `kmwuibutton.ts`. This change removes unused
    exports, moves test-only classes to a `unitTestEndPoints` container and
    marks test-only types with `@internal`.
    
    Build-bot: skip build:web
    Test-bot: skip

--- web/src/app/browser/src/test-index.ts
+++
@@ -1,8 +0,0 @@
-export { BrowserConfiguration, BrowserInitOptionSpec } from './configuration.js';
-export { ContextManager, KeyboardCookie } from "./contextManager.js";
-export { preprocessKeyboardEvent, HardwareEventKeyboard } from './hardwareEventKeyboard.js';
-export { KeyboardDetails } from './keyboardDetails.js';
-
-export { KeymanEngine } from './keymanEngine.js';
-export { KeyboardInterface } from './keyboardInterface.js';
-export { UIModule } from './uiModuleInterface.js';
