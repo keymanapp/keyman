751d5ed6bc5cf64b15434533ffd2969ffe3bf720
Sabine <Sabine_SIL@web.de>
2026-05-04 17:46:30 +0200

    feat(developer): resolve conflict :'key' does not exist in type ...

--- developer/src/kmc-convert/src/keylayout-to-kmn/keylayout-to-kmn-converter.ts
+++ developer/src/kmc-convert/src/keylayout-to-kmn/keylayout-to-kmn-converter.ts
@@ -491,6 +491,7 @@ export class KeylayoutToKmnConverter {
               inputFilename: jsonObj.keyboard['name'] + ".keylayout",
               keymapIndex: jsonObj.keyboard.keyMapSet[0].keyMap[i]['index'],
               output: jsonObj.keyboard.keyMapSet[0].keyMap[i].key[j]['output'],
+              //key: jsonObj.keyboard.keyMapSet[0].keyMap[i].key[j]['code'],
               KeyName: this.mapUkeleleKeycodeToVK(jsonObj.keyboard.keyMapSet[0].keyMap[i].key[j]['code'])
             }));
             return null;
