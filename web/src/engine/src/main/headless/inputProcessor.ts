182fc3de61e608150ad67edc07429473b1cd941f
Marc Durdin <marc@durdin.net>
2026-06-02 13:53:07 +0200

    chore: Revert "chore: remove commented code"
    This reverts commit bf1e272d17ce9fb4317f3e89f0195776d3ba7e33.

--- web/src/engine/src/main/headless/inputProcessor.ts
+++ web/src/engine/src/main/headless/inputProcessor.ts
@@ -79,7 +79,13 @@ export class InputProcessor {
 
   public set activeKeyboard(keyboard: Keyboard) {
 
-    this._keyboardProcessor = this.jsKbdProcessor;
+    // TODO-web-core if (keyboard instanceof JSKeyboard || keyboard == null) {
+      // TODO-web-core: consider keyboard==null scenario; which keyboardProcessor should be active?
+      this._keyboardProcessor = this.jsKbdProcessor;
+    // TODO-web-core } else {
+      // TODO-web-core this._keyboardProcessor = this.coreKbdProcessor;
+    // TODO-web-core }
+
     this.keyboardInterface.activeKeyboard = keyboard;
 
     // All old deadkeys and keyboard-specific cache should immediately be invalidated
