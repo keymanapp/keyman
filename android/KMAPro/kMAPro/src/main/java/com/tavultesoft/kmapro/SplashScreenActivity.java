d1e7839c08dbc9573353561981be9dd5d6c2433c
Mengchou97 <mengchouchey@gmail.com>
2026-06-10 17:00:17 +0700

    reverted the splashscreenactivity

--- android/KMAPro/kMAPro/src/main/java/com/tavultesoft/kmapro/SplashScreenActivity.java
+++ android/KMAPro/kMAPro/src/main/java/com/tavultesoft/kmapro/SplashScreenActivity.java
@@ -1,20 +1,22 @@
 package com.tavultesoft.kmapro;
 
-import android.content.SharedPreferences;
 import android.content.pm.PackageInfo;
 import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.os.Handler;
+import android.app.Activity;
 import android.content.Intent;
 import android.widget.TextView;
 
-import androidx.appcompat.app.AppCompatActivity;
+import com.keyman.engine.util.KMLog;
+
 import java.util.Calendar;
 
-public class SplashScreenActivity extends AppCompatActivity {
+public class SplashScreenActivity extends Activity {
   private final static String TAG = "Splash";
+
   // For now, disable this Activity so app is not delayed showing version/copyright text
-  private static final int SPLASH_TIME_OUT = 0; // msec
+  private static int SPLASH_TIME_OUT = 0; // msec
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
@@ -22,10 +24,13 @@ public class SplashScreenActivity extends AppCompatActivity {
     setContentView(R.layout.activity_splash_screen);
 
     TextView version = (TextView)findViewById(R.id.splash_version);
+    String ver = "";
+    PackageInfo pInfo;
     try {
-      PackageInfo pInfo = getPackageManager().getPackageInfo(getPackageName(), 0);
-      String ver = String.format("%s: %s", version.getText(), pInfo.versionName);
+      pInfo = getPackageManager().getPackageInfo(getPackageName(), 0);
+      ver = String.format("%s: %s", version.getText(), pInfo.versionName);
     } catch (PackageManager.NameNotFoundException e) {
+      KMLog.LogException(TAG, "", e);
       // Could not get version number
     }
     // TODO: Add version and copyright info when Android supports TextViews in the splash screen
