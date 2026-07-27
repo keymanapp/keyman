5d384331aef83b6c1a21064251741925371c6993
Gabriel Schantz <geschantz@gmail.com>
2026-07-27 16:09:31 -0400

    feat(mac): toggle button and code reformatting

--- mac/Config/Config/MainConfigView.swift
+++ mac/Config/Config/MainConfigView.swift
@@ -9,6 +9,7 @@
  */
 
 import SwiftUI
+import Combine
 import KeymanSettings
 
 struct MainConfigView: View {
@@ -33,7 +34,12 @@ struct MainConfigView: View {
   var body: some View {
     VStack(spacing: 0) {
       // the add keyboard button
-      LabelButtonView(action: { isShowingSheet = true }, label: "Add Keyboard", systemImage: "plus", font: .title2 )
+      LabelButtonView(
+        action: { isShowingSheet = true },
+        label: "Add Keyboard",
+        systemImage: "plus",
+        font: .title2
+      )
         .clipShape(.capsule)
         .padding([.top, .leading, .trailing])
       // binds the visibility state to the sheet builder
@@ -46,20 +52,38 @@ struct MainConfigView: View {
       List(settings.singleKeyboardPackages, id: \.id) { package in
         ForEach(package.keyboards) { keyboard in
           DisclosureGroup(isExpanded: isExpanded(package: package)) {
+            // the keyboard info view is shown inside each disclosure group
             KeyboardInfoView(package: package)
           } label: {
+            // the HStack is shown as the label for each disclosure group
             HStack {
               
               Text(keyboard.name)
                 .font(.title)
               
+              // the Spacer pushes the other views inside the HStack to the opposite edge
               Spacer()
               
+              // the toggle button for the keyboard
+              Toggle("enabled", isOn: isEnabled(packageId: package.id, keyboardKey: keyboard.keyboardKey))
+                .labelsHidden()
+                .toggleStyle(.switch)
+              
               // see keyboard help button
-              IconButtonView(action: { print("Show help") }, systemImage: "questionmark.circle", font: .title2, helpText: "Show help")
+              IconButtonView(
+                action: { print("Show help") },
+                systemImage: "questionmark.circle",
+                font: .title2,
+                helpText: "Show help"
+              )
               
               // delete keyboard button
-              IconButtonView(action: { showDeleteAlert(for: package) }, systemImage: "trash", font: .title2, helpText: "Delete keyboard")
+              IconButtonView(
+                action: { showDeleteAlert(for: package) },
+                systemImage: "trash",
+                font: .title2,
+                helpText: "Delete keyboard"
+              )
             }
             .contentShape(Rectangle())
             // handles when the HStack is clicked by the user
@@ -79,9 +103,9 @@ struct MainConfigView: View {
       .alert("Are you sure you want to delete the keyboard \"\(selectedPackage?.packageName ?? "")\"?",
              isPresented: $isShowingDeleteAlert,
              presenting: selectedPackage) { package in
-        
+        // cancel button
         Button("Cancel", role: .cancel) { }
-        
+        // delete button
         Button("Delete", role: .destructive) {
           settings.removeInstalledPackage(with: package.id)
         }
@@ -111,11 +135,25 @@ struct MainConfigView: View {
   // the helper method to generate the custom binding for whether a package's disclosure group is expanded or not
   func isExpanded(package: KeymanPackage) -> Binding<Bool> {
     Binding(
-      // the getter handles the position of the disclosure group to be rendered
+      // the getter renders the position of the disclosure group
       get: { self.expandedPackageID == package.id },
       // setter handles when the chevron arrow is clicked by the user
       // $0 = true when disclosure group is open and $0 = false when disclosure group is closed
       set: { self.expandedPackageID = $0 ? package.id : nil }
+    )
+  }
+  
+  // the helper method to generate the custom binding for whether a keyboard is enabled or not
+  func isEnabled(packageId: UUID, keyboardKey: String) -> Binding<Bool> {
+    Binding(
+      // the getter renders the state of the toggle button based on the enabled property of the keyboard
+      get: { settings.isKeyboardEnabled(packageId: packageId, keyboardKey: keyboardKey) },
+      // the setter handles when the toggle button is clicked by the user
+      // $0 = true when the toggle button is on and $0 = false when the toggle button is off
+      set: {
+        settings.setKeyboardEnabled(packageId: packageId, keyboardKey: keyboardKey, enabled: $0)
+        settings.objectWillChange.send()
+      }
     )
   }
   
