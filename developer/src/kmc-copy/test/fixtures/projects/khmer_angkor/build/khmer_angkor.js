37771ff1f02331a3e24f477888d712be12e1577e
Sabine <Sabine_SIL@web.de>
2026-08-12 14:27:43 +0200

    fix(developer): unchange khmer_angkor.js

--- developer/src/kmc-copy/test/fixtures/projects/khmer_angkor/build/khmer_angkor.js
+++ developer/src/kmc-copy/test/fixtures/projects/khmer_angkor/build/khmer_angkor.js
@@ -2017,6 +2017,9 @@ function Keyboard_khmer_angkor()
   this.gs=function(t,e) {
     return this.g_main_0(t,e);
   };
+  this.gs=function(t,e) {
+    return this.g_main_0(t,e);
+  };
   this.g_main_0=function(t,e) {
     var k=KeymanWeb,r=0,m=0;
     if(k.KKM(e, modCodes.VIRTUAL_KEY /* 0x4000 */, keyCodes.K_BKSP /* 0x08 */)) {
