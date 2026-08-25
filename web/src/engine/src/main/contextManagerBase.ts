27b60a4fcc0c4e61bc324c59a0d5733a75e2d2e3
Joshua Horton <joshua_horton@sil.org>
2026-08-25 13:22:04 -0500

    change(web): enhance missing-stubs error
    Relates-to: #15425
    
    Build-bot: skip build:web
    Test-bot: skip

--- web/src/engine/src/main/contextManagerBase.ts
+++ web/src/engine/src/main/contextManagerBase.ts
@@ -327,7 +327,10 @@ export abstract class ContextManagerBase<MainConfig extends EngineConfiguration>
 
     if(!requestedStub) {
       if(keyboardId) {
-        throw new Error("No matching stub has been registered.");
+        const availableStubList = this.keyboardCache.getStubList().map((stub) => {
+          return `${stub.KI}@${stub.KLC}`
+        });
+        throw new Error(`No matching stub has been registered for keyboard ${keyboardId}.  Available stubs: ${JSON.stringify(availableStubList)}`);
       } else {
         return {
           keyboard: Promise.resolve(null),
