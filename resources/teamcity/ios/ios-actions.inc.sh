7a0fa55a75c49a2c6265a3a6cc48bc18ce7f2d0b
Eberhard Beilharz <eb1@sil.org>
2026-08-31 19:02:05 +0200

    chore(web): address code review comments
    Co-authored-by: Marc Durdin <marc@durdin.net>

--- resources/teamcity/ios/ios-actions.inc.sh
+++ resources/teamcity/ios/ios-actions.inc.sh
@@ -3,21 +3,18 @@
 
 ios_build() {
   builder_echo start "build" "Building KeymanEngine + Keyman for iOS"
-  # shellcheck disable=SC2154
-  "${KEYMAN_ROOT}/ios/ci.sh" build
+  builder_launch /ios/ci.sh build
   builder_echo end "build" success "Finished building KeymanEngine + Keyman for iOS"
 }
 
 ios_capture_build_artifacts() {
   builder_echo start "prep-release" "Using prep-release script to capture build artifacts"
-  "${KEYMAN_ROOT}/ios/tools/prepRelease.sh"
+  builder_launch /ios/tools/prepRelease.sh
   builder_echo end "prep-release" success "Finished capturing build artifacts"
 }
 
 ios_publish_symbols() {
   builder_echo start "publish to Sentry" "Publishing source map to Sentry"
-
   builder_launch /ios/build.sh "publish-symbols"
-
   builder_echo end "publish to Sentry" success "Finished publishing source map to Sentry"
 }
