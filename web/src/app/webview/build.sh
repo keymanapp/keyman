d850c913b9c48b33d382f8a3fceaaede0b29b470
Marc Durdin <marc@durdin.net>
2026-09-17 07:22:46 +0200

    maint(web): improve build perf and test params
    Allow for additional parameters to mocha (e.g. `-f testname`) and remove
    unnecessary build steps; tidy up android-harness build script.
    
    Test-bot: skip

--- web/src/app/webview/build.sh
+++ web/src/app/webview/build.sh
@@ -86,7 +86,7 @@ compile_and_copy() {
   node map-polyfill-bundler.js
 
   # For dependent test pages.
-  builder_launch /web/src/test/manual/embed/android-harness/build.sh configure,build
+  builder_launch /web/src/test/manual/embed/android-harness/build.sh build
 }
 
 builder_run_action configure node_select_version_and_npm_ci
