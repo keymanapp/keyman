4b3b1fe0faeeefe7e2ed47f4ec220ab48fd9b9cf
Dr Mark C. Sinclair <markcsinclair@gmail.com>
2026-09-24 12:33:49 +0100

    feat(developer): added a test to loop over baseline keyboards to aid early debugging

--- developer/src/kmc-kmn/test/ng-compiler/sm-builder.tests.ts
+++ developer/src/kmc-kmn/test/ng-compiler/sm-builder.tests.ts
@@ -10,8 +10,16 @@ import 'mocha';
 import { assert } from 'chai';
 import { ASTNode } from '../../src/ng-compiler/tree-construction.js';
 import { SemanticModelBuilder } from '../../src/ng-compiler/sm-builder.js';
+import { existsSync } from 'node:fs';
+import { baselineKeyboardNames, PATH_TO_BASELINE} from './keyboard-names.js';
+import { readFile } from './token-buffer.tests.js';
+import { TokenBuffer } from '../../src/ng-compiler/token-buffer.js';
+import { stringToTokenBuffer } from './kmn-analyzer.tests.js';
+import { Parser } from '../../src/ng-compiler/kmn-analyzer.js';
 
-let root: ASTNode = null;
+
+let tokenBuffer: TokenBuffer = null;
+let root: ASTNode            = null;
 
 describe("Semantic Model Builder Tests", () => {
   beforeEach(() => {
@@ -20,5 +28,18 @@ describe("Semantic Model Builder Tests", () => {
   it("can construct a SemanticModelBuilder", () => {
     const builder: SemanticModelBuilder = new SemanticModelBuilder(root);
     assert.isNotNull(builder);
+  });
+  it("can provide in-memory model for baseline keyboards", function() {
+    if (!existsSync(PATH_TO_BASELINE)) {
+      this.skip();
+    }
+    baselineKeyboardNames().forEach((name) => {
+      const buffer: string = readFile(`${PATH_TO_BASELINE}${name}.kmn`);
+      tokenBuffer = stringToTokenBuffer(buffer);
+      const parser: Parser = new Parser(tokenBuffer);
+      root = parser.parse();
+      const builder: SemanticModelBuilder = new SemanticModelBuilder(root);
+      assert.isNotNull(builder.build(), `${name}.kmn`);
+    });
   });
 });
